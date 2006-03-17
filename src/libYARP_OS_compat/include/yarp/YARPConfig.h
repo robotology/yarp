29197fd90339afb4a3a9ec474b70af2aa29bb429
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-03-17 16:01:00 +0000

    naive windows conditional compilation fix - need to figure out all the win32 defines
    svn path=/trunk/yarp2/; revision=3155

--- src/libYARP_OS_compat/include/yarp/YARPConfig.h
+++ src/libYARP_OS_compat/include/yarp/YARPConfig.h
@@ -1,18 +1,36 @@
 
+
 //
-// $Id: YARPConfig.h,v 1.1 2006-03-13 13:35:18 eshuy Exp $
+
+// $Id: YARPConfig.h,v 1.2 2006-03-17 16:01:00 eshuy Exp $
+
 // YARP config file (template).
+
 //
+
 //
+
+
 
 #ifndef __YARPConfigh__
+
 #define __YARPConfigh__
 
-#ifdef __WIN32__
+
+
+#ifdef WIN32
+
 #include <yarp/YARPConfigWin32.h>
+
 #else
+
 #include <yarp/YARPConfigLinux.h>
+
 #endif
 
 
+
+
+
 #endif
+
