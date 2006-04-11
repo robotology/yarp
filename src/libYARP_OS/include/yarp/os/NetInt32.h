54230e43e78b7b804b6d3c0f3fcac532cfa24b57
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-04-11 14:29:38 +0000

    preparing for autoconf
    svn path=/trunk/yarp2/; revision=3191

--- src/libYARP_OS/include/yarp/os/NetInt32.h
+++ src/libYARP_OS/include/yarp/os/NetInt32.h
@@ -50,7 +50,18 @@ namespace yarp {
 #  ifdef YARP2_WINDOWS
     typedef __int32 NetInt32;
 #  else
-#    error "need to define NetInt32 for this OS; see yarp/NetType.h"
+    // now we have to assume we are using autoconf etc
+#undef  PACKAGE_BUGREPORT
+#undef  PACKAGE_NAME
+#undef  PACKAGE_STRING
+#undef  PACKAGE_TARNAME
+#undef  PACKAGE_VERSION
+#    include <yarp/config.h>
+#    if YARP_HAS_UINT32_T && !(WORDS_BIGENDIAN)
+       typedef uint32_t NetInt32;
+#    else
+#      error "need to define NetInt32 for this OS; see yarp/NetType.h"
+#    endif
 #  endif
 #endif
 
