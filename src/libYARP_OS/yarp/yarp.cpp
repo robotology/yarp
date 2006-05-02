e5181ca6dd74bdb1ae063dfda25cab30fba1fce6
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-05-02 16:09:58 +0000

    windows cmake issues
    svn path=/trunk/yarp2/; revision=3289

--- src/libYARP_OS/yarp/yarp.cpp
+++ src/libYARP_OS/yarp/yarp.cpp
@@ -1,8 +1,15 @@
 
+#include <yarp/Logger.h>
+#include <yarp/NameClient.h>
+#include <yarp/Companion.h>
+
 #include <yarp/os/Network.h>
 
 int main(int argc, char *argv[]) {
   // call the yarp standard companion
-  return yarp::os::Network::main(argc,argv);
+  ACE::init();
+  int result = yarp::os::Network::main(argc,argv);
+  ACE::fini();
+  return result;
 }
 
