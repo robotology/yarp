15558bab659ca4467beb0e00555d251270baaeae
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-05-16 12:58:52 +0000

    prepare to use pkg-config with cmake
    svn path=/trunk/yarp2/; revision=3346

---
+++ conf/UsePkgConfig.cmake
@@ -0,0 +1,47 @@
+
+# SOURCE: KDE
+# File: [SVN] / trunk / KDE / kdesdk / cmake / modules / UsePkgConfig.cmake
+# Revision: 501797, Mon Jan 23 22:03:18 2006 UTC by neundorf
+
+# - pkg-config module for CMake
+# Defines the following macros:
+#   PKGCONFIG(package includedir libdir linkflags cflags)
+#     - Calling PKGCONFIG will fill the desired information into the 4
+#     given arguments, e.g. PKGCONFIG(libart-2.0 LIBART_INCLUDE_DIR
+#     LIBART_LINK_DIR LIBART_LINK_FLAGS LIBART_CFLAGS) if pkg-config
+#     was not found or the specified software package doesn't exist,
+#     the variable will be empty when the function returns, otherwise
+#     they will contain the respective information
+
+FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS /usr/local/bin)
+MARK_AS_ADVANCED(PKGCONFIG_EXECUTABLE)
+
+MACRO(PKGCONFIG _package _include_DIR _link_DIR _link_FLAGS _cflags)
+# reset the variables at the beginning
+  SET(${_include_DIR})
+  SET(${_link_DIR})
+  SET(${_link_FLAGS})
+  SET(${_cflags})
+
+# if pkg-config has been found
+  IF(PKGCONFIG_EXECUTABLE)
+
+    EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+
+# and if the package of interest also exists for pkg-config, then get the information
+    IF(NOT _return_VALUE)
+
+      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=includedir OUTPUT_VARIABLE ${_include_DIR} )
+
+      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=libdir OUTPUT_VARIABLE ${_link_DIR} )
+
+      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs-only-other OUTPUT_VARIABLE ${_link_FLAGS} )
+
+      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --cflags-only-other OUTPUT_VARIABLE ${_cflags} )
+
+    ENDIF(NOT _return_VALUE)
+
+  ENDIF(PKGCONFIG_EXECUTABLE)
+
+ENDMACRO(PKGCONFIG _include_DIR _link_DIR _link_FLAGS _cflags)
+
