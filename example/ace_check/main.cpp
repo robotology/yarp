18b709dde938f353debc644bda2a6584f17a9d3f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-06-09 15:07:52 +0000

    fixes
    svn path=/trunk/yarp2/; revision=3502

--- example/ace_check/main.cpp
+++ example/ace_check/main.cpp
@@ -1,10 +1,18 @@
-#include <ace/ACE.h>
 #include <stdio.h>
 
+#include <ace/OS.h>
+
+// ACE can be configured to redefine main.
+// we don't want that.
+#ifdef main
+#undef main
+#endif
+
 int main() {
-  ACE::init();
-  printf("Starting, stopping ACE\n");
-  ACE::fini();
-  return 0;
+	// just exercise ACE a tiny bit
+	char buf[256];
+	ACE_OS::sprintf(buf,"hello ACE world!");
+	printf("[%s]\n",buf);
+	return 0;
 }
 
