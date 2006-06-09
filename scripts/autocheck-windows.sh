df100fcc48ee1e43792998dc9dae96022ccbc567
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-06-09 11:24:29 +0000

    run regression repeatedly
    svn path=/trunk/yarp2/; revision=3496

---
+++ scripts/autocheck-windows.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# run as ./scripts/autocheck-windows.sh
+
+#cvs update -d
+
+SOURCE=`cygpath -w $PWD`
+GEN="NMake Makefiles"
+CMAKE="/cygdrive/c/Program Files/CMake 2.4/bin/cmake.exe"
+
+while true; do
+
+(
+
+date
+echo Working in directory $SOURCE
+
+rm -f CMakeCache.txt
+rm -f failure.txt
+"$CMAKE" -G "$GEN" $SOURCE || ( echo YARP_AUTOCHECK cmake configure failed | tee failure.txt )
+
+if [ ! -e failure.txt ]; then
+	nmake || ( echo YARP_AUTOCHECK nmake compile failed | tee failure.txt )
+fi
+
+if [ ! -e failure.txt ]; then
+	nmake test || ( echo YARP_AUTOCHECK nmake regression failed | tee failure.txt )
+fi
+
+if [ -e failure.txt ]; then
+	echo YARP_AUTOCHECK at least one failure happened
+fi
+
+
+) | tee report.txt
+
+scp report.txt eshuy@yarp0.sf.net:www/report-yarp2-windows.txt
+
+sleep 60
+
+done
