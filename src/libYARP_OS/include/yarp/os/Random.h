a0498b9a214e4c7bca65617da16d62ed3a809494
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-06-11 13:11:35 +0000

    brought back YARPRandom
    svn path=/trunk/yarp2/; revision=3513

---
+++ src/libYARP_OS/include/yarp/os/Random.h
@@ -0,0 +1,62 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+#ifndef _YARP2_RANDOM_
+#define _YARP2_RANDOM_
+
+
+/**
+ * \file Random.h Contains methods and classes for generating random
+ * numbers with various (simple) distributions.
+ */
+
+
+namespace yarp {
+    namespace os {
+        class Random;
+    }
+}
+
+/**
+ * A collection of basic random number generation algorithms.
+ */
+class yarp::os::Random {
+public:
+	/**
+	 * Generates a random number in the range 0 to 1.
+	 * @return a random floating point number in the range 0 to 1.
+	 */
+  static double uniform();
+
+	/**
+	 * A normal random number generator.
+	 * @param m is the mean of the gaussian distribution.
+	 * @param s is the standard deviation of the distribution.
+	 * @return a random number with normal distribution.
+	 */
+  static double normal(double m, double s);
+
+	/**
+	 * Sets the seed of the random number generator.
+	 * @param seed is the new seed of the random number generator.
+	 */
+	static void seed(int seed);
+
+	/**
+	 * @return a random number normally distributed with 0 mean and 
+	 * unitary standard deviation.
+	 */
+	static double normal() { 
+        return normal(0.0, 1.0); 
+    }
+
+	/**
+	 * A random number in a specified range.
+	 * @param min is the minimum of the range.
+	 * @param max is the maximum of the range.
+	 * @return a random integer in the specified range.
+	 */
+	static int uniform(int min, int max);
+};
+
+
+#endif 
+
---
+++ src/libYARP_OS/src/Random.cpp
@@ -0,0 +1,76 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+
+#include <yarp/os/Random.h>
+
+#include <math.h>
+#include <ace/OS.h>
+#include <stdlib.h>
+
+using namespace yarp::os;
+
+double Random::uniform() {
+  return double (ACE_OS::rand ()) / double (RAND_MAX);
+}
+
+
+//
+// normal distribution random number generator.
+// - it might be slow, the standard rand() is used as source.
+// - beware of initializing the seed of rand.
+
+// original code copyright reported below.
+
+/* boxmuller.c           
+            Implements the Polar form of the Box-Muller
+                         Transformation
+
+               (c) Copyright 1994, Everett F. Carter Jr.
+              Permission is granted by the author to use
+           this software for any application provided this
+                    copyright notice is preserved.
+
+*/
+
+double Random::normal(double m, double s)	
+	{				        
+		float x1, x2, w, y1;
+		static double y2;
+		static int use_last = 0;
+
+		if (use_last)		        /* use value from previous call */
+		{
+			y1 = y2;
+			use_last = 0;
+		}
+		else
+		{
+			do {
+				x1 = 2.0 * uniform() - 1.0;
+				x2 = 2.0 * uniform() - 1.0;
+				w = x1 * x1 + x2 * x2;
+			} while ( w >= 1.0 );
+
+			w = sqrt( (-2.0 * log( w ) ) / w );
+			y1 = x1 * w;
+			y2 = x2 * w;
+			use_last = 1;
+		}
+
+		return( m + y1 * s );
+	}
+
+void Random::seed(int seed) { 
+    ACE_OS::srand (seed); 
+}
+
+
+int Random::uniform(int min, int max) {
+	int ret = int ((double (ACE_OS::rand()) / double (RAND_MAX)) * (max - min + 1) + min);
+    
+	// there's a small chance the value is = max+1
+	if (ret <= max)
+		return ret;
+	else
+		return max;
+}
