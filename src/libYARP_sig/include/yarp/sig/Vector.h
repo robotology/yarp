a167e599625669dfb59a696fa78db3e19b933038
Giorgio Metta <giorgio.metta@iit.it>
2006-06-20 14:42:24 +0000

    added the Vector type to signal library
    svn path=/trunk/yarp2/; revision=3609

---
+++ src/libYARP_sig/include/yarp/sig/Vector.h
@@ -0,0 +1,85 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+// $Id: Vector.h,v 1.1 2006-06-20 14:42:24 gmetta Exp $
+
+#ifndef _YARP2_VECTOR_
+#define _YARP2_VECTOR_
+
+#include <ace/config.h>
+#include <ace/Containers_T.h>
+#include <yarp/os/Portable.h>
+
+/**
+ * \file Vector.h contains the definition of a Vector type with corresponding 
+ * Portable character.
+ */
+
+namespace yarp {
+    namespace sig {
+        class Vector;
+        // template class VectorOf<class T> ...;
+    }
+}
+
+/**
+ * A simple Vector class derived from ACE and with Portable functionality.
+ * Things you can do on Vector:
+ * - use [] to access single elements without range checking
+ * - use get() to access single elements with range checking
+ * - use size() to get the current size of the Vector
+ * - use max_size() to get the maximum size of the Vector (history of allocated memory)
+ * - use operator= to copy Vectors
+ * This is derived from ACE_Array & ACE_Array_Base but it doesn't need to be the case if this
+ * shows to be unefficient.
+ */
+class yarp::sig::Vector : public ACE_Array<double>, public yarp::os::Portable {
+public:
+    /**
+     * Default constructor.
+     */
+    Vector();
+
+    /**
+     * Copy contructor.
+     */
+    Vector(const Vector& x);
+
+    /**
+     * Destructor.
+     */
+    virtual ~Vector();
+
+    /**
+     * Copies a Vector object into the current one.
+     * @param x is a reference to an object of type Vector.
+     * @return a reference to the current object.
+     */
+    Vector& operator=(const Vector& x);
+
+    /**
+     * Create a Vector object of a given size and fill the content.
+     * @param size is the number of elements of the vector.
+     */
+    Vector (size_t size);
+
+    /**
+     * Initialize a Vector of the given size placing the default_value in each index.
+     * @param size is the size of the vector.
+     * @param default_value is the initial value of the vector.
+     */
+    Vector (size_t size, const double& default_value);
+
+    /**
+     * Read vector from a connection.
+     * return true iff image was read correctly
+     */
+    virtual bool read(yarp::os::ConnectionReader& connection);
+
+    /**
+     * Write vector to a connection.
+     * return true iff image was written correctly
+     */
+    virtual bool write(yarp::os::ConnectionWriter& connection);
+};
+
+
+#endif
---
+++ src/libYARP_sig/src/Vector.cpp
@@ -0,0 +1,88 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+// $Id: Vector.cpp,v 1.1 2006-06-20 14:42:24 gmetta Exp $
+
+#include <yarp/sig/Vector.h>
+#include <yarp/IOException.h>
+#include <yarp/os/Bottle.h>
+
+/*
+preferred network format for a list of doubles.
+
+INT totallen = #bytes whole think
+INT listTag = BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE
+INT listLen = N
+double 1
+double 2
+...
+double N
+*/
+
+using namespace yarp::sig;
+
+Vector::Vector() : ACE_Array<double>() {}
+Vector::Vector(const Vector& x) : ACE_Array<double>((ACE_Array<double>&)x) {}
+Vector::~Vector() {}
+Vector& Vector::operator=(const Vector& x) { ACE_Array<double>::operator=((ACE_Array<double>&)x); return *this; }
+Vector::Vector (size_t size) : ACE_Array<double>(size) {}
+Vector::Vector (size_t size, const double& default_value) : ACE_Array<double>(size, default_value) {}
+
+
+#include <yarp/os/NetInt32.h>
+#include <yarp/os/begin_pack_for_net.h>
+
+class YARPVectorPortContentHeader
+{
+public:
+    yarp::os::NetInt32 totalLen;
+    yarp::os::NetInt32 listTag;
+    yarp::os::NetInt32 listLen;
+} PACKED_FOR_NET;
+
+#include <yarp/os/end_pack_for_net.h>
+
+bool Vector::read(yarp::os::ConnectionReader& connection) {
+    
+    try {
+        // auto-convert text mode interaction
+        connection.convertTextMode();
+        YARPVectorPortContentHeader header;
+        connection.expectBlock((char*)&header, sizeof(header));
+        if (header.listLen > 0)
+        {
+            if (this->size() != header.listLen)
+                this->size(header.listLen);
+            double *ptr = &(this->operator[](0));
+            ACE_ASSERT (ptr != NULL);
+            connection.expectBlock((char *)ptr, sizeof(double)*header.listLen);
+        }
+        else
+            return false;
+    } catch (yarp::IOException e) {
+        return false;
+    }
+
+    return true;
+}
+
+
+bool Vector::write(yarp::os::ConnectionWriter& connection) {
+    YARPVectorPortContentHeader header;
+
+    header.totalLen = sizeof(header)+sizeof(double)*this->size();
+    header.listTag = BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE;
+    header.listLen = this->size();
+
+    connection.appendBlock((char*)&header, sizeof(header));
+    double *ptr = &(this->operator[](0));
+    ACE_ASSERT (ptr != NULL);
+
+    // Note use of external block.  Implies care needed about ownership.
+    connection.appendExternalBlock((char *)ptr, sizeof(double)*header.listLen);
+
+    // if someone is foolish enough to connect in text mode,
+    // let them see something readable.
+    connection.convertTextMode();
+
+    return true;
+}
+
