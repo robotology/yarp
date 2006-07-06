a8b0935ee93116a016b7b9cd59a6c4523f8df3cf
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-06 11:22:03 +0000

    yarp os catchall header file
    svn path=/trunk/yarp2/; revision=3808

---
+++ src/libYARP_OS/include/yarp/os/all.h
@@ -0,0 +1,30 @@
+
+// everything for the yarp::os namespace
+
+#include <yarp/os/BinPortable.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/ConnectionReader.h>
+#include <yarp/os/ConnectionWriter.h>
+#include <yarp/os/ConstString.h>
+#include <yarp/os/Contactable.h>
+#include <yarp/os/Contact.h>
+#include <yarp/os/NetInt32.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Portable.h>
+#include <yarp/os/PortablePair.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/PortReaderBuffer.h>
+#include <yarp/os/PortReaderCreator.h>
+#include <yarp/os/PortReader.h>
+#include <yarp/os/PortWriterBuffer.h>
+#include <yarp/os/PortWriter.h>
+#include <yarp/os/Property.h>
+#include <yarp/os/Random.h>
+#include <yarp/os/Searchable.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/os/Thread.h>
+#include <yarp/os/Time.h>
+#include <yarp/os/Value.h>
+#include <yarp/os/Vocab.h>
+
