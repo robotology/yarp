fcc1927e443747195b7ee4195d381cfbc53d0d5c
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-06 16:28:14 +0000

    catchall header file
    svn path=/trunk/yarp2/; revision=3820

---
+++ src/libYARP_sig/include/yarp/sig/all.h
@@ -0,0 +1,9 @@
+
+// everything for namespace yarp::sig
+
+#include <yarp/sig/ImageDraw.h>
+#include <yarp/sig/ImageFile.h>
+#include <yarp/sig/Image.h>
+#include <yarp/sig/IplImage.h>
+#include <yarp/sig/Sound.h>
+#include <yarp/sig/Vector.h>
