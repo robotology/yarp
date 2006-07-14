37ad5d075c8e7810867418cbf2e8d2270273ea89
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-14 22:09:42 +0000

    OSX header file order
    svn path=/trunk/yarp2/; revision=3860

--- src/libYARP_dev/src/RemoteFrameGrabber.cpp
+++ src/libYARP_dev/src/RemoteFrameGrabber.cpp
@@ -1,11 +1,10 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
+#include <yarp/String.h>
+
 #include <yarp/dev/TestFrameGrabber.h>
 #include <yarp/dev/ServerFrameGrabber.h>
 #include <yarp/dev/RemoteFrameGrabber.h>
-
-#include <yarp/String.h>
-
 #include <yarp/dev/PolyDriver.h>
 
 using namespace yarp::os;
