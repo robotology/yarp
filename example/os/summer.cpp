6a18c595cf1db87269311d3ca2ecb42dbe013e49
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-15 14:05:17 +0000

    add example for summing numbers
    svn path=/trunk/yarp2/; revision=3861

---
+++ example/os/summer.cpp
@@ -0,0 +1,31 @@
+#include <yarp/os/all.h>
+#include <iostream>
+
+using namespace std;
+using namespace yarp::os;
+
+int main(int argc, char *argv) {
+  Network::init();
+  BufferedPort<Bottle> port;
+  port.open("/summer");
+  while (true) {
+    cout << "waiting for input" << endl;
+    Bottle *input = port.read();
+    if (input!=NULL) {
+      cout << "got " << input->toString().c_str() << endl;
+      double total = 0;
+      for (int i=0; i<input->size(); i++) {
+	total += input->get(i).asDouble();
+      }
+      Bottle& output = port.prepare();
+      output.clear();
+      output.addString("total");
+      output.addDouble(total);
+      cout << "writing " << output.toString().c_str() << endl;
+      port.write();
+    }
+  }
+  Network::fini();
+  return 0;
+}
+
