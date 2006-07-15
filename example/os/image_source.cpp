e9b2aeb4e54c55edf1b53bab698f50ee824ffc37
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-15 20:54:52 +0000

    image source example
    svn path=/trunk/yarp2/; revision=3862

---
+++ example/os/image_source.cpp
@@ -0,0 +1,31 @@
+
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::sig::draw;
+
+int main(int argc, char *argv[]) {
+  Network::init();
+  BufferedPort<ImageOf<PixelRgb> > port;
+
+  Property options;
+  options.fromCommand(argc,argv);
+  port.open(options.check("name",Value("/image")).asString());
+  
+  int ct = 0;
+  while (true) {
+    ImageOf<PixelRgb>& img = port.prepare();
+    img.resize(100,100);
+    img.zero();
+    PixelRgb blue(255,0,0);
+    addCircle(img,blue,ct,50,10);
+    ct = (ct+5)%100;
+    port.write();
+    Time::delay(0.25);
+  }
+  
+  Network::fini();
+  return 0;
+}
