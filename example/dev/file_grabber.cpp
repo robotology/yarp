5499a6a6119a54b6f19aa8498340e4df90cbe5aa
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-23 17:51:42 +0000

    file grabber example
    svn path=/trunk/yarp2/; revision=3915

---
+++ example/dev/file_grabber.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <yarp/dev/FrameGrabberInterfaces.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/Drivers.h>
+#include "FileFrameGrabber.h"
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::dev;
+
+int main(int argc, char *argv[]) {
+  Network::init();
+
+  // give YARP a factory for creating instances of FileFrameGrabber
+  DriverCreator *file_grabber_factory = 
+    new DriverCreatorOf<FileFrameGrabber>("file_grabber",
+					  "grabber",
+					  "FileFrameGrabber");
+  Drivers::factory().add(file_grabber_factory); // hand factory over to YARP
+
+  // use YARP to create and configure an instance of FileFrameGrabber
+  Property config;
+  if (argc==1) {
+    // no arguments, use a default
+    config.fromString("(device file_grabber) (pattern \"image/%03d.ppm\")");
+  } else {
+    // expect something like '--device file_grabber --pattern "image/%03d.ppm"'
+    //                    or '--device dragonfly'
+    //                    or '--device test_grabber --period 0.5 --mode [ball]'
+    config.fromCommand(argc,argv);
+  }
+  PolyDriver dd(config);
+  if (!dd.isValid()) {
+    printf("Failed to create and configure a device\n");
+    exit(1);
+  }
+  IFrameGrabberImage *grabberInterface;
+  if (!dd.view(grabberInterface)) {
+    printf("Failed to view device through IFrameGrabberImage interface\n");
+    exit(1);
+  }
+
+  ImageOf<PixelRgb> img;
+  grabberInterface->getImage(img);
+  printf("Got a %dx%d image\n", img.width(), img.height());
+
+  dd.close();
+
+  Network::fini();
+  return 0;
+}
--- example/dev/file_grabber_net.cpp
+++ example/dev/file_grabber_net.cpp
@@ -2,7 +2,7 @@
 #include <yarp/dev/FrameGrabberInterfaces.h>
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/Drivers.h>
-#include "FakeFrameGrabber.h"
+#include "FileFrameGrabber.h"
 using namespace yarp::os;
 using namespace yarp::sig;
 using namespace yarp::dev;
@@ -10,16 +10,17 @@ using namespace yarp::dev;
 int main() {
   Network::init();
 
-  // give YARP a factory for creating instances of FakeFrameGrabber
-  DriverCreator *fakey_factory = 
-    new DriverCreatorOf<FakeFrameGrabber>("fakey",
-                                          "grabber",
-                                          "FakeFrameGrabber");
-  Drivers::factory().add(fakey_factory); // hand factory over to YARP
+  // give YARP a factory for creating instances of FileFrameGrabber
+  DriverCreator *file_grabber_factory = 
+    new DriverCreatorOf<FileFrameGrabber>("file_grabber",
+					  "grabber",
+					  "FileFrameGrabber");
+  Drivers::factory().add(file_grabber_factory); // hand factory over to YARP
 
   // use YARP to create and configure a networked of FakeFrameGrabber
   Property config;
-  config.fromString("(device grabber) (name /fakey) (subdevice fakey) (w 200) (h 200)");
+  // You may have to tweak the "image" path to match where your executable is
+  config.fromString("(device grabber) (name /file) (subdevice file_grabber) (pattern \"image/img%04d.ppm\") (first 250)");
   PolyDriver dd(config);
   if (!dd.isValid()) {
     printf("Failed to create and configure a device\n");
