3f062c0ce32b20c62e9d1d8bf683b7694894a96d
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-07-26 12:39:26 +0000

    vector use of correct doubles
    svn path=/trunk/yarp2/; revision=3938

--- src/libYARP_sig/src/Vector.cpp
+++ src/libYARP_sig/src/Vector.cpp
@@ -1,14 +1,14 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
-// $Id: Vector.cpp,v 1.4 2006-07-07 10:08:53 eshuy Exp $
+// $Id: Vector.cpp,v 1.5 2006-07-26 12:39:26 eshuy Exp $
 
 #include <yarp/sig/Vector.h>
 #include <yarp/IOException.h>
 #include <yarp/os/Bottle.h>
+#include <yarp/ManagedBytes.h>
 
 /*
 preferred network format for a list of doubles.
 
-INT totallen = #bytes whole think
 INT listTag = BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE
 INT listLen = N
 double 1
@@ -18,6 +18,7 @@ double N
 */
 
 using namespace yarp::sig;
+using namespace yarp;
 
 Vector::Vector() : ACE_Array<double>() {}
 Vector::Vector(const Vector& x) : ACE_Array<double>((ACE_Array<double>&)x) {}
@@ -60,7 +61,18 @@ bool Vector::read(yarp::os::ConnectionReader& connection) {
                 this->size(header.listLen);
             double *ptr = &(this->operator[](0));
             ACE_ASSERT (ptr != NULL);
+#ifdef YARP_ACTIVE_DOUBLE
+            // native doubles don't match YARP's expectations
+            int blockLen = sizeof(double)*header.listLen;
+            ManagedBytes bytes(blockLen);
+            connection.expectBlock((char *)bytes.get(), bytes.length());
+            NetFloat64 *floats = (NetFloat64*)bytes.get();
+            for (int i=0; i<header.listLen; i++) {
+                this->operator[](i) = floats[i];
+            }
+#else
             connection.expectBlock((char *)ptr, sizeof(double)*header.listLen);
+#endif
         }
         else
             return false;
@@ -83,8 +95,19 @@ bool Vector::write(yarp::os::ConnectionWriter& connection) {
     double *ptr = &(this->operator[](0));
     ACE_ASSERT (ptr != NULL);
 
+#ifdef YARP_ACTIVE_DOUBLE
+    // native doubles don't match YARP's expectations
+    int blockLen = sizeof(double)*header.listLen;
+    ManagedBytes bytes(blockLen);
+    NetFloat64 *floats = (NetFloat64*)bytes.get();
+    for (int i=0; i<header.listLen; i++) {
+        floats[i] = this->operator[](i);
+    }
+    connection.appendBlock((char *)bytes.get(), bytes.length());
+#else
     // Note use of external block.  Implies care needed about ownership.
     connection.appendExternalBlock((char *)ptr, sizeof(double)*header.listLen);
+#endif
 
     // if someone is foolish enough to connect in text mode,
     // let them see something readable.
