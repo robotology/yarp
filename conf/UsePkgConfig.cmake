d4c82d7b3f39c282c849a97d7b85a15d8fdec167
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-09-05 22:27:54 +0000

    attempt to improve multiple pkg-config version interfacing
    svn path=/trunk/yarp2/; revision=4081

--- conf/UsePkgConfig.cmake
+++ conf/UsePkgConfig.cmake
@@ -16,6 +16,22 @@
 FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS /usr/local/bin /usr/bin /sw/bin)
 MARK_AS_ADVANCED(PKGCONFIG_EXECUTABLE)
 
+
+IF(PKGCONFIG_EXECUTABLE)
+	# test which interface is available
+	EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --libs-only-other RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _var)
+	# in happy case, response is "Must specify package names on the command line"
+	SET(_var "${_var} and some filler here")
+	STRING(SUBSTRING "${_var}" 0 12 _test_VALUE)
+	STRING(COMPARE EQUAL "${_test_VALUE}" "Must specify" _MODERN_PKGCONFIG)
+	IF(_MODERN_PKGCONFIG)
+		MESSAGE(STATUS "Modern pkg-config utility detected")
+	ELSE(_MODERN_PKGCONFIG)
+		MESSAGE(STATUS "Older pkg-config utility detected")
+	ENDIF(_MODERN_PKGCONFIG)	
+ENDIF(PKGCONFIG_EXECUTABLE)
+
+
 MACRO(PKGCONFIG _package _include_DIR _link_DIR _link_FLAGS _cflags)
 # reset the variables at the beginning
   SET(${_include_DIR})
@@ -35,7 +51,17 @@ MACRO(PKGCONFIG _package _include_DIR _link_DIR _link_FLAGS _cflags)
 
       EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs-only-L OUTPUT_VARIABLE ${_link_DIR} )
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs-only-l --libs-only-other OUTPUT_VARIABLE ${_link_FLAGS} )
+	IF(_MODERN_PKGCONFIG)
+	      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs-only-l --libs-only-other OUTPUT_VARIABLE ${_link_FLAGS} )
+	ELSE(_MODERN_PKGCONFIG)
+	      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs-only-l OUTPUT_VARIABLE ${_link_FLAGS} )
+	      # libs-only-L output will have surplus newline
+	      # only cmake 2.2 compatible hack available right now is to delete when just a newline
+	      STRING(LENGTH ${${_link_DIR}} _var)
+	      IF (_var LESS 3)
+		      SET(${_link_DIR} "")
+	      ENDIF (_var LESS 3)
+	ENDIF(_MODERN_PKGCONFIG)
 
       EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --cflags OUTPUT_VARIABLE ${_cflags} )
 
