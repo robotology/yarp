a1cd22bb870614c8e025c0455dddc89cffcad259
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-09-06 12:43:26 +0000

    Renaud's viewer
    svn path=/trunk/yarp2/; revision=4089

---
+++ example/yayarpview/YarpCImgViewer.h
@@ -0,0 +1,69 @@
+/**
+ * @file
+ */
+
+
+#ifndef YarpCImgViewer_h
+#define YarpCImgViewer_h
+
+#include <ace/ACE.h>
+
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <string>
+
+/* Avoid including CImg.h in the header (it takes hours to parse) */
+namespace cimg_library
+{
+  template<typename T> struct CImg;
+  template<typename T> struct CImgl;
+  struct CImgDisplay;
+}
+
+
+/**
+ * @class YarpCImgViewer YarpCImgViewer.h
+ *
+ * @brief A YarpCImgViewer.
+ * @author Renaud Detry <renaud.detry@student.ulg.ac.be>
+ *
+ * This class forwards video from a YARP port to an X11/WIN32
+ * window.
+ */
+
+class YarpCImgViewer
+{
+public:
+
+  /**
+   * @arg \c portName
+   */
+  YarpCImgViewer(const char* portName);
+  ~YarpCImgViewer();
+
+  void acquire();
+  void broadcast();
+
+  void setUdelay(int udelay);
+  void dumpAtLoc(const char* dumpName);
+
+  /**
+   * demo method
+   */
+  static int main(int argc, char ** argv);
+
+private:
+
+  std::string portName;
+  std::string dumpName;
+  bool verbose;
+  int dumpID;
+  int udelay;
+  yarp::os::BufferedPort< yarp::sig::ImageOf<yarp::sig::PixelRgb> > videoPort;
+  cimg_library::CImgDisplay *viewerWindow;
+  cimg_library::CImg<unsigned char> *viewerImage;
+};
+
+
+#endif
+
---
+++ example/yayarpview/main.cpp
@@ -0,0 +1,7 @@
+
+#include "YarpCImgViewer.h"
+
+int main(int argc, char *argv[])
+{
+  YarpCImgViewer::main(argc, argv);
+}
