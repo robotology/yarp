589e4b32f2c5e62dd1f62c15e67bd77f7d82a46a
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-10-23 21:57:30 +0000

    web wrapper update
    svn path=/trunk/yarp2/; revision=4222

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,8 +7,7 @@ if $USE_SOURCEFORGE; then
     WEB_SERVER=yarp0.sourceforge.net
     WEB_DIR=/home/groups/y/ya/yarp0/htdocs
 else
-# only paulfitz has keys set up on eris yet
-    WEB_USER=paulfitz
+    WEB_USER=$USER
     WEB_SERVER=eris.liralab.it
     WEB_DIR=/var/www/html/yarp
 fi
