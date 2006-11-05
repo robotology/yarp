439146e2161a255bcf5b48e0bd07a0fb2292275e
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-11-05 13:46:14 +0000

    hide advanced option
    svn path=/trunk/yarp2/; revision=4315

--- conf/YarpOptions.cmake
+++ conf/YarpOptions.cmake
@@ -5,4 +5,4 @@ SET(CREATE_GUIS FALSE CACHE BOOL "Do you want to compile GUIs")
 # Flag for device testing and documentation - not really for end-user,
 # but instead the library developers
 SET(CREATE_DEVICE_TESTS FALSE CACHE BOOL "Do you want to create device tests")
-MARK_AS_ADVANCED(CREATE_DEVICE_DOC)
+MARK_AS_ADVANCED(CREATE_DEVICE_TESTS)
