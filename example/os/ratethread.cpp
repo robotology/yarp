87db955f7b7e4ad0cabf39a1d665292060289037
Lorenzo Natale <lorenzo.natale@iit.it>
2006-11-29 16:04:31 +0000

    rate thread example
    svn path=/trunk/yarp2/; revision=4436

---
+++ example/os/ratethread.cpp
@@ -0,0 +1,120 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+
+// rate thread example -nat
+
+#include <stdio.h>
+
+#include <yarp/os/RateThread.h>
+#include <yarp/os/Time.h>
+#include <yarp/os/Thread.h>
+
+using namespace yarp::os;
+
+class Thread1 : public RateThread {
+public:
+	Thread1(int r):RateThread(r){}
+    virtual bool threadInit()
+	{
+		printf("Starting thread1\n");
+		return true;
+	}
+
+	//called by start after initThread, s is true iff the thread started
+	//successfully
+	virtual void afterStart(bool s)
+	{
+		if (s)
+			printf("Thread1 started successfully\n");
+		else
+			printf("Thread1 did not start\n");
+	}
+
+    virtual void run() {
+       printf("Hello, from thread1\n");
+    }
+
+    virtual void threadRelease()
+	{
+		printf("Goodbye from thread1\n");
+	}
+};
+
+class Thread2: public RateThread {
+public:
+	Thread2(int r):RateThread(r){}
+	virtual bool threadInit()
+	{
+		printf("Starting thread2\n");
+		return true; 
+	}
+
+	//called by start after initThread, s is true iff the thread started
+	//successfully
+	virtual void afterStart(bool s)
+	{
+		if (s)
+			printf("Thread2 started successfully\n");
+		else
+			printf("Thread2 did not start\n");
+	}
+
+	virtual void run()
+	{
+		printf("Hello, from thread2\n");
+	}
+
+	virtual void threadRelease()
+	{
+		printf("Goodbye from thread2\n");
+	}
+};
+
+int main() {
+    Thread1 t1(500);  //run every 500ms
+	Thread2 t2(1000); //run every 1s
+	printf("thread1 rate is %d[ms]\n", 500);
+	printf("thread2 rate is %d[ms]\n", 1000);
+
+	printf("Starting threads...\n");
+    bool ok=t1.start();
+	ok = ok&&t2.start();
+	if (!ok)
+	{
+		printf("One of the thread failed to initialize, returning\n");
+		return -1;
+	}
+
+    Time::delay(3);
+    printf("suspending threads...\n");
+
+	t1.suspend();
+	t2.suspend();
+
+	printf("Waiting some time");
+	for(int k=1;k<20;k++)
+	{
+		printf(".");
+		Time::delay(0.2); //200[ms]
+	}
+	printf("\n");
+
+	printf("Changing thread1 rate to %d[ms]\n", 250);
+	printf("Changing thread2 rate to %d[ms]\n", 500);
+
+	t1.setRate(250);
+	t2.setRate(500);
+
+	printf("Resuming threads...\n");
+	t1.resume();
+	t2.resume();
+
+	Time::delay(3);
+    
+    t1.stop();
+	t2.stop();
+    printf("stopped\n");
+
+    return 0;
+}
+
