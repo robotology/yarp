9bab8619dd3eeb1c6e12b482e44fe8f8549dc41c
Naeem Assif Mirza <assif42@gmail.com>
2006-11-30 17:38:38 +0000

    Add dimax_u2c motor control device. Very basic position control is iimplemented currently.  Added by UniHer for control of KASPAR robot, but should be generic for the Dimax U2C controller.  Tested on Dimax U2C-12. Naeem Assif Mirza. 30/11/2006
    svn path=/trunk/yarp2/; revision=4452

---
+++ src/libYARP_dev/src/dimax_u2c/common/yarp/DimaxU2C.h
@@ -0,0 +1,64 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+/*
+ * Copyright (C) 2006 Assif Mirza
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+#ifndef __KASPAR_CONTROL_DEVICE_H__
+#define __KASPAR_CONTROL_DEVICE_H__
+
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/ControlBoardInterfacesImpl.h>
+
+#include "Servo.h"
+
+namespace yarp {
+    namespace dev {
+        class DimaxU2C;
+    }
+}
+
+class yarp::dev::DimaxU2C : 
+	public yarp::dev::DeviceDriver ,
+	public yarp::dev::IPositionControlRaw,
+  public yarp::dev::ImplementPositionControl<DimaxU2C, yarp::dev::IPositionControl>
+{
+public:
+    DimaxU2C();
+    ~DimaxU2C();
+
+    // methods to implement DeviceDriver Interface
+    virtual bool open(yarp::os::Searchable& config);
+    virtual bool close();
+
+    // virtual methods overriding implemented IPositionControlRaw Interface
+    /// POSITION CONTROL INTERFACE RAW
+    virtual bool getAxes(int *ax);
+    virtual bool setPositionMode();
+    virtual bool positionMoveRaw(int j, double ref);
+    virtual bool positionMoveRaw(const double *refs);
+    virtual bool relativeMoveRaw(int j, double delta);
+    virtual bool relativeMoveRaw(const double *deltas);
+    virtual bool checkMotionDoneRaw(bool *flag);
+    virtual bool checkMotionDoneRaw(int j, bool *flag);
+    virtual bool setRefSpeedRaw(int j, double sp);
+    virtual bool setRefSpeedsRaw(const double *spds);
+    virtual bool setRefAccelerationRaw(int j, double acc);
+    virtual bool setRefAccelerationsRaw(const double *accs);
+    virtual bool getRefSpeedRaw(int j, double *ref);
+    virtual bool getRefSpeedsRaw(double *spds);
+    virtual bool getRefAccelerationRaw(int j, double *acc);
+    virtual bool getRefAccelerationsRaw(double *accs);
+    virtual bool stopRaw(int j);
+    virtual bool stopRaw();
+    //
+    /////////////////////////////// END Position Control Raw INTERFACE
+private:
+	Servo *servos;
+	int numJoints;
+	double *speeds;
+	double *accels;
+
+};
+
+#endif
