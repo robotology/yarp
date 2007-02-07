91ba4025f31b87b7eff8c23d0b8ed19577451b5e
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-02-07 10:09:59 +0000

    autoconf/make update script for synchronizing with cmake
    svn path=/trunk/yarp2/; revision=4710

--- conf/YarpVersion.cmake
+++ conf/YarpVersion.cmake
@@ -1,3 +1,7 @@
+
+# This file is the official location of the current YARP version number.
+# Please do not add extra numerals in this file or change its formatting.
+
 SET(CPACK_PACKAGE_VERSION_MAJOR "2")
 SET(CPACK_PACKAGE_VERSION_MINOR "1")
 SET(CPACK_PACKAGE_VERSION_PATCH "3")
