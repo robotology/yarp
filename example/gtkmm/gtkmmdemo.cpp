4cc781006289a52a26ff7316234b610006c3113a
Giorgio Metta <giorgio.metta@iit.it>
2007-02-11 10:57:44 +0000

    added little example to compile gtkmm with yarp and glade
    svn path=/trunk/yarp2/; revision=4775

--- example/gtkmm/gtkmmdemo.cpp
+++ example/gtkmm/gtkmmdemo.cpp
@@ -1,22 +1,21 @@
 /*
-  Example taken from GTKMM+ 2.4 Tutorial.
-	http://www.gtkmm.org/
-
-  Copyright information from tutorial:
-    Copyright (c) 2002-2006 Murray Cumming
-    License of the original is FDL.
+    Example glade application. By Giorgio.
+    NOTE: The application doesn't shut down properly.
  */
 
 // just removes the pervading warning that plagues compilation of the gtkmm
 #pragma warning(disable: 4250)
 
 // real code here!
+#include <libglademm.h>
 #include <gtkmm.h>
 
+// the very simplest glade application.
 int main(int argc, char *argv[])
 {
     Gtk::Main kit(argc, argv);
-    Gtk::Window window;
-    Gtk::Main::run(window);
+    Glib::RefPtr<Gnome::Glade::Xml> refXml = Gnome::Glade::Xml::create("basic.glade");
+    kit.run();
+
     return 0;
 }
