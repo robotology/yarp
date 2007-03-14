c7836f4941ed3b4dd6d6df7ce1e7891a0c533303
Alessandro Scalzo <alessandro.scalzo@iit.it>
2007-03-14 11:05:43 +0000

    no message
    svn path=/trunk/yarp2/; revision=4973

--- src/yarp-framegrabbergui/fggui_main.cpp
+++ src/yarp-framegrabbergui/fggui_main.cpp
@@ -9,6 +9,11 @@
 #include <gtkmm.h>
 #include "FrameGrabberGUIControl.h"
 
+//#include <ace/config.h>
+//#include <ace/OS.h>
+#include <yarp/os/Property.h> 
+#include <yarp/os/Network.h> 
+
 int main(int argc, char *argv[])
 {
     Gtk::Main kit(argc, argv);
@@ -18,9 +23,58 @@ int main(int argc, char *argv[])
 		FrameGrabberGUIControl window(argv[1],argv[2]);
 		Gtk::Main::run(window);
 	}
-	else
+	else if (argc<3)
 	{
 		FrameGrabberGUIControl window("","");
+		Gtk::Main::run(window);
+	}
+	else if (argc>3)
+	{
+		yarp::os::Property options;
+        options.fromCommand(argc,argv);
+    
+		// switch to subsections if available
+		yarp::os::Searchable *Network = &options.findGroup("NETWORK");
+		yarp::os::Searchable *Window = &options.findGroup("WINDOW");
+		//yarp::os::Searchable *Program = &options.findGroup("PROGRAM");
+
+		if (Network->isNull()) { Network = &options; }
+		if (Window->isNull()) { Window = &options; }
+		//if (Program->isNull()) { Program = &options; }
+
+		yarp::os::Value *val;
+    
+		char portName[256]="",outPortName[256]="";
+		int posX=0,posY=0;
+
+		//ACE_OS::sprintf(portName,argv[1]);
+		//ACE_OS::sprintf(outPortName,argv[2]);
+
+		if (Network->check("PortName",val)||Network->check("local",val)) 
+		{
+			strcpy(portName, val->asString().c_str());
+		}
+    
+		if (Network->check("OutPortName",val)||Network->check("remote",val)) 
+		{
+			strcpy(outPortName, val->asString().c_str());
+		}
+
+		if (Window->check("PosX",val)||Window->check("x",val)) 
+		{
+			posX = val->asInt();
+		}
+    
+		if (Window->check("PosY",val)||Window->check("y",val)) 
+		{
+			posY = val->asInt();
+		}
+	
+		printf("using local=%s remote=%s x=%d y=%d\n",portName,outPortName,posX,posY);
+		fflush(stdout);
+
+		FrameGrabberGUIControl window(portName,outPortName);
+		window.move(posX,posY);
 		Gtk::Main::run(window);
 	}
 
