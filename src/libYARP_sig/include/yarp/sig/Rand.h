21c4bb70d04683d2811597d3a625ec5a27a1d2a3
Lorenzo Natale <lorenzo.natale@iit.it>
2007-03-22 14:17:43 +0000

    *** empty log message ***
    svn path=/trunk/yarp2/; revision=5030

--- src/libYARP_sig/include/yarp/sig/Rand.h
+++ src/libYARP_sig/include/yarp/sig/Rand.h
@@ -1,3 +1,11 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2007 Lorenzo Natale
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
 #ifndef __YARP_SIG_RAND__
 #define __YARP_SIG_RAND__
 
--- src/libYARP_sig/src/Rand.cpp
+++ src/libYARP_sig/src/Rand.cpp
@@ -1,3 +1,5 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
 #include <yarp/os/Semaphore.h>
 #include <yarp/sig/Rand.h>
 #include <time.h>
