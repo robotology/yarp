9cd8e7455062788f3904f2336f9c0c519015d946
Lorenzo Natale <lorenzo.natale@iit.it>
2007-03-27 13:35:13 +0000

    missing file
    svn path=/trunk/yarp2/; revision=5053

---
+++ src/libYARP_sig/include/yarp/gsl_compatibility.h
@@ -0,0 +1,47 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+* Copyright (C) 2007 Lorenzo Natale
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*
+*/
+
+// $Id: gsl_compatibility.h,v 1.1 2007-03-27 13:35:13 natta Exp $
+
+/** 
+* Defines types for binary compatibily with the GSL. Included by cpp 
+* code within yarp.
+*/
+
+/// GSL TYPES
+#ifndef gsl_block
+typedef struct 
+{
+  size_t size;
+  double *data;
+} gsl_block;
+#endif
+
+#ifndef gsl_vector
+struct gsl_vector
+{
+  size_t size;
+  size_t stride;
+  double *data; 
+  gsl_block *block;
+  int owner;
+};
+#endif
+
+#ifndef gsl_matrix
+struct gsl_matrix
+{
+  size_t size1;
+  size_t size2;
+  size_t tda;
+  double * data;
+  gsl_block * block;
+  int owner;
+};
+#endif
+
