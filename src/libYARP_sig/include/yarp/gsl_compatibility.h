4a87a9a0fb25bb266fcf4dbdfb0534833ee7ff43
YARP Developers <yarp0-devel@lists.sourceforge.net>
2007-03-27 15:07:57 +0000

    *** empty log message ***
    svn path=/trunk/yarp2/; revision=5062

--- src/libYARP_sig/include/yarp/gsl_compatibility.h
+++ src/libYARP_sig/include/yarp/gsl_compatibility.h
@@ -6,7 +6,7 @@
 *
 */
 
-// $Id: gsl_compatibility.h,v 1.3 2007-03-27 15:06:49 babybot Exp $
+// $Id: gsl_compatibility.h,v 1.4 2007-03-27 15:07:57 babybot Exp $
 
 /** 
 * Defines types for binary compatibily with the GSL. Included by cpp 
@@ -44,4 +44,5 @@ struct gsl_matrix
   int owner;
 };
 #endif
-///
+
+//
