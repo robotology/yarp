b6a475fe05e34ed8a8eeb46b17a0451e4af99fa1
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-05-16 20:13:09 +0000

    clash with opencv
    svn path=/trunk/yarp2/; revision=5320

--- src/libYARP_sig/include/yarp/sig/all.h
+++ src/libYARP_sig/include/yarp/sig/all.h
@@ -12,6 +12,6 @@
 #include <yarp/sig/ImageDraw.h>
 #include <yarp/sig/ImageFile.h>
 #include <yarp/sig/Image.h>
-#include <yarp/sig/IplImage.h>
+//#include <yarp/sig/IplImage.h> // clashes with OpenCV - only include if really needed
 #include <yarp/sig/Sound.h>
 #include <yarp/sig/Vector.h>
