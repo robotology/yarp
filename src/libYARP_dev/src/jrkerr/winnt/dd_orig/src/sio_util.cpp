8734dc96c7a5159f66c416126f16ac8a3396ddcf
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-06-04 15:07:08 +0000

    add jrkerr summaries, BSDed
    svn path=/trunk/yarp2/; revision=5600

--- src/libYARP_dev/src/jrkerr/winnt/dd_orig/src/sio_util.cpp
+++ src/libYARP_dev/src/jrkerr/winnt/dd_orig/src/sio_util.cpp
@@ -1,6 +1,7 @@
 /*============================================
 
 Copyright: (c) 2007 Jeffrey Kerr LLC
+CopyPolicy: 3-clause BSD
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
