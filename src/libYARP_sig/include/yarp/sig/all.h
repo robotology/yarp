0870d870fc8481dd03d01f1ddfa271dcd4442f43
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-07-18 13:41:46 +0000

    sig/all.h missing SoundFile
    svn path=/trunk/yarp2/; revision=5780

--- src/libYARP_sig/include/yarp/sig/all.h
+++ src/libYARP_sig/include/yarp/sig/all.h
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2006 Paul Fitzpatrick
+ * Copyright (C) 2006, 2007 Paul Fitzpatrick
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
  */
@@ -14,4 +14,5 @@
 #include <yarp/sig/Image.h>
 //#include <yarp/sig/IplImage.h> // clashes with OpenCV - only include if really needed
 #include <yarp/sig/Sound.h>
+#include <yarp/sig/SoundFile.h>
 #include <yarp/sig/Vector.h>
