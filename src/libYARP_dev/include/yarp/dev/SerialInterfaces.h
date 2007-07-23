991a1075f07600a2cf632a19123dfdac57841e51
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2007-07-23 16:08:31 +0000

    Just updating the authors names
    svn path=/trunk/yarp2/; revision=5816

--- src/libYARP_dev/include/yarp/dev/SerialInterfaces.h
+++ src/libYARP_dev/include/yarp/dev/SerialInterfaces.h
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2006 Lorenzo Natale
+ * Copyright (C) 2006 Alex Bernardino & Carlos Beltran-Gonzalez
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
  */
