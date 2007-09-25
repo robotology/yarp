57e09c29108b53f5c56cc3c40bfd289ae67fc3af
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-09-25 10:04:35 +0000

    normalize carlos name for copyright
    svn path=/trunk/yarp2/; revision=6101

--- src/libYARP_dev/src/serial/default/yarp/SerialDeviceDriver.cpp
+++ src/libYARP_dev/src/serial/default/yarp/SerialDeviceDriver.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2007 Alex Bernardino, Carlos Beltran
+ * Copyright (C) 2007 Alex Bernardino, Carlos Beltran-Gonzalez
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
  */
