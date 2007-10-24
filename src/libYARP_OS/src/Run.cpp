2340ea3459bf615914709b7281b4cb69c9f78fb0
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-10-24 12:30:40 +0000

    correct copyright
    svn path=/trunk/yarp2/; revision=6234

--- src/libYARP_OS/src/Run.cpp
+++ src/libYARP_OS/src/Run.cpp
@@ -1,8 +1,10 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2007 Paul Fitzpatrick - Alessandro Scalzo
+ * Copyright (C) 2007 Alessandro Scalzo
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ * Based on an earlier version by Paul Fitzpatrick
  *
  */
 
