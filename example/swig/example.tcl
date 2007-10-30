4fba0edf43dc78e0cae0e9fbf184ab000b26e95d
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-10-30 15:14:49 +0000

    tcl fixes
    svn path=/trunk/yarp2/; revision=6248

--- example/swig/example.tcl
+++ example/swig/example.tcl
@@ -2,7 +2,9 @@
 
 # You may have to fix this up, depending on your OS
 # I don't really know anything about TCL
+# Use .so version for linux, .dll version for windows
 load ./cyarp.so cyarp
+#load [file join [pwd] cyarp.dll] cyarp
 
 Network_init
 
