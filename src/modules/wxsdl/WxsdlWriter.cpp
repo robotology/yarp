5734e044428566bc7cb35d653f7653272b8f0b70
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-11-26 15:06:19 +0000

    wxsdl stub
    svn path=/trunk/yarp2/; revision=6323

---
+++ src/modules/wxsdl/WxsdlWriter.cpp
@@ -0,0 +1,397 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2007 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ * Originally based on "wx-sdl tutorial v1.1", copyright follows
+ *
+ */
+
+/*******************************************************************************
+ * wx-sdl tutorial v1.1
+ * Copyright (C) 2005,2007 John David Ratliff
+ *
+ * wx-sdl is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wx-sdl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wx-sdl; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*******************************************************************************/
+
+
+#include <iostream>
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include <wx/dcbuffer.h>
+#include <wx/image.h>
+
+#include "SDL.h"
+
+/*******************************************************************************
+ * Global Declarations
+*******************************************************************************/
+
+enum {
+    IDF_FRAME = wxID_HIGHEST + 1,
+    IDP_PANEL
+};
+
+/*******************************************************************************
+ * SDLPanel Class
+*******************************************************************************/
+
+class SDLPanel : public wxPanel {
+    DECLARE_CLASS(SDLPanel)
+    DECLARE_EVENT_TABLE()
+    
+private:
+    SDL_Surface *screen;
+
+    /**
+     * Called to paint the panel.
+     */
+    void onPaint(wxPaintEvent &);
+    
+    /**
+     * Called to erase the background.
+     */
+    void onEraseBackground(wxEraseEvent &);
+    
+    /**
+     * Called to update the panel in idle time.
+     */
+    void onIdle(wxIdleEvent &);
+    
+    /**
+     * Creates the SDL_Surface used by this SDLPanel.
+     */
+    void createScreen();
+    
+public:
+    /**
+     * Creates a new SDLPanel.
+     *
+     * @param parent The wxWindow parent.
+     */
+    SDLPanel(wxWindow *parent);
+    
+    /**
+     * Destructor for the SDLPanel.
+     */
+    ~SDLPanel();
+};
+
+inline void SDLPanel::onEraseBackground(wxEraseEvent &) { /* do nothing */ }
+
+IMPLEMENT_CLASS(SDLPanel, wxPanel)
+
+BEGIN_EVENT_TABLE(SDLPanel, wxPanel)
+    EVT_PAINT(SDLPanel::onPaint)
+    EVT_ERASE_BACKGROUND(SDLPanel::onEraseBackground)
+    EVT_IDLE(SDLPanel::onIdle)
+END_EVENT_TABLE()
+
+SDLPanel::SDLPanel(wxWindow *parent) : wxPanel(parent, IDP_PANEL), screen(0) {
+    // ensure the size of the wxPanel
+    wxSize size(640, 480);
+    
+    SetMinSize(size);
+    SetMaxSize(size);
+}
+
+SDLPanel::~SDLPanel() {
+    if (screen) {
+        SDL_FreeSurface(screen);
+    }
+}
+
+void SDLPanel::onPaint(wxPaintEvent &) {
+    // can't draw if the screen doesn't exist yet
+    if (!screen) {
+        return;
+    }
+    
+    // lock the surface if necessary
+    if (SDL_MUSTLOCK(screen)) {
+        if (SDL_LockSurface(screen) < 0) {
+            return;
+        }
+    }
+    
+    // create a bitmap from our pixel data
+    wxBitmap bmp(wxImage(screen->w, screen->h, 
+                    static_cast<unsigned char *>(screen->pixels), true));
+    
+    // unlock the screen
+    if (SDL_MUSTLOCK(screen)) {
+        SDL_UnlockSurface(screen);
+    }
+    
+    // paint the screen
+    wxBufferedPaintDC dc(this, bmp);
+}
+
+void SDLPanel::onIdle(wxIdleEvent &) {
+    // create the SDL_Surface
+    createScreen();
+    
+    // Lock surface if needed
+    if (SDL_MUSTLOCK(screen)) {
+        if (SDL_LockSurface(screen) < 0) {
+            return;
+        }
+    }
+    
+    // Ask SDL for the time in milliseconds
+    int tick = SDL_GetTicks();
+    
+    for (int y = 0; y < 480; y++) {
+        for (int x = 0; x < 640; x++) {
+            wxUint32 color = (y * y) + (x * x) + tick;
+            wxUint8 *pixels = static_cast<wxUint8 *>(screen->pixels) + 
+                              (y * screen->pitch) +
+                              (x * screen->format->BytesPerPixel);
+
+            #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+                pixels[0] = color & 0xFF;
+                pixels[1] = (color >> 8) & 0xFF;
+                pixels[2] = (color >> 16) & 0xFF;
+            #else
+                pixels[0] = (color >> 16) & 0xFF;
+                pixels[1] = (color >> 8) & 0xFF;
+                pixels[2] = color & 0xFF;
+            #endif
+        }
+    }
+    
+    // Unlock if needed
+    if (SDL_MUSTLOCK(screen)) {
+        SDL_UnlockSurface(screen);
+    }
+    
+    // refresh the panel
+    Refresh(false);
+    
+    // throttle to keep from flooding the event queue
+    wxMilliSleep(33);
+    printf("tick...\n");
+}
+
+void SDLPanel::createScreen() {
+    if (!screen) {
+        int width, height;
+        GetSize(&width, &height);
+        
+        screen = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 
+                                      24, 0, 0, 0, 0);     
+    }
+}
+
+/*******************************************************************************
+// SDLFrame Class
+*******************************************************************************/
+
+class SDLFrame : public wxFrame {
+    DECLARE_CLASS(SDLFrame)
+    DECLARE_EVENT_TABLE()
+    
+private:
+    SDLPanel *panel;
+    
+    /**
+     * Called when exit from the file menu is selected.
+     */
+    void onFileExit(wxCommandEvent &);
+    
+    /**
+     * Called when about from the help menu is selected.
+     */
+    void onHelpAbout(wxCommandEvent &);
+    
+public:
+    /**
+     * Creates a new SDLFrame.
+     */
+    SDLFrame();
+    
+    /**
+     * Gets the SDLPanel within this SDLFrame.
+     *
+     * @return The SDLPanel.
+     */
+    SDLPanel &getPanel();
+};
+
+inline void SDLFrame::onFileExit(wxCommandEvent &) { Close(); }
+inline SDLPanel &SDLFrame::getPanel() { return *panel; }
+
+IMPLEMENT_CLASS(SDLFrame, wxFrame)
+
+BEGIN_EVENT_TABLE(SDLFrame, wxFrame)
+    EVT_MENU(wxID_EXIT, SDLFrame::onFileExit)
+    EVT_MENU(wxID_ABOUT, SDLFrame::onHelpAbout)
+END_EVENT_TABLE()
+
+SDLFrame::SDLFrame() {
+    // Create the SDLFrame
+    Create(0, IDF_FRAME, wxT("wx-sdl Frame"), wxDefaultPosition,
+           wxDefaultSize, wxCAPTION | wxSYSTEM_MENU | 
+           wxMINIMIZE_BOX | wxCLOSE_BOX);
+
+    // create the main menubar
+    wxMenuBar *mb = new wxMenuBar;
+    
+    // create the file menu
+    wxMenu *fileMenu = new wxMenu;
+    fileMenu->Append(wxID_EXIT, wxT("E&xit"));
+    
+    // add the file menu to the menu bar
+    mb->Append(fileMenu, wxT("&File"));
+    
+    // create the help menu
+    wxMenu *helpMenu = new wxMenu;
+    helpMenu->Append(wxID_ABOUT, wxT("About"));
+    
+    // add the help menu to the menu bar
+    mb->Append(helpMenu, wxT("&Help"));
+    
+    // add the menu bar to the SDLFrame
+    SetMenuBar(mb);
+    
+    // create the SDLPanel
+    panel = new SDLPanel(this);
+}
+
+void SDLFrame::onHelpAbout(wxCommandEvent &) {
+    wxMessageBox(wxT("wx-sdl tutorial\nCopyright (C) 2005,2007 John Ratliff"),
+                 wxT("about wx-sdl tutorial"), wxOK | wxICON_INFORMATION);
+}
+
+/*******************************************************************************
+// SDLApp Class
+*******************************************************************************/
+
+class SDLApp : public wxApp {
+    DECLARE_CLASS(SDLApp)
+    
+private:
+    SDLFrame *frame;
+    
+public:
+    /**
+     * Called to initialize this SDLApp.
+     *
+     * @return true if initialization succeeded; false otherwise.
+     */
+    bool OnInit();
+    
+    /**
+     * Called to run this SDLApp.
+     *
+     * @return The status code (0 if good, non-0 if bad).
+     */
+    int OnRun();
+    
+    /**
+     * Called when this SDLApp is ready to exit.
+     *
+     * @return The exit code.
+     */
+    int OnExit();
+};
+
+bool SDLApp::OnInit() {
+    // create the SDLFrame
+    frame = new SDLFrame;
+    frame->SetClientSize(640, 480);
+    frame->Centre();
+    frame->Show();
+    
+    // Our SDLFrame is the Top Window
+    SetTopWindow(frame);
+
+    // initialization should always succeed
+    return true;
+}
+
+int SDLApp::OnRun() {
+  printf("starting...\n");
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "unable to init SDL: " << SDL_GetError() << '\n';
+        
+        return -1;
+    }
+    printf("initialized\n");
+    
+    // generate an initial idle event to start things
+    wxIdleEvent event;
+    event.SetEventObject(&frame->getPanel());
+    frame->getPanel().AddPendingEvent(event);
+
+    // start the main loop
+    return wxApp::OnRun();
+}
+
+int SDLApp::OnExit() {
+    // cleanup SDL
+    SDL_Quit();
+    
+    // return the standard exit code
+    return wxApp::OnExit();
+}
+
+IMPLEMENT_CLASS(SDLApp, wxApp)
+
+IMPLEMENT_APP_NO_MAIN(SDLApp)
+    //IMPLEMENT_APP(SDLApp)
+
+
+
+#include "WxsdlWriter.h"
+
+using namespace yarp::dev;
+
+
+void WxsdlWriter::run() {
+    int argc = 1;
+    char *argv[] = { "yarp", NULL };
+    active = true;
+    wxEntry(argc, argv);
+    printf("WxsdlWriter finished\n");
+    active = false;
+}
+
+bool WxsdlWriter::open(yarp::os::Searchable & config) {
+
+    start();
+    return true;
+}
+    
+bool WxsdlWriter::close() {
+    if (active) {
+        active = false; 
+        //wxGetApp().OnExit();
+        wxGetApp().CleanUp();
+    }
+    stop();
+    return true;
+}
+  
+bool WxsdlWriter::putImage(yarp::sig::ImageOf<yarp::sig::PixelRgb> & image) {
+    return true;
+}
---
+++ src/modules/wxsdl/WxsdlWriter.h
@@ -0,0 +1,56 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2007 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+
+#ifndef WxsdlWriter_INC
+#define WxsdlWriter_INC
+
+/*
+ * Uses wxwidgets and SDL to view image sequences.
+ *
+ */
+
+namespace yarp {
+    namespace dev {
+        class WxsdlWriter;
+    }
+}
+
+#include <yarp/dev/AudioVisualInterfaces.h>
+#include <yarp/dev/DeviceDriver.h>
+
+/**
+ * @ingroup dev_impl_media
+ *
+ * Uses wxwidgets and sdl to view image sequences.
+ *
+ */
+class yarp::dev::WxsdlWriter : public IFrameWriterImage, 
+            public DeviceDriver, public yarp::os::Thread
+{
+public:
+
+    WxsdlWriter() {
+        active = false;
+    }
+  
+    virtual bool open(yarp::os::Searchable & config);
+    
+    virtual bool close();
+  
+    virtual bool putImage(yarp::sig::ImageOf<yarp::sig::PixelRgb> & image);
+
+    virtual void run();
+
+private:
+    bool active;
+};
+
+
+#endif
+
