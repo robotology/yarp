667cbfe0ae4ce638f9cc1e2592900e377fac6356
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-12-05 11:34:25 +0000

    missing file
    svn path=/trunk/yarp2/; revision=6366

---
+++ src/modules/vfw/vfw_test.cpp
@@ -0,0 +1,35 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2007 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+#include <VfwGrabber.h>
+
+#include <yarp/sig/ImageFile.h>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::dev;
+
+int main() {
+  VfwGrabber grabber;
+  Property p;
+  bool ok = grabber.open(p);
+  if (!ok) {
+    printf("CAMERA PROBLEM\n");
+    return 1;
+  }
+  for (int i=0; i<30; i++) {
+    ImageOf<PixelRgb> img;
+    grabber.getImage(img);
+    printf("image size %d %d\n", img.width(), img.height());
+    char fname[256];
+    sprintf(fname,"img%06d.ppm",i);
+    yarp::sig::file::write(img,fname);
+  }
+  return 0;
+}
+
