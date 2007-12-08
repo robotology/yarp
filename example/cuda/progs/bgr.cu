6b8fdcfd70b262420a528457a11c6d2c3f6ffeef
Giacomo Spigler <spiglerg@gmail.com>
2007-12-08 18:46:30 +0000

    As last modification
    svn path=/trunk/yarp2/; revision=6410

--- example/cuda/progs/bgr.cu
+++ example/cuda/progs/bgr.cu
@@ -4,7 +4,7 @@
 __global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
   int i=0;
 
-  for(i=threadIdx.x+blockIdx.x*ATHREAD_COUNT; i<parms.n; i+=ATHREAD_COUNT*BLOCKS) {
+  for(i=threadIdx.x+blockIdx.x*ATHREAD_COUNT; i<parms.n; i+=blockDim.x*gridDim.x) {
     out[i*3]=in[i*3+2];
     out[i*3+1]=in[i*3+1];
     out[i*3+2]=in[i*3];
