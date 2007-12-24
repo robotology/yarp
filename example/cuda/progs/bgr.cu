201087301ed53da608b819b0d447763375037e9b
Giacomo Spigler <spiglerg@gmail.com>
2007-12-24 13:41:29 +0000

    Fix on wrong variables
    svn path=/trunk/yarp2/; revision=6450

--- example/cuda/progs/bgr.cu
+++ example/cuda/progs/bgr.cu
@@ -4,7 +4,7 @@
 __global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
   int i=0;
 
-  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<parms.n; i+=blockDim.x*gridDim.x) {
+  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
     out[i*3]=in[i*3+2];
     out[i*3+1]=in[i*3+1];
     out[i*3+2]=in[i*3];
--- example/cuda/progs/pass-red.cu
+++ example/cuda/progs/pass-red.cu
@@ -4,7 +4,7 @@
 __global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
   int i=0;
 
-  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<parms.n; i+=blockDim.x*gridDim.x) {
+  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
     if(in[i*3]>=200) {
       //out[i*3]=255;
       //out[i*3+1]=255;
