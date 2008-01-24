2801bc672377efabd10c884afb4d7df9c556f40b
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-01-24 19:28:40 +0000

    freshen example
    svn path=/trunk/yarp2/; revision=6483

--- example/dev_check/main.cpp
+++ example/dev_check/main.cpp
@@ -3,27 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <yarp/os/Property.h>
 #include <yarp/sig/Image.h>
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/FrameGrabberInterfaces.h>
 #include <yarp/dev/ControlBoardInterfaces.h>
 
+#ifdef USE_NEW_DEVICE_MECHANISM
+#include "drivers.h"
+#endif
+
+using namespace yarp::os;
 using namespace yarp::dev;
 using namespace yarp::sig;
 
 
 int main() {
-    PolyDriver dd("dragonfly");
+#ifdef USE_NEW_DEVICE_MECHANISM
+    DriverCollection dev;
+#endif
+
+    PolyDriver dd;
+    Property fakebot;
+    fakebot.put("device","fakebot");
+    fakebot.put("background",""); // suppress background image
+    fakebot.put("target","");     // suppress foreground image
+    dd.open(fakebot);
 
     if (!dd.isValid()) {
-        printf("Dragonfly not available\n");
+        printf("fakebot not available\n");
         exit(1);
     }
 
-    printf("*** Dragonfly created\n");
+    printf("*** fakebot created\n");
+
+    PolyDriver dd2("test_grabber");
+    if (!dd2.isValid()) {
+        printf("test_grabber not available\n");
+        exit(1);
+    }
+
   
     IFrameGrabberImage *grabber;
-    dd.view(grabber);
+    dd2.view(grabber);
   
     if (grabber!=NULL) {
         printf("*** It can supply images\n");
