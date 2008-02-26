614c01f17b4be65093e543c0392b9ac1513fe081
YARP Developers <yarp0-devel@lists.sourceforge.net>
2008-02-26 12:15:36 +0000

    fixing link error when using gtk
    svn path=/trunk/yarp2/; revision=6551

--- conf/FindGtkPlus.cmake
+++ conf/FindGtkPlus.cmake
@@ -6,14 +6,21 @@ IF(UNIX)
 	PKGCONFIG("gtk+-2.0" GTKPLUS_INCLUDE_DIR GTKPLUS_LIBRARY_DIR GTKPLUS_LINK_FLAGS GTKPLUS_C_FLAGS)
  ENDIF(PKGCONFIG_EXECUTABLE)
 
+
 #IF (APPLE)
 	# there is a problem with some versions of pkg-config introducing
 	# newline characters that don't get cleaned up in some versions
 	# of cmake.  No time to fix this now...
 	# the same problem seems to happen also on Linux/Debian (ask Giorgio Metta).
-	IF (GTKPLUS_LIBRARY_DIR)
-	SET(GTKPLUS_LINK_FLAGS "${GTKPLUS_LINK_FLAGS} ${GTKPLUS_LIBRARY_DIR}")
-	ENDIF (GTKPLUS_LIBRARY_DIR)
+#The following IF appends /usr/lib as a link flag, this
+#asks the linker to link a directory, which is wrong and causes a link error.
+#It looks more correct to remove the command completely. Another option is 
+#to add -L${GTKPLUS_LIBRARY_DIR} instead of ${GTKPLUS_LIBRARY_DIR}
+#
+#	IF (GTKPLUS_LIBRARY_DIR)
+#	SET(GTKPLUS_LINK_FLAGS "${GTKPLUS_LINK_FLAGS} ${GTKPLUS_LIBRARY_DIR}")
+#	ENDIF (GTKPLUS_LIBRARY_DIR)
+
 #ENDIF (APPLE)
 
  SET(GTKPLUS_LINK_FLAGS "${GTKPLUS_LINK_FLAGS}" CACHE INTERNAL "gtk+ link flags")
@@ -32,3 +39,4 @@ ELSE (UNIX)
  	FIND_PACKAGE(GtkWin32) 
 ENDIF (UNIX)
 
+MESSAGE(FindGtkPlus: ${GTKPLUS_LINK_FLAGS})
