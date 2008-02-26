ebadfb1c1cc2ae75e1ef00e61e89a993bc2ab445
Giacomo Spigler <spiglerg@gmail.com>
2008-02-26 17:50:43 +0000

    Code updated as for CUDA 1.1
    svn path=/trunk/yarp2/; revision=6554

--- example/cuda/progs/bgr.cu
+++ example/cuda/progs/bgr.cu
@@ -1,14 +1,17 @@
 #define NUMTHREADS 32
 #define NUMBLOCKS 96
 
-__global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
-  int i=0;
+extern "C" {
 
-  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
-    out[i*3]=in[i*3+2];
-    out[i*3+1]=in[i*3+1];
-    out[i*3+2]=in[i*3];
+  __global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
+    int i=0;
+
+    for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
+      out[i*3]=in[i*3+2];
+      out[i*3+1]=in[i*3+1];
+      out[i*3+2]=in[i*3];
+    }
   }
-}
 
+}
 
--- example/cuda/progs/pass-red.cu
+++ example/cuda/progs/pass-red.cu
@@ -1,20 +1,24 @@
 #define ATHREAD_COUNT 32
 #define BLOCKS 96
 
-__global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
-  int i=0;
+extern "C" {
 
-  for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
-    if(in[i*3]>=200) {
-      //out[i*3]=255;
-      //out[i*3+1]=255;
-      //out[i*3+2]=255;
-    } else {
-      out[i*3]=0;
-      out[i*3+1]=0;
-      out[i*3+2]=0;
+  __global__ void FragmentProgram(int w, int h, unsigned char *in, unsigned char *out) {
+    int i=0;
+
+    for(i=threadIdx.x+blockIdx.x*blockDim.x; i<w*h; i+=blockDim.x*gridDim.x) {
+      if(in[i*3]>=200) {
+        //out[i*3]=255;
+        //out[i*3+1]=255;
+        //out[i*3+2]=255;
+      } else {
+        out[i*3]=0;
+        out[i*3+1]=0;
+        out[i*3+2]=0;
+      }
     }
   }
+
 }
 
 
