0643ebedf20f1e8a23106443fe0e890370f4e9cb
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-03-18 16:17:41 +0000

    swscale
    svn path=/trunk/yarp2/; revision=6613

--- conf/FindFFMPEG.cmake
+++ conf/FindFFMPEG.cmake
@@ -13,12 +13,62 @@
 # This is usefull to do it this way so that we can always add more libraries
 # if needed to FFMPEG_LIBRARIES if ffmpeg ever changes...
 
-FIND_PATH(FFMPEG_INCLUDE_DIR ffmpeg/avformat.h
-  /usr/local/include
-  /usr/include
+FIND_PATH(FFMPEG_INCLUDE_DIR1 avformat.h
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
   $ENV{FFMPEG_DIR}
-  $ENV{FFMPEG_DIR}/include
+  $ENV{FFMPEG_DIR}/ffmpeg
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/include/libavformat
+  /usr/local/include/libavformat
 )
+
+FIND_PATH(FFMPEG_INCLUDE_DIR2 avutil.h
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/include/libavutil
+  /usr/local/include/libavutil
+)
+
+FIND_PATH(FFMPEG_INCLUDE_DIR3 avcodec.h
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/include/libavcodec
+  /usr/local/include/libavcodec
+)
+
+FIND_PATH(FFMPEG_INCLUDE_DIR4 swscale.h
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/include/libswscale
+  /usr/local/include/libswscale
+)
+
+IF (FFMPEG_INCLUDE_DIR1)
+  IF (FFMPEG_INCLUDE_DIR2)
+    IF (FFMPEG_INCLUDE_DIR3)
+      SET(FFMPEG_INCLUDE_DIR 
+	${FFMPEG_INCLUDE_DIR1}
+	${FFMPEG_INCLUDE_DIR2}
+	${FFMPEG_INCLUDE_DIR3})
+    ENDIF (FFMPEG_INCLUDE_DIR3)
+  ENDIF (FFMPEG_INCLUDE_DIR2)
+ENDIF (FFMPEG_INCLUDE_DIR1)
+
+IF (FFMPEG_INCLUDE_DIR4)
+      SET(FFMPEG_INCLUDE_DIR 
+	${FFMPEG_INCLUDE_DIR}
+	${FFMPEG_INCLUDE_DIR4})
+ENDIF (FFMPEG_INCLUDE_DIR4)
 
 FIND_LIBRARY(FFMPEG_avformat_LIBRARY avformat
   /usr/local/lib
@@ -35,6 +85,13 @@ FIND_LIBRARY(FFMPEG_avcodec_LIBRARY avcodec
 )
 
 FIND_LIBRARY(FFMPEG_avutil_LIBRARY avutil
+  /usr/local/lib
+  /usr/lib
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/lib
+)
+
+FIND_LIBRARY(FFMPEG_swscale_LIBRARY swscale
   /usr/local/lib
   /usr/lib
   $ENV{FFMPEG_DIR}
@@ -60,6 +117,12 @@ IF(FFMPEG_INCLUDE_DIR)
           ${FFMPEG_avcodec_LIBRARY} 
           ${FFMPEG_avutil_LIBRARY} 
           )
+	IF(FFMPEG_swscale_LIBRARY)
+          SET( FFMPEG_LIBRARIES 
+	    ${FFMPEG_LIBRARIES}
+            ${FFMPEG_swscale_LIBRARY}
+          )
+	ENDIF(FFMPEG_swscale_LIBRARY)
 	IF(_FFMPEG_z_LIBRARY_)
           SET( FFMPEG_LIBRARIES 
 	    ${FFMPEG_LIBRARIES}
@@ -73,9 +136,14 @@ ENDIF(FFMPEG_INCLUDE_DIR)
 
 MARK_AS_ADVANCED(
   FFMPEG_INCLUDE_DIR
+  FFMPEG_INCLUDE_DIR1
+  FFMPEG_INCLUDE_DIR2
+  FFMPEG_INCLUDE_DIR3
+  FFMPEG_INCLUDE_DIR4
   FFMPEG_avformat_LIBRARY
   FFMPEG_avcodec_LIBRARY
   FFMPEG_avutil_LIBRARY
+  FFMPEG_swscale_LIBRARY
   _FFMPEG_z_LIBRARY_
   )
 
