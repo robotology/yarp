1344fd9bb830dc4d47dc4bd71eb8e53a7abb3c06
Lorenzo Natale <lorenzo.natale@iit.it>
2008-05-02 12:34:53 +0000

    no message
    svn path=/trunk/yarp2/; revision=6703

--- src/libYARP_dev/src/ControlCalibrationImpl.cpp
+++ src/libYARP_dev/src/ControlCalibrationImpl.cpp
@@ -21,8 +21,10 @@ IControlCalibration::IControlCalibration()
 
 bool IControlCalibration::setCalibrator(ICalibrator *c)
 {
+    fprintf(stderr, "Setting calibrator\n");
     if (c!=0)
         {
+            fprintf(stderr, "Success\n");
             calibrator=c;    
             return true;
         }
