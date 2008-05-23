762ee6f658037c553ff65702e919efa1aa1f65d2
YARP Developers <yarp0-devel@lists.sourceforge.net>
2008-05-23 16:22:17 +0000

    *** empty log message ***
    svn path=/trunk/yarp2/; revision=6721

--- example/profiling/port_latency.cpp
+++ example/profiling/port_latency.cpp
@@ -7,55 +7,100 @@ using namespace yarp::os;
 // Send a sequence of message to a port. Test the 
 // time it takes for the message to be received.
 // Compute average.
+//
+// Time only makes sense if server and client are run
+// on the same machine.
+// 
+// Lorenzo Natale May 2008
 
 class Reader : public TypedReaderCallback<Bottle> {
 public:
     double delay;
     int count;
+    int wait;
 public:
     Reader()
     {
         delay=0;
         count=0;
+        // wait some messages before counting
+        wait=10;
     }
 
     void onRead(Bottle& datum) {
-        double t=datum.get(0).asDouble();
-        double now=Time::now();
-        delay+=(now-t)*1000;
-        count++;
+        //        if (wait>0)
+        //            {
+        //                wait--;
+        //            }
+        //        else
+        {
+
+                double t=datum.get(0).asDouble();
+                double now=Time::now();
+                delay+=(now-t)*1000;
+                count++;
+                // remove this to spare cpu time
+                fprintf(stderr, "%lf\n", (now-t)*1000);
+            }
+        //        else
+
     }
 };
 
-int main() {
-    Network yarp;
-    //    yarp.setLocalMode(true);
-    Reader reader;
-
-    BufferedPort<Bottle> port, port2;
-    port2.useCallback(reader);
+int server(double server_wait)
+{
+    BufferedPort<Bottle> port;
     port.open("/profiling/port");
-    port2.open("/profiling/port2");
-    yarp.connect("/profiling/port","/profiling/port2");
 
-    for (int k=0; k<100; k++) {
+    int k=0;
+    while(true) {
         printf("Sending frame %d\n", k);
         Bottle& b = port.prepare();
         b.clear();
-        for (int i=0; i<1000; i++) {
-            double time=Time::now();
-            b.clear();
-            b.addDouble(time);
-        }
+        double time=Time::now();
+        b.addDouble(time);
         port.write();
         //give the CPU some time
-        Time::delay(0.05);
+        Time::delay(server_wait);
+        k++;
     }
     port.close();
-    port2.close();
-
-    fprintf(stderr, "Average latency %.3lf\n", reader.delay/reader.count);
-
     return 0;
 }
 
+int client(int nframes)
+{
+    Reader reader;
+
+    BufferedPort<Bottle> port;
+    port.useCallback(reader);
+    port.open("/profiling/port2");
+    while(!Network::connect("/profiling/port","/profiling/port2"))
+        {
+            fprintf(stderr, "Waiting for connection\n");
+            Time::delay(0.5);
+        }
+
+    while(reader.count<nframes)
+        {
+            //give the CPU some time
+            Time::delay(0.5);
+        }
+
+    port.close();
+
+    fprintf(stderr, "Received: %d average latency %.3lf[ms]\n", 
+            reader.count, reader.delay/reader.count);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    Network yarp;
+    Property p;
+    p.fromCommand(argc, argv);
+
+    if (p.check("server"))
+        return server(p.find("period").asDouble());
+    else if (p.check("client"))
+        return client(p.find("nframes").asInt());
+}
