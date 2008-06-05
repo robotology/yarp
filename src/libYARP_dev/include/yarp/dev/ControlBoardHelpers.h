7fb26a1341c6f3694f3c602b2fa87c3a31288e51
YARP Developers <yarp0-devel@lists.sourceforge.net>
2008-06-05 15:24:46 +0000

    *** empty log message ***
    svn path=/trunk/yarp2/; revision=6728

---
+++ src/libYARP_dev/include/yarp/dev/ControlBoardHelpers.h
@@ -0,0 +1,39 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+/*
+ * Copyright (C) 2006 Giorgio Metta and Lorenzo Natale
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+#ifndef __CONTROLBOARDHELPERS__
+#define __CONTROLBOARDHELPERS__
+
+#include <yarp/os/PortablePair.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/sig/Vector.h>
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+/* the control command message type 
+ * head is a Bottle which contains the specification of the message type
+ * body is a Vector which move the robot accordingly
+ */
+typedef yarp::os::PortablePair <yarp::os::Bottle, yarp::sig::Vector> CommandMessage;
+
+/* check whether the last command failed */
+inline bool CHECK_FAIL(bool ok, yarp::os::Bottle& response) {
+
+    if (ok) {
+        if (response.get(0).isVocab() && response.get(0).asVocab() == VOCAB_FAILED) {
+            return false;
+        }
+    }
+    else
+        return false;
+
+    return true;
+}
+
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+#endif
