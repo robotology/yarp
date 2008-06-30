3aa43c1a7f985790f3209d6566c2d0b26b5f765f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-06-30 13:46:57 +0000

    copyright/license update
    svn path=/trunk/yarp2/; revision=6759

--- src/libYARP_dev/src/ControlBoardInterfacesImpl.cpp
+++ src/libYARP_dev/src/ControlBoardInterfacesImpl.cpp
@@ -1,3 +1,11 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2008 Lorenzo Natale
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
 #include "yarp/dev/ControlBoardInterfacesImpl.h"
 
 #include <stdio.h>
