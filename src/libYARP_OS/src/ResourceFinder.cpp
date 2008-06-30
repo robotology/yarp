649b09019b38c25181ad27b11359904fd333ce84
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-06-30 19:02:58 +0000

    config file policy
    svn path=/trunk/yarp2/; revision=6766

--- src/libYARP_OS/src/ResourceFinder.cpp
+++ src/libYARP_OS/src/ResourceFinder.cpp
@@ -70,11 +70,59 @@ public:
         return true;
     }
 
-    yarp::os::ConstString findFile(const char *name) {
-        fprintf(RTARGET, "||| findFile not really implemented yet...\n");
-        if (exists(name)) {
-            return name;
+
+    yarp::os::ConstString check(const char *base1, 
+                                const char *base2, 
+                                const char *base3, 
+                                const char *name) {
+        String s = "";
+        if (base1!=NULL) {
+            s = base1;
+            if (String(base1)!="") {
+                s = s + "/";
+            }
         }
+        if (base2!=NULL) {
+            s = s + base2;
+            if (String(base2)!="") {
+                s = s + "/";
+            }
+        }
+        if (base3!=NULL) {
+            s = s + base3;
+            if (String(base3)!="") {
+                s = s + "/";
+            }
+        }
+        s = s + name;
+        if (verbose) {
+            fprintf(RTARGET,"||| checking %s\n", s.c_str());
+        }
+        if (exists(s.c_str())) {
+            fprintf(RTARGET,"||| found %s\n", s.c_str());
+            return s.c_str();
+        }
+        return "";
+    }
+
+    yarp::os::ConstString findFile(const char *name) {
+
+        // check current directory
+        ConstString str = check("","","",name);
+        if (str!="") return str;
+
+        // check ROOT/app/default/
+        str = check(root.c_str(),"app","default",name);
+        if (str!="") return str;
+
+        // check app dirs
+        for (int i=0; i<apps.size(); i++) {
+            str = check(root.c_str(),"app",apps.get(i).asString().c_str(),
+                        name);
+            if (str!="") return str;
+        }
+
+        fprintf(RTARGET,"||| did not find %s\n", name);
         return "";
     }
 
