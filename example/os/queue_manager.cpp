4bae72054996f907e23cce628215cb219413ec8c
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-07-17 17:16:56 +0000

    online queue manager
    svn path=/trunk/yarp2/; revision=6804

---
+++ example/os/queue_manager.cpp
@@ -0,0 +1,150 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+
+/*
+ *
+ * This example maintains a small help queue webserver.
+ * After starting it, point a webbrowser at your local host and
+ * click "help" or "read"
+ *
+ * You may need to run this as superuser in order to use port 80.
+ *
+ *   -- paulfitz
+ *
+ */
+
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <stdio.h>
+using namespace yarp::os;
+using namespace yarp::dev;
+
+#include <deque>
+#include <string>
+using namespace std;
+
+class Entry {
+public:
+    string name;
+};
+
+class QueueManager : public DeviceResponder {
+private:
+    BufferedPort<Bottle> port;
+    deque<Entry> q;
+
+    bool removeName(const char *name) {
+        bool acted = false;
+        for (deque<Entry>::iterator it=q.begin(); it!=q.end(); it++) {
+            string nname = (*it).name;
+            if (nname==name) {
+                acted = true;
+                q.erase(it);
+                // iterators are now invalid
+                break;
+            }
+        }
+        if (acted) {
+            // in case there are other copies, remove recursively
+            removeName(name);
+        }
+        return acted;
+    }
+
+    void addQueue(Bottle& status) {
+        status.add(Value::makeVocab("q"));
+        for (deque<Entry>::iterator it=q.begin(); it!=q.end(); it++) {
+            string name = (*it).name;
+            status.addString(name.c_str());
+        }
+    }
+public:
+    QueueManager() {
+        attach(port);
+        Contact c = Contact::byName("/help");
+        c = c.addSocket("tcp","localhost",80);
+        port.open(c);
+    }
+
+    void updateHelp() {
+        makeUsage();
+        addUsage("[list]", "show the queue");
+        for (deque<Entry>::iterator it=q.begin(); it!=q.end(); it++) {
+            string name = (*it).name;
+            string usage = "[del] ";
+            usage += name;
+            addUsage(usage.c_str(), "remove this name fram the queue");
+        }
+        addUsage("[add] $name", "add a name to the queue");
+        addUsage("[del] $foo", "remove a name from the queue");
+    }
+
+    virtual bool respond(const yarp::os::Bottle& command, 
+                         yarp::os::Bottle& reply) {
+        switch (command.get(0).asVocab()) {
+        case VOCAB3('a','d','d'):
+            {
+                string name = command.get(1).asString().c_str();
+                if (name!="") {
+                    removeName(name.c_str());
+                    Entry entry;
+                    entry.name = name;
+                    q.push_back(entry);
+                    reply.clear();
+                    reply.add(Value::makeVocab("add"));
+                    reply.addString(name.c_str());
+                    addQueue(reply);
+                }
+            }
+            break;
+        case VOCAB3('d','e','l'):
+            {
+                string name = command.get(1).asString().c_str();
+                if (name!="") {
+                    bool acted = removeName(name.c_str());
+                    if (acted) {
+                        reply.clear();
+                        reply.add(Value::makeVocab("del"));
+                        reply.addString(name.c_str());
+                    } else {
+                        reply.clear();
+                        reply.add(Value::makeVocab("no"));
+                        reply.addString(name.c_str());
+                    }
+                    addQueue(reply);
+                }
+            }
+            break;
+        case VOCAB4('l','i','s','t'):
+            {
+                reply.clear();
+                addQueue(reply);
+            }
+            break;
+        default:
+            updateHelp();
+            return DeviceResponder::respond(command,reply);
+        }
+        printf("%s\n", reply.toString().c_str());
+        return true;
+    }
+
+    void update() {
+        Bottle& status = port.prepare();
+        status.clear();
+        addQueue(status);
+        port.write();
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Network yarp;
+    QueueManager man;
+
+    while (true) {
+        Time::delay(5);
+        man.update();
+    }
+
+    return 0;
+}
