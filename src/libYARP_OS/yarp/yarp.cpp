64336d99903e29171cf451321a15b9efa3ca7776
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-10-31 14:41:17 +0000

    remove unused headers
    svn path=/trunk/yarp2/; revision=7019

--- src/libYARP_OS/yarp/yarp.cpp
+++ src/libYARP_OS/yarp/yarp.cpp
@@ -1,23 +1,18 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2006 Paul Fitzpatrick
+ * Copyright (C) 2006, 2008 Paul Fitzpatrick
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
  */
 
 
-#include <yarp/os/impl/Logger.h>
-#include <yarp/os/impl/NameClient.h>
-#include <yarp/os/impl/Companion.h>
-
 #include <yarp/os/Network.h>
+using namespace yarp::os;
 
 int main(int argc, char *argv[]) {
     // call the yarp standard companion
-    ACE::init();
-    int result = yarp::os::Network::main(argc,argv);
-    ACE::fini();
-    return result;
+    Network yarp;
+    return Network::main(argc,argv);
 }
 
