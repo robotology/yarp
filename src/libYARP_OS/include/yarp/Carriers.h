9ad6d7bba3d69a842dac276c3d9c32667f50fc88
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2008-11-18 15:37:41 +0000

    clean up license statements
    svn path=/trunk/yarp2/; revision=7040

--- src/libYARP_OS/include/yarp/Carriers.h
+++ src/libYARP_OS/include/yarp/Carriers.h
@@ -1,3 +1,11 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2008 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
 // support old path to this file
 #include <yarp/os/impl/Carriers.h>
 namespace yarp {
--- src/libYARP_OS/include/yarp/Election.h
+++ src/libYARP_OS/include/yarp/Election.h
@@ -1,2 +1,10 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2008 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
 // support old path to this file
 #include <yarp/os/impl/Election.h>
