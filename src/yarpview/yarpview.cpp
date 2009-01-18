2d14525ead9fcc473c634c90cf5e9b78da194831
Lorenzo Natale <lorenzo.natale@iit.it>
2009-01-18 16:44:45 +0000

    no message
    svn path=/trunk/yarp2/; revision=7079

--- src/yarpview/yarpview.cpp
+++ src/yarpview/yarpview.cpp
@@ -11,7 +11,7 @@
 // gCamView.cpp : Defines the entry point for the application.
 //
 
-#include "gCamView.h"
+#include "yarpview.h"
 #include "FpsStats.h"
 
 #include <yarp/os/Property.h> 
@@ -962,7 +962,7 @@ exitRoutine:
     return 0;
 }
 
-#ifdef NO_WIN32
+#ifdef WIN32
 #include <windows.h>
 // win32 non-console applications define WinMain as the
 // entry point for the linker
