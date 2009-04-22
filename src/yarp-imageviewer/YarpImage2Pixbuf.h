47ab9d45d1ae170ecebe64aefe12bd770304a15f
Lorenzo Natale <lorenzo.natale@iit.it>
2009-04-22 09:45:45 +0000

    no message
    svn path=/trunk/yarp2/; revision=7194

--- src/yarp-imageviewer/YarpImage2Pixbuf.h
+++ src/yarp-imageviewer/YarpImage2Pixbuf.h
@@ -1,4 +1,24 @@
+/* 
+ * Copyright (C) 2009 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author Lorenzo Natale
+ * email:   lorenzo.natale@robotcub.org
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+
 #ifndef __YARPIMAGE2PIXBUF__
+#define __YARPIMAGE2PIXBUF__
 
 #include <yarp/sig/Image.h>
 #include <gtk/gtk.h>
