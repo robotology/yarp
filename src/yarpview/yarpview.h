67fd7f344a9c6cd2047f72a95ee68a39c4b04e7c
Lorenzo Natale <lorenzo.natale@iit.it>
2009-05-08 15:32:59 +0000

    no message
    svn path=/trunk/yarp2/; revision=7214

--- src/yarpview/yarpview.h
+++ src/yarpview/yarpview.h
@@ -1,5 +1,5 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
-
+      
 /*
  * Copyright (C) 2006 Matteo Brunettini
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
