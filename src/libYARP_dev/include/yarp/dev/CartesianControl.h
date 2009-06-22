dddbd23585b962cb2fa2b9766c4669bdbb2caef0
Ugo Pattacini <ugo.pattacini@iit.it>
2009-06-22 08:18:25 +0000

    update
    svn path=/trunk/yarp2/; revision=7315

--- src/libYARP_dev/include/yarp/dev/CartesianControl.h
+++ src/libYARP_dev/include/yarp/dev/CartesianControl.h
@@ -51,7 +51,7 @@ public:
     * Get the current controller mode.
     * @param f: here is returned true if controller is in tracking 
     *         mode, false otherwise.
-    * @return true/false on success/failure.
+    * @return true/false on success/failure. 
     */
     virtual bool getTrackingMode(bool *f)=0;
 
@@ -59,7 +59,7 @@ public:
     * Get the current pose.
     * @param x: a 3-d vector which is filled with the actual position x,y,z (meters)
     * @param od: a 4-d vector which is filled with the actual orientation
-    * using axis-angle representation xa, ya, za, theta (meters and degrees).
+    * using axis-angle representation xa, ya, za, theta (meters and degrees). 
     * @return true/false on success/failure.
     */
     virtual bool getPose(yarp::sig::Vector &x, yarp::sig::Vector &o)=0;
@@ -90,7 +90,7 @@ public:
     *       joints; each vector's position is filled with 1 if the
     *       associated joint is controlled (i.e. it is an actuated
     *       DOF), 0 otherwise.
-    * @return true/false on success/failure.
+    * @return true/false on success/failure. 
     */
     virtual bool getDOF(yarp::sig::Vector &curDof)=0;
 
@@ -103,23 +103,45 @@ public:
     *              request (it may differ from newDof due to the
     *              presence of some internal limb's constraints).
     * \note Eeach vector's position shall contain 1 if the 
-    *       associated joint can be actuated, 0 otherwise.
-    * \note This is a blocking call. 
-    * @return true/false on success/failure.
+    *       associated joint can be actuated, 0 otherwise. The
+    *       special value 2 indicates that the joint status won't be
+    *       modified (useful as a placeholder).
+    * @return true/false on success/failure. 
     */
     virtual bool setDOF(const yarp::sig::Vector &newDof, yarp::sig::Vector &curDof)=0;
 
     /**
+    * Get the current range for the axis.
+    * @param axis: joint index (regardless if it is actuated or 
+    *            not).
+    * @param min: where the minimum value is returned.
+    * @param max: where the maximum value is returned.
+    * @return true/false on success/failure. 
+    */
+    virtual bool getLimits(int axis, double *min, double *max)=0;
+
+    /**
+    * Set new range for the axis. Allowed range shall be a valid 
+    * subset of the real control limits. 
+    * @param axis: joint index (regardless it it is actuated or 
+    *            not).
+    * @param min: the new minimum value. 
+    * @param max: the new maximum value. 
+    * @return true/false on success/failure. 
+    */
+    virtual bool setLimits(int axis, const double min, const double max)=0;
+
+    /**
     * Get the current trajectory duration.
     * @param t: time (seconds).
-    * @return true/false on success/failure.
+    * @return true/false on success/failure. 
     */
     virtual bool getTrajTime(double *t)=0;
 
     /**
     * Set the duration of the trajectory.
     * @param t: time (seconds).
-    * @return true/false on success/failure.
+    * @return true/false on success/failure. 
     */
     virtual bool setTrajTime(const double t)=0;
 
