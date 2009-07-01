f8e30fa685e3859ab2fbe3e493ca3712599f53d0
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2009-07-01 18:45:13 +0000

    fix capitalization on copyright holder
    svn path=/trunk/yarp2/; revision=7337

--- src/libYARP_OS/src/Run.cpp
+++ src/libYARP_OS/src/Run.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2007-2009 Robotcub Consortium
+ * Copyright (C) 2007-2009 RobotCub Consortium
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  */
 
