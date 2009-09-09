1cba1e8b2f4cc34572a5e84383fa211b1ace9fa2
Lorenzo Natale <lorenzo.natale@iit.it>
2009-09-09 19:58:11 +0000

    added missing license
    svn path=/trunk/yarp2/; revision=7431

--- src/libYARP_dev/src/PolyDriverDescriptor.cpp
+++ src/libYARP_dev/src/PolyDriverDescriptor.cpp
@@ -1,4 +1,11 @@
+/*
+* Author: Lorenzo Natale.
+* Copyright (C) 2009 The Robotcub consortium
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*/
+
 #include <yarp/dev/PolyDriverDescriptor.h>
+
 
 //so far all inline
 
--- src/libYARP_dev/src/PolyDriverList.cpp
+++ src/libYARP_dev/src/PolyDriverList.cpp
@@ -1,8 +1,15 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+/*
+* Author: Lorenzo Natale.
+* Copyright (C) 2009 The Robotcub consortium
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*/
+
 #include <yarp/dev/PolyDriverList.h>
 
 #include <ace/config.h>
 #include <ace/Vector_T.h>
+
 
 #define RES(v) ((ACE_Vector<PolyDriverDescriptor> *)v)
 
