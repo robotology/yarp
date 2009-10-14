e4b0b95c23bc9b33693e8f165f0605ab79b4d0c8
Lorenzo Natale <lorenzo.natale@iit.it>
2009-10-14 18:25:50 +0000

    svn path=/trunk/yarp2/; revision=7513

--- src/libYARP_math/src/SVD.cpp
+++ src/libYARP_math/src/SVD.cpp
@@ -1,10 +1,10 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2007 Lorenzo Natale
- * CopyPolicy: Released under the terms of the GNU GPL v2.0.
- *
- */
+* Author: Lorenzo Natale.
+* Copyright (C) 2007 The Robotcub consortium
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*/
 
 /**
  * \infile routines for Singular Value Decomposition
