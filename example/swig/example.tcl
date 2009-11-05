379f94043ad2bd84b6036f48f096b14e648d09b7
Lorenzo Natale <lorenzo.natale@iit.it>
2009-11-05 09:56:47 +0000

    small fix to make tcl work again
    svn path=/trunk/yarp2/; revision=7555

--- example/swig/example.tcl
+++ example/swig/example.tcl
@@ -3,7 +3,7 @@
 # You may have to fix this up, depending on your OS
 # I don't really know anything about TCL
 # Use .so version for linux, .dll version for windows
-load ./cyarp.so cyarp
+load ./libtclyarp.so yarp
 #load [file join [pwd] cyarp.dll] cyarp
 
 Network_init
