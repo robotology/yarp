ac293645cee6d327a5ea3e38f5a4d0b091e122d6
Ugo Pattacini <ugo.pattacini@iit.it>
2009-11-06 21:22:06 +0000

    ICartesianControl: goToPoseSync() and goToPositionSync() added
    svn path=/trunk/yarp2/; revision=7556

--- src/libYARP_dev/include/yarp/dev/CartesianControl.h
+++ src/libYARP_dev/include/yarp/dev/CartesianControl.h
@@ -35,7 +35,8 @@ public:
     virtual ~ICartesianControl() {}
 
     /**
-    * Set the controller in tracking or non-tracking mode.
+    * Set the controller in tracking or non-tracking mode. [wait for
+    * reply] 
     * @param f: true for tracking mode, false otherwise. 
     * \note In tracking mode when the controller reachs the target, 
     *       it keeps on running in order to mantain the limb in the
@@ -47,7 +48,7 @@ public:
     virtual bool setTrackingMode(const bool f)=0;
 
     /**
-    * Get the current controller mode.
+    * Get the current controller mode. [wait for reply]
     * @param f: here is returned true if controller is in tracking 
     *         mode, false otherwise.
     * @return true/false on success/failure. 
@@ -55,7 +56,7 @@ public:
     virtual bool getTrackingMode(bool *f)=0;
 
     /**
-    * Get the current pose.
+    * Get the current pose. [do not wait for reply]
     * @param x: a 3-d vector which is filled with the actual position x,y,z (meters)
     * @param od: a 4-d vector which is filled with the actual orientation
     * using axis-angle representation xa, ya, za, theta (meters and degrees). 
@@ -65,7 +66,7 @@ public:
 
     /**
     * Move the end effector to a specified pose (position
-    * and orientation) in cartesian space.
+    * and orientation) in cartesian space. [do not wait for reply]
     * @param xd: a 3-d vector which contains the desired position x,y,z
     * @param od: a 4-d vector which contains the desired orientation
     * using axis-angle representation (xa, ya, za, theta). 
@@ -77,7 +78,7 @@ public:
 
     /**
     * Move the end effector to a specified position in cartesian space, 
-    * ignore the orientation.
+    * ignore the orientation. [do not wait for reply]
     * @param xd: a 3-d vector which contains the desired position x,y,z 
     * @param t: set the trajectory duration time (seconds). If t<=0 
     *         (as by default) the current execution time is kept. 
@@ -86,7 +87,30 @@ public:
     virtual bool goToPosition(const yarp::sig::Vector &xd, const double t=0.0)=0;
 
     /**
-    * Get the current DOF configuration of the limb.
+    * Move the end effector to a specified pose (position
+    * and orientation) in cartesian space. [wait for reply]
+    * @param xd: a 3-d vector which contains the desired position x,y,z
+    * @param od: a 4-d vector which contains the desired orientation
+    * using axis-angle representation (xa, ya, za, theta). 
+    * @param t: set the trajectory duration time (seconds). If t<=0 
+    *         (as by default) the current execution time is kept.
+    * @return true/false on success/failure.
+    */
+    virtual bool goToPoseSync(const yarp::sig::Vector &xd, const yarp::sig::Vector &od, const double t=0.0)=0;
+
+    /**
+    * Move the end effector to a specified position in cartesian space, 
+    * ignore the orientation. [wait for reply]
+    * @param xd: a 3-d vector which contains the desired position x,y,z 
+    * @param t: set the trajectory duration time (seconds). If t<=0 
+    *         (as by default) the current execution time is kept. 
+    * @return true/false on success/failure.
+    */
+    virtual bool goToPositionSync(const yarp::sig::Vector &xd, const double t=0.0)=0;
+
+    /**
+    * Get the current DOF configuration of the limb. [wait for
+    * reply] 
     * @param curDof: a vector which is filled with the actual DOF 
     *           configuration.
     * \note The vector lenght is equal to the number of limb's 
@@ -98,7 +122,7 @@ public:
     virtual bool getDOF(yarp::sig::Vector &curDof)=0;
 
     /**
-    * Set a new DOF configuration for the limb.
+    * Set a new DOF configuration for the limb. [wait for reply]
     * @param newDof: a vector which contains the new DOF 
     *            configuration.
     * @param curDof: a vector where the DOF configuration is 
@@ -114,7 +138,7 @@ public:
     virtual bool setDOF(const yarp::sig::Vector &newDof, yarp::sig::Vector &curDof)=0;
 
     /**
-    * Get the current range for the axis.
+    * Get the current range for the axis. [wait for reply]
     * @param axis: joint index (regardless if it is actuated or 
     *            not).
     * @param min: where the minimum value is returned [deg].
@@ -125,7 +149,7 @@ public:
 
     /**
     * Set new range for the axis. Allowed range shall be a valid 
-    * subset of the real control limits. 
+    * subset of the real control limits. [wait for reply]
     * @param axis: joint index (regardless it it is actuated or 
     *            not).
     * @param min: the new minimum value [deg]. 
@@ -135,26 +159,27 @@ public:
     virtual bool setLimits(int axis, const double min, const double max)=0;
 
     /**
-    * Get the current trajectory duration.
+    * Get the current trajectory duration. [wait for reply]
     * @param t: time (seconds).
     * @return true/false on success/failure. 
     */
     virtual bool getTrajTime(double *t)=0;
 
     /**
-    * Set the duration of the trajectory.
+    * Set the duration of the trajectory. [wait for reply]
     * @param t: time (seconds).
     * @return true/false on success/failure. 
     */
     virtual bool setTrajTime(const double t)=0;
 
-    /** Check if the current trajectory is terminated. Non blocking.
+    /** Check if the current trajectory is terminated. [wait for
+    *   reply]
     * @param f: where the result is returned.
     * @return true if the trajectory is terminated, false otherwise
     */
     virtual bool checkMotionDone(bool *f)=0;
 
-    /** Ask for an immediate stop motion.
+    /** Ask for an immediate stop motion. [wait for reply]
     * \note the control is completely release, i.e. a direct switch 
     *       to non-tracking mode is executed.     
     * @return true/false on success/failure. 
