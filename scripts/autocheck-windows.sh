a822ab84f5dac9ade9960389d7406018e445433c
YARP Developers <yarp0-devel@lists.sourceforge.net>
2009-11-25 23:43:39 +0000

    removed update web wrapper script from autocheck-windows
    svn path=/trunk/yarp2/; revision=7631

--- scripts/autocheck-windows.sh
+++ scripts/autocheck-windows.sh
@@ -88,5 +88,5 @@ if [ -e should_report.txt ]; then
 	scp report-decor.txt $WEB_USER@$WEB_SERVER:$WEB_DIR/report-yarp2-windows.txt
 fi
 
-./scripts/update-web-wrapper
+#./scripts/update-web-wrapper
 
