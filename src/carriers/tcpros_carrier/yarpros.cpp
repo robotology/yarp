80c90a8e7a8982465d52158809affa1c81c75a6a
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-06-04 18:06:21 +0000

    give yarpros companion method for hooking up a reader
    svn path=/trunk/yarp2/; revision=7969

--- src/carriers/tcpros_carrier/yarpros.cpp
+++ src/carriers/tcpros_carrier/yarpros.cpp
@@ -8,16 +8,6 @@ using namespace std;
 
 #define ROSCORE_PORT "/roscore"
 
-void usage(const char *action,
-	   const char *msg,
-	   const char *example = NULL) {
-  printf("\n  yarpros %s\n", action);
-  printf("  -- %s\n", msg);
-  if (example!=NULL) {
-    printf("  -- example: yarpros %s\n", example);
-  }
-}
-
 bool rpc(const char *target,
 	 const char *carrier,
 	 PortWriter& writer,
@@ -32,6 +22,115 @@ bool rpc(const char *target,
   p.close();
   Network::setVerbosity(0);
   return ok;
+}
+
+
+class RosLookup {
+public:
+  bool valid;
+  string hostname;
+  int portnum;
+  string protocol;
+
+  RosLookup() {
+    valid = false;
+  }
+
+  bool lookupCore(const char *name);
+
+  bool lookupTopic(const char *name);
+
+  string toString() {
+    char buf[1000];
+    sprintf(buf,"/%s:%d/", hostname.c_str(), portnum);
+    return buf;
+  }
+
+};
+
+
+bool RosLookup::lookupCore(const char *name) {
+    Bottle req, reply;
+    req.addString("lookupNode");
+    req.addString("dummy_id");
+    req.addString(name);
+    rpc(ROSCORE_PORT, "xmlrpc", req, reply);
+    if (reply.get(0).asInt()!=1) {
+      fprintf(stderr, "Failure: %s\n", reply.toString().c_str());
+      return false;
+    }
+    string url = reply.get(2).asString().c_str();
+    size_t break1 = url.find_first_of("://",0);
+    if (break1==string::npos) {
+      fprintf(stderr, "url not understood: %s\n", url.c_str());
+      return false;
+    }
+    size_t break2 = url.find_first_of(":",break1+3);
+    if (break2==string::npos) {
+      fprintf(stderr, "url not understood: %s\n", url.c_str());
+      return false;
+    }
+    size_t break3 = url.find_first_of("/",break2+1);
+    if (break3==string::npos) {
+      fprintf(stderr, "url not understood: %s\n", url.c_str());
+      return false;
+    }
+    hostname = url.substr(break1+3,break2-break1-3);
+    Value vportnum;
+    vportnum.fromString(url.substr(break2+1,break3-break2-1).c_str());
+    portnum = vportnum.asInt();
+    printf("%s\n", reply.toString().c_str());
+    valid = (portnum!=0);
+    rpc(ROSCORE_PORT, "xmlrpc", req, reply);
+    return valid;
+}
+
+
+bool RosLookup::lookupTopic(const char *name) {
+  if (!valid) {
+    fprintf(stderr, "Need a node\n");
+    return false;
+  }
+  Bottle req, reply;
+  req.addString("requestTopic");
+  req.addString("dummy_id");
+  req.addString(name);
+  Bottle& lst = req.addList();
+  Bottle& sublst = lst.addList();
+  sublst.addString("TCPROS");
+  rpc(toString().c_str(),"xmlrpc",req,reply);
+  if (reply.get(0).asInt()!=1) {
+    printf("Failure looking up topic %s: %s\n", name, reply.toString().c_str());
+    return false;
+  }
+  Bottle *pref = reply.get(2).asList();
+  if (pref==NULL) {
+    printf("Failure looking up topic %s: expected list of protocols\n", name);
+    return false;
+  }
+  if (pref->get(0).asString()!="TCPROS") {
+    printf("Failure looking up topic %s: unsupported protocol %s\n", name,
+	   pref->get(0).asString().c_str());
+    return false;
+  }
+  Value hostname2 = pref->get(1);
+  Value portnum2 = pref->get(2);
+  hostname = hostname2.asString().c_str();
+  portnum = portnum2.asInt();
+  protocol = "tcpros";
+  printf("topic %s available at %s:%d\n", name, hostname.c_str(), portnum);
+  return true;
+}
+
+
+void usage(const char *action,
+	   const char *msg,
+	   const char *example = NULL) {
+  printf("\n  yarpros %s\n", action);
+  printf("  -- %s\n", msg);
+  if (example!=NULL) {
+    printf("  -- example: yarpros %s\n", example);
+  }
 }
 
 bool register_port(const char *name,
@@ -55,6 +154,7 @@ int main(int argc, char *argv[]) {
     printf("Here are some things you can do:\n");
     usage("roscore <hostname> <port number>","tell yarp how to reach the ros master","roscore 192.168.0.1 11311");
     usage("import <name>","import a ROS name into YARP","import /talker");
+    usage("read <yarpname> <nodename> <topicname>","read to a YARP port from a ROS node's contribution to a topic","read /read /talker /chatter");
     return 0;
   }
 
@@ -85,38 +185,36 @@ int main(int argc, char *argv[]) {
       return 1;
     }
     Bottle req, reply;
-    req.addString("lookupNode");
-    req.addString("dummy_id");
-    req.add(cmd.get(1));
-    rpc(ROSCORE_PORT, "xmlrpc", req, reply);
-    if (reply.get(0).asInt()!=1) {
-      fprintf(stderr, "Failure: %s\n", reply.toString().c_str());
+    RosLookup lookup;
+    bool ok = lookup.lookupCore(cmd.get(1).asString());
+    if (ok) {
+      register_port(cmd.get(1).asString().c_str(),
+		    lookup.hostname.c_str(),
+		    lookup.portnum,
+		    reply);
+      printf("%s\n",reply.toString().c_str());
+    }
+    return ok?0:1;
+  } else if (tag=="read") {
+    if (!cmd.size()==4) {
+      fprintf(stderr,"wrong syntax, run with no arguments for help\n");
       return 1;
     }
-    string url = reply.get(2).asString().c_str();
-    size_t break1 = url.find_first_of("://",0);
-    if (break1==string::npos) {
-      fprintf(stderr, "url not understood: %s\n", url.c_str());
+    ConstString yarp_port = cmd.get(1).asString();
+    ConstString ros_port = cmd.get(2).asString();
+    ConstString topic = cmd.get(3).asString();
+    RosLookup lookup;
+    bool ok = lookup.lookupCore(ros_port.c_str());
+    if (!ok) return 1;
+    ok = lookup.lookupTopic(topic.c_str());
+    if (!ok) return 1;
+    yarp.connect(yarp_port.c_str(),
+		 lookup.toString().c_str(),
+		 (string("tcpros+topic.")+topic.c_str()).c_str());
+    return ok?0:1;
+  } else {
+      fprintf(stderr,"unknown command, run with no arguments for help\n");
       return 1;
-    }
-    size_t break2 = url.find_first_of(":",break1+3);
-    if (break2==string::npos) {
-      fprintf(stderr, "url not understood: %s\n", url.c_str());
-      return 1;
-    }
-    size_t break3 = url.find_first_of("/",break2+1);
-    if (break3==string::npos) {
-      fprintf(stderr, "url not understood: %s\n", url.c_str());
-      return 1;
-    }
-    string hostname = url.substr(break1+3,break2-break1-3);
-    Value portnum;
-    portnum.fromString(url.substr(break2+1,break3-break2-1).c_str());
-    register_port(cmd.get(1).asString().c_str(),
-		  hostname.c_str(),
-		  portnum.asInt(),
-		  reply);
-    printf("%s\n", reply.toString().c_str());
   }
   
   return 0;
