8713e3f9b72d36c11c2baa19b852f3a12f6d5a75
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-06-17 04:11:18 +0000

    alternative source of name information
    svn path=/trunk/yarp2/; revision=8016

---
+++ src/libYARP_OS/include/yarp/os/NameStore.h
@@ -0,0 +1,33 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2010 Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef _YARP2_NAMESTORE_
+#define _YARP2_NAMESTORE_
+
+#include <yarp/os/Contact.h>
+
+namespace yarp {
+    namespace os {
+        class NameStore;
+    }
+}
+
+
+/**
+ *
+ * Abstract interface for a database of port names.
+ *
+ */
+class yarp::os::NameStore {
+public:
+    virtual ~NameStore() {}
+    virtual Contact query(const char *name) = 0;
+};
+
+
+#endif
