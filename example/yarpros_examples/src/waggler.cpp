8ca60c310dc7f8fbd2acbf68db33d9526811f94b
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-07-07 21:55:23 +0000

    first basic example
    svn path=/trunk/yarp2/; revision=8086

---
+++ example/yarpros_examples/src/waggler.cpp
@@ -0,0 +1,58 @@
+
+#include <ros/ros.h>
+#include <waggler/VVLCommand.h>
+#include <sstream>
+
+#include <stdio.h>
+
+#define BOTTLE_TAG_INT 1
+#define BOTTLE_TAG_VOCAB (1+8)
+#define BOTTLE_TAG_DOUBLE (2+8)
+#define BOTTLE_TAG_STRING (4)
+#define BOTTLE_TAG_BLOB (4+8)
+#define BOTTLE_TAG_LIST 256
+#define VOCAB(a,b,c,d) ((((int)(d))<<24)+(((int)(c))<<16)+(((int)(b))<<8)+((int)(a)))
+
+int main(int argc, char** argv) {
+  printf("Will soon be modifying this to send arm commands to icub\n");
+  int joint_count = 4;
+  if (argc>1) {
+    joint_count = atoi(argv[1]);
+  }
+  printf("Working with %d joints\n", joint_count);
+
+  ros::init(argc, argv, "waggler");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<waggler::VVLCommand>("pos_cmd", 100);
+  ros::Rate loop_rate(1);
+
+  int count = 0;
+  double *joints = new double[joint_count];
+  if (joints==NULL) { printf("out of memory\n"); return 1; }
+  while (ros::ok()) {
+    for (int i=0; i<joint_count; i++) {
+      joints[i] = 0;
+    }
+    joints[0] = 5*(count%10);
+
+    waggler::VVLCommand msg;
+    msg.list_tag = BOTTLE_TAG_LIST;
+    msg.list_len = 3;
+    msg.vocab_set_tag = BOTTLE_TAG_VOCAB;
+    msg.vocab_set = VOCAB('s','e','t',0);
+    msg.vocab_poss_tag = BOTTLE_TAG_VOCAB;
+    msg.vocab_poss = VOCAB('p','o','s','s');
+    msg.setpoint_list_tag = BOTTLE_TAG_LIST+BOTTLE_TAG_DOUBLE;
+    msg.setpoint_list.resize(joint_count);
+    for (int i=0; i<joint_count; i++) {
+      msg.setpoint_list[i] = joints[i];
+    }
+    chatter_pub.publish(msg);
+    ROS_INFO("I published a command");
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+  delete[] joints;
+  return 0;
+}
