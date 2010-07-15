d92527c0c07dedb0d2edd7232f9b370f435978de
Lorenzo Natale <lorenzo.natale@iit.it>
2010-07-15 11:18:21 +0000

    fixed test url
    svn path=/trunk/yarp2/; revision=8123

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,6 +9,6 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "dashboard.icub.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=YARP")
+set(CTEST_DROP_LOCATION "/submit.php?project=YARP")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
