92ca5f4406f4b9fa5b1b7b1d3f3e304c5b7da915
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-08-21 11:19:00 +0000

    fix silly string comparison
    svn path=/trunk/yarp2/; revision=8189

--- src/libYARP_OS/src/ResourceFinder.cpp
+++ src/libYARP_OS/src/ResourceFinder.cpp
@@ -283,7 +283,7 @@ public:
             config.findGroup("default_capability").tail();
 
         // check current directory
-		if (name==""&&isDir) return ".";
+		if (ConstString(name)==""&&isDir) return ".";
         ConstString str = check("","","",name,isDir);
         if (str!="") return str;
 
