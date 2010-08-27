edc053e3da3494576c0ea04c71bb7026d2677d36
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-08-27 16:31:49 +0000

    add copyright/license for Renaud Detry's code, per his instructions
    svn path=/trunk/yarp2/; revision=8222

--- example/yayarpview/README.txt
+++ example/yayarpview/README.txt
@@ -1,4 +1,7 @@
 
+# Copyright: (C) 2010 Renaud Detry
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
 Written by: Renaud Detry
 	http://renaud.webhop.net/
 
--- example/yayarpview/stmac.h
+++ example/yayarpview/stmac.h
@@ -1,4 +1,10 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright: (C) 2010 Renaud Detry
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
 #ifndef stmac_h
 #define stmac_h
 
