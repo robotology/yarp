bf3e690d27269f2ed088d772c5cd039ec3bcd025
Ugo Pattacini <ugo.pattacini@iit.it>
2010-08-27 17:06:41 +0000

    [yarpdatadumper] Copyright inserted
    svn path=/trunk/iCub/; revision=7436

--- src/yarpdatadumper/main.cpp
+++ src/yarpdatadumper/main.cpp
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Ugo Pattacini
+ * email:  ugo.pattacini@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 /**
 @ingroup icub_tools
 
@@ -6,7 +24,7 @@
 Acquires and stores Bottles or Images and Videos from a YARP 
 port. 
 
-Copyright (C) 2008 RobotCub Consortium
+Copyright (C) 2010 RobotCub Consortium
  
 Author: Ugo Pattacini 
  
