3ec9e49b81b17dab379e319874c45e1d26472566
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-09-13 14:53:56 +0000

    copyright/license refresh
    svn path=/trunk/yarp2/; revision=8283

--- scripts/junkyard/mingw-crosscompiler-toolchain.cmake
+++ scripts/junkyard/mingw-crosscompiler-toolchain.cmake
@@ -1,3 +1,7 @@
+# Copyright: (C) 2010 RobotCub Consortium
+# Author: Paul Fitzpatrick
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
