3d454dd0007e10e63841b6cb9d215671cb4c0bca
Lorenzo Natale <lorenzo.natale@iit.it>
2010-10-22 03:00:44 +0000

    comment in script
    svn path=/trunk/yarp2/; revision=8346

--- scripts/admin/generate-cmake-options.sh
+++ scripts/admin/generate-cmake-options.sh
@@ -1,5 +1,5 @@
 ### parameters are 
-# $1: hostname
+# $1: hostname (could be also buildtype)
 # $1: os type (winxp, lenny, etch, karmic ...)
 
 CMAKE_OPTIONS="-DCREATE_GUIS:BOOL=TRUE -DCREATE_LIB_MATH:BOOL=TRUE -DENABLE_DASHBOARD_SUBMIT:BOOL=TRUE -DUSE_PORT_AUTHENTICATION:BOOL=TRUE"
