0859d301f6edb513aeecf50b31eae2132ff474b0
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-11-10 17:07:21 +0000

    check name register performance
    svn path=/trunk/yarp2/; revision=8364

---
+++ example/stressTests/stress_name_server/stress_name_server_reg.cpp
@@ -0,0 +1,28 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright: (C) 2010 RobotCub Consortium
+ * Author: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <yarp/os/all.h>
+
+using namespace yarp::os;
+
+int main(int argc, char *argv[]) {
+    Network yarp;
+
+    char buf[256];
+    int top = 500;
+    for (int i=0; i<top; i++) {
+        sprintf(buf,"/stress/%06d",i);
+        yarp.registerName(buf);
+    }
+    for (int i=0; i<top; i++) {
+        sprintf(buf,"/stress/%06d",i);
+        yarp.unregisterName(buf);
+    }
+
+    return 0;
+}
