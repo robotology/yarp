4394a5776387e7fc8504e8666f33b5deedab35ce
Lorenzo Natale <lorenzo.natale@iit.it>
2010-12-08 13:57:42 +0000

    added yarp run tests, under development. By defaultl not enabled in cmake.
    svn path=/trunk/yarp2/; revision=8404

---
+++ src/libYARP_OS/harness/testModule/module.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Module.h>
+#include <yarp/os/Network.h>
+
+using namespace std;
+using namespace yarp::os;
+
+class MyModule:public RFModule
+{
+    Port handlerPort; //a port to handle messages
+    int count;
+public:
+
+    double getPeriod()
+    {
+        return 1; //module periodicity (seconds)
+    }
+
+    /*
+    * This is our main function. Will be called periodically every getPeriod() seconds.
+    */
+    bool updateModule()
+    {
+        count++;
+        //printf("[%d] updateModule\n", count);
+        cout<<"["<<count<<"]"<< " updateModule... "<<endl;
+
+        return true;
+    }
+
+    /*
+    * Message handler. Just echo all received messages.
+    */
+    bool respond(const Bottle& command, Bottle& reply) 
+    {
+        cout<<"Got something, echo is on"<<endl;
+        if (command.get(0).asString()=="quit")
+            return false;     
+        else
+            reply=command;
+        return true;
+    }
+
+    /* 
+    * Configure function. Receive a previously initialized
+    * resource finder object. Use it to configure your module.
+    * Open port and attach it to message handler.
+    */
+    bool configure(yarp::os::ResourceFinder &rf)
+    {
+        count=0;
+      //  handlerPort.open("/regression-test-module");
+     //   attach(handlerPort);
+     //   attachTerminal();
+        return true;
+    }
+
+    /*
+    * Interrupt function.
+    */
+    bool interruptModule()
+    {
+        cout<<"Interrupting your module, for port cleanup"<<endl;
+        return true;
+    }
+
+    /*
+    * Close function, to perform cleanup.
+    */
+    bool close()
+    {
+        cout<<"Calling close function\n";
+        handlerPort.close();
+        return true;
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    Network yarp;
+    MyModule module;
+
+	yarp::os::ResourceFinder rf; //dummy resource finder, empty
+
+	//open a file
+
+    cout<<"Configure module..."<<endl;
+    module.configure(rf);
+    cout<<"Start module..."<<endl;
+    module.runModule();
+
+	//remove file 
+    cout<<"Main returning..."<<endl;
+    return 0;
+}
+
+
