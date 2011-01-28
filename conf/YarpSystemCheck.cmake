0e91b329282ae249a6e2ced882490fdec39f1514
Lorenzo Natale <lorenzo.natale@iit.it>
2011-01-28 00:05:40 +0000

    disable "this" warning C4355 in MSVC
    svn path=/trunk/yarp2/; revision=8444

--- conf/YarpSystemCheck.cmake
+++ conf/YarpSystemCheck.cmake
@@ -121,6 +121,10 @@ if (MSVC)
   add_definitions(-D_CRT_SECURE_NO_DEPRECATE)
   # this gets rid of warning about deprecated POSIX names
   add_definitions(-D_CRT_NONSTDC_NO_DEPRECATE)
+  
+  # disable: warning C4355: 'this' : used ...
+  add_definitions(/wd4355)
+	
   # Traditionally, we add "d" postfix to debug libraries
   set(CMAKE_DEBUG_POSTFIX "d")
 endif (MSVC)
