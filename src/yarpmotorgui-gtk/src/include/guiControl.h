a2cc1ee8d2dbe7665346ece42d557abc3820d606
Francesco Nori <francesco.nori@iit.it>
2011-02-15 20:06:45 +0000

    [yarpmotorgui-gtk]
    svn path=/trunk/iCub/; revision=10429

--- src/yarpmotorgui-gtk/src/include/guiControl.h
+++ src/yarpmotorgui-gtk/src/include/guiControl.h
@@ -1,8 +1,24 @@
-/*
- * Copyright (C) 2006 Francesco Nori
- * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Francesco Nori
+ * email:  francesco.nori@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
  *
- */
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+
 #ifndef GUICONTROL_H
 #define GUICONTROL_H
 
