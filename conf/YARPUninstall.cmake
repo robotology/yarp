3faaba0974833d719ffe91768bb6c9cc13ba5032
Lorenzo Natale <lorenzo.natale@iit.it>
2011-06-14 10:26:04 +0000

    added uninstall target
    svn path=/trunk/yarp2/; revision=8628

---
+++ conf/YARPUninstall.cmake
@@ -0,0 +1,12 @@
+# Copyright: (C) 2011 RobotCub Consortium
+# Authors: Lorenzo Natale
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+## add the "uninstall" target
+configure_file(
+  "${CMAKE_CURRENT_SOURCE_DIR}/conf/template/YARPConfigUninstall.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/YARPConfigUninstall.cmake"
+  IMMEDIATE @ONLY)
+  
+add_custom_target(uninstall
+  "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/YARPConfigUninstall.cmake")
