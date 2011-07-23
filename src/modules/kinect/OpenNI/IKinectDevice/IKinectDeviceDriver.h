247a189d79cefe9a794a73e6a07599504c7fa888
Duarte Aragao <daragao@gmail.com>
2011-07-23 09:32:14 +0000

    svn path=/trunk/yarp2/; revision=8722

--- src/modules/kinect/OpenNI/IKinectDevice/IKinectDeviceDriver.h
+++ src/modules/kinect/OpenNI/IKinectDevice/IKinectDeviceDriver.h
@@ -34,12 +34,46 @@ namespace yarp {
 		class IKinectDeviceDriver;
 	}
 }
-///ola
+/**
+* Interface for the KinectDeviceDriverLocal and KinectDeviceDiverServer 
+*/
 class yarp::dev::IKinectDeviceDriver {
 public:
-	virtual bool getSkeletonOrientation(yarp::sig::Matrix *matrixArray, double *confidence, int userID) = 0;//returns false if the user skeleton is not being tracked
-	virtual bool getSkeletonPosition(yarp::sig::Vector *vectorArray, double *confidence, int userID) = 0;//returns false if the user skeleton is not being tracked
+    /**
+     * Gets kinect skeleton joints orientation 3x3 matrix
+     * @param matrixArray Array of matrices that is filled with the new orientation matrices
+	 * @param matrixArray Array of doubles that is filled with the confidence level of each matrice
+	 * @param userID id of the user skeleton to be retrieved
+     * @return false if the user skeleton is not being tracked
+     */
+	virtual bool getSkeletonOrientation(yarp::sig::Matrix *matrixArray, double *confidence, int userID) = 0;
+	    /**
+     * Gets kinect skeleton joints position 3D vector (in millimeters)
+     * @param matrixArray Array of vectors that is filled with the new position vector
+	 * @param matrixArray Array of doubles that is filled with the confidence level of each vector
+	 * @param userID id of the user skeleton to be retrieved
+     * @return false if the user skeleton is not being tracked
+     */
+	virtual bool getSkeletonPosition(yarp::sig::Vector *vectorArray, double *confidence, int userID) = 0;
+	/**
+	* Gets the user sekelton state
+	* 0 - NO_USER 
+	* 1 - USER_LOST 
+	* 2 - USER_DETECTED 
+	* 3 - CALIBRATING 
+	* 4 - SKELETON_TRACKING
+	* @param userID id of the user skeleton state to be retrieved
+	* @return int value of the user skeleton state
+	*/
 	virtual int getSkeletonState(int userID) = 0;
+	/**
+	* Get RGB camera image
+	* @return camera image
+	*/
 	virtual yarp::sig::ImageOf<yarp::sig::PixelRgb> getImageMap() = 0;
+	/**
+	* Get depth camera image (in millimeters)
+	* @return camera image
+	*/
 	virtual yarp::sig::ImageOf<yarp::sig::PixelInt> getDepthMap() = 0;
 };
