3937ef1c67160df0a00199809f569ca636ebb41e
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-03-29 18:20:19 +0000

    use libdc1394 for bayer carrier
    svn path=/trunk/yarp2/; revision=9102

--- src/carriers/bayer_carrier/BayerCarrier.cpp
+++ src/carriers/bayer_carrier/BayerCarrier.cpp
@@ -1,24 +1,70 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2012 IITRBCS
+ * Authors: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
 
 #include "BayerCarrier.h"
 
 #include <yarp/sig/ImageDraw.h>
 
+#ifndef USE_LIBDC1394
+extern "C" {
+#include "conversions.h"
+}
+#else
+#include <dc1394/dc1394.h>
+#endif
+
 using namespace yarp::os;
 using namespace yarp::os::impl;
 using namespace yarp::sig;
 
+
+void setDcImage(yarp::sig::Image& yimg, dc1394video_frame_t *dc) {
+    if (!dc) return;
+    dc->image = (unsigned char *) yimg.getRawImage();
+    dc->size[0] = (uint32_t) yimg.width();
+    dc->size[1] = (uint32_t) yimg.height();
+    dc->position[0] = 0;
+    dc->position[1] = 0;
+    dc->color_coding = (yimg.getPixelCode()==VOCAB_PIXEL_MONO)?DC1394_COLOR_CODING_RAW8:DC1394_COLOR_CODING_RGB8;
+    dc->color_filter = DC1394_COLOR_FILTER_GRBG; // to be set
+    dc->yuv_byte_order = 0;
+    dc->data_depth = 8;
+    dc->stride = (uint32_t) yimg.getRowSize();
+    dc->video_mode = DC1394_VIDEO_MODE_640x480_RGB8; // we are bluffing
+    dc->image_bytes = (uint32_t)yimg.getRawImageSize();
+    dc->total_bytes = dc->image_bytes;
+    dc->padding_bytes = 0;
+    dc->timestamp = 0;
+    dc->frames_behind = 0;
+    dc->camera = NULL;
+    dc->id = 0;
+    dc->allocated_image_bytes = dc->image_bytes;
+#ifdef YARP_LITTLE_ENDIAN
+    dc->little_endian = DC1394_TRUE;
+#else
+    dc->little_endian = DC1394_FALSE;
+#endif
+    dc->data_in_padding = DC1394_FALSE;
+}
+
 yarp::os::ConnectionReader& BayerCarrier::modifyIncomingData(yarp::os::ConnectionReader& reader) {
 
-  /*
+    /*
     // minimal test of image modification
     in.read(reader);
     out.copy(in);
     out.pixel(0,0).r = 42;
     out.write(con.getWriter());
     return con.getReader();
-  */
+    */
 
-  /*
+    /*
     // minimal test of bottle modification
     con.setTextMode(reader.isTextMode());
     Bottle b;
@@ -27,228 +73,247 @@ yarp::os::ConnectionReader& BayerCarrier::modifyIncomingData(yarp::os::Connectio
     b.addString("(p.s. bork bork bork)");
     b.write(con.getWriter());
     return con.getReader();
-  */
+    */
 
-  bool ok = in.read(reader);
-  if (!ok) {
-    happy = false;
-    return local;
-  }
-  ImageNetworkHeader header_in_cmp;
-  header_in_cmp.setFromImage(in);
-  if (!need_reset) {
-    need_reset = (0!=memcmp(&header_in_cmp,&header_in,sizeof(header_in)));
-  }
-  have_result = false;
-  if (need_reset) {
-    Searchable& config = reader.getConnectionModifiers();
-    half = false;
-    if (config.check("size")) {
-      if (config.find("size").asString() == "half") {
-	half = true;
-      }
+    bool ok = in.read(reader);
+    if (!ok) {
+        happy = false;
+        return local;
     }
-    setFormat(config.check("order",Value("grbg")).asString().c_str());
-    header_in.setFromImage(in);
-    //printf("Need reset.\n");
-    processBuffered();
-    need_reset = false;
-  }
-  Route r;
-  local.reset(*this, NULL, r, sizeof(header)+image_data_len, false);
-  consumed = 0;
+    ImageNetworkHeader header_in_cmp;
+    header_in_cmp.setFromImage(in);
+    if (!need_reset) {
+        need_reset = (0!=memcmp(&header_in_cmp,&header_in,sizeof(header_in)));
+    }
+    have_result = false;
+    if (need_reset) {
+        Searchable& config = reader.getConnectionModifiers();
+        half = false;
+        if (config.check("size")) {
+            if (config.find("size").asString() == "half") {
+                half = true;
+            }
+        }
+        setFormat(config.check("order",Value("grbg")).asString().c_str());
+        header_in.setFromImage(in);
+        //printf("Need reset.\n");
+        processBuffered();
+        need_reset = false;
+    }
+    Route r;
+    local.reset(*this, NULL, r, sizeof(header)+image_data_len, false);
+    consumed = 0;
 
-  return local;
+    return local;
 }
 
 
 bool BayerCarrier::debayerHalf(yarp::sig::ImageOf<PixelMono>& src,
-			       yarp::sig::ImageOf<PixelRgb>& dest) {
-  int w = src.width();
-  int h = src.height();
-  int wo = dest.width();
-  int ho = dest.height();
-  int goff1 = 1-goff;
-  int roffx = roff?goff:goff1;
-  int boff = 1-roff;
-  int boffx = boff?goff:goff1;
-  for (int yo=0; yo<ho; yo++) {
-    for (int xo=0; xo<wo; xo++) {
-      PixelRgb& po = dest.pixel(xo,yo);
-      int x = xo*2;
-      int y = yo*2;
-      if (x+1>=w-1 || y+1>=h-1) {
-	po = PixelRgb(0,0,0);
-	continue;
-      }
-      po.r = src.pixel(x+roffx,y+roff);
-      po.b = src.pixel(x+boffx,y+boff);
-      po.g = (PixelMono)(0.5*(src.pixel(x+goff,y)+src.pixel(x+goff1,y+1)));
+                               yarp::sig::ImageOf<PixelRgb>& dest) {
+    dc1394video_frame_t dc_src;
+    dc1394video_frame_t dc_dest;
+    setDcImage(src,&dc_src);
+    setDcImage(dest,&dc_dest);
+    dc1394_debayer_frames(&dc_src,&dc_dest,DC1394_BAYER_METHOD_DOWNSAMPLE);
+    return true;
+
+    /*
+      // a test implementation that doesn't use dc1394
+    int w = src.width();
+    int h = src.height();
+    int wo = dest.width();
+    int ho = dest.height();
+    int goff1 = 1-goff;
+    int roffx = roff?goff:goff1;
+    int boff = 1-roff;
+    int boffx = boff?goff:goff1;
+    for (int yo=0; yo<ho; yo++) {
+        for (int xo=0; xo<wo; xo++) {
+            PixelRgb& po = dest.pixel(xo,yo);
+            int x = xo*2;
+            int y = yo*2;
+            if (x+1>=w-1 || y+1>=h-1) {
+                po = PixelRgb(0,0,0);
+                continue;
+            }
+            po.r = src.pixel(x+roffx,y+roff);
+            po.b = src.pixel(x+boffx,y+boff);
+            po.g = (PixelMono)(0.5*(src.pixel(x+goff,y)+src.pixel(x+goff1,y+1)));
+        }
     }
-  }
-  return true;
+    */
+    return true;
 }
 
-// A stub for bayer conversion
 bool BayerCarrier::debayerFull(yarp::sig::ImageOf<PixelMono>& src,
-			       yarp::sig::ImageOf<PixelRgb>& dest) {
-  int w = dest.width();
-  int h = dest.height();
-  int goff1 = 1-goff;
-  int roffx = roff?goff:goff1;
-  int boff = 1-roff;
-  int boffx = boff?goff:goff1;
-  for (int y=0; y<h; y++) {
-    for (int x=0; x<w; x++) {
-      PixelRgb& po = dest.pixel(x,y);
+                               yarp::sig::ImageOf<PixelRgb>& dest) {
+    dc1394video_frame_t dc_src;
+    dc1394video_frame_t dc_dest;
+    setDcImage(src,&dc_src);
+    setDcImage(dest,&dc_dest);
+    dc1394_debayer_frames(&dc_src,&dc_dest,DC1394_BAYER_METHOD_BILINEAR);
+    return true;
 
-      // G
-      if ((x+y)%2==goff) {
-	po.g = src.pixel(x,y);
-      } else {
-	float g = 0;
-	int ct = 0;
-	if (x>0) { g += src.pixel(x-1,y); ct++; }
-	if (x<w-1) { g += src.pixel(x+1,y); ct++; }
-	if (y>0) { g += src.pixel(x,y-1); ct++; }
-	if (y<h-1) { g += src.pixel(x,y+1); ct++; }
-	if (ct>0) g /= ct;
-	po.g = (int)g;
-      }
+    /*
+      // a test implementation that doesn't use dc1394
+    int w = dest.width();
+    int h = dest.height();
+    int goff1 = 1-goff;
+    int roffx = roff?goff:goff1;
+    int boff = 1-roff;
+    int boffx = boff?goff:goff1;
+    for (int y=0; y<h; y++) {
+        for (int x=0; x<w; x++) {
+            PixelRgb& po = dest.pixel(x,y);
 
-      // B
-      if (y%2==boff && x%2==boffx) {
-	po.b = src.pixel(x,y);
-      } else if (y%2==boff) {
-	float b = 0;
-	int ct = 0;
-	if (x>0) { b += src.pixel(x-1,y); ct++; }
-	if (x<w-1) { b += src.pixel(x+1,y); ct++; }
-	if (ct>0) b /= ct;
-	po.b = (int)b;
-      } else if (x%2==boffx) {
-	float b = 0;
-	int ct = 0;
-	if (y>0) { b += src.pixel(x,y-1); ct++; }
-	if (y<h-1) { b += src.pixel(x,y+1); ct++; }
-	if (ct>0) b /= ct;
-	po.b = (int)b;
-      } else {
-	float b = 0;
-	int ct = 0;
-	if (x>0&&y>0) { b += src.pixel(x-1,y-1); ct++; }
-	if (x>0&&y<h-1) { b += src.pixel(x-1,y+1); ct++; }
-	if (x<w-1&&y>0) { b += src.pixel(x+1,y-1); ct++; }
-	if (x<w-1&&y<h-1) { b += src.pixel(x+1,y+1); ct++; }
-	if (ct>0) b /= ct;
-	po.b = (int)b;
-      }
+            // G
+            if ((x+y)%2==goff) {
+                po.g = src.pixel(x,y);
+            } else {
+                float g = 0;
+                int ct = 0;
+                if (x>0) { g += src.pixel(x-1,y); ct++; }
+                if (x<w-1) { g += src.pixel(x+1,y); ct++; }
+                if (y>0) { g += src.pixel(x,y-1); ct++; }
+                if (y<h-1) { g += src.pixel(x,y+1); ct++; }
+                if (ct>0) g /= ct;
+                po.g = (int)g;
+            }
 
-      // R
-      if (y%2==roff && x%2==roffx) {
-	po.r = src.pixel(x,y);
-      } else if (y%2==roff) {
-	float r = 0;
-	int ct = 0;
-	if (x>0) { r += src.pixel(x-1,y); ct++; }
-	if (x<w-1) { r += src.pixel(x+1,y); ct++; }
-	if (ct>0) r /= ct;
-	po.r = (int)r;
-      } else if (x%2==roffx) {
-	float r = 0;
-	int ct = 0;
-	if (y>0) { r += src.pixel(x,y-1); ct++; }
-	if (y<h-1) { r += src.pixel(x,y+1); ct++; }
-	if (ct>0) r /= ct;
-	po.r = (int)r;
-      } else {
-	float r = 0;
-	int ct = 0;
-	if (x>0&&y>0) { r += src.pixel(x-1,y-1); ct++; }
-	if (x>0&&y<h-1) { r += src.pixel(x-1,y+1); ct++; }
-	if (x<w-1&&y>0) { r += src.pixel(x+1,y-1); ct++; }
-	if (x<w-1&&y<h-1) { r += src.pixel(x+1,y+1); ct++; }
-	if (ct>0) r /= ct;
-	po.r = (int)r;
-      }
+            // B
+            if (y%2==boff && x%2==boffx) {
+                po.b = src.pixel(x,y);
+            } else if (y%2==boff) {
+                float b = 0;
+                int ct = 0;
+                if (x>0) { b += src.pixel(x-1,y); ct++; }
+                if (x<w-1) { b += src.pixel(x+1,y); ct++; }
+                if (ct>0) b /= ct;
+                po.b = (int)b;
+            } else if (x%2==boffx) {
+                float b = 0;
+                int ct = 0;
+                if (y>0) { b += src.pixel(x,y-1); ct++; }
+                if (y<h-1) { b += src.pixel(x,y+1); ct++; }
+                if (ct>0) b /= ct;
+                po.b = (int)b;
+            } else {
+                float b = 0;
+                int ct = 0;
+                if (x>0&&y>0) { b += src.pixel(x-1,y-1); ct++; }
+                if (x>0&&y<h-1) { b += src.pixel(x-1,y+1); ct++; }
+                if (x<w-1&&y>0) { b += src.pixel(x+1,y-1); ct++; }
+                if (x<w-1&&y<h-1) { b += src.pixel(x+1,y+1); ct++; }
+                if (ct>0) b /= ct;
+                po.b = (int)b;
+            }
+
+            // R
+            if (y%2==roff && x%2==roffx) {
+                po.r = src.pixel(x,y);
+            } else if (y%2==roff) {
+                float r = 0;
+                int ct = 0;
+                if (x>0) { r += src.pixel(x-1,y); ct++; }
+                if (x<w-1) { r += src.pixel(x+1,y); ct++; }
+                if (ct>0) r /= ct;
+                po.r = (int)r;
+            } else if (x%2==roffx) {
+                float r = 0;
+                int ct = 0;
+                if (y>0) { r += src.pixel(x,y-1); ct++; }
+                if (y<h-1) { r += src.pixel(x,y+1); ct++; }
+                if (ct>0) r /= ct;
+                po.r = (int)r;
+            } else {
+                float r = 0;
+                int ct = 0;
+                if (x>0&&y>0) { r += src.pixel(x-1,y-1); ct++; }
+                if (x>0&&y<h-1) { r += src.pixel(x-1,y+1); ct++; }
+                if (x<w-1&&y>0) { r += src.pixel(x+1,y-1); ct++; }
+                if (x<w-1&&y<h-1) { r += src.pixel(x+1,y+1); ct++; }
+                if (ct>0) r /= ct;
+                po.r = (int)r;
+            }
+        }
     }
-  }
-  return true;
+    return true;
+    */
 }
 
 bool BayerCarrier::processBuffered() {
-  if (!have_result) {
-    //printf("Copy-based conversion.\n");
-    if (half) {
-      out.resize(in.width()/2,in.height()/2);
-      debayerHalf(in,out);
-    } else {
-      out.resize(in);
-      debayerFull(in,out);
+    if (!have_result) {
+        //printf("Copy-based conversion.\n");
+        if (half) {
+            out.resize(in.width()/2,in.height()/2);
+            debayerHalf(in,out);
+        } else {
+            out.resize(in);
+            debayerFull(in,out);
+        }
+        header.setFromImage(out);
+        image_data_len = (size_t)out.getRawImageSize();
     }
-    header.setFromImage(out);
-    image_data_len = (size_t)out.getRawImageSize();
-  }
-  have_result = true;
-  return true;
+    have_result = true;
+    return true;
 }
 
 bool BayerCarrier::processDirect(const yarp::os::Bytes& bytes) {
-  if (have_result) {
-    ACE_OS::memcpy(bytes.get(),out.getRawImage(),bytes.length());
+    if (have_result) {
+        ACE_OS::memcpy(bytes.get(),out.getRawImage(),bytes.length());
+        return true;
+    }
+    //printf("Copyless conversion\n");
+    ImageOf<PixelRgb> wrap;
+    wrap.setQuantum(out.getQuantum());
+    wrap.setExternal(bytes.get(),out.width(),out.height());
+    if (half) {
+        debayerHalf(in,wrap);
+    } else {
+        debayerFull(in,wrap);
+    }
     return true;
-  }
-  //printf("Copyless conversion\n");
-  ImageOf<PixelRgb> wrap;
-  wrap.setQuantum(out.getQuantum());
-  wrap.setExternal(bytes.get(),out.width(),out.height());
-  if (half) {
-      debayerHalf(in,wrap);
-  } else {
-      debayerFull(in,wrap);
-  }
-  return true;
 }
 
 
 ssize_t BayerCarrier::read(const yarp::os::Bytes& b) {
-  // copy across small stuff - the image header
-  if (consumed<sizeof(header)) {
-    size_t len = b.length();
-    if (len>sizeof(header)-consumed) {
-      len = sizeof(header)-consumed;
+    // copy across small stuff - the image header
+    if (consumed<sizeof(header)) {
+        size_t len = b.length();
+        if (len>sizeof(header)-consumed) {
+            len = sizeof(header)-consumed;
+        }
+        ACE_OS::memcpy(b.get(),((char*)(&header))+consumed,len);
+        consumed += len;
+        return (ssize_t) len;
     }
-    ACE_OS::memcpy(b.get(),((char*)(&header))+consumed,len);
-    consumed += len;
-    return (ssize_t) len;
-  }
-  // sane client will want to read image into correct-sized block
-  if (b.length()==image_data_len) {
-    // life is good!
-    processDirect(b);
-    consumed += image_data_len;
-    return image_data_len;
-  }
-  // funky client, fall back on image copy
-  processBuffered();
-  if (consumed<sizeof(header)+out.getRawImageSize()) {
-    size_t len = b.length();
-    if (len>sizeof(header)+out.getRawImageSize()-consumed) {
-      len = sizeof(header)+out.getRawImageSize()-consumed;
+    // sane client will want to read image into correct-sized block
+    if (b.length()==image_data_len) {
+        // life is good!
+        processDirect(b);
+        consumed += image_data_len;
+        return image_data_len;
     }
-    ACE_OS::memcpy(b.get(),out.getRawImage()+consumed-sizeof(header),len);
-    consumed += len;
-    return (ssize_t) len;
-  }
-  return -1;
+    // funky client, fall back on image copy
+    processBuffered();
+    if (consumed<sizeof(header)+out.getRawImageSize()) {
+        size_t len = b.length();
+        if (len>sizeof(header)+out.getRawImageSize()-consumed) {
+            len = sizeof(header)+out.getRawImageSize()-consumed;
+        }
+        ACE_OS::memcpy(b.get(),out.getRawImage()+consumed-sizeof(header),len);
+        consumed += len;
+        return (ssize_t) len;
+    }
+    return -1;
 }
 
 
 bool BayerCarrier::setFormat(const char *fmt) {
-  ConstString f(fmt);
-  if (f.length()<2) return false;
-  goff = (f[0]=='g'||f[0]=='G')?0:1;
-  roff = (f[0]=='r'||f[0]=='R'||f[1]=='r'||f[1]=='R')?0:1;
-  return true;
+    ConstString f(fmt);
+    if (f.length()<2) return false;
+    goff = (f[0]=='g'||f[0]=='G')?0:1;
+    roff = (f[0]=='r'||f[0]=='R'||f[1]=='r'||f[1]=='R')?0:1;
+    return true;
 }
 
---
+++ src/carriers/bayer_carrier/conversions.h
@@ -0,0 +1,141 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2012 IITRBCS
+ * Authors: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef YARP2_ADAPT_LIBDC1294
+#define YARP2_ADAPT_LIBDC1294
+
+/*
+  libdc1294_bayer.c is taken verbatim from:
+  http://libdc1394.svn.sourceforge.net/viewvc/libdc1394/trunk/libdc1394/dc1394/bayer.c?revision=535
+  
+  This header file adapts it for use with YARP.
+  We use some tricks for Windows compatibility
+
+*/
+
+#include <yarp/conf/system.h>
+#include <assert.h>
+
+#define restrict
+#define uint8_t unsigned char
+#define uint16_t unsigned YARP_INT16
+#define uint32_t unsigned YARP_INT32
+#define uint64_t unsigned YARP_INT64
+#define dc1394error_t int
+#define dc1394bool_t int
+#define dc1394bayer_method_t int
+#define dc1394color_filter_t int
+#define dc1394color_coding_t int
+#define dc1394video_mode_t int
+#define dc1394camera_t int
+#define DC1394_SUCCESS 0
+#define DC1394_TRUE 1
+#define DC1394_FALSE 0
+
+enum {
+  DC1394_COLOR_CODING_MONO16 = 1,
+  DC1394_COLOR_CODING_MONO8,
+  DC1394_COLOR_CODING_RAW16,
+  DC1394_COLOR_CODING_RAW8,
+  DC1394_COLOR_CODING_RGB16,
+  DC1394_COLOR_CODING_RGB8,
+
+  DC1394_BAYER_METHOD_AHD,
+  DC1394_BAYER_METHOD_BILINEAR,
+  DC1394_BAYER_METHOD_DOWNSAMPLE,
+  DC1394_BAYER_METHOD_EDGESENSE,
+  DC1394_BAYER_METHOD_HQLINEAR,
+  DC1394_BAYER_METHOD_NEAREST,
+  DC1394_BAYER_METHOD_SIMPLE,
+  DC1394_BAYER_METHOD_VNG,
+
+  DC1394_COLOR_FILTER_BGGR,
+  DC1394_COLOR_FILTER_GBRG,
+  DC1394_COLOR_FILTER_GRBG,
+  DC1394_COLOR_FILTER_RGGB,
+
+  DC1394_VIDEO_MODE_640x480_RGB8
+};
+
+#define DC1394_COLOR_FILTER_MIN (DC1394_COLOR_FILTER_BGGR)
+#define DC1394_COLOR_FILTER_MAX (DC1394_COLOR_FILTER_RGGB)
+
+#define DC1394_BAYER_METHOD_MIN (DC1394_BAYER_METHOD_AHD)
+#define DC1394_BAYER_METHOD_MAX (DC1394_BAYER_METHOD_VNG)
+
+#define DC1394_FUNCTION_NOT_SUPPORTED -1
+#define DC1394_INVALID_BAYER_METHOD -2
+#define DC1394_INVALID_COLOR_FILTER -3
+#define DC1394_MEMORY_ALLOCATION_FAILURE -4
+
+/* YARP will in fact use only one color coding type, so we take a shortcut */
+#define dc1394_get_color_coding_bit_size(x,y) (*y) = (3*8)
+
+/* Make sure memory reallocation is not happening without us knowing */
+#define free assert(1==0); free
+
+/*
+ * Structure definition from libdc1394 video.h 
+ *
+ * 1394-Based Digital Camera Control Library
+ *
+ * Video format headers
+ *
+ * Written by Damien Douxchamps <ddouxchamps@users.sf.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+typedef struct __dc1394_video_frame
+{
+  unsigned char          * image;                 /* the image. May contain padding data too (vendor specific). Read/write allowed. Free NOT allowed if
+                                           returned by dc1394_capture_dequeue() */
+  uint32_t                 size[2];               /* the image size [width, height] */
+  uint32_t                 position[2];           /* the WOI/ROI position [horizontal, vertical] == [0,0] for full frame */
+  dc1394color_coding_t     color_coding;          /* the color coding used. This field is valid for all video modes. */
+  dc1394color_filter_t     color_filter;          /* the color filter used. This field is valid only for RAW modes and IIDC 1.31 */
+  uint32_t                 yuv_byte_order;        /* the order of the fields for 422 formats: YUYV or UYVY */
+  uint32_t                 data_depth;            /* the number of bits per pixel. The number of grayscale levels is 2^(this_number).
+						     This is independent from the colour coding */
+  uint32_t                 stride;                /* the number of bytes per image line */
+  dc1394video_mode_t       video_mode;            /* the video mode used for capturing this frame */
+  uint64_t                 total_bytes;           /* the total size of the frame buffer in bytes. May include packet-
+						     multiple padding and intentional padding (vendor specific) */
+  uint32_t                 image_bytes;           /* the number of bytes used for the image (image data only, no padding) */
+  uint32_t                 padding_bytes;         /* the number of extra bytes, i.e. total_bytes-image_bytes.  */
+  uint32_t                 packet_size;           /* the size of a packet in bytes. (IIDC data) */
+  uint32_t                 packets_per_frame;     /* the number of packets per frame. (IIDC data) */
+  uint64_t                 timestamp;             /* the unix time [microseconds] at which the frame was captured in
+						     the video1394 ringbuffer */
+  uint32_t                 frames_behind;         /* the number of frames in the ring buffer that are yet to be accessed by the user */
+  dc1394camera_t           *camera;               /* the parent camera of this frame */
+  uint32_t                 id;                    /* the frame position in the ring buffer */
+  uint64_t                 allocated_image_bytes; /* amount of memory allocated in for the *image field. */
+  dc1394bool_t             little_endian;         /* DC1394_TRUE if little endian (16bpp modes only),
+						     DC1394_FALSE otherwise */
+  dc1394bool_t             data_in_padding;       /* DC1394_TRUE if data is present in the padding bytes in IIDC 1.32 format,
+						     DC1394_FALSE otherwise */
+} dc1394video_frame_t;
+
+
+dc1394error_t dc1394_debayer_frames(dc1394video_frame_t *in, dc1394video_frame_t *out, dc1394bayer_method_t method);
+
+#endif
+
