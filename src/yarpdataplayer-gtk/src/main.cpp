ce7add194e9c50cfce0a71e99012c464c541215f
Ugo Pattacini <ugo.pattacini@iit.it>
2012-06-12 11:34:42 +0000

    [yarpdataplayer-gtk] changed doc group
    svn path=/trunk/iCub/; revision=18503

--- src/yarpdataplayer-gtk/src/main.cpp
+++ src/yarpdataplayer-gtk/src/main.cpp
@@ -18,7 +18,7 @@
 /**
 \defgroup dataSetPlayer dataSetPlayer
 
-@ingroup icub_contrib_modules
+@ingroup icub_tools
 
 A module that reproduces in a synchronized way, previously acquired data (using dataDumper) from the iCub or other source of input.
 
