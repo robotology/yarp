edd663117c78d549e481509e2e9cd081f4eb033e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-06-27 08:18:06 +0000

    [portScope] Add GtkDataboxMM
    https://sourceforge.net/projects/gtkdataboxmm
    
    7b5c773db1e6d3a835a9c3da50117f4e4f29c181 Mon Jun 25 09:34:44 2012 +0200
    
    svn path=/trunk/yarp2/; revision=9226

---
+++ src/yarpscope/src/gtkdatabox/gtkdataboxmmconfig.h
@@ -0,0 +1,11 @@
+/* gtkdatabox/gtkdataboxmmconfig.h.  Generated from gtkdataboxmmconfig.h.in by configure.  */
+/* This file is part of gtkdataboxmm. */
+
+/* Major version number of gtkdataboxmm. */
+#define GTKDATABOXMM_MAJOR_VERSION 0
+
+/* Micro version number of gtkdataboxmm. */
+#define GTKDATABOXMM_MICRO_VERSION 0
+
+/* Minor version number of gtkdataboxmm. */
+#define GTKDATABOXMM_MINOR_VERSION 9
