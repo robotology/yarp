70953a0d929622011a4c961c2cb48f54c9209387
Lorenzo Natale <lorenzo.natale@iit.it>
2012-07-11 20:24:58 +0000

    svn path=/trunk/yarp2/; revision=9276

--- scripts/admin/generate-cmake-options.sh
+++ scripts/admin/generate-cmake-options.sh
@@ -1,6 +1,7 @@
 ### parameters are 
 # $1: hostname (could be also buildtype)
 # $2: os type (macos, winxp, lenny, etch, karmic ...)
+# $3: test type: nightly continuous experimental
 
 case $2 in
 # no special condition for macos any more
@@ -10,4 +11,9 @@ case $2 in
 	*)
 	CMAKE_OPTIONS="-DCREATE_IDLS:BOOL=TRUE -DCREATE_GUIS:BOOL=TRUE -DCREATE_LIB_MATH:BOOL=TRUE -DENABLE_DASHBOARD_SUBMIT:BOOL=TRUE -DUSE_PORT_AUTHENTICATION:BOOL=TRUE -DCREATE_OPTIONAL_CARRIERS=TRUE -DENABLE_yarpcar_tcpros_carrier=TRUE -DENABLE_yarpcar_xmlrpc_carrier=TRUE -DENABLE_yarpcar_bayer_carrier=TRUE -DUSE_LIBDC1394=FALSE -DENABLE_yarpcar_priority_carrier=TRUE -DCREATE_IDLS=TRUE -DENABLE_yarpidl_thrift=TRUE"
 esac
+
+case $3 in
+experimental)
+     CMAKE_OPTIONS="$CMAKE_OPTIONS -DCREATE_YARPSCOPE=TRUE"
+esac	 
 	
