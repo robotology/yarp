593c0a2cdf5c02763101ed60b23322b12985669b
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-07-11 09:06:38 +0000

    Use CopyPolicy tag in headers
    svn path=/trunk/yarp2/; revision=9274

--- src/yarpscope/src/XmlLoader.h
+++ src/yarpscope/src/XmlLoader.h
@@ -2,17 +2,7 @@
  * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
  * Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
  *
- * Permission is granted to copy, distribute, and/or modify this program
- * under the terms of the GNU General Public License, version 2 or any
- * later version published by the Free Software Foundation.
- *
- * A copy of the license can be found at
- * http://www.robotcub.org/icub/license/gpl.txt
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
  */
 
 
