28577605357a1e8202b5fe5e8436afe4e851e500
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-07-11 08:04:19 +0000

    Update Copyright according to policies
    svn path=/trunk/yarp2/; revision=9272

--- src/yarpscope/src/XmlLoader.cpp
+++ src/yarpscope/src/XmlLoader.cpp
@@ -1,21 +1,20 @@
 /*
- *  This file is part of Yarp Port Scope
+ * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
+ * Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
  *
- *  Copyright (C) 2012 Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 2 of the License, or
- *  (at your option) any later version.
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
  */
+
 
 #include "XmlLoader.h"
 #include "PortReader.h"
