71dbfc1a4cd77fc983a3e8b859a80caa6df06d8d
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-07-23 14:09:43 +0000

    Fix typos
    svn path=/trunk/yarp2/; revision=9322

--- src/yarpscope/src/MainWindow.cpp
+++ src/yarpscope/src/MainWindow.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
- * Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+ * Authors: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
  *
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
  */
--- src/yarpscope/src/main.cpp
+++ src/yarpscope/src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2006  Robotcub Consortium 
  * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
- * Author: Francesco Nori, Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+ * Authors: Francesco Nori, Daniele E. Domenichelli <daniele.domenichelli@iit.it>
  *
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
  */
