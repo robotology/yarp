02f001a4283766fe1c89b931e6b0c6f861f52062
Francesco Nori <francesco.nori@iit.it>
2012-07-23 16:09:30 +0000

    Fixed the MAC-OS GTK+ configuration by extending the pkg-config serach tree
    svn path=/trunk/yarp2/; revision=9344

--- conf/FindGtkPlus.cmake
+++ conf/FindGtkPlus.cmake
@@ -35,7 +35,7 @@ if (WIN32)
     endif (GTK_FOUND)
 endif()
 
-IF(UNIX )
+IF(UNIX)
   INCLUDE(FindPkgConfig)
   if (PKG_CONFIG_FOUND)
     pkg_check_modules(GTKPLUS "gtk+-2.0")
@@ -51,14 +51,24 @@ IF(UNIX )
    # Make the following hardcoded links conditional, since e.g. a "brew"
    # installed version of gtk+ will already have been found.
    IF (NOT GTKPLUS_LIBRARY_DIR)
-     ### THERE IS AN OFFICIAL GTK PACKAGE on www.gtk-osx.org
-     ### The following setup is relative to the official GTK package for mac os-x
-     SET( GTKPLUS_INCLUDE_DIR "/Library/Frameworks/Gtk.framework/Headers;/Library/Frameworks/GLib.framework/Headers;/Library/Frameworks/Cairo.framework/Headers;" 
-)
-     SET( GTKPLUS_LIBRARY_DIR " " )
-     SET( GTKPLUS_LINK_FLAGS "-framework Gtk -framework GLib" )
-     SET( GTKPLUS_C_FLAGS " " )
-   ENDIF ()
+      IF (PKG_CONFIG_FOUND)
+      	 SET(ENV{PKG_CONFIG_PATH} $ENV{PKG_CONFIG_PATH}:/usr/X11/lib/pkgconfig)
+	 pkg_check_modules(GTKPLUS "gtk+-2.0")
+           IF (GTKPLUS_FOUND)
+             set(GTKPLUS_INCLUDE_DIR ${GTKPLUS_INCLUDE_DIRS})
+             set(GTKPLUS_LIBRARY_DIR ${GTKPLUS_LIBRARY_DIRS})
+             set(GTKPLUS_LINK_FLAGS ${GTKPLUS_LDFLAGS})
+             set(GTKPLUS_C_FLAGS ${GTKPLUS_CFLAGS})
+           ENDIF (GTKPLUS_FOUND)
+      ELSE (PKG_CONFIG_FOUND)
+         ### THERE IS AN OFFICIAL GTK PACKAGE on www.gtk-osx.org
+         ### The following setup is relative to the official GTK package for mac os-x
+         SET( GTKPLUS_INCLUDE_DIR "/Library/Frameworks/Gtk.framework/Headers;/Library/Frameworks/GLib.framework/Headers;/Library/Frameworks/Cairo.framework/Headers;")
+         SET( GTKPLUS_LIBRARY_DIR " " )
+         SET( GTKPLUS_LINK_FLAGS "-framework Gtk -framework GLib" )
+         SET( GTKPLUS_C_FLAGS " " )
+      ENDIF (PKG_CONFIG_FOUND)
+   ENDIF (NOT GTKPLUS_LIBRARY_DIR)
  ENDIF (APPLE)
 
  SET(GTKPLUS_LINK_FLAGS "${GTKPLUS_LINK_FLAGS}" CACHE INTERNAL "gtk+ link flags")
