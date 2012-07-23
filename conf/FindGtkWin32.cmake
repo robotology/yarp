54a807fbb18fcd0d5b331e1f2c4d6f4e13428e38
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-07-23 14:15:00 +0000

    Fix FindGtkWin32.cmake
    svn path=/trunk/yarp2/; revision=9340

--- conf/FindGtkWin32.cmake
+++ conf/FindGtkWin32.cmake
@@ -39,9 +39,9 @@ FIND_PATH(GTK_cairo_INCLUDE_PATH cairo.h ${GTK_BASEPATH}/include/cairo/)
 FIND_PATH(GTK_glib_INCLUDE_PATH glib.h ${GTK_BASEPATH}/include/glib-2.0/)
 FIND_PATH(GTK_pango_INCLUDE_PATH pango/pango.h ${GTK_BASEPATH}/include/pango-1.0/)
 FIND_PATH(GTK_atk_INCLUDE_PATH atk/atk.h ${GTK_BASEPATH}/include/atk-1.0/)
-FIND_PATH(GTK_glib-config_INCLUDE_PATH glibconfig.h ${GTK_BASEPATH}/lib/glib-2.0/include)
-FIND_PATH(GTK_gdk-config_INCLUDE_PATH gdkconfig.h ${GTK_BASEPATH}/lib/gtk-2.0/include)
-FIND_PATH(GTK_gdk-pixbuf_INCLUDE_PATH gdk-pixbuf/gdk-pixbuf.h ${GTK_BASEPATH}/include/gdk-pixbuf-2.0)
+FIND_PATH(GTK_glib_config_INCLUDE_PATH glibconfig.h ${GTK_BASEPATH}/lib/glib-2.0/include)
+FIND_PATH(GTK_gdk_config_INCLUDE_PATH gdkconfig.h ${GTK_BASEPATH}/lib/gtk-2.0/include)
+FIND_PATH(GTK_gdk_pixbuf_INCLUDE_PATH gdk-pixbuf/gdk-pixbuf.h ${GTK_BASEPATH}/include/gdk-pixbuf-2.0)
 
 # libraries
 FIND_LIBRARY( GTK_gtk_LIBRARY
@@ -52,7 +52,7 @@ FIND_LIBRARY(GTK_gdk_LIBRARY
  NAMES gdk-win32-2.0
  PATHS ${GTK_BASEPATH}/lib)
 
-FIND_LIBRARY(GTK_gdk-pixbuf_LIBRARY
+FIND_LIBRARY(GTK_gdk_pixbuf_LIBRARY
   NAMES gdk_pixbuf-2.0
   PATHS ${GTK_BASEPATH}/lib)
 
@@ -64,33 +64,50 @@ FIND_LIBRARY(GTK_gobject_LIBRARY
   NAMES gobject-2.0
   PATHS ${GTK_BASEPATH}/lib)
 
+FIND_LIBRARY(GTK_cairo_LIBRARY
+  NAMES cairo
+  PATHS ${GTK_BASEPATH}/lib)
+
+FIND_LIBRARY(GTK_pango_LIBRARY
+  NAMES pango-1.0
+  PATHS ${GTK_BASEPATH}/lib)
+
+FIND_LIBRARY(GTK_atk_LIBRARY
+  NAMES atk-1.0
+  PATHS ${GTK_BASEPATH}/lib)
+
 # GTK_INCLUDE_DIR   - Directories to include to use GTK
 # GTK_LINK_FLAGS    - Files to link against to use GTK
 # GTK_FOUND         - If false, don't try to use GTK
 # GTK_GL_FOUND      - If false, don't try to use GTK's GL features
 
-# check only gtk include and library... lazy behavior
-IF(GTK_gtk_INCLUDE_PATH AND GTK_cairo_INCLUDE_PATH
-    AND GTK_glib_INCLUDE_PATH AND GTK_pango_INCLUDE_PATH
-    AND GTK_atk_INCLUDE_PATH AND GTK_glib-config_INCLUDE_PATH
-    AND GTK_gdk-config_INCLUDE_PATH AND GTK_gtk_LIBRARY
-    AND GTK_gdk_LIBRARY AND GTK_glib_LIBRARY
-    AND GTK_gdk-pixbuf_LIBRARY AND GTK_gobject_LIBRARY)
-    SET(GtkPlus_FOUND TRUE)
-    SET(GTKPLUS_INCLUDE_DIR ${GTK_gtk_INCLUDE_PATH} 
-      ${GTK_cairo_INCLUDE_PATH}
-      ${GTK_glib_INCLUDE_PATH}
-      ${GTK_pango_INCLUDE_PATH}
-      ${GTK_atk_INCLUDE_PATH}
-      ${GTK_glib-config_INCLUDE_PATH}
-      ${GTK_gdk-config_INCLUDE_PATH})
+IF(GTK_gtk_INCLUDE_PATH AND GTK_gtk_LIBRARY
+    AND GTK_glib_INCLUDE_PATH AND GTK_glib_config_INCLUDE_PATH AND GTK_glib_LIBRARY
+    AND GTK_gobject_LIBRARY
+    AND GTK_gdk_config_INCLUDE_PATH AND GTK_gdk_LIBRARY
+    AND GTK_gdk_pixbuf_INCLUDE_PATH AND GTK_gdk_pixbuf_LIBRARY
+    AND GTK_cairo_INCLUDE_PATH AND GTK_cairo_LIBRARY
+    AND GTK_pango_INCLUDE_PATH AND GTK_pango_LIBRARY
+    AND GTK_atk_INCLUDE_PATH AND GTK_atk_LIBRARY)
+    SET(GTK_FOUND TRUE)
+    SET(GTKPLUS_INCLUDE_DIR ${GTK_gtk_INCLUDE_PATH}
+                            ${GTK_glib_INCLUDE_PATH}
+                            ${GTK_glib_config_INCLUDE_PATH}
+                            ${GTK_gdk_config_INCLUDE_PATH}
+                            ${GTK_gdk_pixbuf_INCLUDE_PATH}
+                            ${GTK_cairo_INCLUDE_PATH}
+                            ${GTK_pango_INCLUDE_PATH}
+                            ${GTK_atk_INCLUDE_PATH})
     SET( GTKPLUS_LINK_FLAGS  ${GTK_gtk_LIBRARY}
-      ${GTK_gdk_LIBRARY}
-      ${GTK_glib_LIBRARY}
-      ${GTK_gdk-pixbuf_LIBRARY}
-      ${GTK_gobject_LIBRARY})
+                             ${GTK_glib_LIBRARY}
+                             ${GTK_gobject_LIBRARY}
+                             ${GTK_gdk_LIBRARY}
+                             ${GTK_gdk_pixbuf_LIBRARY}
+                             ${GTK_cairo_LIBRARY}
+                             ${GTK_pango_LIBRARY}
+                             ${GTK_atk_LIBRARY})
 ENDIF()
 
-if (GTK_gdk-pixbuf_INCLUDE_PATH)
-    set(GTKPLUS_INCLUDE_DIR ${GTKPLUS_INCLUDE_DIR} ${GTK_gdk-pixbuf_INCLUDE_PATH})
+if (GTK_gdk_pixbuf_INCLUDE_PATH)
+    set(GTKPLUS_INCLUDE_DIR ${GTKPLUS_INCLUDE_DIR} ${GTK_gdk_pixbuf_INCLUDE_PATH})
 endif()
