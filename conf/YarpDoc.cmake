9abc25cf0cc9c8d2c5fb12aa66c99b7be2ab7b3b
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-07-31 19:04:32 +0000

    make man target for man pages
    svn path=/trunk/yarp2/; revision=9389

--- conf/YarpDoc.cmake
+++ conf/YarpDoc.cmake
@@ -5,6 +5,7 @@
 find_program(DOXYGEN_EXE NAMES doxygen)
 mark_as_advanced(DOXYGEN_EXE)
 if (DOXYGEN_EXE)
+  set(DOX_GENERATE_HTML YES)
   set(DOX_GENERATE_XML NO)
   option(YARP_DOXYGEN_XML "Generate doxygen XML output, for use by SWIG" NO)
   mark_as_advanced(YARP_DOXYGEN_XML)
@@ -15,8 +16,18 @@ if (DOXYGEN_EXE)
   # YARP documentation is always verbose now.  It used to be
   # partially stripped of stuff less interesting to end-users.
   set(DOX_EXTRA_DIR src conf doc/spec)
-  set(DOX_EXTRA_PATTERNS "*.cpp")
+  set(DOX_PATTERNS "*.h *.dox *.cpp")
+  set(DOX_GENERATE_MAN NO)
   configure_file(${CMAKE_SOURCE_DIR}/conf/doxygen/${DOX_FILE}.template
                  ${CMAKE_BINARY_DIR}/dox/${DOX_FILE} IMMEDIATE)
   add_custom_target(dox COMMAND ${DOXYGEN_EXE} ${CMAKE_BINARY_DIR}/dox/${DOX_FILE})
+
+  set(DOX_GENERATE_HTML NO)
+  set(DOX_GENERATE_MAN YES)
+  set(DOX_GENERATE_XML NO)
+  set(DOX_PATTERNS "cmd_*.dox")
+  configure_file(${CMAKE_SOURCE_DIR}/conf/doxygen/${DOX_FILE}.template
+                 ${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man IMMEDIATE)
+  add_custom_target(man COMMAND ${DOXYGEN_EXE} ${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man)
+
 endif ()
