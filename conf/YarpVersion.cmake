caa9ab6caab0165598323c3157daa17ab494401e
Lorenzo Natale <lorenzo.natale@iit.it>
2012-08-02 11:07:55 +0000

    added YARP_SVN_REVISION number filled by subversion (when available)
    svn path=/trunk/yarp2/; revision=9392

--- conf/YarpVersion.cmake
+++ conf/YarpVersion.cmake
@@ -4,10 +4,21 @@
 
 # This file is the official location of the current YARP version number.
 
+## We use subversion to indentify current revision number (optional)
+# the FindSubversion.cmake module is part of the standard distribution
+find_package(Subversion)
+if (Subversion_FOUND)
+    # extract working copy information for SOURCE_DIR into YARP_XXX variables
+    Subversion_WC_INFO(${CMAKE_SOURCE_DIR} YARP)
+    # write a file with the SVNVERSION define
+    message(STATUS "Detected svn revision number: ${YARP_WC_REVISION}")
+endif()
+
 set(YARP_VERSION_MAJOR "2")
 set(YARP_VERSION_MINOR "3")
 set(YARP_VERSION_PATCH "19")
-set(YARP_VERSION_MODIFIER "svn1")
+set(YARP_VERSION_MODIFIER "")
+set(YARP_SVN_REVISION "${YARP_WC_REVISION}")
 set(YARP_VERSION_ABI "1")
 
 set(CPACK_PACKAGE_VERSION_MAJOR "${YARP_VERSION_MAJOR}")
