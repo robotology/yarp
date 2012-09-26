877a4b2a9315baf4b4f132cee4d837dd8d583eee
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-09-26 13:54:53 +0000

    test resourcefinder class
    svn path=/trunk/yarp2/; revision=9479

---
+++ example/property/finder.cpp
@@ -0,0 +1,36 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright: (C) 2012 IITRBCS
+ * Author: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <yarp/os/ResourceFinder.h>
+
+using namespace yarp::os;
+
+int main(int argc, char *argv[]) {
+    ResourceFinder robot;
+    robot.configure(argc,argv);
+
+    if (!robot.check("GENERAL")) {
+        printf("Cannot find needed configuration (try --from config.txt)\n");
+        return 1;
+    }
+
+    int joints = robot.findGroup("GENERAL").find("Joints").asInt();
+    printf("Robot has %d joints\n", joints);
+
+    Bottle& maxes = robot.findGroup("LIMITS").findGroup("Max");
+    printf("Robot has limits: ");
+    for (int i=1; i<maxes.size(); i++) {
+        printf("%d ", maxes.get(i).asInt());
+    }
+    printf("\n");
+
+    return 0;
+}
