d1eb6ab025e6aa3387e40cdb07e754b948f0d28f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-10-04 14:28:32 +0000

    add yarp bindings source to install, for user convenience
    svn path=/trunk/yarp2/; revision=9497

---
+++ conf/YarpBindingsSource.cmake
@@ -0,0 +1,20 @@
+# Copyright: (C) 2012 IITRBCS
+# Authors: Paul Fitzpatrick
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+
+file(GLOB examples RELATIVE ${CMAKE_SOURCE_DIR}/bindings bindings/example*.*)
+
+foreach(f CMakeLists.txt yarp.i README swig-allegro-and-chicken-fix.patch compat.h ${examples})
+  install(FILES ${CMAKE_SOURCE_DIR}/bindings/${f} COMPONENT development
+    DESTINATION share/yarp/bindings)
+endforeach()
+
+file(GLOB java_srcs RELATIVE ${CMAKE_SOURCE_DIR}/bindings/src bindings/src/*.java)
+
+foreach(f ${java_srcs})
+  install(FILES ${CMAKE_SOURCE_DIR}/bindings/src/${f} COMPONENT development
+    DESTINATION share/yarp/bindings/src)
+endforeach()
+
+
