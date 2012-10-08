3f266992d886eab85c6b951d395b5263f55583c8
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-10-08 09:05:12 +0000

    [yarprobotinterface] Add Debug utilities
    svn path=/trunk/iCub/; revision=20582

---
+++ src/yarprobotinterface/Debug.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
+ * Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+ *
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+
+#include "Debug.h"
+
+#include <iostream>
+#include <sstream>
+
+#include <stdlib.h>
+
+
+#define RED    "\033[01;31m"
+#define GREEN  "\033[01;32m"
+#define YELLOW "\033[01;33m"
+#define BLUE   "\033[01;34m"
+#define CLEAR  "\033[00m"
+
+void RobotInterface::Debug::print_output(MsgType t, const std::ostringstream &s)
+{
+    switch (t) {
+        case DebugType:
+            std::cout << BLUE   << "DEBUG"   << CLEAR << ": " << s.str() << std::endl;
+            break;
+        case WarningType:
+            std::cout << YELLOW << "WARNING" << CLEAR << ": " << s.str() << std::endl;
+            break;
+        case ErrorType:
+            std::cout << RED    << "ERROR"   << CLEAR << ": " << s.str() << std::endl;
+            break;
+        case FatalType:
+            std::cout << RED    << "FATAL"   << CLEAR << ": " << s.str() << std::endl;
+            exit(1);
+            break;
+    }
+}
---
+++ src/yarprobotinterface/Debug.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
+ * Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+ *
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+
+#ifndef ROBOTINTERFACE_DEBUG_H
+#define ROBOTINTERFACE_DEBUG_H
+
+#include <iosfwd>
+#include <sstream>
+
+#include <yarp/os/ConstString.h>
+
+
+namespace RobotInterface
+{
+
+enum MsgType { DebugType, WarningType, ErrorType, FatalType };
+
+
+class Debug {
+    struct Stream {
+        Stream(MsgType t) : type(t), ref(1) {}
+        std::ostringstream oss;
+        MsgType type;
+        int ref;
+    } *stream;
+public:
+    inline Debug(MsgType type) :
+        stream(new Stream(type))
+    {
+    }
+
+    inline Debug(const Debug &o) : stream(o.stream) { ++stream->ref; }
+
+    inline ~Debug() {
+        if (!--stream->ref) {
+            print_output(stream->type, stream->oss);
+            delete stream;
+        }
+    }
+
+    inline Debug& operator<<(bool t) { stream->oss << (t ? "true" : "false"); stream->oss << ' '; return *this; }
+    inline Debug& operator<<(char t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(signed short t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(unsigned short t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(signed int t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(unsigned int t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(signed long t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(unsigned long t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(signed long long t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(unsigned long long t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(float t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(double t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(const char* t) { stream->oss << t; stream->oss << ' '; return *this; }
+    inline Debug& operator<<(const void * t) { stream->oss << t; stream->oss << ' '; return *this; }
+
+    inline Debug& operator<<(yarp::os::ConstString t) { stream->oss << t.c_str(); stream->oss << ' '; return *this; }
+
+private:
+    void print_output(MsgType t, const std::ostringstream &s);
+};
+
+}
+
+inline RobotInterface::Debug debug() { return RobotInterface::Debug(RobotInterface::DebugType); }
+inline RobotInterface::Debug warning() { return RobotInterface::Debug(RobotInterface::WarningType); }
+inline RobotInterface::Debug error() { return RobotInterface::Debug(RobotInterface::ErrorType); }
+inline RobotInterface::Debug fatal() { return RobotInterface::Debug(RobotInterface::FatalType); }
+
+
+#endif // ROBOTINTERFACE_DEBUG_H
