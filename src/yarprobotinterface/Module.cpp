ae53939153b76abc8cef987e8697160065f9ec3a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2012-10-22 09:13:49 +0000

    [yarprobotinterface] Temp fix file path
    svn path=/trunk/iCub/; revision=20920

--- src/yarprobotinterface/Module.cpp
+++ src/yarprobotinterface/Module.cpp
@@ -53,7 +53,7 @@ double RobotInterface::Module::getPeriod()
 bool RobotInterface::Module::open(yarp::os::Searchable& config)
 {
     RobotInterface::XMLReader reader;
-    mPriv->robot = reader.getRobot("/opt/iit/src/robotInterface/iCubTest/icub.xml");
+    mPriv->robot = reader.getRobot("/opt/iit/src/iCub/main/src/core/robotInterface/iCubTest/icub.xml");
     yDebug() << mPriv->robot;
 
     return mPriv->robot.enterPhase(RobotInterface::ActionPhaseStartup);
