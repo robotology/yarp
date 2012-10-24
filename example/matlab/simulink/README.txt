e24a0416fc28ebb8f1e352cbe580c953381ef96c
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-10-24 20:06:14 +0000

    simulink example from Juan Victores
    svn path=/trunk/yarp2/; revision=9518

---
+++ example/matlab/simulink/README.txt
@@ -0,0 +1,32 @@
+# Copyright: (C) RoboticsLab @ 2012 Universidad Carlos III de Madrid
+# Authors: Juan G. Victores
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+Legal
+-----
+The files "sfun_time.c" "utility.mdl" are originally part of Simulink® Real Time Execution
+by Guy Rouleau (http://www.mathworks.com/matlabcentral/fileexchange/authors/31651)
+27 Oct 2008 (Updated 22 Jul 2010), downloaded 22 Oct 2012 from:
+http://www.mathworks.com/matlabcentral/fileexchange/21908-simulink%C2%AE-real-time-execution
+The licence of Simulink® Real Time Execution is attached as "license.txt" in this directory.
+
+These three YARP/Simulink examples have been developed by Juan G. Victores
+(http://roboticslab.uc3m.es/roboticslab/persona.php?id_pers=72), licenced as the rest of
+the YARP Repository (http://eris.liralab.it/yarpdoc/index.html) from where this
+source can be downloaded.
+
+Install
+-------
+Run the following command in MATLAB for the examples to work:
+mex sfun_time.c
+
+You also need to have YARP integrated into MATLAB. Make sure you can execute the following from MATLAB:
+LoadYarp
+
+Running
+-------
+Have a yarp server running. Additional dependencies can be solved with the following terminal commands:
+* yarpWriteDoubles: yarp read /read
+* yarpReadDoubles: yarp write /write
+* yarpRemotePosEnc: yarpdev --device test_motor
+
---
+++ example/matlab/simulink/sfun_time.c
@@ -0,0 +1,101 @@
+#define S_FUNCTION_LEVEL 2
+#define S_FUNCTION_NAME  sfun_time
+
+#define TIME_SCALE_FACTOR(S) ssGetSFcnParam(S,0)
+
+/*
+ * Need to include simstruc.h for the definition of the SimStruct and
+ * its associated macro definitions.
+ */
+#include "simstruc.h"
+
+/*
+ *  Include the standard ANSI C header for handling time functions:
+ *  ---------------------------------------------------------------
+ */
+#include <time.h>
+
+static void mdlInitializeSizes(SimStruct *S)
+{
+
+   ssSetNumSFcnParams(S, 1);  /* Number of expected parameters */
+
+   if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) return;
+
+    ssSetNumContStates(S, 0);
+    ssSetNumDiscStates(S, 0);
+
+   if (!ssSetNumInputPorts(S, 0)) return;
+
+   if (!ssSetNumOutputPorts(S, 0)) return;
+   
+   ssSetNumSampleTimes(S, 1);
+   ssSetNumRWork(S, 1);
+   ssSetNumIWork(S, 0);
+   ssSetNumPWork(S, 0);
+   ssSetNumModes(S, 0);
+   ssSetNumNonsampledZCs(S, 0);
+   ssSetOptions(S, 0);
+}
+
+#define MDL_INITIALIZE_SAMPLE_TIMES
+static void mdlInitializeSampleTimes(SimStruct *S)
+{
+   ssSetSampleTime(S, 0, CONTINUOUS_SAMPLE_TIME);
+   ssSetOffsetTime(S, 0, 0.0);
+}
+
+#define MDL_START
+static void mdlStart(SimStruct *S)
+{
+    ssSetRWorkValue(S,0,ssGetTStart(S));
+}
+
+static void mdlOutputs(SimStruct *S, int_T tid)
+{
+   real_T             t_previousSimTime = ssGetRWorkValue(S,0);
+   const real_T      *scaleFactor = mxGetPr(TIME_SCALE_FACTOR(S));
+   time_T             t_SimTime = ssGetT(S);
+   real_T             t_diff = 0.0;
+   real_T             dt;
+   real_T             t_current;
+   real_T             t_0;
+   real_T             t_previous;
+   real_T             t_elapsed;
+   
+   /* Desired Delta time */
+   dt = (t_SimTime - t_previousSimTime) * (scaleFactor[0]);
+   /* Get clock time at the beginning of this step*/   
+   t_previous = (real_T)clock()/CLOCKS_PER_SEC;
+   t_0 = (real_T)clock()/CLOCKS_PER_SEC;
+
+   /* Wait to reach the desired time */
+   while (t_diff<dt){
+        t_current = (real_T) clock()/CLOCKS_PER_SEC;
+        /* Look for wrapup */
+        if (t_current<t_previous){
+            t_elapsed = t_previous - t_0;
+            t_0 = (real_T) clock()/CLOCKS_PER_SEC - t_elapsed;
+        }
+        t_diff = t_current - t_0;
+        t_previous = t_current;
+   }
+   
+   /* Store current time to be used in next time step*/
+   ssSetRWorkValue(S, 0, t_SimTime);
+}
+
+static void mdlTerminate(SimStruct *S)
+{
+    UNUSED_ARG(S); /* unused input argument */
+}
+
+/*
+ *  Required S-function trailer:
+ *  ----------------------------
+ */
+#ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
+#include "simulink.c"      /* MEX-file interface mechanism */
+#else
+#include "cg_sfun.h"       /* Code generation registration function */
+#endif
