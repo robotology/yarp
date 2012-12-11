45c40eb6e4d4ba8d3a428e9c7bfae6d72aad79ff
Lorenzo Natale <lorenzo.natale@iit.it>
2012-12-11 18:04:10 +0000

    cleanup tabs
    svn path=/trunk/yarp2/; revision=9592

--- src/libYARP_OS/include/yarp/os/impl/RunProcManager.h
+++ src/libYARP_OS/include/yarp/os/impl/RunProcManager.h
@@ -1,9 +1,9 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2007-2009 RobotCub Consortium
- * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
- */
+* Copyright (C) 2007-2009 RobotCub Consortium
+* CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+*/
 
 #ifndef __RUN_PROC_MANAGER_H__
 #define __RUN_PROC_MANAGER_H__
@@ -28,15 +28,15 @@
 #include <yarp/os/ConstString.h>
 
 #if defined(WIN32)
-    typedef DWORD PID;
-    typedef HANDLE FDESC;
+typedef DWORD PID;
+typedef HANDLE FDESC;
 #else
-    typedef int PID;
-    typedef int FDESC;
-    typedef void* HANDLE;
-    int CLOSE(int h);
-    int SIGNAL(int pid,int signum,bool wait);
-    void sigchild_handler(int sig);
+typedef int PID;
+typedef int FDESC;
+typedef void* HANDLE;
+int CLOSE(int h);
+int SIGNAL(int pid,int signum,bool wait);
+void sigchild_handler(int sig);
 #endif
 
 #define YARPRUN_ERROR -1
@@ -44,111 +44,111 @@
 class YarpRunProcInfo
 {
 public:
-	YarpRunProcInfo(yarp::os::ConstString& alias,yarp::os::ConstString& on,PID pidCmd,HANDLE handleCmd,bool hold);
+    YarpRunProcInfo(yarp::os::ConstString& alias,yarp::os::ConstString& on,PID pidCmd,HANDLE handleCmd,bool hold);
     virtual ~YarpRunProcInfo(){}
-	virtual bool Match(yarp::os::ConstString& alias){ return mAlias==alias; }
+    virtual bool Match(yarp::os::ConstString& alias){ return mAlias==alias; }
 
-	virtual bool Signal(int signum);
+    virtual bool Signal(int signum);
 
     virtual void Clean(){ mPidCmd=0; }
-    
-    #if !defined(WIN32)
+
+#if !defined(WIN32)
     virtual bool waitPid()
     {
         return waitpid(mPidCmd,0,WNOHANG)==mPidCmd;
     }
-    #endif
+#endif
 
-	virtual bool IsActive();
+    virtual bool IsActive();
 
     void setCmd(yarp::os::ConstString cmd) { mCmd = cmd; }
     void setEnv(yarp::os::ConstString env) { mEnv = env; }
 
 protected:
-	yarp::os::ConstString mAlias;
-	yarp::os::ConstString mOn;
-	PID mPidCmd;
-	HANDLE mHandleCmd; // only windows
-	bool mHold;        // only linux
+    yarp::os::ConstString mAlias;
+    yarp::os::ConstString mOn;
+    PID mPidCmd;
+    HANDLE mHandleCmd; // only windows
+    bool mHold;        // only linux
 
     yarp::os::ConstString mCmd;
     yarp::os::ConstString mEnv;
 
-	friend class YarpRunInfoVector; 
+    friend class YarpRunInfoVector; 
 };
 
 class YarpRunInfoVector
 {
 public:
-	YarpRunInfoVector();
-	~YarpRunInfoVector();
+    YarpRunInfoVector();
+    ~YarpRunInfoVector();
 
-	int Size(){ return m_nProcesses; }
-	bool Add(YarpRunProcInfo *process);
-	int Signal(yarp::os::ConstString& alias,int signum);
-	int Killall(int signum);
-	
-	#if defined(WIN32)
-	void GetHandles(HANDLE* &lpHandles,DWORD &nCount);
-	#else
-	void CleanZombies();
-	#endif
+    int Size(){ return m_nProcesses; }
+    bool Add(YarpRunProcInfo *process);
+    int Signal(yarp::os::ConstString& alias,int signum);
+    int Killall(int signum);
+
+#if defined(WIN32)
+    void GetHandles(HANDLE* &lpHandles,DWORD &nCount);
+#else
+    void CleanZombies();
+#endif
 
     yarp::os::Bottle PS();
-	bool IsRunning(yarp::os::ConstString &alias);
+    bool IsRunning(yarp::os::ConstString &alias);
 
-	yarp::os::Semaphore mutex;
+    yarp::os::Semaphore mutex;
 
 protected:
-	void Pack();
+    void Pack();
 
-	static const int MAX_PROCESSES=1024;
-	int m_nProcesses;
-	YarpRunProcInfo* m_apList[MAX_PROCESSES];
-	YarpRunInfoVector *m_pStdioMate;
+    static const int MAX_PROCESSES=1024;
+    int m_nProcesses;
+    YarpRunProcInfo* m_apList[MAX_PROCESSES];
+    YarpRunInfoVector *m_pStdioMate;
 };
 
 class YarpRunCmdWithStdioInfo : public YarpRunProcInfo
 {
 public:
-	YarpRunCmdWithStdioInfo(yarp::os::ConstString& alias,
-                            yarp::os::ConstString& on,
-                            yarp::os::ConstString& stdio,
-                            PID pidCmd,
-                            yarp::os::ConstString& stdioUUID,
-                            YarpRunInfoVector* stdioVector,
-                            PID pidStdin,
-                            PID pidStdout,
-                            FDESC readFromPipeStdinToCmd,
-                            FDESC writeToPipeStdinToCmd,
-                            FDESC readFromPipeCmdToStdout,
-                            FDESC writeToPipeCmdToStdout,
-                            HANDLE handleCmd,
-                            bool hold);
+    YarpRunCmdWithStdioInfo(yarp::os::ConstString& alias,
+        yarp::os::ConstString& on,
+        yarp::os::ConstString& stdio,
+        PID pidCmd,
+        yarp::os::ConstString& stdioUUID,
+        YarpRunInfoVector* stdioVector,
+        PID pidStdin,
+        PID pidStdout,
+        FDESC readFromPipeStdinToCmd,
+        FDESC writeToPipeStdinToCmd,
+        FDESC readFromPipeCmdToStdout,
+        FDESC writeToPipeCmdToStdout,
+        HANDLE handleCmd,
+        bool hold);
 
-	virtual ~YarpRunCmdWithStdioInfo(){}
-	
-	virtual void Clean();
+    virtual ~YarpRunCmdWithStdioInfo(){}
 
-    #if !defined(WIN32)
-	virtual bool waitPid();
-    #endif
+    virtual void Clean();
 
-	void TerminateStdio();
+#if !defined(WIN32)
+    virtual bool waitPid();
+#endif
+
+    void TerminateStdio();
 
 protected:
-	PID mPidStdin;
+    PID mPidStdin;
     PID mPidStdout;
 
     yarp::os::ConstString mStdio;
     yarp::os::ConstString mStdioUUID;
 
-	FDESC mWriteToPipeStdinToCmd;
+    FDESC mWriteToPipeStdinToCmd;
     FDESC mReadFromPipeStdinToCmd;
-	FDESC mWriteToPipeCmdToStdout;
+    FDESC mWriteToPipeCmdToStdout;
     FDESC mReadFromPipeCmdToStdout;
 
-	YarpRunInfoVector *mStdioVector;
+    YarpRunInfoVector *mStdioVector;
 };
 
 inline yarp::os::ConstString int2String(int x)
