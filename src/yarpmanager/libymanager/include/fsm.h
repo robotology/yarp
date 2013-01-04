804dccc8166b20180ef626ed68668d8f46d9c4ed
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-01-04 15:12:38 +0000

    [clang] Fix -Wunused-value warnings (remove unused typeid()s)
    svn path=/trunk/yarp2/; revision=9680

--- src/yarpmanager/libymanager/include/fsm.h
+++ src/yarpmanager/libymanager/include/fsm.h
@@ -122,7 +122,7 @@ public:
     StateBase* currentState(void) {
         try
         {
-            typeid(*state);
+    //        typeid(*state);
             return state;
         }
         catch (std::exception& )
@@ -139,9 +139,9 @@ public:
     void addTransition(StateBase* source, Event* event, StateBase* target) {
         try
         {
-            typeid(*target);
-            typeid(*target);
-            typeid(*target);
+//             typeid(*target);
+//             typeid(*target);
+//             typeid(*target);
 
             transitions[source][event] = target;
         }
