c175af7d794bf883f5b7f76b834d417a0062087f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-01-25 15:45:07 +0000

    add guidance on finding plugin runtimes in plugin config files (assuming those can be found - commits to come)
    svn path=/trunk/yarp2/; revision=9829

--- conf/YarpPlugin.cmake
+++ conf/YarpPlugin.cmake
@@ -42,6 +42,23 @@
 ## Skip this whole file if it has already been included
 if(NOT COMMAND YARP_END_PLUGIN_LIBRARY)
 
+## Prepare path information for the benefit of clients
+get_target_property(YARP_LIBRARY_PATH YARP_OS LOCATION)
+get_target_property(YARP_LIBRARY_TYPE YARP_OS TYPE)
+string(REPLACE "STATIC_LIBRARY" "static" YARP_LIBRARY_TYPE ${YARP_LIBRARY_TYPE})
+string(REPLACE "SHARED_LIBRARY" "shared" YARP_LIBRARY_TYPE ${YARP_LIBRARY_TYPE})
+get_filename_component(YARP_LIBRARY_EXTENSION ${YARP_LIBRARY_PATH} EXT)
+get_filename_component(YARP_LIBRARY_PREFIX ${YARP_LIBRARY_PATH} NAME_WE)
+string(REPLACE "YARP_OS" "" YARP_LIBRARY_PREFIX ${YARP_LIBRARY_PREFIX})
+get_filename_component(YARP_LIBRARY_PATH ${YARP_LIBRARY_PATH} ABSOLUTE)
+get_filename_component(YARP_LIBRARY_PATH ${YARP_LIBRARY_PATH} PATH)
+configure_file(${CMAKE_SOURCE_DIR}/conf/template/YarpPluginPath.cmake
+               ${CMAKE_BINARY_DIR}/etc/yarp/plugins/path.ini @ONLY IMMEDIATE)
+set(YARP_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib)
+configure_file(${CMAKE_SOURCE_DIR}/conf/template/YarpPluginPath.cmake
+               ${CMAKE_BINARY_DIR}/path_for_install.ini @ONLY IMMEDIATE)
+install(FILES ${CMAKE_BINARY_DIR}/path_for_install.ini RENAME path.ini COMPONENT configuration DESTINATION etc/yarp/plugins)
+
 
 #########################################################################
 # YARP_BEGIN_PLUGIN_LIBRARY: this macro makes sure that all the hooks
---
+++ conf/template/YarpPluginPath.cmake
@@ -0,0 +1,5 @@
+[search basic]
+path "@YARP_LIBRARY_PATH@"
+extension "@YARP_LIBRARY_EXTENSION@"
+prefix "@YARP_LIBRARY_PREFIX@"
+type "@YARP_LIBRARY_TYPE@"
