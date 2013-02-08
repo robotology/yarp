d8506d67a16ffbf9b86bbb6d0595b36dbbea223f
Alberto Cardellino <alberto.cardellino@iit.it>
2013-02-08 11:08:46 +0000

    added Interface for Virtual Analog Sensors
    svn path=/trunk/yarp2/; revision=9875

---
+++ src/libYARP_dev/include/yarp/dev/IVirtualAnalogSensor.h
@@ -0,0 +1,56 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2013 RobotCub Consortium
+ * Author: Alberto Cardellino
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef __YARPVIRTUALANALOGSENSORINTERFACE__
+#define __YARPVIRTUALANALOGSENSORINTERFACE__
+
+#include <yarp/dev/DeviceDriver.h>
+#include <yarp/sig/Vector.h>
+
+#define VOCAB_IANALOG VOCAB4('i','a','n','a')
+#define VOCAB_CALIBRATE_CHANNEL VOCAB4('c','a','l','c')
+
+/*! \file IVirtualAnalogSensor.h virtual analog sensor interface */
+namespace yarp {
+    namespace dev {
+        class IVirtualAnalogSensor;
+    }
+}
+
+/**
+ * @ingroup dev_iface_other
+ *
+ * A generic interface to a virtual sensors. A virtual sensor is any device that
+ * can generate values used as a measure by robot
+ */
+
+class YARP_dev_API yarp::dev::IVirtualAnalogSensor
+{
+public:
+    enum
+    {
+        AS_OK=0,
+        AS_ERROR=1,
+        AS_OVF=2,
+        AS_TIMEOUT=3
+    };
+
+    virtual ~IVirtualAnalogSensor(){}
+
+    /* Set a vector of torque values for virtual sensor
+     * @param vals a vector containing the sensor's last readings.
+     * @return AS_OK or return code. 
+     **/
+    virtual int configure(yarp::sig::Vector &vals)=0;
+    virtual int setTorque(yarp::sig::Vector &vals)=0;
+    virtual int getChannels(yarp::sig::Vector &vals)=0;
+};
+
+#endif
+//
