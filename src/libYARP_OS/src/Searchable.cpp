6f3cfbceb28874a41cfd5ee46a8600a36a914b08
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-02-25 15:57:24 +0000

    [Cleanup] Move Searchable implementation in separate cpp file
    svn path=/trunk/yarp2/; revision=9898

---
+++ src/libYARP_OS/src/Searchable.cpp
@@ -0,0 +1,80 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2006, 2008 RobotCub Consortium, Arjan Gijsberts
+ * Authors: Paul Fitzpatrick, Arjan Gijsberts
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <yarp/os/Searchable.h>
+#include <yarp/os/Value.h>
+
+#include <cstddef>
+
+
+bool yarp::os::Searchable::check(const char *txt,
+                                 yarp::os::Value *& result,
+                                 const char *comment) {
+    if (getMonitor()!=NULL && comment!=NULL) {
+        SearchReport report;
+        report.key = txt;
+        report.value = comment;
+        report.isComment = true;
+        reportToMonitor(report);
+    }
+    yarp::os::Value& bit = find(txt);
+    bool ok = !(bit.isNull());
+    if (ok) {
+        result = &bit;
+    }
+    return ok;
+}
+
+yarp::os::Value yarp::os::Searchable::check(const char *txt,
+                                  const yarp::os::Value& fallback,
+                                  const char *comment) {
+    if (getMonitor()!=NULL && comment!=NULL) {
+        yarp::os::SearchReport report;
+        report.key = txt;
+        report.value = comment;
+        report.isComment = true;
+        reportToMonitor(report);
+    }
+    if (getMonitor()!=NULL) {
+        yarp::os::SearchReport report;
+        report.key = txt;
+        report.value = fallback.toString();
+        report.isDefault = true;
+        reportToMonitor(report);
+    }
+    yarp::os::Value& bit = find(txt);
+    bool ok = !(bit.isNull());
+    if (ok) {
+        return bit;
+    }
+    return fallback;
+}
+
+bool yarp::os::Searchable::check(const char *key,
+                                 const char *comment) {
+    if (getMonitor()!=NULL && comment!=NULL) {
+        yarp::os::SearchReport report;
+        report.key = key;
+        report.value = comment;
+        report.isComment = true;
+        reportToMonitor(report);
+    }
+    return check(key);
+}
+
+yarp::os::Bottle& yarp::os::Searchable::findGroup(const char *key,
+                                                  const char *comment) {
+    if (getMonitor()!=NULL && comment!=NULL) {
+        yarp::os::SearchReport report;
+        report.key = key;
+        report.value = comment;
+        report.isComment = true;
+        reportToMonitor(report);
+    }
+    return findGroup(key);
+}
