0f144e997423d6ae30f77f925c54328a64fb3cec
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-02-26 10:51:27 +0000

    [Cleanup] Move PortablePair implementation in .cpp file
    svn path=/trunk/yarp2/; revision=9921

--- src/libYARP_OS/include/yarp/os/PortablePair.h
+++ src/libYARP_OS/include/yarp/os/PortablePair.h
@@ -31,22 +31,7 @@ public:
      */
     static bool readPair(ConnectionReader& connection,
                          Portable& head,
-                         Portable& body) {
-        // if someone connects in text mode, use standard
-        // text-to-binary mapping
-        connection.convertTextMode();
-
-        int header = connection.expectInt();
-        if (header!=BOTTLE_TAG_LIST) { return false; }
-        int len = connection.expectInt();
-        if (len!=2) { return false; }
-
-        bool ok = head.read(connection);
-        if (ok) {
-            ok = body.read(connection);
-        }
-        return ok;
-    }
+                         Portable& body);
 
     /**
      * Writes an object pair to a network connection.
@@ -57,22 +42,7 @@ public:
      */
     static bool writePair(ConnectionWriter& connection,
                           Portable& head,
-                          Portable& body) {
-        connection.appendInt(BOTTLE_TAG_LIST); // nested structure
-        connection.appendInt(2);               // with two elements
-        bool ok = head.write(connection);
-        if (ok) {
-            ok = body.write(connection);
-        }
-
-        if (ok) {
-            // if someone connects in text mode,
-            // let them see something readable.
-            connection.convertTextMode();
-        }
-
-        return ok;
-    }
+                          Portable& body);
 };
 
 /**
---
+++ src/libYARP_OS/src/PortablePair.cpp
@@ -0,0 +1,48 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2006 RobotCub Consortium
+ * Authors: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <yarp/os/PortablePair.h>
+
+
+bool yarp::os::PortablePairBase::readPair(ConnectionReader& connection,
+                                      Portable& head,
+                                      Portable& body) {
+    // if someone connects in text mode, use standard
+    // text-to-binary mapping
+    connection.convertTextMode();
+
+    int header = connection.expectInt();
+    if (header!=BOTTLE_TAG_LIST) { return false; }
+    int len = connection.expectInt();
+    if (len!=2) { return false; }
+
+    bool ok = head.read(connection);
+    if (ok) {
+        ok = body.read(connection);
+    }
+    return ok;
+}
+
+bool yarp::os::PortablePairBase::writePair(ConnectionWriter& connection,
+                                           Portable& head,
+                                           Portable& body) {
+    connection.appendInt(BOTTLE_TAG_LIST); // nested structure
+    connection.appendInt(2);               // with two elements
+    bool ok = head.write(connection);
+    if (ok) {
+        ok = body.write(connection);
+    }
+
+    if (ok) {
+        // if someone connects in text mode,
+        // let them see something readable.
+        connection.convertTextMode();
+    }
+
+    return ok;
+}
