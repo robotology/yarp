60da9b78c86e912ddecdbace6c45e3c4d6b241a4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-04-23 17:29:31 +0200

    Improve some "standard" CMake Find modules by using macros

--- conf/FindGooCanvasMM.cmake
+++ conf/FindGooCanvasMM.cmake
@@ -1,40 +1,20 @@
-# Try to find the GooCanvasMM library
-# GooCanvasMM_FOUND - system has GooCanvasMM
-# GooCanvasMM_INCLUDE_DIRS - GooCanvasMM include directory
-# GooCanvasMM_LIBRARY_DIRS - GooCanvasMM library directory
-# GooCanvasMM_LIBRARIES - GooCanvasMM libraries
-
-# Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
-# Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+# Try to find the GooCanvasMM library.
+# Once done this will define the following variables:
 #
+# GooCanvasMM_FOUND         - System has GooCanvasMM
+# GooCanvasMM_INCLUDE_DIRS  - GooCanvasMM include directory
+# GooCanvasMM_LIBRARIES     - GooCanvasMM libraries
+# GooCanvasMM_DEFINITIONS   - Additional compiler flags for GooCanvasMM
+# GooCanvasMM_VERSION       - GooCanvasMM version
+# GooCanvasMM_MAJOR_VERSION - GooCanvasMM major version
+# GooCanvasMM_MINOR_VERSION - GooCanvasMM minor version
+# GooCanvasMM_PATCH_VERSION - GooCanvasMM patch version
+# GooCanvasMM_TWEAK_VERSION - GooCanvasMM tweak version
+
+# Copyright (C) 2012, 2013  iCub Facility, Istituto Italiano di Tecnologia
+# Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
+include(MacroStandardFindModule)
+macro_standard_find_module(GooCanvasMM goocanvasmm-1.0)
 
-if(NOT WIN32)
-    find_package(PkgConfig)
-    if(PKG_CONFIG_FOUND)
-        if(GooCanvasMM_FIND_VERSION)
-            if(GooCanvasMM_FIND_VERSION_EXACT)
-                pkg_check_modules(PC_GOOCANVASMM QUIET goocanvasmm-1.0=${GooCanvasMM_FIND_VERSION})
-            else(GooCanvasMM_FIND_VERSION_EXACT)
-                pkg_check_modules(PC_GOOCANVASMM QUIET goocanvasmm-1.0>=${GooCanvasMM_FIND_VERSION})
-            endif(GooCanvasMM_FIND_VERSION_EXACT)
-        else(GooCanvasMM_FIND_VERSION)
-            pkg_check_modules(PC_GOOCANVASMM QUIET goocanvasmm-1.0)
-        endif(GooCanvasMM_FIND_VERSION)
-    endif(PKG_CONFIG_FOUND)
-endif(NOT WIN32)
-
-set(GooCanvasMM_INCLUDE_DIRS ${PC_GOOCANVASMM_INCLUDE_DIRS} CACHE PATH "GooCanvasMM include directory" FORCE)
-set(GooCanvasMM_LIBRARY_DIRS ${PC_GOOCANVASMM_LIBRARY_DIRS} CACHE PATH "GooCanvasMM library directory" FORCE)
-set(GooCanvasMM_LIBRARIES ${PC_GOOCANVASMM_LIBRARIES} CACHE STRING "GooCanvasMM libraries" FORCE)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GooCanvasMM
-                                  DEFAULT_MSG
-                                  GooCanvasMM_LIBRARIES
-)
-
-set(GooCanvasMM_FOUND ${GOOCANVASMM_FOUND})
-
-mark_as_advanced(GooCanvasMM_INCLUDE_DIRS GooCanvasMM_LIBRARY_DIRS GooCanvasMM_LIBRARIES)
--- conf/FindGtkDataboxMM.cmake
+++ conf/FindGtkDataboxMM.cmake
@@ -1,40 +1,19 @@
-# Try to find the GtkDataboxMM library
-# GtkDataboxMM_FOUND - system has GtkDataboxMM
-# GtkDataboxMM_INCLUDE_DIRS - GtkDataboxMM include directory
-# GtkDataboxMM_LIBRARY_DIRS - GtkDataboxMM library directory
-# GtkDataboxMM_LIBRARIES - GtkDataboxMM libraries
-
-# Copyright (C) 2012  iCub Facility, Istituto Italiano di Tecnologia
-# Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+# Try to find the GtkDataboxMM library.
+# Once done this will define the following variables:
 #
+# GtkDataboxMM_FOUND         - System has GtkDataboxMM
+# GtkDataboxMM_INCLUDE_DIRS  - GtkDataboxMM include directory
+# GtkDataboxMM_LIBRARIES     - GtkDataboxMM libraries
+# GtkDataboxMM_DEFINITIONS   - Additional compiler flags for GtkDataboxMM
+# GtkDataboxMM_VERSION       - GtkDataboxMM version
+# GtkDataboxMM_MAJOR_VERSION - GtkDataboxMM major version
+# GtkDataboxMM_MINOR_VERSION - GtkDataboxMM minor version
+# GtkDataboxMM_PATCH_VERSION - GtkDataboxMM patch version
+# GtkDataboxMM_TWEAK_VERSION - GtkDataboxMM tweak version
+
+# Copyright (C) 2012, 2013  iCub Facility, Istituto Italiano di Tecnologia
+# Author: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
-
-if(NOT WIN32)
-    find_package(PkgConfig)
-    if(PKG_CONFIG_FOUND)
-        if(GtkDataboxMM_FIND_VERSION)
-            if(GtkDataboxMM_FIND_VERSION_EXACT)
-                pkg_check_modules(PC_GTKDATABOXMM QUIET gtkdataboxmm-0.9=${GtkDataboxMM_FIND_VERSION})
-            else(GtkDataboxMM_FIND_VERSION_EXACT)
-                pkg_check_modules(PC_GTKDATABOXMM QUIET gtkdataboxmm-0.9>=${GtkDataboxMM_FIND_VERSION})
-            endif(GtkDataboxMM_FIND_VERSION_EXACT)
-        else(GtkDataboxMM_FIND_VERSION)
-            pkg_check_modules(PC_GTKDATABOXMM QUIET gtkdataboxmm-0.9)
-        endif(GtkDataboxMM_FIND_VERSION)
-    endif(PKG_CONFIG_FOUND)
-endif(NOT WIN32)
-
-set(GtkDataboxMM_INCLUDE_DIRS ${PC_GTKDATABOXMM_INCLUDE_DIRS} CACHE PATH "GtkDataboxMM include directory" FORCE)
-set(GtkDataboxMM_LIBRARY_DIRS ${PC_GTKDATABOXMM_LIBRARY_DIRS} CACHE PATH "GtkDataboxMM library directory" FORCE)
-set(GtkDataboxMM_LIBRARIES ${PC_GTKDATABOXMM_LIBRARIES} CACHE STRING "GtkDataboxMM libraries" FORCE)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GtkDataboxMM
-                                  DEFAULT_MSG
-                                  GtkDataboxMM_LIBRARIES
-)
-
-set(GtkDataboxMM_FOUND ${GTKDATABOXMM_FOUND})
-
-mark_as_advanced(GtkDataboxMM_INCLUDE_DIRS GtkDataboxMM_LIBRARY_DIRS GtkDataboxMM_LIBRARIES)
+include(MacroStandardFindModule)
+macro_standard_find_module(GtkDataboxMM gtkdataboxmm-0.9)
