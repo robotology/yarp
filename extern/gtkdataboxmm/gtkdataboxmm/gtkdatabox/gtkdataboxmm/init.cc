485510cfdfdfdb13cedd4e1e4195fa7bd269a79e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-05-02 12:01:23 +0200

    Import gtkdataboxmm 0.9.4

--- extern/gtkdataboxmm/gtkdataboxmm/gtkdatabox/gtkdataboxmm/init.cc
+++ extern/gtkdataboxmm/gtkdataboxmm/gtkdatabox/gtkdataboxmm/init.cc
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <gtkmm/main.h>
+#include <gtkmm.h>
 #include <gtkdataboxmmconfig.h>
 #include <gtkdataboxmm/wrap_init.h>
 
