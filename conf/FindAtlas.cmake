c0cc791883905e08264bd2dd5adecf14461a49f4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-05-14 10:56:02 +0200

    Cleanup cmake files and fix use of FindPackageHandleStandardArgs

--- conf/FindAtlas.cmake
+++ conf/FindAtlas.cmake
@@ -1,82 +1,60 @@
+# Find the Atlas (and Lapack) libraries
+
 # Copyright: (C) 2010 RobotCub Consortium
 # Authors: Arjan Gijsberts
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
-#
-# Find the Atlas (and Lapack) libraries
-#
 
-SET(ATLAS_POSSIBLE_INCLUDE_PATHS
-  /usr/include
-  /usr/include/atlas
-  /usr/local/include
-  $ENV{ATLAS_DIR}
-  $ENV{ATLAS_DIR}/include
+
+set(ATLAS_POSSIBLE_INCLUDE_PATHS
+    /usr/include
+    /usr/include/atlas
+    /usr/local/include
+    $ENV{ATLAS_DIR}
+    $ENV{ATLAS_DIR}/include
 )
 
 # Ubuntu's package management does not handle blas elegantly, causing
 # many possible locations here.
-SET(ATLAS_POSSIBLE_LIBRARY_PATHS
-  /usr/lib/libatlas-corei7sse3
-  /usr/lib/atlas-amd64sse3
-  /usr/lib/atlas-base     
-  /usr/lib/sse2
-  /usr/lib/sse
-  /usr/local/lib/sse2
-  /usr/local/lib/sse
-  /usr/lib
-  /usr/local/lib
-  $ENV{ATLAS_DIR}
-  $ENV{ATLAS_DIR}/lib
+set(ATLAS_POSSIBLE_LIBRARY_PATHS
+    /usr/lib/libatlas-corei7sse3
+    /usr/lib/atlas-amd64sse3
+    /usr/lib/atlas-base
+    /usr/lib/sse2
+    /usr/lib/sse
+    /usr/local/lib/sse2
+    /usr/local/lib/sse
+    /usr/lib
+    /usr/local/lib
+    $ENV{ATLAS_DIR}
+    $ENV{ATLAS_DIR}/lib
 )
 
-FIND_PATH(ATLAS_CBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${ATLAS_POSSIBLE_INCLUDE_PATHS})
-FIND_PATH(ATLAS_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${ATLAS_POSSIBLE_INCLUDE_PATHS})
-FIND_LIBRARY(ATLAS_CBLAS_LIBRARY NAMES ptcblas_r ptcblas cblas_r cblas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
-FIND_LIBRARY(ATLAS_ATLAS_LIBRARY NAMES atlas_r atlas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
-FIND_LIBRARY(ATLAS_LAPACK_ATLAS_LIBRARY NAMES alapack_r alapack lapack_atlas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
+find_path(ATLAS_CBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${ATLAS_POSSIBLE_INCLUDE_PATHS})
+find_path(ATLAS_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${ATLAS_POSSIBLE_INCLUDE_PATHS})
+find_library(ATLAS_CBLAS_LIBRARY NAMES ptcblas_r ptcblas cblas_r cblas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
+find_library(ATLAS_ATLAS_LIBRARY NAMES atlas_r atlas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
+find_library(ATLAS_LAPACK_ATLAS_LIBRARY NAMES alapack_r alapack lapack_atlas PATHS ${ATLAS_POSSIBLE_LIBRARY_PATHS})
 
-SET(ATLAS_FOUND ON)
-
-FOREACH(INCDIR ATLAS_CBLAS_INCLUDE_DIR ATLAS_CLAPACK_INCLUDE_DIR)
-  IF(${INCDIR})
-    SET(ATLAS_INCLUDE_DIR ${ATLAS_INCLUDE_DIR} ${${INCDIR}} )
-  ELSE(${INCDIR})
-    MESSAGE("${INCDIR} not found turning off ATLAS_FOUND")
-    SET(OPENCV_FOUND OFF)
-  ENDIF (${INCDIR})
-ENDFOREACH(INCDIR)
-
-# could make LAPACK_ATLAS optional
-FOREACH(LIBNAME ATLAS_LAPACK_ATLAS_LIBRARY ATLAS_CBLAS_LIBRARY ATLAS_ATLAS_LIBRARY)
-  IF(${LIBNAME})
-    SET(ATLAS_LIBRARIES ${ATLAS_LIBRARIES} ${${LIBNAME}} )
-  ELSE(${LIBNAME})
-    MESSAGE("${LIBNAME} not found turning off ATLAS_FOUND")
-    SET(ATLAS_FOUND OFF)
-  ENDIF (${LIBNAME})
-ENDFOREACH(LIBNAME)
+set(ATLAS_FOUND ON)
 
 
-IF (ATLAS_FOUND)
-  IF (NOT Atlas_FIND_QUIETLY)
-    MESSAGE(STATUS "Found Atlas libraries: ${ATLAS_LIBRARIES}")
-    MESSAGE(STATUS "Found Atlas include: ${ATLAS_INCLUDE_DIR}")
-  ENDIF (NOT Atlas_FIND_QUIETLY)
-ELSE (ATLAS_FOUND)
-  IF (Atlas_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find Atlas")
-  ENDIF (Atlas_FIND_REQUIRED)
-ENDIF (ATLAS_FOUND)
-
-MARK_AS_ADVANCED(
-  ATLAS_INCLUDE_DIR
-  ATLAS_CBLAS_INCLUDE_DIR
-  ATLAS_CLAPACK_INCLUDE_DIR
-  ATLAS_LIBRARIES
-  ATLAS_CBLAS_LIBRARY
-  ATLAS_ATLAS_LIBRARY
-  ATLAS_LAPACK_ATLAS_LIBRARY
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Atlas
+                                  DEFAULT_MSG
+                                  ATLAS_ATLAS_LIBRARY
+                                  ATLAS_CBLAS_INCLUDE_DIR
+                                  ATLAS_CLAPACK_INCLUDE_DIR
+                                  ATLAS_LAPACK_ATLAS_LIBRARY
+                                  ATLAS_CBLAS_LIBRARY
 )
+
+mark_as_advanced(ATLAS_INCLUDE_DIR
+                 ATLAS_CBLAS_INCLUDE_DIR
+                 ATLAS_CLAPACK_INCLUDE_DIR
+                 ATLAS_LIBRARIES
+                 ATLAS_CBLAS_LIBRARY
+                 ATLAS_ATLAS_LIBRARY
+                 ATLAS_LAPACK_ATLAS_LIBRARY)
 
 set(Atlas_FOUND ${ATLAS_FOUND})
--- conf/FindGthread.cmake
+++ conf/FindGthread.cmake
@@ -1,11 +1,4 @@
-# Copyright: (C) 2009 RobotCub Consortium
-# Authors: Alessandro Scalzo, Lorenzo Natale, Paul Fitzpatrick, Gianluca Massera
-# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
-
-# Tried to make variables uniform with cmake "standards"
-# Now set Gthread_* variables (respect capitalization rule).
-# Maintain backwards compatibiliy.
-# Lorenzo Natale 14/05/2010
+# Try to find Gthread package
 #
 # On exit set:
 # Gthread_FOUND
@@ -17,78 +10,93 @@
 # GTHREAD_FOUND
 # GTHREAD_LINK_FLAGS
 # GTHREAD_INCLUDE_DIR
-#
+
+
+# Copyright: (C) 2009 RobotCub Consortium
+# Authors: Alessandro Scalzo, Lorenzo Natale, Paul Fitzpatrick, Gianluca Massera
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+# Tried to make variables uniform with cmake "standards"
+# Now set Gthread_* variables (respect capitalization rule).
+# Maintain backwards compatibiliy.
+# Lorenzo Natale 14/05/2010
+
 # Added: 03/11/2011, Lorenzo
 # Windows: support gtkmm x64, search also using GTKMM64_BASEPATH (this variable is set by the installer).
-#
 
-INCLUDE(FindPackageHandleStandardArgs)
 
-IF(UNIX)
- INCLUDE(FindPkgConfig)
- if (PKG_CONFIG_FOUND)
-   pkg_check_modules(GTHREAD "gthread-2.0")
-   if (GTHREAD_FOUND)
-     set(GTHREAD_INCLUDE_DIR ${GTHREAD_INCLUDE_DIRS})
-     set(GTHREAD_LIBRARY_DIR ${GTHREAD_LIBRARY_DIRS})
-     set(GTHREAD_LINK_FLAGS ${GTHREAD_LDFLAGS})
-     set(GTHREAD_C_FLAGS ${GTHREAD_CFLAGS})
-   endif (GTHREAD_FOUND)
- endif (PKG_CONFIG_FOUND)
- 
- IF (APPLE)
-  IF (NOT GTHREAD_LINK_FLAGS)
-    ### THERE IS AN OFFICIAL GTK PACKAGE on www.gtk-osx.org
-    ### The following setup is relative to the official GTK package for mac os-x
-    SET( GTHREAD_INCLUDE_DIR "/Library/Frameworks/GLib.framework/Headers" )
-    SET( GTHREAD_LIBRARY_DIR " " )
-    SET( GTHREAD_LINK_FLAGS "-framework GLib" )
-    SET( GTHREAD_C_FLAGS " " )
-  ENDIF ()
- ENDIF (APPLE)
+if (NOT Gthread_FOUND)
 
- SET(GTHREAD_LINK_FLAGS "${GTHREAD_LINK_FLAGS}" CACHE INTERNAL "gthread link flags")
- SET(GTHREAD_C_FLAGS "${GTHREAD_C_FLAGS}" CACHE INTERNAL "gthread include flags")
- SET(GTHREAD_INCLUDE_DIR "${GTHREAD_INCLUDE_DIR}" CACHE INTERNAL "gthread include directory")
+    if(UNIX)
 
- SET(Gthread_LIBRARIES "${GTHREAD_LINK_FLAGS}" CACHE INTERNAL "gthread link flags")
- SET(Gthread_C_FLAGS "${GTHREAD_C_FLAGS}" CACHE INTERNAL "gthread include flags")
- SET(Gthread_INCLUDE_DIRS "${GTHREAD_INCLUDE_DIR}" CACHE INTERNAL "gthread include directory")
+        find_package(PkgConfig QUIET)
+        if (PKG_CONFIG_FOUND)
+        pkg_check_modules(GTHREAD "gthread-2.0")
+        if (GTHREAD_FOUND)
+            set(GTHREAD_INCLUDE_DIR ${GTHREAD_INCLUDE_DIRS})
+            set(GTHREAD_LIBRARY_DIR ${GTHREAD_LIBRARY_DIRS})
+            set(GTHREAD_LINK_FLAGS ${GTHREAD_LDFLAGS})
+            set(GTHREAD_C_FLAGS ${GTHREAD_CFLAGS})
+        endif (GTHREAD_FOUND)
+        endif (PKG_CONFIG_FOUND)
 
- IF (GTHREAD_C_FLAGS)
- 	SET(Gthread_FOUND TRUE)
- ELSE (GTHREAD_C_FLAGS)
-	SET(Gthread_FOUND FALSE)
- ENDIF (GTHREAD_C_FLAGS)
- 
- FIND_PACKAGE_HANDLE_STANDARD_ARGS(Gthread "GTHREAD not found" Gthread_LIBRARIES Gthread_INCLUDE_DIRS)
-ELSE (UNIX)
+        if(APPLE)
+            if(NOT GTHREAD_LINK_FLAGS)
+                ### THERE IS AN OFFICIAL GTK PACKAGE on www.gtk-osx.org
+                ### The following setup is relative to the official GTK package for mac os-x
+                set( GTHREAD_INCLUDE_DIR "/Library/Frameworks/GLib.framework/Headers" )
+                set( GTHREAD_LIBRARY_DIR " " )
+                set( GTHREAD_LINK_FLAGS "-framework GLib" )
+                set( GTHREAD_C_FLAGS " " )
+            endif()
+        endif(APPLE)
 
-  # search GTKMM_BASEPATH to support gthread shipped and installed from gtkmm
-  # priority to GTKMM64_BASEPATH (x64 build)
-  set(GTK_BASEPATH $ENV{GTKMM64_BASEPATH})
-  if (NOT GTK_BASEPATH)
-      set(GTK_BASEPATH $ENV{GTKMM_BASEPATH})
-  endif()
-  
-  if (NOT GTK_BASEPATH)
-      set(GTK_BASEPATH $ENV{GTK_BASEPATH})
-  endif()
-	  
-  FIND_LIBRARY(GTK_thread_lib
-	NAMES gthread-2.0
-	PATHS ${GTK_BASEPATH}/lib)
-  
-  SET(Gthread_INCLUDE_DIRS "" CACHE PATH "Gthread include directory")
-  
-  if (GTK_thread_lib)
-    set(Gthread_LIBRARIES ${GTK_thread_lib} CACHE STRING "Gthread libraries")
-  endif(GTK_thread_lib)
-  
-  find_package_handle_standard_args(Gthread DEFAULT_MSG Gthread_LIBRARIES)
+        set(GTHREAD_LINK_FLAGS "${GTHREAD_LINK_FLAGS}" CACHE INTERNAL "gthread link flags")
+        set(GTHREAD_C_FLAGS "${GTHREAD_C_FLAGS}" CACHE INTERNAL "gthread include flags")
+        set(GTHREAD_INCLUDE_DIR "${GTHREAD_INCLUDE_DIR}" CACHE INTERNAL "gthread include directory")
 
-  set(Gthread_FOUND = ${GTHREAD_FOUND})
+        set(Gthread_LIBRARIES "${GTHREAD_LINK_FLAGS}" CACHE INTERNAL "gthread link flags")
+        set(Gthread_C_FLAGS "${GTHREAD_C_FLAGS}" CACHE INTERNAL "gthread include flags")
+        set(Gthread_INCLUDE_DIRS "${GTHREAD_INCLUDE_DIR}" CACHE INTERNAL "gthread include directory")
 
-ENDIF (UNIX)
+        if(GTHREAD_C_FLAGS)
+            set(Gthread_FOUND TRUE)
+        else(GTHREAD_C_FLAGS)
+            set(Gthread_FOUND FALSE)
+        endif(GTHREAD_C_FLAGS)
+        include(FindPackageHandleStandardArgs)
 
-mark_as_advanced(Gthread_INCLUDE_DIRS Gthread_LIBRARIES)
+        find_package_handle_standard_args(Gthread DEFAULT_MSG Gthread_LIBRARIES Gthread_INCLUDE_DIRS)
+
+    else(UNIX)
+
+        # search GTKMM_BASEPATH to support gthread shipped and installed from gtkmm
+        # priority to GTKMM64_BASEPATH (x64 build)
+        set(GTK_BASEPATH $ENV{GTKMM64_BASEPATH})
+        if (NOT GTK_BASEPATH)
+            set(GTK_BASEPATH $ENV{GTKMM_BASEPATH})
+        endif()
+
+        if (NOT GTK_BASEPATH)
+            set(GTK_BASEPATH $ENV{GTK_BASEPATH})
+        endif()
+
+        find_library(GTK_thread_lib
+                     NAMES gthread-2.0
+                     PATHS ${GTK_BASEPATH}/lib)
+
+        set(Gthread_INCLUDE_DIRS "" CACHE PATH "Gthread include directory")
+
+        if (GTK_thread_lib)
+            set(Gthread_LIBRARIES ${GTK_thread_lib} CACHE STRING "Gthread libraries")
+        endif(GTK_thread_lib)
+
+        find_package_handle_standard_args(Gthread DEFAULT_MSG Gthread_LIBRARIES)
+
+        set(Gthread_FOUND ${GTHREAD_FOUND})
+
+    endif(UNIX)
+
+    mark_as_advanced(Gthread_INCLUDE_DIRS Gthread_LIBRARIES)
+
+endif (NOT Gthread_FOUND)
