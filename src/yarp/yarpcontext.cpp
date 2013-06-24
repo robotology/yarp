44f331e14ed78980afef495c1353a541d94c0966
Elena Ceseracciu <eowyn83@gmail.com>
2013-06-24 22:24:21 +0200

    Fixed yarp-context file copying on Windows

--- src/yarp/yarpcontext.cpp
+++ src/yarp/yarpcontext.cpp
@@ -34,7 +34,7 @@ int recursiveCopy(ConstString srcDirName, ConstString destDirName)
     if (n<0)
     {
         printf("Could not read from  directory %s\n", srcDirName.c_str());
-        return 1;
+        return -1;
     }
     if (yarp::os::mkdir((destDirName).c_str()) < 0)
     {
@@ -42,7 +42,7 @@ int recursiveCopy(ConstString srcDirName, ConstString destDirName)
             printf("Directory %s already exist; remove it first, or use the diff/merge commands\n", destDirName.c_str());
         else
             printf("Could not create directory %s\n", destDirName.c_str());
-        return 1;
+        return -1;
     }
     for (int i=0; i<n; i++)
     {
@@ -59,35 +59,34 @@ int recursiveCopy(ConstString srcDirName, ConstString destDirName)
                 recursiveCopy(srcPath, destDirName + PATH_SEPERATOR + name);
             if ((statbuf.st_mode & S_IFMT)== S_IFREG)
             {
-                ACE_HANDLE fdSrc=ACE_OS::open(srcPath.c_str(),O_RDONLY);
+                char buf[BUFSIZ];
+                size_t size;
 
-                if ( fdSrc <0 )
+                FILE* source = fopen(srcPath.c_str(), "rb");
+                if (source == NULL)
                 {
-                   printf("BAD OPEN %s\n",srcPath.c_str());
-                   // return 1;
-                    ok = false;
+                    ok=false;
+                    printf("Could not open source file %s\n", srcPath.c_str());
+                    continue;
                 }
-                else
+                FILE* dest = fopen((destDirName + PATH_SEPERATOR + name).c_str(), "wb");
+                if(dest ==NULL)
                 {
-                    unsigned char c;
-                    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-                    ACE_HANDLE fdDest=ACE_OS::creat  ((destDirName + PATH_SEPERATOR + name).c_str(), mode) ;
-                    
-                    if ( fdDest<0 )
-                    {
-                         printf("BAD OPEN %s\n",(destDirName + PATH_SEPERATOR + name).c_str());
-                        
-                        ACE_OS::close(fdSrc);
-                        ok=false;
-                        //return 1;
-                    }
-                    while (ACE_OS::read(fdSrc,&c,1)==1)
-                    {
-                        ACE_OS::write(fdDest,&c,1);
-                    }
-                    ACE_OS::close(fdSrc);
-                    ACE_OS::close(fdDest);
+                    ok=false;
+                    printf("Could not open target file %s\n",(destDirName + PATH_SEPERATOR + name).c_str());
+                    fclose(source);
+                    continue;
                 }
+                // clean and more secure
+                // feof(FILE* stream) returns non-zero if the end of file indicator for stream is set
+
+                while (size = fread(buf, 1, BUFSIZ, source)) 
+                {
+                    fwrite(buf, 1, size, dest);
+                }
+
+                fclose(source);
+                fclose(dest);
             }
         }
         free(namelist[i]);
@@ -156,6 +155,29 @@ void printContentDirs(ConstString curPath)
     free(namelist);
 };
 
+void prepareHomeFolder(ResourceFinder &rf)
+{
+
+    ACE_DIR* dir= ACE_OS::opendir((rf.getDataHome()).c_str()); //TODO: wrap in yarp
+    if (dir!=NULL)
+        //ACE_OS::closedir(dir);
+        YARP_closedir(dir);
+    else
+    {
+        yarp::os::mkdir((rf.getDataHome()).c_str());
+    }
+
+    dir= ACE_OS::opendir((rf.getDataHome() + PATH_SEPERATOR + "contexts").c_str()); //TODO: wrap in yarp
+    if (dir!=NULL)
+        //ACE_OS::closedir(dir);
+        YARP_closedir(dir);
+    else
+    {
+        yarp::os::mkdir((rf.getDataHome() + PATH_SEPERATOR + "contexts").c_str());
+    }
+}
+
+
 void show_help() {
     printf("Usage: yarp-context [OPTION]\n\n");
     printf("Known values for OPTION are:\n\n");
@@ -222,25 +244,19 @@ int main(int argc, char *argv[]) {
         ConstString originalpath=rf.findPath((ConstString("contexts") + PATH_SEPERATOR +contextName).c_str(), opts);
         ConstString destDirname=rf.getDataHome() + PATH_SEPERATOR + "contexts" + PATH_SEPERATOR + contextName;
 
-		ACE_DIR* dir= ACE_OS::opendir((rf.getDataHome()).c_str()); //TODO: wrap in yarp
-        if (dir!=NULL)
-            //ACE_OS::closedir(dir);
-            YARP_closedir(dir);
+        prepareHomeFolder(rf);
+
+        int result= recursiveCopy(originalpath, destDirname);
+        if (result < 0)
+            printf("ERRORS OCCURRED WHILE IMPORTING CONTEXT %s\n", contextName.c_str());
         else
         {
-            yarp::os::mkdir((rf.getDataHome()).c_str());
+            printf("Copied context %s from %s to %s .\nCurrent locations for this context:\n", contextName.c_str(), originalpath.c_str(), destDirname.c_str());
+            yarp::os::Bottle paths=rf.findPaths((ConstString("contexts") + PATH_SEPERATOR +contextName).c_str());
+            for (int curCont=0; curCont<paths.size(); ++curCont)
+                printf("%s\n", paths.get(curCont).asString().c_str());
         }
-
-
-        dir= ACE_OS::opendir((rf.getDataHome() + PATH_SEPERATOR + "contexts").c_str()); //TODO: wrap in yarp
-        if (dir!=NULL)
-            //ACE_OS::closedir(dir);
-            YARP_closedir(dir);
-        else
-        {
-            yarp::os::mkdir((rf.getDataHome() + PATH_SEPERATOR + "contexts").c_str());
-        }
-        return recursiveCopy(originalpath, destDirname);// TODO: check result!
+        return result;
     }
     
     if(options.check("import-all"))
@@ -249,6 +265,8 @@ int main(int argc, char *argv[]) {
         yarp::os::ResourceFinder rf;
         if (options.check("verbose"))
             rf.setVerbose(true);
+
+        prepareHomeFolder(rf);
         ResourceFinderOptions opts;
         opts.searchLocations=ResourceFinderOptions::Installed;
         yarp::os::Bottle contextPaths=rf.findPaths("contexts", opts);
@@ -281,6 +299,9 @@ int main(int argc, char *argv[]) {
             }
             free(namelist);
         }
+
+        printf("New user contexts:\n");
+        printContentDirs(rf.getDataHome() + PATH_SEPERATOR + "contexts");
         return 0;
     }
     
@@ -310,7 +331,14 @@ int main(int argc, char *argv[]) {
             printf("Are you sure you want to remove this folder: %s ? (y/n): ", targetPath.c_str());
             scanf("%c",&choice);
             if (choice=='y')
-                return recursiveRemove(targetPath.c_str());
+            {
+                int result= recursiveRemove(targetPath.c_str());
+                if (result < 0)
+                    printf("ERRORS OCCURRED WHILE REMOVING %s\n", targetPath.c_str());
+                else
+                    printf("Removed folder %s\n", targetPath.c_str());
+                return result;
+            }
             else
             {
                 printf("Skipped\n");
