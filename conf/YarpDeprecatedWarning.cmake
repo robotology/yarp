59c9f5ae06224350cb689b1a2045ffd1b2e966f7
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-07-05 11:20:30 +0200

    Add missing copyright statement

--- conf/YarpDeprecatedWarning.cmake
+++ conf/YarpDeprecatedWarning.cmake
@@ -1,3 +1,7 @@
+# Copyright (C) 2013  iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
 macro(YARP_DEPRECATED_WARNING)
     if(NOT YARP_NO_DEPRECATED_WARNINGS)
         message(WARNING "${ARGN}")
