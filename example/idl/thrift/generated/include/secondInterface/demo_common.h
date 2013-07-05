8128d78558d459758609e96dd2ead68352b0d4c0
Elena Ceseracciu <eowyn83@gmail.com>
2013-07-05 14:27:49 +0200

    Added warning-header to "_common.h" Thrift-generated files

--- example/idl/thrift/generated/include/secondInterface/demo_common.h
+++ example/idl/thrift/generated/include/secondInterface/demo_common.h
@@ -1,3 +1,6 @@
+// This is an automatically-generated file.
+// It could get re-generated if the ALLOW_IDL_GENERATION flag is on.
+
 #ifndef YARP_THRIFT_GENERATOR_COMMON_demo
 #define YARP_THRIFT_GENERATOR_COMMON_demo
 
