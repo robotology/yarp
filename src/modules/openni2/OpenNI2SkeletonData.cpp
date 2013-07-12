edfc845213e08db14ef6ed1ad2983a52ad1a9135
Konstantinos Theofilis <k.theofilis@herts.ac.uk>
2013-07-12 12:16:54 +0200

    openni2Master branch

---
+++ src/modules/openni2/OpenNI2SkeletonData.cpp
@@ -0,0 +1,113 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+* Copyright (C) 2011 Duarte Aragao
+* Author: Duarte Aragao
+* CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+* Copyright
+* Author: Konstantinos Theofilis, University of Hertfordshire, 2013
+*/
+
+
+#include "OpenNI2SkeletonData.h"
+
+OpenNI2SkeletonData::OpenNI2SkeletonData()
+{
+	initUserSkeletons();
+}
+
+OpenNI2SkeletonData::~OpenNI2SkeletonData()
+{
+}
+
+void OpenNI2SkeletonData::initUserSkeletons(){
+	userSkeleton = new UserSkeleton[MAX_USERS];
+	UserSkeleton *tuserSkeleton;
+	for(int i = 0; i < MAX_USERS; i++){
+		tuserSkeleton = &(userSkeleton[i]);
+		for(int j = 0; j < TOTAL_JOINTS; j++){
+			tuserSkeleton->skeletonPointsOri[j].resize(4);
+			tuserSkeleton->skeletonPointsOri[j].zero();
+			tuserSkeleton->skeletonPointsPos[j].resize(3);
+			tuserSkeleton->skeletonPointsPos[j].zero();
+			tuserSkeleton->skeletonPosConf[j] = -1;
+			tuserSkeleton->skeletonOriConf[j] = -1;
+			//tuserSkeleton->skeletonState = NO_USER;
+		}
+	}
+}
+
+void OpenNI2SkeletonData::storeData(ImageOf<PixelRgb>& img){
+	imageFrame = img;
+}
+
+void OpenNI2SkeletonData::storeData(ImageOf<PixelMono16>& img){
+		depthFrame = img;
+}
+
+void OpenNI2SkeletonData::storeData(Bottle& b){
+	Bottle *list;
+	int userID;
+	UserSkeleton *tuserSkeleton;
+	if(b.get(0).isString()){
+		userID = b.get(1).asInt();
+		string vocab = b.get(0).asString().c_str();
+		if(vocab.compare(USER_CALIBRATING_MSG) == 0){
+			userSkeleton[userID].skeletonState = nite::SKELETON_CALIBRATING;
+        }
+//		else if(vocab.compare(USER_DETECTED_MSG) == 0){
+//			userSkeleton[userID].skeletonState = USER_DETECTED;}
+		else if(vocab.compare(USER_LOST_MSG) == 0){
+			userSkeleton[userID].skeletonState = nite::SKELETON_NONE;
+		}
+	}else if(b.get(0).isList()){
+		list = b.get(0).asList();
+		userID = list->get(1).asInt();
+		userSkeleton[userID].skeletonState = nite::SKELETON_TRACKED;//USER STATUS
+		tuserSkeleton = &(userSkeleton[userID]);
+		for(int i = 1; i < b.size(); i+=6){
+			int jointIndex = (i-1)/6;
+			list = b.get(i+1).asList();//position elements
+			tuserSkeleton->skeletonPointsPos[jointIndex][0] = list->get(0).asDouble();
+			tuserSkeleton->skeletonPointsPos[jointIndex][1] = list->get(1).asDouble();
+			tuserSkeleton->skeletonPointsPos[jointIndex][2] = list->get(2).asDouble();
+			tuserSkeleton->skeletonPosConf[jointIndex] = b.get(i+2).asDouble();
+			list = b.get(i+4).asList();//orientation elements
+			tuserSkeleton->skeletonPointsOri[jointIndex][0] = list->get(0).asDouble();
+			tuserSkeleton->skeletonPointsOri[jointIndex][1] = list->get(1).asDouble();
+			tuserSkeleton->skeletonPointsOri[jointIndex][2] = list->get(2).asDouble();
+			tuserSkeleton->skeletonPointsOri[jointIndex][3] = list->get(3).asDouble();
+			tuserSkeleton->skeletonOriConf[jointIndex] = b.get(i+5).asDouble();
+		}
+	}
+	//std::cout<<"onRead(Bottle)"<<std::endl;
+}
+
+Vector* OpenNI2SkeletonData::getOrientation(int userID){
+	return userSkeleton[userID].skeletonPointsOri;
+}
+
+Vector* OpenNI2SkeletonData::getPosition(int userID){
+	return userSkeleton[userID].skeletonPointsPos;
+}
+
+double* OpenNI2SkeletonData::getOrientationConf(int userID){
+	return userSkeleton[userID].skeletonOriConf;
+}
+
+double* OpenNI2SkeletonData::getPositionConf(int userID){
+	return userSkeleton[userID].skeletonPosConf;
+}
+
+int OpenNI2SkeletonData::getSkeletonState(int userID){
+	return userSkeleton[userID].skeletonState;
+}
+
+ImageOf<PixelMono16> OpenNI2SkeletonData::getDepthFrame(){
+	return depthFrame;
+}
+
+ImageOf<PixelRgb> OpenNI2SkeletonData::getImageFrame(){
+	return imageFrame;
+}
+
---
+++ src/modules/openni2/OpenNI2SkeletonData.h
@@ -0,0 +1,124 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+* Copyright (C) 2011 Duarte Aragao
+* Author: Duarte Aragao
+* CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+* Copyright
+* Author: Konstantinos Theofilis, University of Hertfordshire, 2013
+*/
+
+
+#pragma once
+
+#include <yarp/os/Network.h>
+#include <yarp/os/Vocab.h>
+#include <yarp/math/Math.h>
+#include <yarp/sig/Image.h>
+#include <string>
+#include <iostream>
+#include "NiTE.h"
+
+//included so that the enumeration can be accessed
+#include <yarp/dev/IOpenNI2DeviceDriver.h>
+
+#define USER_DETECTED_MSG "[USER_DETECTED]"
+#define USER_CALIBRATING_MSG "[USER_CALIBRATING]"
+#define USER_LOST_MSG "[USER_LOST]"
+#define USER_VOCAB VOCAB4('U','S','E','R')
+#define POSITION_VOCAB VOCAB3('P','O','S')
+#define ORIENTATION_VOCAB VOCAB3('O','R','I')
+#define DMAP_VOCAB VOCAB4('D','M','A','P')
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::sig;
+
+#define MAX_USERS 10
+#define TOTAL_JOINTS 15
+
+class OpenNI2SkeletonData
+{
+public:
+	OpenNI2SkeletonData();
+	~OpenNI2SkeletonData();
+	/**
+	* This should be called each time the user skeleton data is updated
+	*
+	* @param b Bottle with the user/skeleton data
+	*/
+	void storeData(Bottle& b);
+	/**
+	* This should be called each time the kinect rgb camera data is updated
+	*
+	* @param img rgb camera image
+	*/
+	void storeData(ImageOf<PixelRgb>& img);
+	/**
+	* This should be called each time the kinect depth camera data is updated
+	*
+	* @param img depth camera image (in millimeters)
+	*/
+	void storeData(ImageOf<PixelMono16>& img);
+	/**
+	* Get a users orientation matrices array
+	*
+	* @param userID  detected user id
+	* @return matrices array with the orientation matrices (3x3)
+	*/
+	Vector* getOrientation(int userID);
+	/**
+	* Get a users position vector array
+	*
+	* @param userID  detected user id
+	* @return vectors array with the 3D position (in millimeters)
+	*/
+	Vector* getPosition(int userID);
+	/**
+	* Get a users orientation confidence array
+	*
+	* @param userID  detected user id
+	* @return doubles array with confidence values per each orientation matrix
+	*/
+	double* getOrientationConf(int userID);
+	/**
+	* Get a users position confidence array
+	*
+	* @param userID  detected user id
+	* @return doubles array with confidence values per each 3D position vector
+	*/
+	double* getPositionConf(int userID);
+	/**
+	* Get a single user status
+	*
+	* @param userID  detected user id
+	* @return int of status
+	*/
+	int getSkeletonState(int userID);
+	/**
+	* Get depth frame
+	*
+	* @return image with the depth values in (millimeters)
+	*/
+	ImageOf<PixelMono16> getDepthFrame();
+	/**
+	* Get rgb frame
+	*
+	* @return image with the rgb values
+	*/
+	ImageOf<PixelRgb> getImageFrame();
+private:
+	typedef struct USER_SKELETON {
+        nite::SkeletonState skeletonState;
+		Vector skeletonPointsPos[TOTAL_JOINTS];
+		Vector skeletonPointsOri[TOTAL_JOINTS];
+		double skeletonPosConf[TOTAL_JOINTS];
+		double skeletonOriConf[TOTAL_JOINTS];
+        bool visible;
+	}UserSkeleton;
+	UserSkeleton *userSkeleton;
+	ImageOf<PixelMono16> depthFrame;
+	ImageOf<PixelRgb> imageFrame;
+	void initUserSkeletons();
+};
+
