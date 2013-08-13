cf095f1b6ef02c1b244aff779340f3917b83e0f9
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-08-13 15:34:01 +0200

    Remove stuff marked as no longer relevant since 2010

--- src/libYARP_dev/src/PopulateDrivers.cpp
+++ src/libYARP_dev/src/PopulateDrivers.cpp
@@ -20,32 +20,6 @@
 #include <yarp/os/Bottle.h>
 #include <yarp/os/impl/PlatformStdio.h>
 #include <yarp/os/impl/PlatformStdlib.h>
-
-using namespace yarp::os::impl;
-using namespace yarp::os;
-using namespace yarp::dev;
-
-
-// NONE of this stuff is relevant anymore -- begins
-
-void addExternalDevices();
-
-//#ifndef YARP_AUTOCONF
-//#cmakedefine EMBED_DEVICE_LIBRARY_CALL
-//#cmakedefine CREATE_DEVICE_LIBRARY_MODULES
-//#cmakedefine MERGE_DEVICE_LIBRARY_MODULES
-//#endif
-
-#ifdef CREATE_DEVICE_LIBRARY_MODULES_DISABLE
-#ifdef MERGE_DEVICE_LIBRARY_MODULES
-extern "C" void add_yarpmod_devices();
-#endif
-#endif
-
-// NONE of this stuff is relevant anymore -- ends
-
-
-
 #include <yarp/dev/RemoteFrameGrabber.h>
 #include <yarp/dev/ServerFrameGrabber.h>
 #include <yarp/dev/DevicePipe.h>
@@ -54,6 +28,11 @@ extern "C" void add_yarpmod_devices();
 #include <yarp/dev/TestFrameGrabber.h>
 #include <yarp/dev/ServerSoundGrabber.h>
 #include <yarp/dev/TestMotor.h>
+
+using namespace yarp::os::impl;
+using namespace yarp::os;
+using namespace yarp::dev;
+
 
 extern DriverCreator *createRemoteControlBoard();
 extern DriverCreator *createServerControlBoard();
@@ -93,20 +72,8 @@ void Drivers::init() {
     add(new DriverCreatorOf<DeviceGroup>("group",
                                         "",
                                         "DeviceGroup"));
-                                        
+
     add(createRemoteControlBoard());
     add(createServerControlBoard());
     add(createAnalogSensorClient());
-
-#ifndef YARP_AUTOCONF
-#ifdef EMBED_DEVICE_LIBRARY_CALL
-    addExternalDevices();
-#endif
-#endif
-
-#ifdef CREATE_DEVICE_LIBRARY_MODULES
-#ifdef MERGE_DEVICE_LIBRARY_MODULES
-    add_yarpmod_devices();
-#endif
-#endif
 }
