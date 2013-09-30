873bebe827d94d3fc9d280f01363a10713bbda93
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-09-30 11:24:46 +0200

    Check if __ACE_INLINE__ is needed before other checks
    The other tests will fail during linking if this flag is required,
    therefore the returned variable will be false, even if they should be
    true.

--- conf/YarpAceCheck.cmake
+++ conf/YarpAceCheck.cmake
@@ -17,23 +17,6 @@ else(SKIP_ACE)
 
     find_package(ACE REQUIRED)
 
-    include(YarpCheckTypeSize) # regular script does not do C++ types
-    set(CMAKE_EXTRA_INCLUDE_FILES ace/config.h ace/String_Base_Const.h)
-    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
-    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
-    yarp_check_type_size(ACE_String_Base_Const::size_type SIZE_TYPE)
-    unset(CMAKE_EXTRA_INCLUDE_FILES)
-    unset(CMAKE_REQUIRED_INCLUDES)
-    unset(CMAKE_REQUIRED_LIBRARIES)
-    set(YARP_USE_ACE_STRING_BASE_CONST_SIZE_TYPE ${HAVE_SIZE_TYPE})
-
-    include(YarpCheckStructHasMember)
-    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
-    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
-    yarp_check_struct_has_member("ACE_INET_Addr" is_loopback ace/INET_Addr.h YARP_ACE_ADDR_HAS_LOOPBACK_METHOD)
-    unset(CMAKE_REQUIRED_INCLUDES)
-    unset(CMAKE_REQUIRED_LIBRARIES)
-
     # From ACE 6.0.2 on, __ACE_INLINE__ is needed in some configurations
     include (CheckCXXSourceCompiles)
     file(READ ${YARP_MODULE_DIR}/ace_test.cpp YARP_ACE_NEEDS_INLINE_CPP)
@@ -51,14 +34,6 @@ else(SKIP_ACE)
     unset(CMAKE_REQUIRED_LIBRARIES)
     unset(CMAKE_REQUIRED_DEFINITIONS)
 
-    # With migration to std::string, ACE may need a hash
-    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
-    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
-    file(READ ${YARP_MODULE_DIR}/ace_hash_test.cpp YARP_ACE_HAS_STRING_HASH_CPP)
-    check_cxx_source_compiles("${YARP_ACE_HAS_STRING_HASH_CPP}" YARP_ACE_HAS_STRING_HASH)
-    unset(CMAKE_REQUIRED_INCLUDES)
-    unset(CMAKE_REQUIRED_LIBRARIES)
-
     if(NOT YARP_ACE_COMPILES_WITHOUT_INLINE_RELEASE)
         foreach(_config ${YARP_OPTIMIZED_CONFIGURATIONS})
             string(TOUPPER ${_config} _CONFIG)
@@ -74,4 +49,30 @@ else(SKIP_ACE)
             set(CMAKE_CXX_FLAGS_${_CONFIG} "${CMAKE_CXX_FLAGS_${_CONFIG}} -D__ACE_INLINE__")
         endforeach()
     endif()
+
+    include(YarpCheckTypeSize) # regular script does not do C++ types
+    set(CMAKE_EXTRA_INCLUDE_FILES ace/config.h ace/String_Base_Const.h)
+    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
+    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
+    yarp_check_type_size(ACE_String_Base_Const::size_type SIZE_TYPE)
+    unset(CMAKE_EXTRA_INCLUDE_FILES)
+    unset(CMAKE_REQUIRED_INCLUDES)
+    unset(CMAKE_REQUIRED_LIBRARIES)
+    set(YARP_USE_ACE_STRING_BASE_CONST_SIZE_TYPE ${HAVE_SIZE_TYPE})
+
+    include(YarpCheckStructHasMember)
+    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
+    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
+    yarp_check_struct_has_member("ACE_INET_Addr" is_loopback ace/INET_Addr.h YARP_ACE_ADDR_HAS_LOOPBACK_METHOD)
+    unset(CMAKE_REQUIRED_INCLUDES)
+    unset(CMAKE_REQUIRED_LIBRARIES)
+
+    # With migration to std::string, ACE may need a hash
+    set(CMAKE_REQUIRED_INCLUDES ${ACE_INCLUDE_DIRS})
+    set(CMAKE_REQUIRED_LIBRARIES ${ACE_LIBRARIES})
+    file(READ ${YARP_MODULE_DIR}/ace_hash_test.cpp YARP_ACE_HAS_STRING_HASH_CPP)
+    check_cxx_source_compiles("${YARP_ACE_HAS_STRING_HASH_CPP}" YARP_ACE_HAS_STRING_HASH)
+    unset(CMAKE_REQUIRED_INCLUDES)
+    unset(CMAKE_REQUIRED_LIBRARIES)
+
 endif(SKIP_ACE)
