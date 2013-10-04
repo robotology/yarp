c1e6c1d2760ea7544b5ebbaf011f3f72d4b94f62
Andrea Del Prete <andre.delprete@gmail.com>
2013-10-04 16:54:51 +0200

    Add template class for fixed-size vectors. Add tests.

--- src/libYARP_sig/include/yarp/sig/Vector.h
+++ src/libYARP_sig/include/yarp/sig/Vector.h
@@ -19,8 +19,9 @@
 #include <yarp/os/Portable.h>
 #include <yarp/os/ConstString.h>
 #include <yarp/os/ManagedBytes.h>
-
+#include <yarp/os/Log.h>
 #include <yarp/sig/api.h>
+
 
 /**
 * \file Vector.h contains the definition of a Vector type 
@@ -30,6 +31,7 @@ namespace yarp {
     namespace sig {
 		class VectorBase;
         class Vector;
+        template<unsigned int _Size> class VectorN;
         template<class T> class VectorOf;
     }
 }
@@ -95,15 +97,28 @@ private:
 
 public:
     VectorOf() {
-        bytes.allocate(16*sizeof(T)); // preallocate space for 16 elements
-        bytes.setUsed(0);
-        first = 0/*NULL*/;
+        // Del Prete 3/10/2013: I prefer not to preallocate memory if not necessary
+        //bytes.allocate(16*sizeof(T)); // preallocate space for 16 elements
+        //bytes.setUsed(0);
+        first = 0;
         len = 0;
     }
 
     VectorOf(size_t size) : bytes(size*sizeof(T)) {
         bytes.setUsed(size*sizeof(T));
         _updatePointers();
+    }
+
+    VectorOf(size_t size, const T &def) : bytes(size*sizeof(T)) {
+        bytes.setUsed(size*sizeof(T));
+        _updatePointers();
+        for (size_t i=0; i<size; i++) { (*this)[i] = def; }
+    }
+
+    VectorOf(size_t size, const T *v) : bytes(size*sizeof(T)) {
+        bytes.setUsed(size*sizeof(T));
+        _updatePointers();
+        for (size_t i=0; i<size; i++) { (*this)[i] = v[i]; }
     }
 
     VectorOf(const VectorOf &r) : VectorBase() {
@@ -152,12 +167,14 @@ public:
 
     void resize(size_t size, const T&def)
     {
-        /* 
-        bytes.allocateOnNeed(size*sizeof(T),size*sizeof(T));
-        bytes.setUsed(size*sizeof(T));
-        _updatePointers(); */
         resize(size); 
         for (size_t i=0; i<size; i++) { (*this)[i] = def; }
+    }
+
+    virtual void assign(size_t size, const T *v)
+    {
+        resize(size);
+        for (size_t i=0; i<size; i++) { (*this)[i] = v[i]; }
     }
 
     inline void push_back (const T &elem)
@@ -243,6 +260,7 @@ public:
 */
 class YARP_sig_API yarp::sig::Vector : public yarp::os::Portable
 {
+protected:
     void *gslData;
     VectorOf<double> storage;
 
@@ -267,9 +285,8 @@ public:
     * @param s the size
     * @param def a default value used to fill the vector
     */
-    explicit Vector(size_t s, const double &def)
+    explicit Vector(size_t s, const double &def): storage(s, def)
     {
-        storage.resize(s,def);
         allocGslData();
         updateGslData();
     }
@@ -288,8 +305,11 @@ public:
     /**
     * Copy constructor.
     */
-    Vector(const Vector &r): yarp::os::Portable(), storage(r.storage)
+    Vector(const Vector &r): storage(r.size(), r.data())    
     {
+        // DelPrete 4/10/13
+        // Note: since the introduction of VectorN I can no longer do "storage(r.storage)"
+        // because if r is a VectorN then the vector data are located in storageStatic.
         allocGslData();
         updateGslData();
     }
@@ -297,27 +317,33 @@ public:
     /**
     * Copy operator;
     */
-    const Vector &operator=(const Vector &r);
+    virtual const Vector &operator=(const Vector &r);
+
+    /**
+    * Set all elements of the vector to a scalar.
+    */
+    virtual const Vector &operator=(double v)
+    {
+        double *tmp=data();
+        for(size_t k=0; k<size(); k++)
+            tmp[k]=v;
+        return *this;
+    }
 
     /**
     * Resize the vector, (warning: deprecated, use resize) here
     * to maintain compatibility with the old Vector class.
     * @param s the new size
     */
-    void size(size_t s)
-    {
-        Vector::resize(s);
-        updateGslData();
-    }
+    virtual void size(size_t s){ Vector::resize(s); }
 
     /**
     * Resize the vector.
     * @param s the new size
     */
-    void resize(size_t s)
+    virtual void resize(size_t s)
     {
         storage.resize(s);
-        //allocGslData();
         updateGslData();
     }
 
@@ -326,14 +352,13 @@ public:
     * @param size the new size
     * @param def a default value used to fill the vector
     */
-    void resize(size_t size, const double &def)
+    virtual void resize(size_t size, const double &def)
     {
         storage.resize(size, def);
-        //allocGslData();
         updateGslData();
     }
 
-    inline size_t size() const
+    virtual inline size_t size() const
     { return storage.size();}
 
     /**
@@ -341,12 +366,12 @@ public:
     * @return the length of the vector.
     */
     inline size_t length() const
-    { return storage.size();}
+    { return size();}
 
     /**
     * Zero the elements of the vector.
     */
-    void zero();
+    virtual void zero();
 
     /**
     * Creates a string object containing a text representation of the object. Useful for printing.
@@ -357,7 +382,7 @@ public:
     * Warning: the string format might change in the future. This method
     * is here to ease debugging.
     */
-    yarp::os::ConstString toString(int precision=-1, int width=-1) const;
+    virtual yarp::os::ConstString toString(int precision=-1, int width=-1) const;
 
 
     /**
@@ -366,7 +391,27 @@ public:
     * in the subvector. The indexes are checked: if wrong, a null vector is
     * returned.
     */
-    Vector subVector(unsigned int first, unsigned int last) const;
+    virtual Vector subVector(unsigned int first, unsigned int last) const
+    {
+        Vector ret;
+        if((first<=last)&&((int)last<(int)size()))
+        {
+            ret.resize(last-first+1);
+            for(unsigned int k=first; k<=last; k++)
+                ret[k-first]=(*this)[k];
+        }
+        return ret;
+    }
+
+    template<unsigned int _size>
+    VectorN<_size> subVector(unsigned int first) const
+    {
+        VectorN<_size> ret;
+        if(first+_size < size())
+            for(unsigned int k=0; k<_size; k++)
+                ret[k]=(*this)[first+k];
+        return ret;
+    }
 
     /**
      * Set a portion of this vector with the values of the specified vector.
@@ -377,18 +422,20 @@ public:
      * @param v vector containing the values to set
      * @return true if the operation succeeded, false otherwise
      */
-    bool setSubvector(int position, const Vector &v);
-
-    /**
-    * Set all elements of the vector to a scalar.
-    */
-    const Vector &operator=(double v);
+    virtual bool setSubvector(int position, const Vector &v)
+    {    
+        if(position+v.size() > size())
+            return false;
+        for(size_t i=0;i<v.size();i++)
+            (*this)[position+i] = v(i);
+        return true;
+    }
 
     /**
     * Return a pointer to the first element of the vector.
     * @return a pointer to double.
     */
-    inline double *data()
+    virtual inline double *data()
     { return storage.getFirst(); }
 
     /**
@@ -396,18 +443,35 @@ public:
     * const version
     * @return a (const) pointer to double.
     */
-    inline const double *data() const
+    virtual inline const double *data() const
     { return storage.getFirst();}
 
     /**
     * True iff all elements of 'a' match all element of 'b'.
     */
-    bool operator==(const yarp::sig::Vector &r) const;
+    virtual bool operator==(const yarp::sig::Vector &r) const
+    {
+        //check dimensions first
+        size_t c=size();
+        if (c!=r.size())
+            return false;
+
+        const double *tmp1=data();
+        const double *tmp2=r.data();
+
+        while(c--)
+        {
+            if (*tmp1++!=*tmp2++)
+                return false;
+        }
+
+        return true;
+    }
 
     /**
     * Push a new element in the vector: size is changed
     */
-    inline void push_back (const double &elem)
+    virtual inline void push_back (const double &elem)
     {
         storage.push_back(elem);
         updateGslData();
@@ -416,7 +480,7 @@ public:
     /**
     * Pop an element out of the vector: size is changed
     */
-    inline void pop_back (void)
+    virtual inline void pop_back (void)
     {
         storage.pop_back();
         updateGslData();
@@ -426,20 +490,20 @@ public:
     * Return a void pointer to a gsl compatible structure.
     * This pointer can be safely cast to a gsl_vector*.
     */
-    void *getGslVector();
+    virtual void *getGslVector(){ return gslData; }
     
     /**
     * Return a void pointer to a gsl compatible structure.
     * This pointer can be safely cast to a gsl_vector*.
     */
-    const void *getGslVector() const;
+    virtual const void *getGslVector() const{ return gslData; }
 
         /**
     * Single element access, no range check.
     * @param i the index of the element to access.
     * @return a reference to the requested element.
     */
-    inline double &operator[](size_t i)
+    virtual inline double &operator[](size_t i)
     {return storage[i];}
 
     /**
@@ -447,7 +511,7 @@ public:
     * @param i the index of the element to access.
     * @return a reference to the requested element.
     */
-    inline const double &operator[](size_t i) const
+    virtual inline const double &operator[](size_t i) const
     {return storage[i];}
 
     /**
@@ -455,7 +519,7 @@ public:
     * @param i the index of the element to access.
     * @return a reference to the requested element.
     */
-    inline double &operator()(size_t i)
+    virtual inline double &operator()(size_t i)
     {return storage(i);}
 
     /**
@@ -463,14 +527,14 @@ public:
     * @param i the index of the element to access.
     * @return a reference to the requested element.
     */
-    inline const double &operator()(size_t i) const
+    virtual inline const double &operator()(size_t i) const
     {return storage(i);}
 
     /**
     * Clears out the vector, it does not reallocate
     * the buffer, but just sets the dynamic size to 0.
     */
-    void clear ()
+    virtual void clear ()
     { storage.clear();}
 
     ///////// Serialization methods
@@ -486,6 +550,188 @@ public:
     */
     virtual bool write(yarp::os::ConnectionWriter& connection);
 };
+
+
+/** Vector of constant size (known at compile time). 
+*/
+template<unsigned int _Size>
+class YARP_sig_API yarp::sig::VectorN: public yarp::sig::Vector
+{
+protected:
+    double  storageStatic[_Size];   // static memory of the vector
+
+public:
+    VectorN() { updateGslData(); }
+
+    /**
+    * Build a vector and initialize it with def.
+    * @param def a default value used to fill the vector
+    */
+    explicit VectorN(const double &def){ *this = def; updateGslData(); }
+
+    /**
+    * Copy constructors.
+    */
+    VectorN(const double *p){           *this = p; updateGslData(); }
+    VectorN(const VectorN<_Size> &r) {  *this = r; updateGslData(); }
+    VectorN(const Vector  &r) {         *this = r; updateGslData(); }
+
+    /**
+    * Set all elements of the vector to the specified scalar value v.
+    */
+    const VectorN &operator=(double v)
+    {
+        for(size_t k=0; k<_Size; k++)
+            storageStatic[k] = v;
+        return *this;
+    }
+
+    /**
+    * Copy the values of the specified vector v into this vector.
+    */
+    const VectorN &operator=(double *v)
+    {
+        for(size_t k=0; k<_Size; k++)
+            storageStatic[k] = v[k];
+        return *this;
+    }
+
+    /** If r has a size different from this vector' size, the program terminates. */
+    const VectorN<_Size> &operator=(const Vector &r)
+    {
+        YARP_ASSERT(_Size == r.size())
+        memcpy(data(), r.data(), sizeof(double)*_Size);
+        return *this;
+    }
+
+    yarp::sig::Vector subVector(unsigned int first, unsigned int last) const
+    {
+        YARP_ASSERT(last>=first && last<_Size);
+        yarp::sig::Vector ret(last-first+1);
+        for(unsigned int k=first; k<=last; k++)
+            ret[k-first] = storageStatic[k];
+        return ret;
+    }
+
+    template<unsigned int _s>
+    VectorN<_s> subVector(unsigned int first) const
+    {
+        YARP_ASSERT(first+_s < _Size);
+        VectorN<_s> ret;
+        for(unsigned int k=0; k<_s; k++)
+            ret[k] = (*this)[first+k];
+        return ret;
+    }
+
+    /**
+     * Set a portion of this vector with the values of the specified vector.
+     * If the specified vector v is to big the method does not resize the vector,
+     * but return false.
+     * @param position index of the first value to set
+     * @param v vector containing the values to set
+     * @return true if the operation succeeded, false otherwise
+     */
+    bool setSubvector(int position, const Vector &v)
+    {    
+        if(position+v.size() > _Size)
+            return false;
+        for(size_t i=0;i<v.size();i++)
+            storageStatic[position+i] = v(i);
+        return true;
+    }
+
+    /**
+    * Resize the vector, (warning: deprecated, use resize) here to maintain compatibility with the old Vector class.
+    * @param s the new size
+    */
+    void size(size_t s){ VectorN::resize(s); }
+
+    /**
+    * Resize the vector.
+    * @param s the new size
+    */
+    void resize(size_t s){ YARP_ASSERT(s==_Size); }
+
+    /**
+    * Resize the vector, if the vector is not empty preserve old content.
+    * @param size the new size
+    * @param def a default value used to fill the vector
+    */
+    void resize(size_t size, const double &def){ YARP_ASSERT(size==_Size); *this = def; }
+
+    inline size_t size() const { return _Size;}
+    inline size_t length() const { return _Size;}
+
+    /**
+    * Return a pointer to the first element of the vector.
+    * @return a pointer to double.
+    */
+    inline double *data()
+    { return storageStatic; }
+
+    /**
+    * Return a pointer to the first element of the vector,
+    * const version
+    * @return a (const) pointer to double.
+    */
+    inline const double *data() const
+    { return storageStatic;}
+
+        /**
+    * Single element access, no range check.
+    * @param i the index of the element to access.
+    * @return a reference to the requested element.
+    */
+    inline double &operator[](size_t i)
+    {return storageStatic[i];}
+
+    /**
+    * Single element access, no range check, const version.
+    * @param i the index of the element to access.
+    * @return a reference to the requested element.
+    */
+    inline const double &operator[](size_t i) const
+    {return storageStatic[i];}
+
+    /**
+    * Single element access, no range check.
+    * @param i the index of the element to access.
+    * @return a reference to the requested element.
+    */
+    inline double &operator()(size_t i)
+    {return storageStatic[i];}
+
+    /**
+    * Single element access, no range check, const version.
+    * @param i the index of the element to access.
+    * @return a reference to the requested element.
+    */
+    inline const double &operator()(size_t i) const
+    {return storageStatic[i];}
+
+    /**
+    * Operation now allowed on a constant-size vector.
+    */
+    inline void push_back (const double &elem){ YARP_ASSERT(false); }
+
+    /**
+    * Operation now allowed on a constant-size vector.
+    */
+    inline void pop_back (void){ YARP_ASSERT(false); }
+
+    /**
+    * Operation now allowed on a constant-size vector.
+    */
+    void clear (){ YARP_ASSERT(false); }
+};
+
+typedef yarp::sig::VectorN<1> Vector1;
+typedef yarp::sig::VectorN<2> Vector2;
+typedef yarp::sig::VectorN<3> Vector3;
+typedef yarp::sig::VectorN<4> Vector4;
+typedef yarp::sig::VectorN<5> Vector5;
+typedef yarp::sig::VectorN<6> Vector6;
+typedef yarp::sig::VectorN<7> Vector7;
 
 #endif
 
--- src/libYARP_sig/src/Vector.cpp
+++ src/libYARP_sig/src/Vector.cpp
@@ -23,13 +23,17 @@ using namespace yarp::os;
 #define RES(v) ((PlatformVector<T> *)v)
 #define RES_ITERATOR(v) ((PLATFORM_VECTOR_ITERATOR(double) *)v)
 
-/// network stuff
-#include <yarp/os/NetInt32.h>
 #include <yarp/os/begin_pack_for_net.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/NetInt32.h>
+
+#include <yarp/os/impl/PlatformVector.h>
+#include <yarp/os/impl/PlatformStdio.h>
+#include <yarp/os/impl/PlatformStdlib.h>
+#include <yarp/os/impl/Logger.h>
 
 #include <yarp/gsl_compatibility.h>
 
-///////////////////
 
 class VectorPortContentHeader
 {
@@ -39,6 +43,7 @@ public:
 } PACKED_FOR_NET;
 
 #include <yarp/os/end_pack_for_net.h>
+
 
 bool VectorBase::read(yarp::os::ConnectionReader& connection) {
     // auto-convert text mode interaction
@@ -83,11 +88,36 @@ bool VectorBase::write(yarp::os::ConnectionWriter& connection) {
     return !connection.isError();
 }
 
+/***************************************************************************************************************************/
+/******************************************************** VECTOR ***********************************************************/
+/***************************************************************************************************************************/
 
-/**
-* Quick implementation, space for improvement.
-*/
-ConstString Vector::toString(int precision, int width) const
+Vector::Vector(size_t s, const double *p)
+{
+    storage.assign(s, p);
+    allocGslData();
+    updateGslData();
+}
+
+const Vector &Vector::operator=(const Vector &r)
+{
+    if(size() == r.size())
+        memcpy(data(), r.data(), sizeof(double)*size());
+    else
+    {
+        // DelPrete 04/10/2013:
+        // If r is a VectorN then it is wrong to copy the content of storage
+        // because the data are inside storageStatic! Solution: use data() rather than storage.
+        //storage=r.storage;
+        storage.assign(r.size(), r.data());
+        updateGslData();
+    }
+    return *this;
+}
+
+void Vector::zero(){ ACE_OS::memset(data(), 0, sizeof(double)*size()); }
+
+yarp::os::ConstString Vector::toString(int precision, int width) const
 {
     ConstString ret = "";
     size_t c;
@@ -109,94 +139,63 @@ ConstString Vector::toString(int precision, int width) const
     return ret;
 }
 
-Vector Vector::subVector(unsigned int first, unsigned int last) const
-{
-    Vector ret;
-    if((first<=last)&&((int)last<(int)storage.size()))
-    {
-        ret.resize(last-first+1);
-        for(unsigned int k=first; k<=last; k++)
-            ret[k-first]=storage[k];
-    }
-    return ret;
-}
 
-bool Vector::setSubvector(int position, const Vector &v)
-{    
-    if(position+v.size() > storage.size())
+/*********************************************************** SERIALIZATION METHODS *******************************************/
+
+
+/*
+* Read vector from a connection.
+* return true iff a vector was read correctly
+*/
+bool Vector::read(yarp::os::ConnectionReader& connection)
+{
+    // auto-convert text mode interaction
+    connection.convertTextMode();
+    VectorPortContentHeader header;
+    bool ok = connection.expectBlock((char*)&header, sizeof(header));
+    if (!ok) return false;
+    if (header.listLen > 0 && 
+        header.listTag == BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE) {
+        if (size() != (size_t)(header.listLen))
+            resize(header.listLen);
+        
+        int k=0;
+        for (k=0;k<header.listLen;k++)
+            (*this)[k]=connection.expectDouble();
+    } else {
         return false;
-    for(size_t i=0;i<v.size();i++)
-        storage[position+i] = v(i);
-    return true;
-}
-
-const Vector &Vector::operator=(const Vector &r)
-{
-    if(storage.size() == r.storage.size())
-    {
-        ACE_OS::memcpy(storage.getFirst(), r.storage.getFirst(), sizeof(double)*storage.size());
-    }
-    else
-    {
-        storage=r.storage;
-        updateGslData();
-    }
-    return *this;
-}
-
-Vector::Vector(size_t s, const double *p)
-{
-    storage.resize(s);
-
-    ACE_OS::memcpy(storage.getFirst(), p, sizeof(double)*s);
-
-    allocGslData();
-    updateGslData();
-}
-
-void Vector::zero()
-{
-    ACE_OS::memset(storage.getFirst(), 0, sizeof(double)*storage.size());
-}
-
-const Vector &Vector::operator=(double v)
-{
-    double *tmp=storage.getFirst();
-
-    for(size_t k=0; k<length(); k++)
-        tmp[k]=v;
-
-    return *this;
-}
-
-bool Vector::operator==(const yarp::sig::Vector &r) const
-{
-    //check dimensions first
-    size_t c=size();
-    if (c!=r.size())
-        return false;
-
-    const double *tmp1=data();
-    const double *tmp2=r.data();
-
-    while(c--)
-    {
-        if (*tmp1++!=*tmp2++)
-            return false;
     }
 
-    return true;
+    return !connection.isError();
 }
 
-void *Vector::getGslVector()
+/**
+* Write vector to a connection.
+* return true iff a vector was written correctly
+*/
+bool Vector::write(yarp::os::ConnectionWriter& connection)
 {
-    return gslData;
+    VectorPortContentHeader header;
+
+    header.listTag = BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE;
+    header.listLen = (int)size();
+
+    connection.appendBlock((char*)&header, sizeof(header));
+
+    int k=0;
+    for (k=0;k<header.listLen;k++)
+        connection.appendDouble((*this)[k]);
+
+    // if someone is foolish enough to connect in text mode,
+    // let them see something readable.
+    connection.convertTextMode();
+
+    return !connection.isError();
 }
 
-const void *Vector::getGslVector() const
-{
-    return gslData;
-}
+
+/****************************************************** PRIVATE METHODS ***********************************************/
+
 
 void Vector::allocGslData()
 {
@@ -227,52 +226,17 @@ void Vector::freeGslData()
 
 void Vector::updateGslData()
 {
-    gsl_vector *tmp=static_cast<gsl_vector *>(gslData);
-    tmp->block->data=Vector::data();
-    tmp->data=tmp->block->data;
-    tmp->block->size=Vector::size();
-    tmp->owner=1;
-    tmp->stride=1;
-    tmp->size=tmp->block->size;
+    gsl_vector *tmp = static_cast<gsl_vector *>(gslData);
+    tmp->block->data = data();
+    tmp->data = tmp->block->data;
+    tmp->block->size = size();
+    tmp->owner = 1;
+    tmp->stride = 1;
+    tmp->size = tmp->block->size;
 }
 
-bool Vector::read(yarp::os::ConnectionReader& connection) {
-    // auto-convert text mode interaction
-    connection.convertTextMode();
-    VectorPortContentHeader header;
-    bool ok = connection.expectBlock((char*)&header, sizeof(header));
-    if (!ok) return false;
-    if (header.listLen > 0 && 
-        header.listTag == BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE) {
-        if (size() != (size_t)(header.listLen))
-            resize(header.listLen);
-        
-        int k=0;
-        for (k=0;k<header.listLen;k++)
-            (*this)[k]=connection.expectDouble();
-    } else {
-        return false;
-    }
 
-    return !connection.isError();
-}
 
-bool Vector::write(yarp::os::ConnectionWriter& connection) {
-    VectorPortContentHeader header;
-
-    header.listTag = BOTTLE_TAG_LIST + BOTTLE_TAG_DOUBLE;
-    header.listLen = (int)size();
-
-    connection.appendBlock((char*)&header, sizeof(header));
-
-    int k=0;
-    for (k=0;k<header.listLen;k++)
-        connection.appendDouble((*this)[k]);
-
-    // if someone is foolish enough to connect in text mode,
-    // let them see something readable.
-    connection.convertTextMode();
-
-    return !connection.isError();
-}
-
+/***************************************************************************************************************************/
+/******************************************************** VECTORN **********************************************************/
+/***************************************************************************************************************************/
