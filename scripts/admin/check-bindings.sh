9c6a3564658a36e1b6b22f5cf75f5168ca03a0d2
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-10-24 18:31:13 +0200

    [bindings] add tests for swig versions 1.3.39 1.3.40

--- scripts/admin/check-bindings.sh
+++ scripts/admin/check-bindings.sh
@@ -6,22 +6,26 @@ set -e
 # You'll probably need to remove your global swig version to prevent
 # cmake finding it later.
 swig_base="/cache/swig"
-swig_versions="2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11"
+swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11"
 
 if [ "$1" = "swig" ]; then
     echo "Compiling all swig versions"
-    if [ ! -e swig.spec ]; then
+    if [ ! -e swig.spec.in ]; then
 	echo "Please get swig git repo https://github.com/swig/swig and run from repo"
 	exit 1
     fi
     for f in $swig_versions; do
-	echo $f
-	git checkout rel-$f
-	./autogen.sh
-	mkdir -p $f
-	./configure --prefix=$swig_base/$f
-	make
-	make install
+	if [ ! -e $swig_base/$f ] ; then
+	    echo $f
+	    git clean -f -d
+	    git checkout .
+	    git checkout rel-$f
+	    ./autogen.sh
+	    ./configure --prefix=$swig_base/$f
+	    make clean
+	    make
+	    make install
+	fi
     done
     exit 0
 fi
@@ -37,6 +41,11 @@ if [ ! -e "$YARP_ROOT/conf/YarpVersion.cmake" ] ; then
 fi
 
 SUPPORTED_LANGUAGES="LUA PYTHON JAVA RUBY CSHARP PERL TCL"
+
+echo $1 | grep -q "[.]" && {
+    swig_versions=$1
+    shift
+}
 
 if [ ! "$1" = "" ]; then
     SUPPORTED_LANGUAGES="$*"
@@ -57,7 +66,7 @@ for lang in $SUPPORTED_LANGUAGES; do
 	echo "* In $PWD"
 	cmake -DCREATE_$lang=TRUE -DPREPARE_CLASS_FILES=TRUE $search_path $YARP_ROOT/bindings
 	grep SWIG_EXECUTABLE CMakeCache.txt | grep "/cache/swig/$swig_ver/"
-	make
+	make VERBOSE=1
 	lc=`echo $lang | tr '[:upper:]' '[:lower:]'`
 	run=$YARP_ROOT/bindings/tests/$lc/run.sh
 	if [ -e $run ]; then
