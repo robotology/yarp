d35ddd07c25fc8aed7cea58defcc81613e425f62
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-11-05 11:56:06 +0100

    [Cleanup] Clean testing scripts
    Use CTest instead of Darts

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,14 +1,11 @@
-# Copyright (C) 2010 RobotCub Consortium
-# Authors: Paul Fitzpatrick, Lorenzo Natale
-# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
-
+# From: http://www.cmake.org/Wiki/CMake_Testing_With_CTest
 
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
-##   INCLUDE(Dart)
+##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "YARP")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
@@ -16,4 +13,3 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "dashboard.icub.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=YARP")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
