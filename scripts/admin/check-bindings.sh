4ef51ed90e7ebf4d3d5f35b4bbf4e87519f57eb8
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-01-02 23:12:27 +0100

    remind myself how to run swig tests

--- scripts/admin/check-bindings.sh
+++ scripts/admin/check-bindings.sh
@@ -8,10 +8,27 @@ set -e
 swig_base="/cache/swig"
 swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11"
 
+SUPPORTED_LANGUAGES="LUA PYTHON JAVA RUBY CSHARP PERL TCL"
+
+if [ "$1" = "" ]; then
+    echo "Possible SWIG versions are $swig_versions"
+    echo "Possible languages are $SUPPORTED_LANGUAGES"
+    echo "To test a specific SWIG version / language combination, call as e.g.:"
+    echo "  ./scripts/admin/check-bindings.sh 2.0.11 LUA"
+    echo "or to test many SWIG versions, call as:"
+    echo "  ./scripts/admin/check-bindings.sh LUA"
+    echo "Before anything works you need to compile many SWIG versions:"
+    echo "  create a directory /cache/swig writeable by you"
+    echo "  get git repo https://github.com/swig/swig"
+    echo "  then run \"..../scripts/admin/check-bindings.sh swig\" from that directory"
+    echo "  you should end up with lots of swig version in /cache/swig/N.N.N"
+    exit 0
+fi
+
 if [ "$1" = "swig" ]; then
     echo "Compiling all swig versions"
     if [ ! -e swig.spec.in ]; then
-	echo "Please get swig git repo https://github.com/swig/swig and run from repo"
+	echo "Please get swig git repo https://github.com/swig/swig and run this script from the root directory of that repo"
 	exit 1
     fi
     for f in $swig_versions; do
@@ -39,8 +56,6 @@ if [ ! -e "$YARP_ROOT/conf/YarpVersion.cmake" ] ; then
     echo "Please run either from \$YARP_ROOT or with \$YARP_ROOT set"
     exit 1
 fi
-
-SUPPORTED_LANGUAGES="LUA PYTHON JAVA RUBY CSHARP PERL TCL"
 
 echo $1 | grep -q "[.]" && {
     swig_versions=$1
@@ -76,7 +91,11 @@ for lang in $SUPPORTED_LANGUAGES; do
 	lang_var=YARP_${lang}_FLAGS
 	lang_flags=${!lang_var}
 	cmake -DCREATE_$lang=TRUE $lang_flags $search_path $YARP_ROOT/bindings
-	grep SWIG_EXECUTABLE CMakeCache.txt | grep "/cache/swig/$swig_ver/"
+	grep SWIG_EXECUTABLE CMakeCache.txt | grep "/cache/swig/$swig_ver/" || {
+	    echo "CMake is picking up wrong SWIG version"
+	    echo "Remove this: `grep SWIG_EXECUTABLE CMakeCache.txt`"
+	    exit 1
+	}
 	make VERBOSE=1 || { 
 	    echo "make failed for $lang $swig_ver" >> $log
 	    ok=false
