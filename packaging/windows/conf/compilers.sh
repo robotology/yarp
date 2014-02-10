2d2de56eb953867ce7686fb7282e0254aaf9c15c
Matteo Brunettini <matteo.brunettini@iit.it>
2014-02-10 00:01:46 -0800

    added yarp 2.3.61 packaging configuration file

--- packaging/windows/conf/compilers.sh
+++ packaging/windows/conf/compilers.sh
@@ -1,6 +1,4 @@
-
-#compilers="v9 v10 v11 v8" # list of compilers
-compilers="v10"
+compilers="v9 v10 v11" # list of compilers
 compiler_v11_family="msvc"
 compiler_v11_loader="F:/VS11/VC/vcvarsall.bat"
 compiler_v11_variants="x86 x86_amd64"
@@ -10,9 +8,10 @@ compiler_v10_variants="x86 x86_amd64" # x86, 64-bit, etc
 compiler_v9_family="msvc"
 compiler_v9_loader="F:/VS9/VC/vcvarsall.bat"
 compiler_v9_variants="x86 x86_amd64"
-compiler_v8_family="msvc"
-compiler_v8_loader="F:/VS8/VC/vcvarsall.bat"
-compiler_v8_variants="x86"
+# V8 in not supported anymore
+#compiler_v8_family="msvc"
+#compiler_v8_loader="F:/VS8/VC/vcvarsall.bat"
+#compiler_v8_variants="x86"
 compiler_mingw_variants="x86"
 compiler_mingw_family="mingw"
 compiler_mingw_loader="F:/MinGW"
