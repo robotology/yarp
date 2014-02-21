ff34d26d35c475b4dc45342efe3b72989995625c
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-02-21 21:05:32 +0100

    Partially re-write FindReadline.cmake
    Update and cleanup dependent files

--- conf/FindReadline.cmake
+++ conf/FindReadline.cmake
@@ -1,27 +1,74 @@
-# Try to find readline, a library for easy editing of command lines.
-# Variables used by this module:
-# READLINE_ROOT_DIR - Readline root directory
-# Variables defined by this module:
-# READLINE_FOUND - system has Readline
-# READLINE_INCLUDE_DIR - the Readline include directory (cached)
-# READLINE_INCLUDE_DIRS - the Readline include directories
-# (identical to READLINE_INCLUDE_DIR)
-# READLINE_LIBRARY - the Readline library (cached)
+# Try to find GNU Readline, a library for easy editing of command lines.
+# Once done this will define the following variables:
+#
+# Readline_FOUND         - System has GNU Readline
+# Readline_INCLUDE_DIRS  - GNU Readline include directory
+# Readline_LIBRARIES     - GNU Readline libraries
+# Readline_DEFINITIONS   - Additional compiler flags for GNU Readline
+# Readline_VERSION       - GNU Readline version
+# Readline_MAJOR_VERSION - GNU Readline major version
+# Readline_MINOR_VERSION - GNU Readline minor version
+#
+# Environment variables used to locate the GNU Readline library:
+# READLINE_DIR - Readline root directory
+#
+# Cached variables used to locate the GNU Readline library:
+# Readline_INCLUDE_DIR - the Readline include directory
+# Readline_LIBRARY_RELEASE - GNU Readline library (release)
+# Readline_LIBRARY_DEBUG - GNU Readline library (debug)
 
 # Copyright (C) 2014  iCub Facility, Istituto Italiano di Tecnologia
-# Author: Elena Ceseracciu <elena.ceseracciu@iit.it>
+# Authors: Elena Ceseracciu <elena.ceseracciu@iit.it>
+#          Daniele E. Domenichelli <daniele.domenichelli@iit.it>
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
 
-find_path(READLINE_INCLUDE_DIR readline/readline.h
-          HINTS ${READLINE_DIR} ENV READLINE_DIR PATH_SUFFIXES include)
-find_library(READLINE_LIBRARY readline
-          HINTS ${READLINE_DIR} ENV READLINE_DIR PATH_SUFFIXES lib)
-mark_as_advanced(READLINE_INCLUDE_DIR READLINE_LIBRARY)
-
+include(SelectLibraryConfigurations)
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Readline DEFAULT_MSG READLINE_LIBRARY READLINE_INCLUDE_DIR)
 
-set(READLINE_INCLUDE_DIRS ${READLINE_INCLUDE_DIR})
+# Include directories
+find_path(Readline_INCLUDE_DIR
+          NAMES readline/readline.h
+          HINTS ENV Readline_DIR
+          PATH_SUFFIXES include)
+mark_as_advanced(Readline_INCLUDE_DIR)
+set(Readline_INCLUDE_DIRS ${Readline_INCLUDE_DIR})
 
 
+# Libraries
+find_library(Readline_LIBRARY_RELEASE
+             NAMES readline
+             HINTS ENV Readline_DIR
+             PATH_SUFFIXES lib)
+find_library(Readline_LIBRARY_DEBUG
+             NAMES readlined
+             HINTS ENV Readline_DIR
+             PATH_SUFFIXES lib)
+mark_as_advanced(Readline_LIBRARY_RELEASE
+                 READ_LIBRARY_DEBUG)
+select_library_configurations(Readline)
+
+
+# Version
+set(Readline_VERSION Readline_VERSION-NOTFOUND)
+if (Readline_INCLUDE_DIR)
+  if(EXISTS "${Readline_INCLUDE_DIR}/readline/readline.h")
+    file(STRINGS "${Readline_INCLUDE_DIR}/readline/readline.h" _Readline_HEADER_CONTENTS REGEX "#define RL_VERSION_[A-Z]+")
+    string(REGEX REPLACE ".*#define RL_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" Readline_VERSION_MAJOR "${_Readline_HEADER_CONTENTS}")
+    string(REGEX REPLACE ".*#define RL_VERSION_MINOR[ \t]+([0-9]+).*" "\\1" Readline_VERSION_MINOR "${_Readline_HEADER_CONTENTS}")
+    set(Readline_VERSION ${Readline_VERSION_MAJOR}.${Readline_VERSION_MINOR})
+    unset(_Readline_HEADER_CONTENTS)
+  endif()
+endif()
+
+
+# Check required variables
+find_package_handle_standard_args(Readline FOUND_VAR Readline_FOUND
+                                           REQUIRED_VARS Readline_LIBRARY Readline_INCLUDE_DIR
+                                           VERSION_VAR Readline_VERSION)
+
+# Set package properties if FeatureSummary was included
+if(COMMAND set_package_properties)
+    set_package_properties(Readline PROPERTIES DESCRIPTION "A software library that provides line-editing and history capabilities"
+                                               URL "http://cnswww.cns.cwru.edu/php/chet/readline/rltop.html")
+endif()
