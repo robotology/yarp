b9b98b2afce3c36bc952827242a382d33820d86e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-03-25 21:10:05 +0100

    Remove libraries that user should not link directly from YARP_LIBRARIES

--- conf/YarpDescribe.cmake
+++ conf/YarpDescribe.cmake
@@ -8,14 +8,14 @@ include(GNUInstallDirs)
 # ("uninstalled") use of YARP.
 get_property(YARP_INCLUDE_DIRS GLOBAL PROPERTY YARP_TREE_INCLUDE_DIRS)
 get_property(YARP_LINK_DIRS GLOBAL PROPERTY YARP_TREE_LINK_DIRS)
-get_property(YARP_LIBRARIES GLOBAL PROPERTY YARP_LIBS)
+get_property(YARP_LIBS GLOBAL PROPERTY YARP_LIBS)
 # Oops, cannot use YARP_DEFINES name, conflicts with an old variable
 # that might be lurking in CMakeCache.txt as people upgrade.  Insert
 # an "_ALL_" for now.
 get_property(YARP_ALL_DEFINES GLOBAL PROPERTY YARP_DEFS)
 get_property(YARP_HAS_MATH_LIB GLOBAL PROPERTY YARP_HAS_MATH_LIB)
 message(STATUS "In-tree includes: ${YARP_INCLUDE_DIRS}")
-message(STATUS "YARP libraries: ${YARP_LIBRARIES}")
+message(STATUS "YARP libraries: ${YARP_LIBS}")
 
 set(YARP_HAS_IDL FALSE)
 if(ENABLE_yarpidl_thrift)
@@ -29,6 +29,20 @@ endif(ENABLE_yarpidl_thrift)
 set(YARP_DEPENDENCY_FILE ${CMAKE_BINARY_DIR}/YARPDependencies.cmake)
 set(YARP_DEPENDENCY_FILENAME YARPDependencies.cmake)
 set(YARP_BINDINGS ${CMAKE_SOURCE_DIR}/bindings)
+
+# Filter out from YARP_LIBRARIES all the plugins, yarpmod, yarpcar
+# and all the other libraries that should not be linked by the user
+set(YARP_LIBRARIES)
+foreach(lib ${YARP_LIBS})
+  if(NOT "${lib}" MATCHES "carrier$" AND
+     NOT "${lib}" MATCHES "^yarp_" AND
+     NOT "${lib}" STREQUAL "yarpcar" AND
+     NOT "${lib}" STREQUAL "yarpmod" AND
+     NOT "${lib}" STREQUAL "YARP_wire_rep_utils" AND
+     NOT "${lib}" STREQUAL "YARP_manager")
+    list(APPEND YARP_LIBRARIES ${lib})
+  endif()
+endforeach()
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/template/YARPConfig.cmake.in
                ${CMAKE_BINARY_DIR}/YARPConfig.cmake @ONLY)
@@ -61,7 +75,7 @@ install(FILES ${CMAKE_BINARY_DIR}/YARPConfigForInstall.cmake RENAME YARPConfig.c
 install(FILES ${CMAKE_BINARY_DIR}/YARPConfigVersion.cmake COMPONENT configuration DESTINATION ${VERSIONED_LIB})
 install(EXPORT YARP COMPONENT configuration DESTINATION ${VERSIONED_LIB})
 
-foreach(lib ${YARP_LIBRARIES})
+foreach(lib ${YARP_LIBS})
     set_target_properties(${lib} PROPERTIES VERSION ${YARP_VERSION}
                                             SOVERSION ${YARP_GENERIC_SOVERSION})
 endforeach(lib)
