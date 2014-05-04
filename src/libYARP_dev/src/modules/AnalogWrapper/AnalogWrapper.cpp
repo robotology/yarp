90fe3a236be57fee0150d507b3a476187300171e
Lorenzo Natale <lorenzo.natale@iit.it>
2014-05-04 23:14:26 +0200

    fix warning

--- src/libYARP_dev/src/modules/AnalogWrapper/AnalogWrapper.cpp
+++ src/libYARP_dev/src/modules/AnalogWrapper/AnalogWrapper.cpp
@@ -461,7 +461,7 @@ void AnalogWrapper::run()
             }
             else
             {
-                printf("%s: vector size non valid: %lu\n", id.c_str(), lastDataRead.size());
+                printf("%s: vector size non valid: %lu\n", id.c_str(), static_cast<unsigned long> (lastDataRead.size()));
             }
         }
         else
