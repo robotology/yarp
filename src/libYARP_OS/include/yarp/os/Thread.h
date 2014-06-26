b322f035e6bdf4a12111a88fcda3f0163b5110e7
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-06-26 17:27:15 +0200

    fix spelling of termination

--- src/libYARP_OS/include/yarp/os/Thread.h
+++ src/libYARP_OS/include/yarp/os/Thread.h
@@ -78,7 +78,7 @@ public:
      * Thread::isStopping will start returning true.
      * The user-defined Thread::onStop method will be called.
      * Then, this simply sits back and waits. Wait for thread
-     * tarmingation so cannot be called from within run().
+     * termination so cannot be called from within run().
      * @return true iff the thread stops successfully
      */
     bool stop();
