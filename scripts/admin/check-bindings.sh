c47156430eec4b0c400269c1db9c6228da68ee1b
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-08-06 16:40:10 +0200

    [bindings] update swig compatibility test matrix, see #206

--- scripts/admin/check-bindings.sh
+++ scripts/admin/check-bindings.sh
@@ -6,7 +6,7 @@ set -e
 # You'll probably need to remove your global swig version to prevent
 # cmake finding it later.
 swig_base="/cache/swig"
-swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11"
+swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11 2.0.12 3.0.0 3.0.1 3.0.2"
 
 SUPPORTED_LANGUAGES="LUA PYTHON JAVA RUBY CSHARP PERL TCL"
 
