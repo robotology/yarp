4d4a4980cf652d8af8a99976e9fe3681f8175288
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-08-26 18:36:31 +0200

    Clean and improve FindPortAudio

--- conf/FindPortAudio.cmake
+++ conf/FindPortAudio.cmake
@@ -1,36 +1,41 @@
-# Copyright: (C) 2008 RobotCub Consortium
+# Copyright:(C) 2008 RobotCub Consortium
 # Authors: Paul Fitzpatrick, Hatice Kose-Bagci
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
 # hacked together for unix by Paul Fitzpatrick
 # updated to work on windows by Hatice Kose-Bagci
 
-IF(PKGCONFIG_EXECUTABLE)
-	PKGCONFIG(portaudio-2.0 PORTAUDIO_INCLUDE_DIR PORTAUDIO_LINK_DIRECTORIES
-			        PORTAUDIO_LIBRARIES PORTAUDIO_CFLAGS)
-ENDIF(PKGCONFIG_EXECUTABLE)
 
+include(MacroStandardFindModule)
+include(FindPackageHandleStandardArgs)
 
-IF (NOT PORTAUDIO_LIBRARIES)
-  IF (WIN32)
-	FIND_LIBRARY(PORTAUDIO_LIBRARY NAMES portaudio_x86 PATHS C:/portaudio/build/msvc/Debug_x86)
-	GET_FILENAME_COMPONENT(PORTAUDIO_LINK_DIRECTORIES ${PORTAUDIO_LIBRARY} PATH)
-	FIND_PATH(PORTAUDIO_INCLUDE_DIR portaudio.h C:/portaudio/include)
+macro_standard_find_module(PortAudio portaudio-2.0)
 
-	SET(PORTAUDIO_LIBRARIES ${PORTAUDIO_LIBRARY})
-  ENDIF (WIN32)
-ENDIF (NOT PORTAUDIO_LIBRARIES)
+if(NOT PortAudio_FOUND)
+  if(WIN32)
+    find_library(PortAudio_LIBRARY NAMES portaudio_x86 PATHS C:/portaudio/build/msvc/Debug_x86)
+    get_filename_component(PORTAUDIO_LINK_DIRECTORIES ${PortAudio_LIBRARY} PATH)
+    find_path(PortAudio_INCLUDE_DIR portaudio.h C:/portaudio/include)
+    set(PortAudio_LIBRARIES ${PortAudio_LIBRARY})
 
-#message(STATUS "inc: -${PORTAUDIO_INCLUDE_DIR}")
-#message(STATUS "link flags: -${PORTAUDIO_LINK_FLAGS}")
-#message(STATUS "cflags: -${PORTAUDIO_CFLAGS}")
-#message(STATUS "link libs: -${PORTAUDIO_LIBRARIES}")
+    #message(STATUS "inc: -${PortAudio_INCLUDE_DIR}")
+    #message(STATUS "link flags: -${PortAudio_LINK_FLAGS}")
+    #message(STATUS "cflags: -${PortAudio_CFLAGS}")
+    #message(STATUS "link libs: -${PortAudio_LIBRARIES}")
 
-IF (PORTAUDIO_LIBRARIES)
-	SET(PortAudio_FOUND TRUE)
-ELSE (PORTAUDIO_LIBRARIES)
-	SET(PortAudio_FOUND FALSE)
-ENDIF (PORTAUDIO_LIBRARIES)
+    find_package_handle_standard_args(PortAudio DEFAULT_MSG PortAudio_LIBRARIES)
+    set(PortAudio_FOUND ${PORTAUDIO_FOUND})
 
-SET(PortAudio_LIBRARIES ${PORTAUDIO_LIBRARIES})
-SET(PortAudio_INCLUDE_DIR ${PORTAUDIO_INCLUDE_DIR})
+  endif()
+endif()
+
+# Compatibility
+set(PORTAUDIO_LIBRARIES ${PortAudio_LIBRARIES})
+set(PORTAUDIO_INCLUDE_DIR ${PortAudio_INCLUDE_DIR})
+# Set package properties if FeatureSummary was included
+
+if(COMMAND set_package_properties)
+    set_package_properties(PortAudio PROPERTIES DESCRIPTION "Portable Cross-platform Audio I/O")
+    set_package_properties(PortAudio PROPERTIES URL "http://www.portaudio.com/")
+endif()
+
