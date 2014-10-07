da3aafd0b5ad4f0b322bb58756b787c7784d6d58
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-10-07 20:58:04 +0200

    [idl] add example of using ::Editor class

---
+++ example/idl/thriftEditor/main.cpp
@@ -0,0 +1,79 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/** 
+ * Copyright: (C) 2014 iCub Facility
+ * Authors: Paul Fitzpatrick
+ * CopyPolicy:  Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <stdio.h>
+#include <yarp/os/all.h>
+#include "Settings.h"
+
+// class to test callbacks when Settings are changed.
+class Receiver : public Settings::Editor {
+public:
+    virtual bool will_set_id() { 
+        printf("[id] is about to be set, it is currently %d\n",
+               state().id);
+    }
+
+    virtual bool did_set_id() { 
+        printf("[id] was set, to %d\n", state().id);
+    }
+
+    virtual bool will_set_name() { 
+        printf("[name] is about to be set, it is currently '%s'\n",
+               state().name.c_str());
+    }
+
+    virtual bool did_set_name() { 
+        printf("[name] was set, to '%s'\n", state().name.c_str());
+    }
+};
+
+
+int main() {
+    yarp::os::Network yarp;
+  
+    Settings::Editor settings; // original copy of Settings
+    Receiver receiver;         // a copy of Settings updated over network
+
+    yarp::os::RpcClient sender_port;    // a port for sending out Settings
+    yarp::os::RpcServer receiver_port;  // a port for receiving Settings
+
+    // link copies of Settings to the ports that transmit/recieve them
+    settings.yarp().attachAsClient(sender_port);
+    receiver.yarp().attachAsServer(receiver_port);
+
+    // open ports and connect them
+    if (!sender_port.open("/sender")) return 1;
+    if (!receiver_port.open("/receiver")) return 1;
+    yarp.connect("/sender","/receiver");
+
+    printf("####################################################\n");
+    printf("Setting [id]\n");
+
+    // change one field in Settings (should see receiver callbacks trigger)
+    settings.set_id(5);
+
+    printf("####################################################\n");
+    printf("Setting [name]\n");
+
+    // change one field in Settings (should see receiver callbacks trigger)
+    settings.set_name("hello");
+
+    printf("####################################################\n");
+    printf("Setting [id] and [name]\n");
+
+    // change two fields at a time, in case we want to be efficient of
+    // network use
+    settings.begin();
+    settings.set_id(6);
+    settings.set_name("world");
+    settings.end();
+
+    printf("####################################################\n");
+
+    return 0;
+}
