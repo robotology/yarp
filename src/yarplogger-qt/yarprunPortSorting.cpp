cc662c1d85fe14de98413446b4e65e3b575a71ae
Marco Randazzo <marco.randazzo@iit.it>
2014-10-28 17:26:40 +0100

    added copyright stuff

--- src/yarplogger-qt/yarprunPortSorting.cpp
+++ src/yarplogger-qt/yarprunPortSorting.cpp
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C)2014  iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Marco Randazzo
+ * email:  marco.randazzo@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 #include <QString>
 #include <string>
 #include <cstdio>
@@ -25,6 +43,16 @@ bool YarprunPortsSortFilterProxyModel::lessThan( const QModelIndex &left, const 
       int rval = (r1 * 16777216) + (r2 * 65536) + (r3 * 256) + (r4);
       return lval < rval;
   }
+  /*else if (left.column()==2)
+  {
+      QString leftStr     = this->sourceModel()->data( left ).toString();
+      QString rightStr    = this->sourceModel()->data( right ).toString();
+      if (leftStr.at(0) < '0'  || leftStr.at(0) > '9')  
+          return false;
+      if (rightStr.at(0) < '0' || rightStr.at(0) > '9')
+          return true;
+      return QSortFilterProxyModel::lessThan(left,right);
+  }*/
   else if (left.column()==3 || left.column()==4 || left.column()==5)
   {
       //sorting by log size
