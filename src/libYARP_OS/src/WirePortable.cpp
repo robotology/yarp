14ecb206be222944dff7c9dc9c9cdfce88435f44
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-11-21 17:17:29 +0100

    move unused arg to cpp file where types are defined

---
+++ src/libYARP_OS/src/WirePortable.cpp
@@ -0,0 +1,22 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2014 iCub Facility
+ * Authors: Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#include <yarp/os/idl/WirePortable.h>
+#include <yarp/os/idl/WireReader.h>
+#include <yarp/os/idl/WireWriter.h>
+
+bool yarp::os::idl::WirePortable::read(yarp::os::idl::WireReader& reader) {
+    YARP_UNUSED(reader);
+    return false;
+}
+
+bool yarp::os::idl::WirePortable::write(yarp::os::idl::WireWriter& writer) {
+    YARP_UNUSED(writer);
+    return false;
+}
