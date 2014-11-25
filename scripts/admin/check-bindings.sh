a9f3129ec71509596ca359093dccfd785f52b26c
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-11-25 11:58:01 +0100

    check-bindings.sh: Allow to change the tested swig versions using the SWIG_VERSIONS env var

--- scripts/admin/check-bindings.sh
+++ scripts/admin/check-bindings.sh
@@ -9,8 +9,14 @@ swig_base="/cache/swig"
 if [ -e "cache/swig" ]; then
     swig_base="$PWD/cache/swig"
 fi
+
 # swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11 2.0.12 3.0.0 3.0.1 3.0.2"
-swig_versions="1.3.39 1.3.40 2.0.2 2.0.5 2.0.6 2.0.10 2.0.12 3.0.0 3.0.1 3.0.2"
+default_swig_versions="1.3.39 1.3.40 2.0.2 2.0.5 2.0.6 2.0.10 2.0.12 3.0.0 3.0.1 3.0.2"
+if [ -z "$SWIG_VERSIONS" ]; then
+swig_versions="$default_swig_versions"
+else
+swig_versions="$SWIG_VERSIONS"
+fi
 
 skip_versions="RUBY_1.3.39 RUBY_1.3.40 LUA_2.0.11"
 
