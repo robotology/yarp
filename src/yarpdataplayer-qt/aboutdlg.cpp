94e35a995d60e1633dfc8ec193a53f5ebdd1bd9a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-01-22 17:19:09 +0100

    [yarpdataplayer-qt] Fix unknown escape sequence warning

--- src/yarpdataplayer-qt/aboutdlg.cpp
+++ src/yarpdataplayer-qt/aboutdlg.cpp
@@ -34,7 +34,8 @@ void AboutDlg::onCredits()
             </style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\"> \
             <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">Vadim Tikhanoff &lt;</span><a href=\"mailto:vadim.tikhanoff@iit.it\"><span style=\" text-decoration: underline; color:#0000ff;\">vadim.tikhanoff@iit.it</span></a><span style=\" font-size:8.25pt;\">&gt;</span></p> \
             <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">Ali Paikan &lt;</span><a href=\"mailto:vadim.tikhanoff@iit.it\"><span style=\" text-decoration: underline; color:#0000ff;\">ali.paikan@iit.it</span></a><span style=\" font-size:8.25pt;\">&gt;</span></p> \
-            <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">Davide Perrone &lt;</span><a href=\"mailto:aitek4iit@aitek.it\"><span style=\" text-decoration: underline; color:#0000ff;\">aitek4iit@aitek.it</span></a><span style=\" font-size:8.25pt;\">&gt;</span></p> \</body></html>";
+            <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">Davide Perrone &lt;</span><a href=\"mailto:aitek4iit@aitek.it\"><span style=\" text-decoration: underline; color:#0000ff;\">aitek4iit@aitek.it</span></a><span style=\" font-size:8.25pt;\">&gt;</span></p> \
+            </body></html>";
     GenericInfoDlg dlg("Credits","Written by:",text);
     dlg.exec();
 }
