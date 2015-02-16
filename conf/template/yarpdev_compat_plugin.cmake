6cf3e012385f89d53cb0ecf4013054717fe59dc5
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-02-16 20:17:21 +0100

    [CMake] Remove more deprecated and unused stuff

--- conf/template/yarpdev_compat_plugin.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright: (C) 2009 RobotCub Consortium
-# Author: Paul Fitzpatrick
-# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
-
-
-FIND_PACKAGE(YARP)
-LINK_LIBRARIES(@libname@)
-SET(@libname@_FOUND TRUE)
