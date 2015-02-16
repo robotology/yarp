0b9af5e58ca9979bbebcd5d6d6de0523668a153a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-02-16 11:57:15 +0100

    [generate-cmake-options.sh] Enable some more devices and carriers

--- scripts/admin/generate-cmake-options.sh
+++ scripts/admin/generate-cmake-options.sh
@@ -8,17 +8,22 @@ CMAKE_OPTIONS="\
 -DTEST_MACHINE_OS_TYPE:STRING=$2 \
 -DTEST_MACHINE_TEST_TYPE:STRING=$3 \
 -DENABLE_DASHBOARD_SUBMIT:BOOL=TRUE \
--DUSE_LIBDC1394=FALSE \
+-DUSE_LIBDC1394:BOOL=FALSE \
 -DCREATE_GUIS:BOOL=TRUE \
 -DCREATE_YARPBUILDER:BOOL=TRUE \
 -DCREATE_LIB_MATH:BOOL=TRUE \
 -DUSE_PORT_AUTHENTICATION:BOOL=FALSE \
--DCREATE_OPTIONAL_CARRIERS=TRUE \
--DENABLE_yarpcar_tcpros_carrier=TRUE \
--DENABLE_yarpcar_rossrv_carrier=TRUE \
--DENABLE_yarpcar_xmlrpc_carrier=TRUE \
--DENABLE_yarpcar_bayer_carrier=TRUE \
--DENABLE_yarpcar_priority_carrier=TRUE \
+-DCREATE_OPTIONAL_CARRIERS:BOOL=TRUE \
+-DENABLE_yarpcar_tcpros_carrier:BOOL=TRUE \
+-DENABLE_yarpcar_rossrv_carrier:BOOL=TRUE \
+-DENABLE_yarpcar_xmlrpc_carrier:BOOL=TRUE \
+-DENABLE_yarpcar_bayer_carrier:BOOL=TRUE \
+-DENABLE_yarpcar_priority_carrier:BOOL=TRUE \
+-DENABLE_yarpcar_human_carrier:BOOL=TRUE \
+-DCREATE_DEVICE_LIBRARY_MODULES:BOOL=TRUE \
+-DENABLE_yarpmod_fakebot:BOOL=TRUE \
+-DTEST_yarpidl_rosmsg:BOOL=TRUE
+-DTEST_yarpidl_thrift:BOOL=TRUE
 "
 
 case $3 in
