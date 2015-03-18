c55b061a9bf6477db88a7d025de8445af0559fc6
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-03-18 18:40:15 +0100

    [YARP_OS] remove unused code

--- src/libYARP_OS/src/Property.cpp
+++ src/libYARP_OS/src/Property.cpp
@@ -342,22 +342,6 @@ public:
             result += "\n[]\n";  // reset any nested sections
         }
         free(namelist);
-
-        /*
-        struct YARP_DIRENT *ent = YARP_readdir(dir);
-        while (ent) {
-            ConstString name = ent->d_name;
-            ent = ACE_OS::readdir(dir);
-            int len = (int)name.length();
-            if (len<4) continue;
-            if (name.substr(len-4)!=".ini") continue;
-            ConstString fname = dirname + "/" + name;
-            ok = ok && readFile(fname,result,false);
-            result += "\n[]\n";  // reset any nested sections
-        }
-        YARP_closedir(dir);
-        dir = NULL;
-        */
         return ok;
     }
 
