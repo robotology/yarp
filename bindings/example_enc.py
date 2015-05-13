a24b800ddd109db99e5cdef3f1895213b2e7cc84
Ali Paikan <ali.paikan@gmail.com>
2015-05-13 10:34:17 +0200

    updating yarp-python binding example

--- bindings/example_enc.py
+++ bindings/example_enc.py
@@ -7,40 +7,50 @@
 import sys
 import yarp
 
-yarp.Network.init()
+def main():
+    yarp.Network.init()
 
-options = yarp.Property()
-driver = yarp.PolyDriver()
+    options = yarp.Property()
+    driver = yarp.PolyDriver()
 
-# set the poly driver options 
-options.put("robot", "icub")
-options.put("device", "remote_controlboard")
-options.put("local", "/example_enc/client")
-options.put("remote", "/icub/left_arm")
+    # set the poly driver options 
+    options.put("robot", "icub")
+    options.put("device", "remote_controlboard")
+    options.put("local", "/example_enc/client")
+    options.put("remote", "/icub/left_arm")
 
-# opening the drivers
-print 'Opening the motor driver...'
-driver.open(options)
-if not driver.isValid():
-    print 'Cannot open the driver!'
-    sys.exit()
+    # opening the drivers
+    print 'Opening the motor driver...'
+    driver.open(options)
+    if not driver.isValid():
+        print 'Cannot open the driver!'
+        sys.exit()
 
-# opening the drivers
-print 'Viewing motor position/encoders...'
-ipos = driver.viewIPositionControl()
-ienc = driver.viewIEncoders()
-if ienc is None or ipos is None:
-    print 'Cannot view motor positions/encoders!'
-    sys.exit()
+    # opening the drivers
+    print 'Viewing motor position/encoders...'
+    ipos = driver.viewIPositionControl()
+    ienc = driver.viewIEncoders()
+    if ienc is None or ipos is None:
+        print 'Cannot view motor positions/encoders!'
+        sys.exit()
+     
+    # wait a bit for the interface 
+    yarp.Time_delay(1.0)
 
-# read encoders
-encs = yarp.Vector(ipos.getAxes())
-ienc.getEncoders(encs.data())
+    encs = yarp.Vector(ipos.getAxes())
+    for i in range(0,10):
+        # read encoders
+        ret = ienc.getEncoders(encs.data())
+        if ret is False: continue
 
-print "Current encoders value: "
-print encs.toString(-1, -1)
+        print "Current encoders value: "
+        print encs.toString(-1, -1)
+        yarp.Time_delay(0.01)
+    
+    # closing the driver
+    driver.close()
+    yarp.Network.fini()
 
-driver.close()
-
-yarp.Network.fini()
+if __name__ == "__main__":
+    main()
 
