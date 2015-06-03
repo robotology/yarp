robotology/yarp

commit 86b041f2e77290d21ca8d7ea936b49b4b1b9a3ef

clean up some warnings under clang

src/libYARP_OS/include/yarp/os/impl/AuthHMAC.h
@@ -55,7 +55,9 @@ private:
     bool check_hmac(unsigned char * mac, unsigned char * mac_check);
     void fill_nonce(unsigned char* nonce);
 
+#ifdef DEBUG_HMAC
     HMAC_CONTEXT context;
+#endif
 };
 
 #endif

src/libYARP_OS/include/yarp/os/Searchable.h
@@ -184,7 +184,7 @@ public:
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
     virtual void setMonitor(SearchMonitor *monitor, const char *context="");
     virtual SearchMonitor *getMonitor() const;
-    virtual ConstString getContext() const;
+    virtual ConstString getMonitorContext() const;
     virtual void reportToMonitor(const SearchReport& report) const;
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 };

src/libYARP_OS/include/yarp/os/impl/Logger.h
@@ -93,50 +93,50 @@ public:
 
 
 
-    void debug(const String& txt) {
+    void internal_debug(const String& txt) {
         show(LM_DEBUG,txt);
     }
 
-    void info(const String& txt) {
+    void internal_info(const String& txt) {
         show(LM_INFO,txt);
     }
 
-    void warning(const String& txt) {
+    void internal_warning(const String& txt) {
         show(LM_WARNING,txt);
     }
 
-    void error(const String& txt) {
+    void internal_error(const String& txt) {
         show(LM_ERROR,txt);
     }
 
-    void fail(const String& txt) {
+    void internal_fail(const String& txt) {
         show(LM_ERROR,txt);
         exit(1);
     }
 
 
 
-    void debug(const char *txt) {
+    void internal_debug(const char *txt) {
         String stxt(txt);
         show(LM_DEBUG,stxt);
     }
 
-    void info(const char *txt) {
+    void internal_info(const char *txt) {
         String stxt(txt);
         show(LM_INFO,stxt);
     }
 
-    void warning(const char *txt) {
+    void internal_warning(const char *txt) {
         String stxt(txt);
         show(LM_WARNING,stxt);
     }
 
-    void error(const char *txt) {
+    void internal_error(const char *txt) {
         String stxt(txt);
         show(LM_ERROR,stxt);
     }
 
-    void fail(const char *txt) {
+    void internal_fail(const char *txt) {
         String stxt(txt);
         show(LM_ERROR,stxt);
         exit(1);
@@ -191,11 +191,11 @@ private:
 // compromise - use macros so that debugging can evaporate in optimized code.
 // also, make a printf-style adaptor since c++ is a bit of a pain to
 // build strings in.
-#define YARP_ERROR(log,x) ((Logger*)&log)->error(x)
-#define YARP_WARN(log,x)  ((Logger*)&log)->warning(x)
-#define YARP_INFO(log,x)  ((Logger*)&log)->info(x)
-#define YARP_DEBUG(log,x) ((Logger*)&log)->debug(x)
-#define YARP_FAIL(log,x)  ((Logger*)&log)->fail(x)
+#define YARP_ERROR(log,x) ((Logger*)&log)->internal_error(x)
+#define YARP_WARN(log,x)  ((Logger*)&log)->internal_warning(x)
+#define YARP_INFO(log,x)  ((Logger*)&log)->internal_info(x)
+#define YARP_DEBUG(log,x) ((Logger*)&log)->internal_debug(x)
+#define YARP_FAIL(log,x)  ((Logger*)&log)->internal_fail(x)
 
 #define YARP_LONGEST_MESSAGE 1000
 #define YARP_SPRINTF0(log,mode,msg)  { char _yarp_buf[YARP_LONGEST_MESSAGE]; ACE_OS::snprintf(_yarp_buf,YARP_LONGEST_MESSAGE,msg); log.mode(_yarp_buf); }

src/libYARP_OS/src/Bottle.cpp
@@ -328,7 +328,7 @@ Bottle& Bottle::findGroup(const ConstString& key) const {
         }
         reportToMonitor(report);
         if (bb.isList()) {
-            String context = getContext().c_str();
+            String context = getMonitorContext().c_str();
             context += ".";
             context += key;
             bb.asList()->setMonitor(getMonitor(),

src/libYARP_OS/src/Port.cpp
@@ -28,7 +28,6 @@ using namespace yarp::os;
 
 class PortCoreAdapter : public PortCore {
 private:
-    Port& owner;
     SemaphoreImpl stateMutex;
     PortReader *readDelegate;
     PortReader *permanentReadDelegate;
@@ -54,8 +53,8 @@ public:
     bool commitToRpc;
     bool active;
 
-    PortCoreAdapter(Port& owner) :
-        owner(owner), stateMutex(1),
+    PortCoreAdapter() :
+        stateMutex(1),
         readDelegate(NULL),
         permanentReadDelegate(NULL),
         adminReadDelegate(NULL),
@@ -81,7 +80,6 @@ public:
         commitToRpc(false),
         active(false)
     {
-        setContactable(&owner);
     }
 
     void openable() {
@@ -363,7 +361,7 @@ public:
 void *Port::needImplementation() const {
     if (implementation) return implementation;
     Port *self = (Port *)this;
-    self->implementation = new PortCoreAdapter(*self);
+    self->implementation = new PortCoreAdapter();
     yAssert(self->implementation!=NULL);
     self->owned = true;
     return self->implementation;
@@ -502,7 +500,7 @@ bool Port::open(const Contact& contact, bool registerName,
 
     // Allow for open() to be called safely many times on the same Port
     if (currentCore->isOpened()) {
-        PortCoreAdapter *newCore = new PortCoreAdapter(*this);
+        PortCoreAdapter *newCore = new PortCoreAdapter();
         yAssert(newCore!=NULL);
         // copy state that should survive in a new open()
         if (currentCore->checkPortReader()!=NULL) {

src/libYARP_OS/src/Property.cpp
@@ -928,7 +928,7 @@ Bottle& Property::findGroup(const ConstString& key) const {
         }
         reportToMonitor(report);
         if (result!=0/*NULL*/) {
-            String context = getContext().c_str();
+            String context = getMonitorContext();
             context += ".";
             context += key;
             result->setMonitor(getMonitor(),

src/libYARP_OS/src/Searchable.cpp
@@ -115,7 +115,7 @@ yarp::os::SearchMonitor *yarp::os::Searchable::getMonitor() const {
     return monitor;
 }
 
-yarp::os::ConstString yarp::os::Searchable::getContext() const {
+yarp::os::ConstString yarp::os::Searchable::getMonitorContext() const {
     return monitorContext;
 }
 

commit d3e33d407221d91ae35648250005660a4db35abc

Fix build with YARP_PORT_AUTHENTICATION=ON
This was broken since commit 86b041f2e77290d21ca8d7ea936b49b4b1b9a3ef

src/libYARP_OS/include/yarp/os/impl/AuthHMAC.h
@@ -55,7 +55,7 @@ private:
     bool check_hmac(unsigned char * mac, unsigned char * mac_check);
     void fill_nonce(unsigned char* nonce);
 
-#ifdef DEBUG_HMAC
+#ifdef PORT_AUTH
     HMAC_CONTEXT context;
 #endif
 };