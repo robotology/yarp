d04e37d1af75074500acf85e384eb708fa5e0616
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-06-07 01:22:55 +0200

    [ovrheadset] Import FindGLEW.cmake from CMake 3.1

--- conf/FindGLEW.cmake
+++
@@ -1,7 +0,0 @@
-include(MacroStandardFindModule)
-macro_standard_find_module(GLEW glew)
-
-if(NOT GLEW_FOUND)
-  set(GLEW_INCLUDE_DIRS "D:\\ROBOT\\glew-1.12.0\\include")
-  set(GLEW_LIBRARIES "D:\\ROBOT\\glew-1.12.0\\lib\\Release\\Win32\\glew32.lib")
-endif()
