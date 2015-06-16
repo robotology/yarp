8c5c89f2a17204781c2556f23130741d5f7b06d6
Matteo Brunettini <matteo.brunettini@iit.it>
2015-06-16 14:59:33 +0200

    added configuration for version 2.3.64 windows pckages

---
+++ packaging/windows/conf/bundle-2-3-64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export BUNDLE_YARP_VERSION=2.3.64
+export BUNDLE_TWEAK=0
+export BUNDLE_ACE_VERSION=6.1.1
+export BUNDLE_CMAKE_VERSION=3.1.3
+export BUNDLE_NSIS_VERSION=2.46
+export BUNDLE_GSL_VERSION=1.14
+export BUNDLE_EXT_REPO_URL=http://www.icub.org/download/software/windows/common
+# Empty version means no support, yarp will be compiled without it
+export BUNDLE_QT_VERSION_v10_x86=5.4.1
+export BUNDLE_QT_VERSION_v10_x86_amd64=
+export BUNDLE_QT_VERSION_v11_x86=5.4.1
+export BUNDLE_QT_VERSION_v11_x86_amd64=5.2.1
+export BUNDLE_QT_VERSION_v12_x86=5.4.1
+export BUNDLE_QT_VERSION_v12_x86_amd64=5.4.1
+export BUNDLE_GTKMM_VERSION_v10_x86=2.22.0
+export BUNDLE_GTKMM_VERSION_v10_x86_amd64=2.22.0
+export BUNDLE_GTKMM_VERSION_v11_x86=2.22.0
+export BUNDLE_GTKMM_VERSION_v11_x86_amd64=2.22.0
+export BUNDLE_GTKMM_VERSION_v12_x86=
+export BUNDLE_GTKMM_VERSION_v12_x86_amd64=
