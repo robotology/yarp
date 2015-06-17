a5511b892fafee727de010517d95978d78fb1136
Marco Randazzo <marco.randazzo@iit.it>
2015-06-17 17:33:50 +0200

    fixed typo

--- src/yarpbatterygui-qt/main.cpp
+++ src/yarpbatterygui-qt/main.cpp
@@ -15,7 +15,7 @@
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/Os.h>
 #include <yarp/os/Log.h>
-#include <yarp/os/Logstream.h>
+#include <yarp/os/LogStream.h>
 #include <yarp/os/Time.h>
 #include <yarp/sig/Vector.h>
 
