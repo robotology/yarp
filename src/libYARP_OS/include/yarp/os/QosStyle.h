584ad636b5d80dacb7b877c562568ca9be9d3c87
Ali Paikan <ali.paikan@gmail.com>
2015-08-05 17:44:41 +0200

    applying comments from @barbalberto and @drdanz: having different methodes to set the QosStyle attributes (e.g., byType, byDscp)

--- src/libYARP_OS/include/yarp/os/QosStyle.h
+++ src/libYARP_OS/include/yarp/os/QosStyle.h
@@ -32,43 +32,100 @@ public:
      * The PacketPriorityType defines the packets
      * quality of service (priority) levels
      */
-    enum PacketPriorityType {
-        LOW = VOCAB3('L','O','W'),
-        NORM = VOCAB4('N','O','R', 'M'),
-        HIGH = VOCAB4('H','I','G','H'),
-        CRIT = VOCAB4('C','R','I','T')
+    enum PacketPriorityType {        
+        NORM = 0,
+        LOW  = 10,
+        HIGH = 36,
+        CRIT = 44
     };
 
     /**
-     * @brief threadPriority controls the priority of the yarp port's
-     * thread (if available) which handles the data transmission.
-     * Notice that not every yarp port has dedicated
-     * thread for communication (e.g., BufferedPorts have dedicated thread)
-     */
-    int threadPriority;
-
-    /**
-     * @brief threadPolicy controls the scheduling policy of the yarp
-     * port's thread (if available) which handles the data transmission.
-     * Notice that not every yarp port has dedicated
-     * thread for communication (e.g., BufferedPorts have dedicated thread)
-     */
-    int threadPolicy;
-
-
-    /**
-     * @brief packetPriority controls the communication packets priority levels
-     * by adjusting the IPV4/6 DSCP value.
-     */
-    PacketPriorityType packetPriority;
-
-
-    /**
-     *
      * Constructor.  Sets all options to reasonable defaults.
-     *
      */
     explicit QosStyle();
+
+
+    /**
+     * @brief sets the packet priority given as DSCP class
+     * @param dscp the packet DSCP class
+     * @return true if the packet priority is correctly given (e.g., "CS0", "AF42")
+     */
+    bool setPacketPriorityByDscp(const ConstString& dscp);
+
+
+    /**
+     * @brief sets the packet priority given as PacketPriorityType
+     * @param priority the packet priority
+     */
+    void setPacketPriorityByType(PacketPriorityType priority);
+
+
+    /**
+     * @brief sets the packet priority given as TOS value
+     * @param tos the packet TOS
+     */
+    void setPacketPriority(int tos) {
+        packetPriority = tos;
+    }
+
+
+    /**
+     * @brief sets the communication thread priority level
+     * @param priority the thread's priority
+     */
+    void setThreadPriority(int priority) {
+        threadPriority = priority;
+    }
+
+
+    /**
+     * @brief sets the communication thread scheduling policy
+     * @param policy the thread's real-time scheduling policy
+     */
+    void setThreadPolicy(int policy) {
+        threadPolicy = policy;
+    }
+
+
+    /**
+     * @brief returns the packet TOS value
+     * @return the TOS
+     */
+    int getPacketPriorty() const {
+        return packetPriority;
+    }
+
+
+    /**
+     * @brief returns the communication thread priority level
+     * @return the thread priority
+     */
+    int getThreadPriority() const {
+        return threadPriority;
+    }
+
+
+    /**
+     * @brief returns the communication thread scheduling policy
+     * @return the thread scheduling policy
+     */
+    int getThreadPolicy() const {
+        return threadPolicy;
+    }
+
+
+    /**
+     * @brief returns the IPV4/6 DSCP value given as DSCP code
+     * @param vocab a DSCP code (e.g., CS0)
+     * @return the actual DSCP value
+     */
+    static int getDSCPByVocab(int vocab);
+
+private:
+    int threadPriority;
+    int threadPolicy;
+    int packetPriority;
+
 };
 
 #endif
--- src/libYARP_OS/src/QosStyle.cpp
+++ src/libYARP_OS/src/QosStyle.cpp
@@ -10,7 +10,57 @@
 
 
 yarp::os::QosStyle::QosStyle() :
-        threadPriority(0),
+        threadPriority(-1),
         threadPolicy(-1),
-        packetPriority(yarp::os::QosStyle::NORM){
+        packetPriority(-1){
+}
+
+bool yarp::os::QosStyle::setPacketPriorityByDscp(const ConstString& dscp) {
+    int value = yarp::os::QosStyle::getDSCPByVocab(yarp::os::Vocab::encode(dscp));
+    if(value == -1)
+        return false;
+    packetPriority = value<<2;
+    return true;
+}
+
+
+void yarp::os::QosStyle::setPacketPriorityByType(PacketPriorityType priority) {
+    packetPriority = ((int)priority)<<2;
+}
+
+
+
+//                Class 1          Class 2         Class 3         Class 4
+//          ------------------------------------------------------------------
+// Low Drop	 | AF11 (DSCP 10)	AF21 (DSCP 18)	AF31 (DSCP 26)	AF41 (DSCP 34)
+// Med Drop	 | AF12 (DSCP 12)	AF22 (DSCP 20)	AF32 (DSCP 28)	AF42 (DSCP 36)
+// High Drop | AF13 (DSCP 14)	AF23 (DSCP 22)	AF33 (DSCP 30)	AF43 (DSCP 38)
+int yarp::os::QosStyle::getDSCPByVocab(int vocab) {
+    int dscp;
+    switch(vocab) {
+        case VOCAB3('C','S','0')    : dscp = 0; break;
+        case VOCAB3('C','S','1')    : dscp = 8; break;
+        case VOCAB3('C','S','2')    : dscp = 16; break;
+        case VOCAB3('C','S','3')    : dscp = 24; break;
+        case VOCAB3('C','S','4')    : dscp = 32; break;
+        case VOCAB3('C','S','5')    : dscp = 40; break;
+        case VOCAB3('C','S','6')    : dscp = 48; break;
+        case VOCAB3('C','S','7')    : dscp = 56; break;
+        case VOCAB4('A','F','1','1'): dscp = 10; break;
+        case VOCAB4('A','F','1','2'): dscp = 12; break;
+        case VOCAB4('A','F','1','3'): dscp = 14; break;
+        case VOCAB4('A','F','2','1'): dscp = 18; break;
+        case VOCAB4('A','F','2','2'): dscp = 20; break;
+        case VOCAB4('A','F','2','3'): dscp = 22; break;
+        case VOCAB4('A','F','3','1'): dscp = 26; break;
+        case VOCAB4('A','F','3','2'): dscp = 28; break;
+        case VOCAB4('A','F','3','3'): dscp = 30; break;
+        case VOCAB4('A','F','4','1'): dscp = 34; break;
+        case VOCAB4('A','F','4','2'): dscp = 36; break;
+        case VOCAB4('A','F','4','3'): dscp = 38; break;
+        case VOCAB2('V','A')        : dscp = 44; break;
+        case VOCAB2('E','F')        : dscp = 46; break;
+        default: dscp = -1;
+    };
+    return dscp;
 }
