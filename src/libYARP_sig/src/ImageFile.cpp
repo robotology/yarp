d76fa3ec28f0307ada530f9a53612a7e577e2915
Ugo Pattacini <ugo.pattacini@iit.it>
2015-08-28 18:15:48 +0200

    made file::write(const Image& src, ...) call the corresponding file::write() according to the type of Image

--- src/libYARP_sig/src/ImageFile.cpp
+++ src/libYARP_sig/src/ImageFile.cpp
@@ -11,22 +11,25 @@
 ///
 ///
 
+#include <cstdio>
+
 #include <yarp/sig/ImageFile.h>
 #include <yarp/os/Log.h>
 
-#include <stdio.h>
-
+using namespace std;
 using namespace yarp::os;
 using namespace yarp::sig;
 
+
 static void warn(const char *message)
 {
-	fprintf(stderr, "pgm/ppm: Error - %s\n", message);
+    fprintf(stderr, "pgm/ppm: Error - %s\n", message);
 }
 
 
-bool file::write(const ImageOf<PixelFloat>& src, const ConstString& dest) {
-	FILE *fp = fopen(dest.c_str(), "w");
+bool file::write(const ImageOf<PixelFloat>& src, const ConstString& dest)
+{
+    FILE *fp = fopen(dest.c_str(), "w");
     if (fp==NULL) {
         return false;
     }
@@ -41,10 +44,12 @@ bool file::write(const ImageOf<PixelFloat>& src, const ConstString& dest) {
     return false;
 }
 
-bool file::read(ImageOf<PixelFloat>& dest, const ConstString& src) {
+
+bool file::read(ImageOf<PixelFloat>& dest, const ConstString& src)
+{
     int hh = 0, ww = 0;
 
-	FILE *fp = fopen(src.c_str(), "r");
+    FILE *fp = fopen(src.c_str(), "r");
     if (fp==NULL) {
         return false;
     }
@@ -71,7 +76,7 @@ bool file::read(ImageOf<PixelFloat>& dest, const ConstString& src) {
         }
     }
     fclose(fp);
-	fp = fopen(src.c_str(), "rb");
+    fp = fopen(src.c_str(), "rb");
     if (fp==NULL) {
         return false;
     }
@@ -110,18 +115,19 @@ bool file::read(ImageOf<PixelFloat>& dest, const ConstString& src) {
         }
     }
     fclose(fp);
-	return true;
+    return true;
 }
+
 
 static bool SavePGM(char *src, const char *filename, int h, int w, int rowSize)
 {
-	FILE *fp = fopen(filename, "wb");
-	if (!fp) 
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) 
         {
             printf("cannot open file %s for writing\n", filename);
             return false;
         }
-	else
+    else
         {
             const int inc = rowSize; ////YARPSimpleOperation::ComputePadding (w, YarpImageAlign) + w;
 
@@ -135,18 +141,19 @@ static bool SavePGM(char *src, const char *filename, int h, int w, int rowSize)
             fclose(fp);
         }
 
-	return true;
+    return true;
 }
+
 
 static bool SavePPM(char *src, const char *filename, int h, int w, int rowSize)
 {
-	FILE *fp = fopen(filename, "wb");
-	if (!fp) 
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) 
         {
             printf("cannot open file %s for writing\n", filename);
             return false;
         }
-	else
+    else
         {
             const int inc = rowSize;//YARPSimpleOperation::ComputePadding (w*3, YarpImageAlign) + w * 3;
 
@@ -161,30 +168,30 @@ static bool SavePPM(char *src, const char *filename, int h, int w, int rowSize)
             fclose(fp);
         }
 
-	return true;
+    return true;
 }
 
 
 static bool ReadHeader(FILE *fp, int *height, int *width, int *color)
 {
-	char ch;
-	int  maxval;
+    char ch;
+    int  maxval;
 
-	*color = 0;
+    *color = 0;
 
-	//// LATER: replace fscanf (said to be non portable) with getc style functions.
-	if (fscanf(fp, "P%c\n", &ch) != 1 || (ch!='6'&&ch!='5')) 
+    //// LATER: replace fscanf (said to be non portable) with getc style functions.
+    if (fscanf(fp, "P%c\n", &ch) != 1 || (ch!='6'&&ch!='5')) 
         //die("file is not in pgm/ppm raw format; cannot read");
         {
             warn("file is not in pgm/ppm raw format; cannot read");
             return false;
         }
 
-	if (ch=='6') *color = 1;
+    if (ch=='6') *color = 1;
 
-	// skip comments
-	ch = fgetc(fp);
-	while (ch == '#')
+    // skip comments
+    ch = fgetc(fp);
+    while (ch == '#')
         {
             do 
                 {
@@ -193,47 +200,48 @@ static bool ReadHeader(FILE *fp, int *height, int *width, int *color)
             while (ch != '\n');   
             ch = fgetc(fp);    
         }
-	/*
+    /*
       while (ch=='\n' || ch=='\r')
       {
       ch = getc(fp);
       }
       ungetc(ch,fp);
-	*/
+    */
 
-	while(ch<'0'&&ch>'9'&&ch!=-1)
+    while(ch<'0'&&ch>'9'&&ch!=-1)
         {
             ch = fgetc(fp);
         }
 
-	if (ch<'0'&&ch>'9')
+    if (ch<'0'&&ch>'9')
         {
             warn("cannot read header information from pgm/ppm file");
             return false;
         }
-	ungetc(ch, fp);
+    ungetc(ch, fp);
 
-	/// LATER: not portable?
-	int n=fscanf(fp, "%d%d%d", width, height, &maxval);
+    /// LATER: not portable?
+    int n=fscanf(fp, "%d%d%d", width, height, &maxval);
     if (n!=3)
         return false;
 
-	fgetc(fp);
-	if (maxval != 255)
+    fgetc(fp);
+    if (maxval != 255)
         {
             //die("image is not true-color (24 bit); read failed");
             warn("image is not true-color (24 bit); read failed");
             return false;
         }
 
-	return true;
+    return true;
 }
+
 
 static bool ImageReadRGB(ImageOf<PixelRgb> &img, const char *filename)
 {
-   	int width, height, color, num;
+    int width, height, color, num;
     FILE *fp=0;
-	fp = fopen(filename, "rb");
+    fp = fopen(filename, "rb");
 
     if(fp==0)
         {
@@ -241,7 +249,7 @@ static bool ImageReadRGB(ImageOf<PixelRgb> &img, const char *filename)
             return false;
         }
 
-	if (!ReadHeader(fp, &height, &width, &color))
+    if (!ReadHeader(fp, &height, &width, &color))
         {
             fclose (fp);
             fprintf(stderr, "Error reading header, is file a valid ppm/pgm?\n");
@@ -268,31 +276,32 @@ static bool ImageReadRGB(ImageOf<PixelRgb> &img, const char *filename)
             img.copy(tmp);
             return true;
         }
-	
+    
     img.resize(width,height);
 
-	const int w = img.width() * img.getPixelSize();
-	const int h = img.height();
-	const int pad = img.getRowSize();
-	unsigned char *dst = img.getRawImage ();
+    const int w = img.width() * img.getPixelSize();
+    const int h = img.height();
+    const int pad = img.getRowSize();
+    unsigned char *dst = img.getRawImage ();
 
-	num = 0;
-	for (int i = 0; i < h; i++)
+    num = 0;
+    for (int i = 0; i < h; i++)
         {
             num += (int)fread((void *) dst, 1, (size_t) w, fp);
             dst += pad;
         }
     
-	fclose(fp);
+    fclose(fp);
 
-	return true;
+    return true;
 }
+
 
 static bool ImageReadBGR(ImageOf<PixelBgr> &img, const char *filename)
 {
-   	int width, height, color, num;
+    int width, height, color, num;
     FILE *fp=0;
-	fp = fopen(filename, "rb");
+    fp = fopen(filename, "rb");
 
     if(fp==0)
         {
@@ -300,7 +309,7 @@ static bool ImageReadBGR(ImageOf<PixelBgr> &img, const char *filename)
             return false;
         }
 
-	if (!ReadHeader(fp, &height, &width, &color))
+    if (!ReadHeader(fp, &height, &width, &color))
         {
             fclose (fp);
             fprintf(stderr, "Error reading header, is file a valid ppm/pgm?\n");
@@ -312,32 +321,33 @@ static bool ImageReadBGR(ImageOf<PixelBgr> &img, const char *filename)
             fprintf(stderr, "File is grayscale, conversion not yet supported\n");
             return false;
         }
-	
+    
     ImageOf<PixelRgb> tmpImg;
     tmpImg.resize(width, height);
 
-	const int w = tmpImg.width() * img.getPixelSize();
-	const int h = tmpImg.height();
-	const int pad = tmpImg.getRowSize();
-	unsigned char *dst = tmpImg.getRawImage ();
+    const int w = tmpImg.width() * img.getPixelSize();
+    const int h = tmpImg.height();
+    const int pad = tmpImg.getRowSize();
+    unsigned char *dst = tmpImg.getRawImage ();
 
-	num = 0;
-	for (int i = 0; i < h; i++)
+    num = 0;
+    for (int i = 0; i < h; i++)
         {
             num += (int)fread((void *) dst, 1, (size_t) w, fp);
             dst += pad;
         }
 
-	fclose(fp);
-	
-	return img.copy(tmpImg);
+    fclose(fp);
+    
+    return img.copy(tmpImg);
 }
+
 
 static bool ImageReadMono(ImageOf<PixelMono> &img, const char *filename)
 {
-   	int width, height, color, num;
+    int width, height, color, num;
     FILE *fp=0;
-	fp = fopen(filename, "rb");
+    fp = fopen(filename, "rb");
 
     if(fp==0)
         {
@@ -345,7 +355,7 @@ static bool ImageReadMono(ImageOf<PixelMono> &img, const char *filename)
             return false;
         }
 
-	if (!ReadHeader(fp, &height, &width, &color))
+    if (!ReadHeader(fp, &height, &width, &color))
         {
             fclose (fp);
             fprintf(stderr, "Error reading header, is file a valid ppm/pgm?\n");
@@ -357,56 +367,62 @@ static bool ImageReadMono(ImageOf<PixelMono> &img, const char *filename)
             fprintf(stderr, "File is color, conversion not yet supported\n");
             return false;
         }
-	
+    
     img.resize(width,height);
 
-	const int w = img.width() * img.getPixelSize();
-	const int h = img.height();
-	const int pad = img.getRowSize();
-	unsigned char *dst = img.getRawImage ();
+    const int w = img.width() * img.getPixelSize();
+    const int h = img.height();
+    const int pad = img.getRowSize();
+    unsigned char *dst = img.getRawImage ();
 
-	num = 0;
-	for (int i = 0; i < h; i++)
+    num = 0;
+    for (int i = 0; i < h; i++)
         {
             num += (int)fread((void *) dst, 1, (size_t) w, fp);
             dst += pad;
         }
 
-	fclose(fp);
+    fclose(fp);
 
     return true;
 }
+
 
 static bool ImageWriteRGB(ImageOf<PixelRgb>& img, const char *filename)
 {
     return SavePPM((char*)img.getRawImage(),filename,img.height(),img.width(),img.getRowSize());
 }
 
+
 static bool ImageWriteBGR(ImageOf<PixelBgr>& img, const char *filename)
 {
-	ImageOf<PixelRgb> img2;
-	img2.copy(img);
-	return SavePPM((char*)img2.getRawImage(),filename,img2.height(), img2.width(), img2.getRowSize());	  
+    ImageOf<PixelRgb> img2;
+    img2.copy(img);
+    return SavePPM((char*)img2.getRawImage(),filename,img2.height(), img2.width(), img2.getRowSize());    
 }
+
 
 static bool ImageWriteMono(ImageOf<PixelMono>& img, const char *filename)
 {
     return SavePGM((char*)img.getRawImage(), filename, img.height(), img.width(), img.getRowSize());
 }
 
+
 bool file::read(ImageOf<PixelRgb> & dest, const ConstString& src)
 {
     return ImageReadRGB(dest,src.c_str());
 }
+
 
 bool file::read(ImageOf<PixelBgr> & dest, const ConstString& src)
 {
     return ImageReadBGR(dest,src.c_str());
 }
 
+
 bool file::read(ImageOf<PixelRgba> & dest, const ConstString& src)
 {
-	ImageOf<PixelRgb> img2;
+    ImageOf<PixelRgb> img2;
     bool ok = ImageReadRGB(img2,src.c_str());
     if (ok) {
         dest.copy(img2);
@@ -414,27 +430,32 @@ bool file::read(ImageOf<PixelRgba> & dest, const ConstString& src)
     return ok;
 }
 
+
 bool file::write(const ImageOf<PixelRgb> & src, const ConstString& dest)
 {
     return ImageWriteRGB(const_cast<ImageOf<PixelRgb> &>(src), dest.c_str());
 }
+
 
 bool file::write(const ImageOf<PixelBgr> & src, const ConstString& dest)
 {
     return ImageWriteBGR(const_cast<ImageOf<PixelBgr> &>(src), dest.c_str());
 }
 
+
 bool file::write(const ImageOf<PixelRgba> & src, const ConstString& dest)
 {
-	ImageOf<PixelRgb> img2;
-	img2.copy(src);
+    ImageOf<PixelRgb> img2;
+    img2.copy(src);
     return ImageWriteRGB(const_cast<ImageOf<PixelRgb> &>(img2), dest.c_str());
 }
+
 
 bool file::read(ImageOf<PixelMono> & dest, const ConstString& src)
 {
     return ImageReadMono(dest,src.c_str());
 }
+
 
 bool file::write(const ImageOf<PixelMono> & src, const ConstString& dest)
 {
@@ -444,8 +465,22 @@ bool file::write(const ImageOf<PixelMono> & src, const ConstString& dest)
 
 bool file::write(const Image& src, const ConstString& dest)
 {
-    ImageOf<PixelRgb> img;
-    img.copy(src);
-    return write(img,dest);
+    int code=src.getPixelCode();
+    if ((code==VOCAB_PIXEL_MONO) || (code==VOCAB_PIXEL_MONO16))
+        return write(static_cast<const ImageOf<PixelMono>&>(src),dest);
+    else if (code==VOCAB_PIXEL_MONO_FLOAT)
+        return write(static_cast<const ImageOf<PixelFloat>&>(src),dest);
+    else if (code==VOCAB_PIXEL_BGR)
+        return write(static_cast<const ImageOf<PixelBgr>&>(src),dest);
+    else if (code==VOCAB_PIXEL_RGB)
+        return write(static_cast<const ImageOf<PixelRgb>&>(src),dest);
+    else if (code==VOCAB_PIXEL_RGBA)
+        return write(static_cast<const ImageOf<PixelRgba>&>(src),dest);
+    else
+    {
+        ImageOf<PixelRgb> img;
+        img.copy(src);
+        return write(img,dest);
+    }
 }
 
