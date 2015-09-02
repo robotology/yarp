c3cf5f43742e9e26c4578e426b266efdc2e37402
Francesco Romano <francesco.romano.1987@gmail.com>
2015-09-02 18:44:30 +0200

    Fixed issue with MACOSX_BUNDLE and RPATH

---
+++ conf/YarpOSXBundleUpdateRPATH.cmake
@@ -0,0 +1,13 @@
+
+function(yarp_osx_bundle_update_rpath _target_name)
+    if(INSTALL_WITH_RPATH OR ENABLE_FORCE_RPATH)
+        get_target_property(APPNAME "${_target_name}" OUTPUT_NAME)
+        file(RELATIVE_PATH _rel_path "${CMAKE_INSTALL_FULL_BINDIR}/${APPNAME}.app/Contents/MacOS/" "${CMAKE_INSTALL_FULL_LIBDIR}")
+        if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+            get_target_property(CURRENT_RPATH "${_target_name}" INSTALL_RPATH)
+            list(APPEND CURRENT_RPATH "@loader_path/${_rel_path}")
+            set_target_properties("${_target_name}" PROPERTIES INSTALL_RPATH "${CURRENT_RPATH}")
+            get_target_property(CURRENT_RPATH "${_target_name}" INSTALL_RPATH)
+        endif()
+    endif()
+endfunction()
