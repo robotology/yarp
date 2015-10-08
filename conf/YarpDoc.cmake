846168eccfdad046d1205af33abe1739f1eb71e4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-10-08 16:30:48 +0200

    [dox] Improve doxygen generation
    The target dox now generate documentation depending on which of these
    options are enabled:
    
     - YARP_DOXYGEN_HTML
     - YARP_DOXYGEN_HTML_DOCSET (for Apple's Xcode 3)
     - YARP_DOXYGEN_HTML_HTMLHELP (for Microsoft's HTML Help Workshop)
     - YARP_DOXYGEN_HTML_QHP (for QtDeveloper and KDevelop)
     - YARP_DOXYGEN_HTML_ECLIPSEHELP (for Eclipse)
     - YARP_DOXYGEN_LATEX
     - YARP_DOXYGEN_XML
     - YARP_DOXYGEN_RTF
     - YARP_DOXYGEN_TAGFILE
     - YARP_DOXYGEN_DOCBOOK
     - YARP_DOXYGEN_MAN
    
    The "man" target was removed and manual pages are generated
    automatically if YARP_DOXYGEN_MAN is enabled.

--- conf/YarpDoc.cmake
+++ conf/YarpDoc.cmake
@@ -2,38 +2,129 @@
 # Authors: Paul Fitzpatrick
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
+include(CMakeDependentOption)
+
 find_package(Doxygen)
 
 if(DOXYGEN_FOUND)
-    set(DOX_GENERATE_HTML YES)
-    set(DOX_GENERATE_XML NO)
-    set(DOX_GENERATE_TAGFILE "")
-    option(YARP_DOXYGEN_XML "Generate doxygen XML output, for use by SWIG" NO)
-    option(YARP_DOXYGEN_TAGFILE "Generate doxygen tag file, see http://www.doxygen.nl/manual/external.html" NO)
-    mark_as_advanced(YARP_DOXYGEN_XML YARP_DOXYGEN_TAGFILE)
-    if(YARP_DOXYGEN_XML)
+  option(YARP_DOXYGEN_HTML "Generate doxygen HTML output" ON)
+  cmake_dependent_option(YARP_DOXYGEN_HTML_DOCSET "Generate doxygen docset output (for Apple's Xcode 3)" OFF
+                         YARP_DOXYGEN_HTML OFF)
+  cmake_dependent_option(YARP_DOXYGEN_HTML_HTMLHELP "Generate doxygen htmlhelp output (for Microsoft's HTML Help Workshop)" OFF
+                         YARP_DOXYGEN_HTML OFF)
+  cmake_dependent_option(YARP_DOXYGEN_HTML_QHP "Generate doxygen Qt Compressed Help output (for QtCreator and KDevelop)" OFF
+                         YARP_DOXYGEN_HTML OFF)
+  cmake_dependent_option(YARP_DOXYGEN_HTML_ECLIPSEHELP "Generate doxygen Eclipse help plugin output (for Eclipse)" OFF
+                         YARP_DOXYGEN_HTML OFF)
+  option(YARP_DOXYGEN_LATEX "Generate doxygen LATEX output" OFF)
+  option(YARP_DOXYGEN_XML "Generate doxygen XML output, for use by SWIG" OFF)
+  option(YARP_DOXYGEN_RTF "Generate doxygen RTF output" OFF)
+  option(YARP_DOXYGEN_TAGFILE "Generate doxygen tag file (see http://www.doxygen.nl/manual/external.html)" OFF)
+  option(YARP_DOXYGEN_DOCBOOK "Generate doxygen DOCBOOK output" OFF)
+  option(YARP_DOXYGEN_MAN "Generate doxygen MAN output" OFF)
+
+  mark_as_advanced(YARP_DOXYGEN_HTML
+                   YARP_DOXYGEN_HTML_DOCSET
+                   YARP_DOXYGEN_HTML_HTMLHELP
+                   YARP_DOXYGEN_HTML_QHP
+                   YARP_DOXYGEN_HTML_ECLIPSEHELP
+                   YARP_DOXYGEN_LATEX
+                   YARP_DOXYGEN_XML
+                   YARP_DOXYGEN_RTF
+                   YARP_DOXYGEN_TAGFILE
+                   YARP_DOXYGEN_DOCBOOK
+                   YARP_DOXYGEN_MAN)
+
+  set(DOX_FILE Doxyfile)
+
+  if(YARP_DOXYGEN_HTML OR
+     YARP_DOXYGEN_LATEX OR
+     YARP_DOXYGEN_XML OR
+     YARP_DOXYGEN_RTF OR
+     YARP_DOXYGEN_TAGFILE OR
+     YARP_DOXYGEN_MAN OR
+     YARP_DOXYGEN_DOCBOOK)
+    add_custom_target(dox SOURCES "${YARP_MODULE_DIR}/template/${DOX_FILE}.in")
+
+    if(YARP_DOXYGEN_HTML OR
+       YARP_DOXYGEN_LATEX OR
+       YARP_DOXYGEN_XML OR
+       YARP_DOXYGEN_RTF OR
+       YARP_DOXYGEN_TAGFILE OR
+       YARP_DOXYGEN_DOCBOOK)
+
+      # Prepare configuration for normal documentation
+      set(DOX_PATTERNS "*.h *.dox *.cpp")
+      set(DOX_GENERATE_HTML NO)
+      set(DOX_GENERATE_DOCSET NO)
+      set(DOX_GENERATE_HTMLHELP NO)
+      set(DOX_GENERATE_QHP NO)
+      set(DOX_GENERATE_ECLIPSEHELP NO)
+      set(DOX_GENERATE_LATEX NO)
+      set(DOX_GENERATE_XML NO)
+      set(DOX_GENERATE_RTF NO)
+      set(DOX_GENERATE_DOCBOOK NO)
+      set(DOX_GENERATE_TAGFILE "")
+      set(DOX_GENERATE_MAN NO)
+
+      if(YARP_DOXYGEN_HTML)
+        set(DOX_GENERATE_HTML YES)
+        if(YARP_DOXYGEN_HTML_DOCSET)
+          set(DOX_GENERATE_DOCSET YES)
+        endif()
+        if(YARP_DOXYGEN_HTML_HTMLHELP)
+          set(DOX_GENERATE_HTMLHELP YES)
+        endif()
+        if(YARP_DOXYGEN_HTML_QHP)
+          set(DOX_GENERATE_QHP YES)
+        endif()
+        if(YARP_DOXYGEN_HTML_ECLIPSEHELP)
+          set(DOX_GENERATE_ECLIPSEHELP YES)
+        endif()
+      endif()
+      if(YARP_DOXYGEN_LATEX)
+        set(DOX_GENERATE_LATEX YES)
+      endif()
+      if(YARP_DOXYGEN_XML)
         set(DOX_GENERATE_XML YES)
-    endif()
-    if(YARP_DOXYGEN_TAGFILE)
+      endif()
+      if(YARP_DOXYGEN_RTF)
+        set(DOX_GENERATE_RTF YES)
+      endif()
+      if(YARP_DOXYGEN_DOCBOOK)
+        set(DOX_GENERATE_DOCBOOK YES)
+      endif()
+      if(YARP_DOXYGEN_TAGFILE)
         set(DOX_GENERATE_TAGFILE "${CMAKE_BINARY_DIR}/dox/YARP.tag")
+      endif()
+
+      configure_file("${YARP_MODULE_DIR}/template/${DOX_FILE}.in"
+                     "${CMAKE_BINARY_DIR}/dox/${DOX_FILE}")
+      add_custom_command(TARGET dox
+                         COMMAND "${DOXYGEN_EXECUTABLE}" "${CMAKE_BINARY_DIR}/dox/${DOX_FILE}"
+                         COMMENT "Generating doxygen documentation")
     endif()
 
-    set(DOX_FILE Doxyfile.part)
-    # YARP documentation is always verbose now.  It used to be
-    # partially stripped of stuff less interesting to end-users.
-    set(DOX_PATTERNS "*.h *.dox *.cpp")
-    set(DOX_GENERATE_MAN NO)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/doxygen/${DOX_FILE}.template
-                   ${CMAKE_BINARY_DIR}/dox/${DOX_FILE})
-    add_custom_target(dox COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/dox/${DOX_FILE})
+    if(YARP_DOXYGEN_MAN)
 
-    set(DOX_GENERATE_HTML NO)
-    set(DOX_GENERATE_MAN YES)
-    set(DOX_GENERATE_XML NO)
-    set(DOX_GENERATE_TAGFILE "")
-    set(DOX_PATTERNS "cmd_*.dox")
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/doxygen/${DOX_FILE}.template
-                   ${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man)
-    add_custom_target(man COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man)
-
-endif(DOXYGEN_FOUND)
+      # Prepare configuration for man documentation
+      set(DOX_PATTERNS "cmd_*.dox")
+      set(DOX_GENERATE_HTML NO)
+      set(DOX_GENERATE_HTML_DOCSET NO)
+      set(DOX_GENERATE_HTML_HTMLHELP NO)
+      set(DOX_GENERATE_HTML_QHP NO)
+      set(DOX_GENERATE_HTML_ECLIPSEHELP NO)
+      set(DOX_GENERATE_LATEX NO)
+      set(DOX_GENERATE_XML NO)
+      set(DOX_GENERATE_RTF NO)
+      set(DOX_GENERATE_DOCBOOK NO)
+      set(DOX_GENERATE_TAGFILE "")
+      set(DOX_GENERATE_MAN YES)
+      configure_file(${YARP_MODULE_DIR}/template/${DOX_FILE}.in
+                     ${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man)
+      add_custom_command(TARGET dox
+                         COMMAND "${DOXYGEN_EXECUTABLE}" "${CMAKE_BINARY_DIR}/dox/${DOX_FILE}.man"
+                         COMMENT "Generating man pages")
+    endif()
+  endif()
+endif()
