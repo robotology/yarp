0635a1444b85d2267684ce6a91295857d8353a12
Francesco Romano <francesco.romano.1987@gmail.com>
2015-10-12 18:33:41 +0200

    Removed bundle option to generate OS X bundles

--- conf/YarpOptions.cmake
+++ conf/YarpOptions.cmake
@@ -308,19 +308,9 @@ endif()
 
 
 #########################################################################
-# If system is OSX add the option to enable / disable Bundle generation
-if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    option(YARP_OSX_GENERATE_BUNDLES "Generate OSX bundles for Yarp (.app) instead of plain UNIX binaries" TRUE)
-    mark_as_advanced(YARP_OSX_GENERATE_BUNDLES)
-    set(MACOSX_BUNDLE_COPYRIGHT "© Istituto Italiano di Tecnologia and RobotCub Consortium. YARP is released under the terms of the LGPL v2.1 or later.")
-    set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${YARP_VERSION_STRING}")
-    # this should be removed as soon as CMake is updated
-    execute_process(COMMAND /usr/bin/sw_vers -productVersion OUTPUT_VARIABLE OSX_VERSION)
-    string(REPLACE "." ";" OSX_VERSION_LIST ${OSX_VERSION})
-    list(GET OSX_VERSION_LIST 0 OSX_VERSION_MAJOR)
-    list(GET OSX_VERSION_LIST 1 OSX_VERSION_MINOR)
-    set(CMAKE_OSX_DEPLOYMENT_TARGET "${OSX_VERSION_MAJOR}.${OSX_VERSION_MINOR}")
-endif()
+# Specify yarp version and copyright into OSX bundles
+set(MACOSX_BUNDLE_COPYRIGHT "© Istituto Italiano di Tecnologia and RobotCub Consortium. YARP is released under the terms of the LGPL v2.1 or later.")
+set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${YARP_VERSION_STRING}")
 
 
 #########################################################################
