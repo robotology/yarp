2ae37918fc9982e4780d5531e194db5025184ce4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-01-05 18:56:18 +0100

    Cleanup

--- bindings/src/LoadYarp.java
+++ bindings/src/LoadYarp.java
@@ -6,28 +6,24 @@
 
 import yarp.Network;
 
-//run this to load the yarp library and initialize the network in Java
+// run this to load the yarp library and initialize the network in Java
 
-class LoadYarp{
-	private static int yarpInitialized=0;
+class LoadYarp {
+    private static int yarpInitialized = 0;
 
-	public static int isLoaded()
-	{ return yarpInitialized; }
+    public static int isLoaded() { return yarpInitialized; }
 
     public LoadYarp() {
-		if (yarpInitialized==0)
-			{
-				System.loadLibrary("jyarp");
-				Network.init(); 
-				System.out.println("Yarp library loaded and initialized");
-				System.out.flush();
-			}
-		else
-			{
-				System.out.println("Yarp library already loaded and initialized, doing nothing");
-				System.out.flush();
-			}
-			
-		yarpInitialized=1;
+        if (yarpInitialized == 0) {
+            System.loadLibrary("jyarp");
+            Network.init();
+            System.out.println("Yarp library loaded and initialized");
+            System.out.flush();
+        } else {
+            System.out.println(
+                "Yarp library already loaded and initialized, doing nothing");
+            System.out.flush();
+        }
+        yarpInitialized = 1;
     }
 }
