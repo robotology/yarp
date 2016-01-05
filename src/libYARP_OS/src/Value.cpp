efde89b7291ebfaae979750183f57fc6f29187e3
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-01-05 13:53:48 +0100

    Cleanup

--- src/libYARP_OS/src/Value.cpp
+++ src/libYARP_OS/src/Value.cpp
@@ -26,7 +26,8 @@ Value::Value() :
 Value::Value(int x, bool isVocab) :
         Portable(),
         Searchable(),
-        proxy(NULL) {
+        proxy(NULL)
+{
     if (!isVocab) {
         setProxy(makeInt(x));
     } else {
@@ -37,14 +38,16 @@ Value::Value(int x, bool isVocab) :
 Value::Value(double x) :
         Portable(),
         Searchable(),
-        proxy(NULL) {
+        proxy(NULL)
+{
     setProxy(makeDouble(x));
 }
 
 Value::Value(const ConstString& str, bool isVocab) :
         Portable(),
         Searchable(),
-        proxy(NULL) {
+        proxy(NULL)
+{
     if (!isVocab) {
         setProxy(makeString(str));
     } else {
@@ -55,19 +58,22 @@ Value::Value(const ConstString& str, bool isVocab) :
 Value::Value(void *data, int length) :
         Portable(),
         Searchable(),
-        proxy(NULL) {
+        proxy(NULL)
+{
     setProxy(makeBlob(data, length));
 }
 
 Value::Value(const Value& alt) :
         Portable(),
         Searchable(),
-        proxy(NULL) {
+        proxy(NULL)
+{
     setProxy(alt.clone());
 }
 
 
-const Value& Value::operator = (const Value& alt) {
+const Value& Value::operator=(const Value& alt)
+{
     if(&alt != this) {
         if (proxy == 0) {
             if (isLeaf() && alt.proxy) {
@@ -98,105 +104,125 @@ const Value& Value::operator = (const Value& alt) {
     return *this;
 }
 
-Value::~Value() {
+Value::~Value()
+{
     if (proxy != NULL) {
         delete proxy;
         proxy = NULL;
     }
 }
 
-bool Value::isBool() const {
+bool Value::isBool() const
+{
     ok();
     return proxy->isBool();
 }
 
-bool Value::isInt() const {
+bool Value::isInt() const
+{
     ok();
     return proxy->isInt();
 }
 
-bool Value::isInt64() const {
+bool Value::isInt64() const
+{
     ok();
     return proxy->isInt64();
 }
 
-bool Value::isString() const {
+bool Value::isString() const
+{
     ok();
     return proxy->isString();
 }
 
-bool Value::isDouble() const {
+bool Value::isDouble() const
+{
     ok();
     return proxy->isDouble();
 }
 
-bool Value::isList() const {
+bool Value::isList() const
+{
     ok();
     return proxy->isList();
 }
 
-bool Value::isDict() const {
+bool Value::isDict() const
+{
     ok();
     return proxy->isDict();
 }
 
-bool Value::isVocab() const {
+bool Value::isVocab() const
+{
     ok();
     return proxy->isVocab();
 }
 
-bool Value::isBlob() const {
+bool Value::isBlob() const
+{
     ok();
     return proxy->isBlob();
 }
 
-bool Value::asBool() const {
+bool Value::asBool() const
+{
     ok();
     return proxy->asBool();
 }
 
-int Value::asInt() const {
+int Value::asInt() const
+{
     ok();
     return proxy->asInt();
 }
 
-YARP_INT64 Value::asInt64() const {
+YARP_INT64 Value::asInt64() const
+{
     ok();
     return proxy->asInt64();
 }
 
-int Value::asVocab() const {
+int Value::asVocab() const
+{
     ok();
     return proxy->asVocab();
 }
 
-double Value::asDouble() const {
+double Value::asDouble() const
+{
     ok();
     return proxy->asDouble();
 }
 
-ConstString Value::asString() const {
+ConstString Value::asString() const
+{
     ok();
     return proxy->asString();
 }
 
-Bottle *Value::asList() const {
+Bottle *Value::asList() const
+{
     ok();
     return proxy->asList();
 }
 
-Property *Value::asDict() const {
+Property *Value::asDict() const
+{
     ok();
     return proxy->asDict();
 }
 
-Searchable *Value::asSearchable() const {
+Searchable *Value::asSearchable() const
+{
     ok();
     if (proxy->isDict()) return proxy->asDict();
     return proxy->asList();
 }
 
-const char *Value::asBlob() const {
+const char *Value::asBlob() const
+{
     ok();
     return proxy->asBlob();
 }
@@ -207,7 +233,8 @@ size_t Value::asBlobLength() const
     return proxy->asBlobLength();
 }
 
-bool Value::read(ConnectionReader& connection) {
+bool Value::read(ConnectionReader& connection)
+{
     if (proxy) {
         delete proxy;
         proxy = 0;
@@ -230,7 +257,8 @@ bool Value::read(ConnectionReader& connection) {
     return s->readRaw(connection);
 }
 
-bool Value::write(ConnectionWriter& connection) {
+bool Value::write(ConnectionWriter& connection)
+{
     if (!proxy) {
         connection.appendInt(BOTTLE_TAG_LIST);
         connection.appendInt(0);
@@ -241,28 +269,33 @@ bool Value::write(ConnectionWriter& connection) {
     return proxy->write(connection);
 }
 
-bool Value::check(const ConstString& key) const {
+bool Value::check(const ConstString& key) const
+{
     ok();
     return proxy->check(key);
 }
 
-Value& Value::find(const ConstString& key) const {
+Value& Value::find(const ConstString& key) const
+{
     ok();
     return proxy->find(key);
 }
 
-Bottle& Value::findGroup(const ConstString& key) const {
+Bottle& Value::findGroup(const ConstString& key) const
+{
     ok();
     return proxy->findGroup(key);
 }
 
-bool Value::operator == (const Value& alt) const {
+bool Value::operator==(const Value& alt) const
+{
     ok();
     return (*proxy)==alt;
 }
 
 
-bool Value::operator != (const Value& alt) const {
+bool Value::operator!=(const Value& alt) const
+{
     return !((*this)==alt);
 }
 
@@ -270,72 +303,86 @@ void Value::fromString(const char *str) {
     setProxy(makeValue(str));
 }
 
-ConstString Value::toString() const {
+ConstString Value::toString() const
+{
     ok();
     return proxy->toString();
 }
 
-Value *Value::create() const {
+Value *Value::create() const
+{
     ok();
     return proxy->create();
 }
 
-Value *Value::clone() const  {
+Value *Value::clone() const
+{
     ok();
     return proxy->clone();
 }
 
-int Value::getCode() const {
+int Value::getCode() const
+{
     ok();
     return proxy->getCode();
 }
 
-bool Value::isNull() const  {
+bool Value::isNull() const
+{
     ok();
     return proxy->isNull();
 }
 
-bool Value::isLeaf() const {
+bool Value::isLeaf() const
+{
     return false;
 }
 
-Value *Value::makeInt(int x) {
+Value *Value::makeInt(int x)
+{
     return new StoreInt(x);
 }
 
 
-Value *Value::makeDouble(double x) {
+Value *Value::makeDouble(double x)
+{
     return new StoreDouble(x);
 }
 
 
-Value *Value::makeString(const ConstString& str) {
+Value *Value::makeString(const ConstString& str)
+{
     return new StoreString(str);
 }
 
 
-Value *Value::makeVocab(int v) {
+Value *Value::makeVocab(int v)
+{
     return new StoreVocab(v);
 }
 
 
-Value *Value::makeVocab(const ConstString& str) {
+Value *Value::makeVocab(const ConstString& str)
+{
     return new StoreVocab(Vocab::encode(str));
 }
 
 
-Value *Value::makeBlob(void *data, int length) {
+Value *Value::makeBlob(void *data, int length)
+{
     String s((char*)data,length);
     return new StoreBlob(s);
 }
 
 
-Value *Value::makeList() {
+Value *Value::makeList()
+{
     return new StoreList();
 }
 
 
-Value *Value::makeList(const char *txt) {
+Value *Value::makeList(const char *txt)
+{
     Value *v = makeList();
     if (v!=NULL) {
         v->asList()->fromString(txt);
@@ -344,7 +391,8 @@ Value *Value::makeList(const char *txt) {
 }
 
 
-Value *Value::makeValue(const ConstString& txt) {
+Value *Value::makeValue(const ConstString& txt)
+{
     Bottle bot(txt);
     if (bot.size()>1) {
         return makeString(txt);
@@ -353,12 +401,14 @@ Value *Value::makeValue(const ConstString& txt) {
 }
 
 
-Value& Value::getNullValue() {
+Value& Value::getNullValue()
+{
     return BottleImpl::getNull();
 }
 
 
-void Value::setProxy(Value *proxy) {
+void Value::setProxy(Value *proxy)
+{
     if (this->proxy!=NULL) {
         delete this->proxy;
         this->proxy = NULL;
@@ -368,13 +418,10 @@ void Value::setProxy(Value *proxy) {
 }
 
 
-void Value::ok() const {
+void Value::ok() const
+{
     const Value *op = this;
     if (proxy==NULL) {
         ((Value*)op)->setProxy(makeList());
     }
 }
-
-
-
-
