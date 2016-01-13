282dc215544241fbbd65383049ded62beb3d4468
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-01-13 17:00:46 +0100

    Cleanup and remove code that is not executed since 2010

--- tests/libYARP_OS/TerminatorTest.cpp
+++ tests/libYARP_OS/TerminatorTest.cpp
@@ -1,10 +1,7 @@
-// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
-
 /*
  * Copyright (C) 2006 RobotCub Consortium
  * Authors: Giorgio Metta
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
- *
  */
 
 #include <yarp/os/Terminator.h>
@@ -13,7 +10,6 @@
 #include <yarp/os/Time.h>
 
 #include <yarp/os/impl/UnitTest.h>
-//#include "TestList.h"
 
 using namespace yarp::os::impl;
 using namespace yarp::os;
@@ -25,12 +21,6 @@ public:
     virtual void testTerminationPair() {
         report(0,"checking terminator connection");
 
-#if WIN32NURG
-        // Lorenzo: skip test because it caused deadlock and slowed 
-        // down automatic tests (I could not change the default ctest timeout
-        // of 1500s).
-        report(1,"testTerminationPair fails");
-#else
         Network::setLocalMode(true);
 
         ACE_OS::printf("registering port name: ");
@@ -41,8 +31,6 @@ public:
             ACE_OS::printf("failed\n");
             report(1,"failed to set terminator socket");
         }
-        //Time::delay(1.4);
-
 
         ACE_OS::printf("sending quit message: ");
         if (Terminator::terminateByName("/tmp/quit"))
@@ -51,7 +39,6 @@ public:
             ACE_OS::printf("failed\n");
             report(1,"failed to set termination connection");
         }
-        //Time::delay(1);
 
         ACE_OS::printf("quit flag was set properly: ");
         if (!terminee.mustQuit()) {
@@ -61,7 +48,6 @@ public:
         else {
             ACE_OS::printf("ok\n");
         }
-#endif 
     }
 
     virtual void runTests() {
@@ -74,4 +60,3 @@ static TerminatorTest theTerminatorTest;
 UnitTest& getTerminatorTest() {
     return theTerminatorTest;
 }
-
