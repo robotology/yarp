cd1c6c81e6122eecaa4fef795667035a35b2f2ba
Alberto Cardellino <alberto.cardellino@iit.it>
2016-01-21 17:06:07 +0100

    yarp_dev: definition of new methods for IAmplifierControl

--- src/libYARP_dev/include/yarp/dev/ControlBoardInterfaces.h
+++ src/libYARP_dev/include/yarp/dev/ControlBoardInterfaces.h
@@ -383,38 +383,6 @@ public:
      */
     virtual bool disableAmp(int j)=0;
 
-    /* Read the electric current going to all motors.
-     * @param vals pointer to storage for the output values
-     * @return hopefully true, false in bad luck.
-     */
-    virtual bool getCurrents(double *vals)=0;
-
-    /* Read the electric current going to a given motor.
-     * @param j motor number
-     * @param val pointer to storage for the output value
-     * @return probably true, might return false in bad times
-     */
-    virtual bool getCurrent(int j, double *val)=0;
-
-    /**
-    * Returns the maximum electric current allowed for a given motor. The behavior
-    * of the board/amplifier when this limit is reached depends on the
-    * implementation.
-    * @param j motor number
-    * @param v the return value
-    * @return probably true, might return false in bad times
-    */
-    virtual bool getMaxCurrent(int j, double *v)=0;
-
-    /* Set the maximum electric current going to a given motor. The behavior
-     * of the board/amplifier when this limit is reached depends on the
-     * implementation.
-     * @param j motor number
-     * @param v the new value
-     * @return probably true, might return false in bad times
-     */
-    virtual bool setMaxCurrent(int j, double v)=0;
-
     /* Get the status of the amplifiers, coded in a 32 bits integer for
      * each amplifier (at the moment contains only the fault, it will be
      * expanded in the future).
@@ -429,6 +397,100 @@ public:
      * @return true/false success failure.
      */
     virtual bool getAmpStatus(int j, int *v)=0;
+
+    /* Read the electric current going to all motors.
+     * @param vals pointer to storage for the output values
+     * @return hopefully true, false in bad luck.
+     */
+    virtual bool getCurrents(double *vals)=0;
+
+    /* Read the electric current going to a given motor.
+     * @param j motor number
+     * @param val pointer to storage for the output value
+     * @return probably true, might return false in bad times
+     */
+    virtual bool getCurrent(int j, double *val)=0;
+
+    /**
+    * Returns the maximum electric current allowed for a given motor.
+    * Exceeding this value will trigger instantaneous hardware fault.
+    * @param j motor number
+    * @param v the return value
+    * @return probably true, might return false in bad times
+    */
+    virtual bool getMaxCurrent(int j, double *v)=0;
+
+    /* Set the maximum electric current going to a given motor.
+     * Exceeding this value will trigger instantaneous hardware fault.
+     * @param j motor number
+     * @param v the new value
+     * @return probably true, might return false in bad times
+     */
+    virtual bool setMaxCurrent(int j, double v)=0;
+
+    /* Get the the nominal current which can be kept for an indefinite amount of time
+     * without harming the motor. This value is specific for each motor and it is typically
+     * found in its datasheet. The units are Ampere.
+     * This value and the peak current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool getNominalCurrent(int m, double *val) {return false;};
+
+    /* Get the the peak current which causes damage to the motor if maintained
+     * for a long amount of time.
+     * The value is often found in the motor datasheet, units are Ampere.
+     * This value and the nominal current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool getPeakCurrent(int m, double *val) {return false;};
+
+    /* Set the the peak current. This value  which causes damage to the motor if maintained
+     * for a long amount of time.
+     * The value is often found in the motor datasheet, units are Ampere.
+     * This value and the nominal current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool setPeakCurrent(int m, const double val) {return false;};
+
+    /* Get the the current PWM value used to control the motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val filled with PWM value.
+     * @return true/false success failure.
+     */
+    virtual bool getPWM(int j, double* val) {return false;};
+
+    /* Get the PWM limit fot the given motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val filled with PWM limit value.
+     * @return true/false success failure.
+     */
+    virtual bool getPWMLimit(int j, double* val) {return false;};
+
+    /* Set the PWM limit fot the given motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val new value for the PWM limit.
+     * @return true/false success failure.
+     */
+    virtual bool setPWMLimit(int j, const double val) {return false;};
+
+    /* Get the power source voltage for the given motor in Volt.
+     * @param j joint number
+     * @param val filled with return value.
+     * @return true/false success failure.
+     */
+    virtual bool getPowerSupplyVoltage(int j, double* val) {return false;};
 };
 
 /**
@@ -457,38 +519,6 @@ public:
      */
     virtual bool disableAmpRaw(int j)=0;
 
-    /* Read the electric current going to all motors.
-     * @param vals pointer to storage for the output values
-     * @return hopefully true, false in bad luck.
-     */
-    virtual bool getCurrentsRaw(double *vals)=0;
-
-    /* Read the electric current going to a given motor.
-     * @param j motor number
-     * @param val pointer to storage for the output value
-     * @return probably true, might return false in bad times
-     */
-    virtual bool getCurrentRaw(int j, double *val)=0;
-
-    /* Set the maximum electric current going to a given motor. The behavior
-     * of the board/amplifier when this limit is reached depends on the
-     * implementation.
-     * @param j motor number
-     * @param v the new value
-     * @return probably true, might return false in bad times
-     */
-    virtual bool setMaxCurrentRaw(int j, double v)=0;
-
-    /**
-    * Returns the maximum electric current allowed for a given motor. The behavior
-    * of the board/amplifier when this limit is reached depends on the
-    * implementation.
-    * @param j motor number
-    * @param v the return value
-    * @return probably true, might return false in bad times
-    */
-    virtual bool getMaxCurrentRaw(int j, double *v)=0;
-
     /* Get the status of the amplifiers, coded in a 32 bits integer for
      * each amplifier (at the moment contains only the fault, it will be
      * expanded in the future).
@@ -503,6 +533,100 @@ public:
      * @return true/false success failure.
      */
     virtual bool getAmpStatusRaw(int j, int *st)=0;
+
+    /* Read the electric current going to all motors.
+     * @param vals pointer to storage for the output values
+     * @return hopefully true, false in bad luck.
+     */
+    virtual bool getCurrentsRaw(double *vals)=0;
+
+    /* Read the electric current going to a given motor.
+     * @param j motor number
+     * @param val pointer to storage for the output value
+     * @return probably true, might return false in bad times
+     */
+    virtual bool getCurrentRaw(int j, double *val)=0;
+
+    /* Set the maximum electric current going to a given motor.
+     * Exceeding this value will trigger instantaneous hardware fault.
+     * @param j motor number
+     * @param v the new value
+     * @return probably true, might return false in bad times
+     */
+    virtual bool setMaxCurrentRaw(int j, double v)=0;
+
+    /**
+    * Returns the maximum electric current allowed for a given motor.
+    * Exceeding this value will trigger instantaneous hardware fault.
+    * @param j motor number
+    * @param v the return value
+    * @return probably true, might return false in bad times
+    */
+    virtual bool getMaxCurrentRaw(int j, double *v)=0;
+
+    /* Get the the nominal current which can be kept for an indefinite amount of time
+     * without harming the motor. This value is specific for each motor and it is typically
+     * found in its datasheet. The units are Ampere.
+     * This value and the peak current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool getNominalCurrentRaw(int m, double *val) {return false;};
+
+    /* Get the the peak current which causes damage to the motor if maintained
+     * for a long amount of time.
+     * The value is often found in the motor datasheet, units are Ampere.
+     * This value and the nominal current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool getPeakCurrentRaw(int m, double *val) {return false;};
+
+    /* Set the the peak current. This value  which causes damage to the motor if maintained
+     * for a long amount of time.
+     * The value is often found in the motor datasheet, units are Ampere.
+     * This value and the nominal current may be used by the firmware to configure
+     * an I2T filter.
+     * @param j joint number
+     * @param val storage for return value. [Ampere]
+     * @return true/false success failure.
+     */
+    virtual bool setPeakCurrentRaw(int m, const double val) {return false;};
+
+    /* Get the the current PWM value used to control the motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val filled with PWM value.
+     * @return true/false success failure.
+     */
+    virtual bool getPWMRaw(int j, double* val) {return false;};
+
+    /* Get the PWM limit fot the given motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val filled with PWM limit value.
+     * @return true/false success failure.
+     */
+    virtual bool getPWMLimitRaw(int j, double* val) {return false;};
+
+    /* Set the PWM limit fot the given motor.
+     * The units are firmware dependent, either machine units or percentage.
+     * @param j joint number
+     * @param val new value for the PWM limit.
+     * @return true/false success failure.
+     */
+    virtual bool setPWMLimitRaw(int j, const double val) {return false;};
+
+    /* Get the power source voltage for the given motor in Volt.
+     * @param j joint number
+     * @param val filled with return value. [Volt]
+     * @return true/false success failure.
+     */
+    virtual bool getPowerSupplyVoltageRaw(int j, double* val) {return false;};
 };
 
 /**
@@ -877,13 +1001,18 @@ public:
 #define VOCAB_VELOCITY_MOVES VOCAB4('v','m','o','s')
 
 // interface IAmplifierControl sets/gets
-#define VOCAB_AMP_ENABLE VOCAB3('a','e','n')
-#define VOCAB_AMP_DISABLE VOCAB3('a','d','i')
-#define VOCAB_AMP_CURRENT VOCAB3('a','c','u')
-#define VOCAB_AMP_CURRENTS VOCAB4('a','c','u','s')
-#define VOCAB_AMP_MAXCURRENT VOCAB4('m','a','x','c')
-#define VOCAB_AMP_STATUS VOCAB4('a','s','t','a')
-#define VOCAB_AMP_STATUS_SINGLE VOCAB4('a','s','t','s')
+#define VOCAB_AMP_ENABLE            VOCAB3('a','e','n')
+#define VOCAB_AMP_DISABLE           VOCAB3('a','d','i')
+#define VOCAB_AMP_STATUS            VOCAB4('a','s','t','a')
+#define VOCAB_AMP_STATUS_SINGLE     VOCAB4('a','s','t','s')
+#define VOCAB_AMP_CURRENT           VOCAB3('a','c','u')
+#define VOCAB_AMP_CURRENTS          VOCAB4('a','c','u','s')
+#define VOCAB_AMP_MAXCURRENT        VOCAB4('m','a','x','c')
+#define VOCAB_AMP_NOMINAL_CURRENT   VOCAB4('a','c','n','o')
+#define VOCAB_AMP_PEAK_CURRENT      VOCAB4('a','c','p','k')
+#define VOCAB_AMP_PWM               VOCAB3('p','w','m')
+#define VOCAB_AMP_PWM_LIMIT         VOCAB4('p','w','m','l')
+#define VOCAB_AMP_VOLTAGE_SUPPLY    VOCAB4('a','v','s','u')
 
 // interface IControlLimits sets/gets
 #define VOCAB_LIMITS VOCAB4('l','l','i','m')
