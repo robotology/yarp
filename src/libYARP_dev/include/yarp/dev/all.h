4b508a03e829f0d62f20902f31be9c089284968d
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-01-28 18:23:44 +0100

    Cleanup

--- src/libYARP_dev/include/yarp/dev/all.h
+++ src/libYARP_dev/include/yarp/dev/all.h
@@ -10,33 +10,33 @@
 #ifndef YARP2_DEV_ALL
 #define YARP2_DEV_ALL
 
-#include<yarp/dev/AudioGrabberInterfaces.h>
-#include<yarp/dev/AudioVisualInterfaces.h>
-#include<yarp/dev/CalibratorInterfaces.h>
-#include<yarp/dev/CartesianControl.h>
-#include<yarp/dev/ControlBoardInterfaces.h>
-#include<yarp/dev/ControlBoardInterfacesImpl.h>
-#include<yarp/dev/ControlBoardPid.h>
-#include<yarp/dev/DataSource.h>
-#include<yarp/dev/DeviceDriver.h>
-#include<yarp/dev/DriverLinkCreator.h>
-#include<yarp/dev/Drivers.h>
-#include<yarp/dev/FrameGrabberInterfaces.h>
-#include<yarp/dev/FrameGrabberControl2.h>
-#include<yarp/dev/IRangefinder2D.h>
-#include<yarp/dev/IDepthSensor.h>
-#include<yarp/dev/IRGBDSensor.h>
-#include<yarp/dev/GazeControl.h>
-#include<yarp/dev/GenericSensorInterfaces.h>
-#include<yarp/dev/GPUInterface.h>
-#include<yarp/dev/IKinectDeviceDriver.h>
-#include<yarp/dev/PolyDriver.h>
-#include<yarp/dev/RemoteFrameGrabber.h>
-#include<yarp/dev/ServerFrameGrabber.h>
-#include<yarp/dev/ServerSoundGrabber.h>
-#include<yarp/dev/ServiceInterfaces.h>
-#include<yarp/dev/TestFrameGrabber.h>
-#include<yarp/dev/TestMotor.h>
-#include<yarp/dev/Wrapper.h>
+#include <yarp/dev/AudioGrabberInterfaces.h>
+#include <yarp/dev/AudioVisualInterfaces.h>
+#include <yarp/dev/CalibratorInterfaces.h>
+#include <yarp/dev/CartesianControl.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/ControlBoardInterfacesImpl.h>
+#include <yarp/dev/ControlBoardPid.h>
+#include <yarp/dev/DataSource.h>
+#include <yarp/dev/DeviceDriver.h>
+#include <yarp/dev/DriverLinkCreator.h>
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/FrameGrabberInterfaces.h>
+#include <yarp/dev/FrameGrabberControl2.h>
+#include <yarp/dev/GazeControl.h>
+#include <yarp/dev/GenericSensorInterfaces.h>
+#include <yarp/dev/GPUInterface.h>
+#include <yarp/dev/IDepthSensor.h>
+#include <yarp/dev/IKinectDeviceDriver.h>
+#include <yarp/dev/IRangefinder2D.h>
+#include <yarp/dev/IRGBDSensor.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/RemoteFrameGrabber.h>
+#include <yarp/dev/ServerFrameGrabber.h>
+#include <yarp/dev/ServerSoundGrabber.h>
+#include <yarp/dev/ServiceInterfaces.h>
+#include <yarp/dev/TestFrameGrabber.h>
+#include <yarp/dev/TestMotor.h>
+#include <yarp/dev/Wrapper.h>
 
 #endif
