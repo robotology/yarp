2ab8aa8df6bac2de31360b27b99a8d609c063940
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-02-25 22:46:47 +0100

    Update swig tests

--- tests/integration/check-bindings.sh
+++ tests/integration/check-bindings.sh
@@ -11,17 +11,19 @@ if [ ! -e $swig_base ]; then
     mkdir -p $swig_base
 fi
 
-# swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11 2.0.12 3.0.0 3.0.1 3.0.2"
-default_swig_versions="1.3.40 2.0.12 3.0.2"
+known_swig_versions="\
+ 1.3.37 1.3.38 1.3.39 1.3.40\
+ 2.0.0 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11 2.0.12\
+ 3.0.0 3.0.1 3.0.2 3.0.3 3.0.4 3.0.5 3.0.6 3.0.7 3.0.8"
+
 if [ -z "$SWIG_VERSIONS" ]; then
-swig_versions="$default_swig_versions"
+    swig_versions="$known_swig_versions"
 else
-swig_versions="$SWIG_VERSIONS"
+    swig_versions="$SWIG_VERSIONS"
 fi
 
-skip_versions="RUBY_1.3.39 RUBY_1.3.40 LUA_1.3.39 LUA_1.3.40 LUA_2.0.11"
-
 SUPPORTED_LANGUAGES="PYTHON JAVA LUA RUBY CSHARP PERL TCL"
+
 
 if [ "$1" = "" ]; then
     echo "Possible SWIG versions are $swig_versions"
@@ -87,17 +89,10 @@ failed=false
 for lang in $SUPPORTED_LANGUAGES; do
     successes="$lang success: "
     failures="$lang failure: "
-    skips="$lang skips: "
     for swig_ver in $swig_versions; do
     echo " "
     echo "================================================================"
     echo "Working on $lang with $swig_ver"
-    echo " $skip_versions " | grep " ${lang}_${swig_ver} " && skip=true || skip=false
-    if $skip; then
-        echo "Skipping $lang $swig_ver"
-        skips="$skips $swig_ver"
-        continue
-    fi
     if [ ! -e "$swig_base/$swig_ver" ]; then
         echo "CANNOT FIND precompiled versions of swig."
         echo "In $PWD, looking in $swig_base/$sig_ver"
@@ -202,7 +197,6 @@ for lang in $SUPPORTED_LANGUAGES; do
     done
     echo $successes >> $log
     echo $failures >> $log
-    echo $skips >> $log
 done
 cat $log
 if $failed; then
