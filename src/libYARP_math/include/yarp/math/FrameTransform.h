3de870f527626e6a30f4f3dc0610c25997e309b2
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-07-21 11:47:13 +0200

    YARP_math/FrameTransform: Add missing include
    Also move toString() implementation to the .cpp file.

--- src/libYARP_math/include/yarp/math/FrameTransform.h
+++ src/libYARP_math/include/yarp/math/FrameTransform.h
@@ -12,13 +12,14 @@
 #include <yarp/math/api.h>
 #include <yarp/math/Math.h>
 
+
 namespace yarp
 {
-    namespace math 
+    namespace math
     {
         class FrameTransform
         {
-            public:           
+            public:
             std::string src_frame_id;
             std::string dst_frame_id;
             double      timestamp;
@@ -170,13 +171,7 @@ namespace yarp
                 return true;
             }
 
-            std::string toString()
-            {
-                char buff[1024];
-                sprintf(buff,"%s -> %s \n tran: %f %f %f \n rot: %f %f %f %f \n\n", src_frame_id.c_str(), dst_frame_id.c_str(), translation.tX, translation.tY, translation.tZ, rotation.rX, rotation.rY, rotation.rZ, rotation.rW);
-                return std::string(buff);
-            }
-
+            std::string toString();
         };
     }
 }
--- src/libYARP_math/src/FrameTransform.cpp
+++ src/libYARP_math/src/FrameTransform.cpp
@@ -4,8 +4,22 @@
 * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 */
 
-#include <yarp/os/Log.h>
-#include <yarp/math/Math.h>
 #include <yarp/math/FrameTransform.h>
 
+#include <cstdio>
 
+std::string yarp::math::FrameTransform::toString()
+{
+    char buff[1024];
+    sprintf(buff, "%s -> %s \n tran: %f %f %f \n rot: %f %f %f %f \n\n",
+                  src_frame_id.c_str(),
+                  dst_frame_id.c_str(),
+                  translation.tX,
+                  translation.tY,
+                  translation.tZ,
+                  rotation.rX,
+                  rotation.rY,
+                  rotation.rZ,
+                  rotation.rW);
+    return std::string(buff);
+}
