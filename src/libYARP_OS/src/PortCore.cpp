robotology/yarp

commit 8cec8d42d9d3b65e24df979074af7494d7e5e4c8

YARP_OS: Improve PortCore list [in|out] commands
They also return now:

* (connectionless 1) when the connection is connectionless (i.e. udp)
* (push 0) when the connection is not push (i.e. mjpeg)

src/libYARP_OS/src/PortCore.cpp
@@ -1472,6 +1472,7 @@ bool PortCore::getEnvelope(PortReader& envelope) {
 
 // Shorthand to create a nested (tag, val) pair to add to a message.
 #define STANZA(name,tag,val) Bottle name; name.addString(tag); name.addString(val.c_str());
+#define STANZA_INT(name,tag,val) Bottle name; name.addString(tag); name.addInt(val);
 
 // Make an RPC connection to talk to a ROS API, send a message, get reply.
 // NOTE: ROS support can now be moved out of here, once all documentation
@@ -1702,6 +1703,15 @@ bool PortCore::adminBlock(ConnectionReader& reader, void *id,
                                 result.addList() = bfrom;
                                 result.addList() = bto;
                                 result.addList() = bcarrier;
+                                Carrier *carrier = Carriers::chooseCarrier(route.getCarrierName());
+                                if (carrier->isConnectionless()) {
+                                    STANZA_INT(bconnectionless, "connectionless", 1);
+                                    result.addList() = bconnectionless;
+                                }
+                                if (!carrier->isPush()) {
+                                    STANZA_INT(breverse, "push", 0);
+                                    result.addList() = breverse;
+                                }
                             }
                         }
                     }
@@ -1730,6 +1740,15 @@ bool PortCore::adminBlock(ConnectionReader& reader, void *id,
                                 result.addList() = bfrom;
                                 result.addList() = bto;
                                 result.addList() = bcarrier;
+                                Carrier *carrier = Carriers::chooseCarrier(route.getCarrierName());
+                                if (carrier->isConnectionless()) {
+                                    STANZA_INT(bconnectionless, "connectionless", 1);
+                                    result.addList() = bconnectionless;
+                                }
+                                if (!carrier->isPush()) {
+                                    STANZA_INT(breverse, "push", 0);
+                                    result.addList() = breverse;
+                                }
                             }
                         }
                     }

commit 0865860e6e90c5e2c6675ea948ed1e8737814cd1

YARP_OS/PortCore: Fix memory leaks
They were introduced by 8cec8d42d9d3b65e24df979074af7494d7e5e4c8

src/libYARP_OS/src/PortCore.cpp
@@ -1714,6 +1714,7 @@ bool PortCore::adminBlock(ConnectionReader& reader, void *id,
                                     STANZA_INT(breverse, "push", 0);
                                     result.addList() = breverse;
                                 }
+                                delete carrier;
                             }
                         }
                     }
@@ -1751,6 +1752,7 @@ bool PortCore::adminBlock(ConnectionReader& reader, void *id,
                                     STANZA_INT(breverse, "push", 0);
                                     result.addList() = breverse;
                                 }
+                                delete carrier;
                             }
                         }
                     }