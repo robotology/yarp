robotology/yarp

commit b1c8f3059d52dfeae5bba35a50caebae4059fdd7

YARP_dev: Add DeprecatedDeviceDriver to allow deprecation of device drivers

src/libYARP_dev/src/PolyDriver.cpp
@@ -270,6 +270,17 @@ bool PolyDriver::coreOpen(yarp::os::Searchable& prop) {
             //YARP_DEBUG(Logger::get(),String("Discarded ") + str);
             driver = NULL;
         } else {
+            yarp::dev::DeprecatedDeviceDriver *ddd = NULL;
+            driver->view(ddd);
+            if(ddd) {
+                if(config->check("allow-deprecated-devices")) {
+                    yWarning("Device \"%s\" is deprecated. Opening since the \"allow-deprecated-devices\" option was passed in the configuration.", str.c_str());
+                } else {
+                    yError("Device \"%s\" is deprecated. Pass the \"allow-deprecated-devices\" option in the configuration if you want to open it anyway.", str.c_str());
+                    driver->close();
+                    return false;
+                }
+            }
             if (creator!=NULL) {
                 ConstString name = creator->getName();
                 ConstString wrapper = creator->getWrapper();

src/libYARP_dev/include/yarp/dev/DeviceDriver.h
@@ -22,6 +22,7 @@ namespace yarp {
      */
     namespace dev {
         class DeviceDriver;
+        class DeprecatedDeviceDriver;
         class DeviceResponder;
     }
 }
@@ -98,6 +99,22 @@ public:
     virtual DeviceDriver *getImplementation() {
         return this;
     }
+};
+
+/**
+ * \ingroup dev_class
+ *
+ * Interface implemented by deprecated device drivers.
+ *
+ * When a device is deprecated, replace yarp::dev::DeviceDriver with
+ * yarp::dev::DeprecatedDeviceDriver in the list of interfaces implemented by
+ * the class, in order to let YARP know that the class is deprecated.
+ *
+ * Deprecated device drivers cannot be opened as PolyDriver unless the
+ * "allow-deprecated-devices" option is passed in the configuration.
+ */
+class YARP_dev_API yarp::dev::DeprecatedDeviceDriver : public yarp::dev::DeviceDriver
+{
 };
 
 

commit 2fbd182de46e3819743a9c87ed1775c700242994

YARP_dev/PolyDriver: Fix memory leak
Introduced by b1c8f3059d52dfeae5bba35a50caebae4059fdd7

src/libYARP_dev/src/PolyDriver.cpp
@@ -278,6 +278,7 @@ bool PolyDriver::coreOpen(yarp::os::Searchable& prop) {
                 } else {
                     yError("Device \"%s\" is deprecated. Pass the \"allow-deprecated-devices\" option in the configuration if you want to open it anyway.", str.c_str());
                     driver->close();
+                    delete driver;
                     return false;
                 }
             }