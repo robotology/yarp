7b49e6ece1ce8b273f036252f06810091e202aae
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-08-09 10:10:51 +0200

    Fix build with SKIP_ACE=ON

--- src/libYARP_OS/include/yarp/os/impl/Logger.h
+++ src/libYARP_OS/include/yarp/os/impl/Logger.h
@@ -174,14 +174,14 @@ public:
     }
 
 private:
-    void show(ACE_UINT32 level, const String& txt);
+    void show(unsigned YARP_INT32 level, const String& txt);
     void exit(int level);
 
     static Logger *root;
     String prefix;
     Logger *parent;
     int verbose;
-    ACE_UINT32 low;
+    unsigned YARP_INT32 low;
     long int pid;
     FILE *stream;
 };
