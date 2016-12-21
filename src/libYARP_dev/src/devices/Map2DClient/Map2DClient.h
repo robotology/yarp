5ea6043f05310bd787d6590dd860cecc26bf935c
Marco Randazzo <marco.randazzo@iit.it>
2016-12-21 18:23:03 +0100

    initial commit for iMap2D interface, Map2DClient, Map2Dserver

---
+++ src/libYARP_dev/src/devices/Map2DClient/Map2DClient.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Marco Randazzo <marco.randazzo@iit.it>
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#ifndef YARP_DEV_MAP2DCLIENT_H
+#define YARP_DEV_MAP2DCLIENT_H
+
+
+#include <yarp/os/Network.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/dev/PreciselyTimed.h>
+#include <yarp/dev/IMap2D.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/sig/MapGrid2D.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/os/Time.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/RecursiveMutex.h>
+
+namespace yarp {
+    namespace dev {
+        class Map2DClient;
+    }
+}
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+class Map2D_type
+{
+    private:
+
+
+
+};
+
+#endif /*DOXYGEN_SHOULD_SKIP_THIS*/
+
+/**
+* @ingroup dev_impl_wrapper
+*
+* The client side of any IBattery capable device.
+* Still single thread! concurrent access is unsafe.
+*/
+class yarp::dev::Map2DClient : public DeviceDriver,
+                               public IMap2D
+{
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+protected:
+
+    yarp::os::Port                m_rpcPort;
+    yarp::os::ConstString         m_local_name;
+    yarp::os::ConstString         m_remote_name;
+
+#endif /*DOXYGEN_SHOULD_SKIP_THIS*/
+
+public:
+
+     /* DeviceDriver methods */
+    bool open(yarp::os::Searchable& config);
+    bool close();
+
+    virtual bool     clear      ();
+    virtual bool     remove_map (std::string map_name);
+    virtual bool     store_map  (const yarp::sig::MapGrid2D& map);
+    virtual bool     get_map    (std::string map_name, yarp::sig::MapGrid2D& map);
+    virtual bool     get_map_names(std::vector<std::string>& map_names);
+};
+
+#endif // YARP_DEV_MAP2DCLIENT_H
---
+++ src/libYARP_dev/src/devices/msgs/ros/include/nav_msgs/MapMetaData.h
@@ -0,0 +1,12 @@
+// This is an automatically generated file.
+#ifndef YARPMSG_TYPE_wrap_nav_msgs_MapMetaData
+#define YARPMSG_TYPE_wrap_nav_msgs_MapMetaData
+
+#include <nav_msgs_MapMetaData.h>
+
+namespace nav_msgs {
+  typedef nav_msgs_MapMetaData MapMetaData;
+}
+
+#endif
+
