431f3664a7cc0e5e410982aa4ff24844ce65c691
Francesco Romano <francesco.romano.1987@gmail.com>
2017-03-30 10:28:00 +0200

    Added macOS-only api calls
    At the current state, this objective-c++ file is used only to enable-disable AppNap.
    In the future it can be used for more macOS-only related activities
    
    Co-authored-by: Claudio Fantacci <claudio.fantacci@iit.it>

---
+++ src/libYARP_OS/src/MacOSAPI.mm
@@ -0,0 +1,26 @@
+#import "yarp/os/impl/MacOSAPI.h"
+
+#import <Foundation/Foundation.h>
+
+void* disableAppNap()
+{
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    id activity = nil;
+    // beginActivityWithOptions:reason: is only in macOS 10.9+
+    if ([processInfo respondsToSelector:@selector(beginActivityWithOptions:reason:)]) {
+        activity = [processInfo beginActivityWithOptions:NSActivityLatencyCritical | NSActivityUserInitiated
+                                                 reason:@"YARP requires AppNap off"];
+    }
+    return activity;
+}
+
+void restoreAppNap(void *activityInfo)
+{
+    id activity = (id)activityInfo;
+    if (!activity) return;
+    // There is no need to check for macOS versions here,
+    // as we are passing an object which is obtained only
+    // on the correct version of macOS
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    [processInfo endActivity:activity];
+}
