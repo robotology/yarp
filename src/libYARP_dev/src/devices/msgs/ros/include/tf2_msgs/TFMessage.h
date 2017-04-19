0bf762f33e81afb646b5808d426d48b6311e1b32
Silvio Traversaro <silvio.traversaro@iit.it>
2017-04-19 14:33:20 +0200

    Commit autogenerated files from tf2_msgs/TFMessage

---
+++ src/libYARP_dev/src/devices/msgs/ros/include/tf2_msgs/TFMessage.h
@@ -0,0 +1,12 @@
+// This is an automatically generated file.
+#ifndef YARPMSG_TYPE_wrap_tf2_msgs_TFMessage
+#define YARPMSG_TYPE_wrap_tf2_msgs_TFMessage
+
+#include <tf2_msgs_TFMessage.h>
+
+namespace tf2_msgs {
+  typedef tf2_msgs_TFMessage TFMessage;
+}
+
+#endif
+
