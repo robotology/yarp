8477629d3df6a0e6ff2f62aebbc2a75de4a26d80
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2017-05-30 16:16:42 +0200

    Fix warning with tests in msvc

--- cmake/template/TestConfig.cmake
+++ cmake/template/TestConfig.cmake
@@ -5,13 +5,13 @@
 # If we are using ACE on MSVC, the RUN_TESTS target will fail
 # unless we add the ACE dlls to our path (or the user does
 # that globally themselves).
-set(ACE_ACE_LIBRARY_DEBUG @ACE_ACE_LIBRARY_DEBUG@)
-set(ACE_ACE_LIBRARY_RELEASE @ACE_ACE_LIBRARY_RELEASE@)
+set(ACE_ACE_LIBRARY_DEBUG "@ACE_ACE_LIBRARY_DEBUG@")
+set(ACE_ACE_LIBRARY_RELEASE "@ACE_ACE_LIBRARY_RELEASE@")
 if (ACE_ACE_LIBRARY_DEBUG)
-  get_filename_component(ACE_DEBUG ${ACE_ACE_LIBRARY_DEBUG} PATH)
+  get_filename_component(ACE_DEBUG "${ACE_ACE_LIBRARY_DEBUG}" PATH)
   set(ENV{PATH} "$ENV{PATH};${ACE_DEBUG}")
 endif()
 if (ACE_ACE_LIBRARY_RELEASE)
-  get_filename_component(ACE_RELEASE ${ACE_ACE_LIBRARY_RELEASE} PATH)
+  get_filename_component(ACE_RELEASE "${ACE_ACE_LIBRARY_RELEASE}" PATH)
   set(ENV{PATH} "$ENV{PATH};${ACE_RELEASE}")
 endif()
