43ecf811c8d6accaed69f224aac4a64914464c45
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2017-06-07 16:10:19 +0200

    OS/RFModule: Ensure that the network is initialized
    Fixes: #1013

--- src/libYARP_OS/src/RFModule.cpp
+++ src/libYARP_OS/src/RFModule.cpp
@@ -350,6 +350,7 @@ static void handler_sigbreak(int sig) {
 
 
 RFModule::RFModule() {
+    yarp::os::Network::initMinimum();
     implementation = new RFModuleHelper(*this);
     stopFlag = false;
 
@@ -376,6 +377,7 @@ RFModule::~RFModule() {
         delete &HELPER(implementation);
         implementation = YARP_NULLPTR;
     }
+    yarp::os::Network::finiMinimum();
 }
 
 
