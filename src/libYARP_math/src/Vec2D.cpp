f73b2caaae17f72452e9a9946cf77325379aa215
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2017-06-07 16:55:14 +0200

    Revert "math/Vec2D: Fix explicit instantiation"
    This broke all clang builds
    
    This reverts commit 2619308f2f804854131bdd28bfff084dcc86da61.

--- src/libYARP_math/src/Vec2D.cpp
+++ src/libYARP_math/src/Vec2D.cpp
@@ -220,3 +220,7 @@ template yarp::math::Vec2D<double> YARP_math_API operator - (yarp::math::Vec2D<d
 template yarp::math::Vec2D<int>    YARP_math_API operator - (yarp::math::Vec2D<int> lhs, const yarp::math::Vec2D<int>& rhs);
 template yarp::math::Vec2D<double> YARP_math_API operator * (const yarp::sig::Matrix& lhs, yarp::math::Vec2D<double> rhs);
 template yarp::math::Vec2D<int>    YARP_math_API operator * (const yarp::sig::Matrix& lhs, yarp::math::Vec2D<int> rhs);
+
+template class YARP_math_API yarp::math::Vec2D<double>;
+template class YARP_math_API yarp::math::Vec2D<int>;
+
