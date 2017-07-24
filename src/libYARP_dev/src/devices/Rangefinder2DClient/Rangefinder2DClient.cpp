robotology/yarp

commit cb5370c1afe515d82f3393fe7a4e07caa62a9849

laser angular measurements not read by the device are now set to infinity and not to zero

src/libYARP_dev/src/devices/Rangefinder2DClient/Rangefinder2DClient.cpp
@@ -315,7 +315,11 @@ bool yarp::dev::Rangefinder2DClient::getLaserMeasurement(std::vector<LaserMeasur
     {
         double angle = (i / double(size)*laser_angle_of_view + device_position_theta + scan_angle_min)* DEG2RAD;
         double value = ranges[i];
+#if USE_SET_CARTESIAN
         data[i].set_cartesian(value * cos(angle) + device_position_x, value * sin(angle) + device_position_y);
+#else
+        data[i].set_polar(value,angle);
+#endif
     }
     return true;
 }

src/devices/rpLidar2/rpLidar2.cpp
@@ -309,7 +309,7 @@ void RpLidar2::run()
             angle -= 360;
         }
 
-        if (quality == 0)
+        if (quality == 0 || distance == 0)
         {
             distance = std::numeric_limits<double>::infinity();
         }
@@ -323,7 +323,8 @@ void RpLidar2::run()
         {
             if (distance < min_distance)
             {
-                distance = max_distance;
+                //laser angular measurements not read by the device are now set to infinity and not to zero
+                distance = std::numeric_limits<double>::infinity(); 
             }
         }
         if (clip_max_enable)

commit 6cc560259795e36d47fcdd343a01df3295ee5fc4

fixing issue introduced with https://github.com/robotology/yarp/commit/cb5370c1afe515d82f3393fe7a4e07caa62a9849

src/libYARP_dev/src/devices/Rangefinder2DClient/Rangefinder2DClient.cpp
@@ -315,7 +315,7 @@ bool yarp::dev::Rangefinder2DClient::getLaserMeasurement(std::vector<LaserMeasur
     {
         double angle = (i / double(size)*laser_angle_of_view + device_position_theta + scan_angle_min)* DEG2RAD;
         double value = ranges[i];
-#if USE_SET_CARTESIAN
+#if 1 //cartesian version is preferrable, even if more computationally expensive, since it takes in account device_position
         data[i].set_cartesian(value * cos(angle) + device_position_x, value * sin(angle) + device_position_y);
 #else
         data[i].set_polar(value,angle);

src/libYARP_dev/src/devices/Rangefinder2DWrapper/Rangefinder2DWrapper.cpp
@@ -536,17 +536,17 @@ bool Rangefinder2DWrapper::initialize_YARP(yarp::os::Searchable &params)
 {
     if(useROS != ROS_only)
     {
-	    if (!streamingPort.open(streamingPortName.c_str()))
+        if (!streamingPort.open(streamingPortName.c_str()))
             {
                 yError("Rangefinder2DWrapper: failed to open port %s", streamingPortName.c_str());
                 return false;
             }
-	    if (!rpcPort.open(rpcPortName.c_str()))
+        if (!rpcPort.open(rpcPortName.c_str()))
             {
                 yError("Rangefinder2DWrapper: failed to open port %s", rpcPortName.c_str());
                 return false;
             }
-	    rpcPort.setReader(*this);
+        rpcPort.setReader(*this);
     }
     return true;
 }