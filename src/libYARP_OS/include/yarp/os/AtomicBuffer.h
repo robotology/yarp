0eaebeb125ddcf6440a3f8cb8e274f201e390ed8
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-01-18 16:05:35 +0100

    libYARP_OS: add AtomicBuffer and PipelineThread classes.

---
+++ src/libYARP_OS/include/yarp/os/AtomicBuffer.h
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2018 Istituto Italiano di Tecnologia (IIT)
+ * Authors: Nicolò Genesio, Ali Paikan, Tariq Abuhashim
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#ifndef YARP_OS_ATOMICBUFFER_H
+#define YARP_OS_ATOMICBUFFER_H
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <yarp/os/Mutex.h>
+#include <yarp/os/Semaphore.h>
+
+#include <functional>
+
+namespace yarp {
+    namespace os {
+    template <class T> class AtomicBuffer;
+    template <class T, class TComparison> class AtomicPriorityBuffer;
+    }
+}
+
+namespace yarp {
+namespace os {
+
+/**
+ * Class designed for multithreading pipeline processing.
+ * Buffer multi-thread safe, that incapsulate a queue.
+ **/
+
+template <class T>
+class AtomicBuffer
+{
+protected:
+    yarp::os::Mutex bufMutex; //mutex to lock the array, for avoiding race condition
+    bool interrupted;
+    yarp::os::Semaphore semArray;
+    std::queue<T> array;
+
+public:
+    AtomicBuffer() : interrupted(false), semArray(0) {}
+    virtual ~AtomicBuffer(){}
+
+    /**
+     * @brief This function is blocking until this.write() has not been invoked.
+     * This means that until there is no new data in the buffer, the read
+     * is blocked. Once read the data is removed from the queue.
+     * @param data
+     * @return true if data has been read successfully, false otherwise.
+     */
+
+    virtual bool read(T& data) {
+
+        //wait for new data
+        /*
+         * Decrement the counter, even if we must wait to do that.  If the counter
+         * would decrement below zero, the calling thread must stop and
+         * wait for another thread to call Semaphore::post on this semaphore.
+         */
+        semArray.wait();
+
+        if(interrupted) {
+            return false;
+        }
+
+        // read
+        bufMutex.lock();
+        data=array.front();
+        array.pop();
+        bufMutex.unlock();
+        return true;
+    }
+    /**
+     * @brief write, afte pushing the data in the queue it triggers
+     * the wake up of the thread that it is waiting on this.read(..)
+     * @param data
+     * @return true if the data has been written correctly the queue
+     */
+
+    virtual bool write(T& data) {
+        //write
+        bufMutex.lock();
+        array.push(data);
+        bufMutex.unlock();
+        //signal that a new data is ready
+        /*
+         * Increment the counter.  If another thread is waiting to decrement the
+         * counter, it is woken up.
+         */
+        semArray.post();
+        return true;
+    }
+
+    void interrupt() {
+        // This function has to be called in the interrupt() of the thread that uses
+        // this buffer
+        bufMutex.lock();
+        interrupted = true;
+        bufMutex.unlock();
+        semArray.post();
+    }
+
+};
+
+/**
+ * Class designed for multithreading pipeline processing.
+ * Buffer multi-thread safe, that incapsulate a priority queue.
+ * the priority_queue needs the type, and the operator() to do
+ * the comparison necessary for ordering the queue.
+ **/
+
+template <class T, class TComparison>
+class AtomicPriorityBuffer : public AtomicBuffer<T> {
+
+    typedef std::priority_queue<T, std::vector<T>, TComparison> mypq_type;
+
+private:
+    mypq_type p_array;
+
+public:
+    AtomicPriorityBuffer() : p_array(TComparison()) {}
+
+    virtual bool read(T& data) {
+
+        //wait
+        AtomicBuffer<T>::semArray.wait();
+        if(AtomicBuffer<T>::interrupted) {
+            return false;
+        }
+
+        // read
+        AtomicBuffer<T>::bufMutex.lock();
+        data = p_array.top();
+//        yDebug()<<"Priority access";//ok
+        p_array.pop();
+        AtomicBuffer<T>::bufMutex.unlock();
+        return true;
+    }
+
+    virtual bool write(T& data) {
+        //write
+        AtomicBuffer<T>::bufMutex.lock();
+        p_array.push(data);
+        AtomicBuffer<T>::bufMutex.unlock();
+        //signal
+        AtomicBuffer<T>::semArray.post();
+        return true;
+    }
+};
+
+} // namespace os
+} // namespace yarp
+
+#endif // YARP_OS_ATOMICBUFFER_H
---
+++ src/libYARP_OS/include/yarp/os/PipelineThread.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2018 Istituto Italiano di Tecnologia (IIT)
+ * Authors: Nicolò Genesio, Ali Paikan, Tariq Abuhashim
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#ifndef YARP_OS_PIPELINETHREAD_H
+#define YARP_OS_PIPELINETHREAD_H
+#include <yarp/os/Thread.h>
+#include <yarp/os/AtomicBuffer.h>
+
+namespace yarp {
+    namespace os {
+    template <class T1, class T2> class PipelineThread;
+    }
+}
+
+namespace yarp {
+namespace os {
+
+/**
+ * \ingroup key_class
+ * Class designed for multithreading pipeline processing.
+ */
+
+template <class T1, class T2>
+class PipelineThread : public Thread
+{
+
+public:
+
+    /**
+     * @brief PipelineThread constructor, to exploit the functionality that this class has been designed to,
+     * bufferIn and bufferOut has to be shared between the different "PipelineThreads",
+     * passing them by reference in the constructor.
+     * The bufferOut of one thread has to be the bufferIn of the thread representing the next
+     * step of the pipeline.
+     * @param _bufferIn, input buffer.
+     * @param _bufferOut, output buffer.
+     */
+    PipelineThread(AtomicBuffer<T1> &_bufferIn, AtomicBuffer<T2> &_bufferOut) :
+        interrupted(false), countProcessed(0), bufferIn(&_bufferIn), bufferOut(&_bufferOut)
+    {
+    }
+
+    ~PipelineThread()
+    {
+        if (!interrupted)
+        {
+            close();
+        }
+    }
+
+    /**
+     * @brief
+     * @return return the number of element processed
+     */
+    unsigned int getCountProcessed(){
+        return countProcessed;
+    }
+
+    /**
+     * @brief Interrupt the input buffer.
+     */
+
+    virtual void interrupt() {
+        interrupted = true;
+        if (bufferIn)
+        {
+            bufferIn->interrupt();
+        }
+    }
+
+    /**
+     * @brief It is a working example for the case
+     * one input buffer and one output buffer. If needed it can be
+     * overridden for the specific use case, otherwise it will necessary
+     * only to implement processData(...).
+     */
+
+    virtual void run()
+    {
+        // We continue to read, process and write data while it is not interrupted and there is data to process.
+        // The interrupted flag is setted to true in the function interrupt().
+        // interrupt() is called by thread.close() when we close the module.
+        while(!interrupted) {
+            T1 dataIn;
+            if(bufferIn->read(dataIn)){
+                // As soon as new data is ready, process the data and write it to the output buffer
+                T2 dataOut;
+                this->processData(dataIn, dataOut);
+                bufferOut->write(dataOut);
+                countProcessed++;
+                yarp::os::Thread::yield();
+
+            }
+
+        }
+    }
+
+    /**
+     * @brief close, stop the thread.
+     */
+    virtual void close() {
+        interrupt();
+        Thread::stop();
+        countProcessed = 0;
+        interrupted = false;
+    }
+
+protected:
+
+    virtual void processData(T1& dataIn, T2& dataOut)
+    {
+        // This function has to be overridden, implementing the
+        // own processData.
+        YARP_UNUSED(dataIn);
+        YARP_UNUSED(dataOut);
+        return;
+    }
+
+protected:
+    bool interrupted;
+    unsigned int countProcessed;
+    AtomicBuffer<T1> *bufferIn;
+    AtomicBuffer<T2> *bufferOut;
+
+};
+
+} // namespace os
+} // namespace yarp
+
+#endif // YARP_OS_PIPELINETHREAD_H
