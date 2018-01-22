c992bfa23058cffe9d098247f71fd5da52df43e0
Alberto Cardellino <alberto.cardellino@iit.it>
2018-01-22 13:40:12 +0100

    Examples: add RGBD example

---
+++ example/dev/RGBD/RGBD_test_1b_server.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2018 Istituto Italiano di Tecnologia (IIT)
+ * Author: Alberto Cardellino <alberto.cardellino@iit.it>
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <yarp/os/Time.h>
+#include <yarp/dev/Wrapper.h>
+#include <yarp/dev/Drivers.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/IRGBDSensor.h>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::dev;
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+    PolyDriver wrapperDriver, rgbdDriver;
+    Property   wrapperConfig, rgbdConfig;
+
+    // Keyword 'device' is used by YARP framework
+
+    // Wrapper config
+    wrapperConfig.put("device", "RGBDSensorWrapper");       // Network server broadcasting data through YARP network
+    wrapperConfig.put("name",   "/server");                 // Prefix for port names opened by the YARP server
+
+    // Device driver config
+    rgbdConfig.put("device", "fakeDepthCamera");            // Device producing (fake) data
+    rgbdConfig.put("mode",   "ball");                       // (fake) data type to be produced
+
+    yInfo() << "Opening server";
+    // Instantiating RGBD server
+    wrapperDriver.open(wrapperConfig);
+    if (!wrapperDriver.isValid())
+    {
+        yError("Failed to create and configure the RGBD server\n");
+        return 1;
+    }
+
+    yInfo() << "Opening fake device";
+    // Instantiating RGBD device
+    rgbdDriver.open(rgbdConfig);
+    if (!rgbdDriver.isValid())
+    {
+        yError("Failed to create and configure the fake device\n");
+        return 1;
+    }
+
+    yInfo() << "Attaching server to device";
+    yarp::dev::IWrapper *wrapperInterface;
+    wrapperDriver.view(wrapperInterface);
+    wrapperInterface->attach(&rgbdDriver);                // Bind the wrapper with the actual device
+
+    // Stay up some time waiting for the client to do its work
+    yarp::os::Time::delay(10);
+    wrapperDriver.close();
+    rgbdDriver.close();
+
+    return 0;
+}
