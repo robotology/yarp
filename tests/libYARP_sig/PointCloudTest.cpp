6943416f0aaa7052cb5ce2c3349a8aec2d88b15d
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-02-14 18:31:26 +0100

    PointCloudTest: add matching and mismatching tests

--- tests/libYARP_sig/PointCloudTest.cpp
+++ tests/libYARP_sig/PointCloudTest.cpp
@@ -31,10 +31,10 @@ public:
     virtual ConstString getName() override
     { return "PointCloudTest"; }
 
-
-    virtual void runTests() override
+    void readWriteMatch()
     {
-        report(0, "Checking XYZ_RGBA_DATA sending");
+        Network::setLocalMode(true);
+        report(0, "Checking XYZ_RGBA_DATA sending - Type match");
         BufferedPort< PointCloud<XYZ_RGBA_DATA> > outPort;
         Port inPort;
         checkTrue(outPort.open("/test/pointcloud/out"),"Opening output port");
@@ -79,11 +79,116 @@ public:
 
         checkTrue(ok, "Checking data validity");
 
+        outPort.close();
+        inPort.close();
+
+        // TODO : check other data types..
+        //        once implemented check toString()
+
+    }
+
+    virtual void readWriteMisMatch1()
+    {
+        Network::setLocalMode(true);
+        report(0,"Testing the case in which we receive a bigger structure");
+        BufferedPort< PointCloud<XYZ_RGBA_DATA> > outPort;
+        Port inPort;
+        checkTrue(outPort.open("/test/pointcloud/out"),"Opening output port");
+        checkTrue(inPort.open("/test/pointcloud/in"),"Opening input port");
+        checkTrue(NetworkBase::connect(outPort.getName(), inPort.getName()),"Checking connection");
+        PointCloud<XYZ_RGBA_DATA>& testPC = outPort.prepare();
+        int width  = 200;
+        int height = 20;
+        testPC.resize(width, height);
+
+        for (int i=0; i<width*height; i++)
+        {
+            testPC.data[i].x = i;
+            testPC.data[i].y = i + 1;
+            testPC.data[i].z = i + 2;
+            testPC.data[i].r = '1';
+            testPC.data[i].g = '2';
+            testPC.data[i].b = '3';
+            testPC.data[i].a = '4';
+        }
+
+        yarp::os::Time::delay(0.3);
+
+        outPort.write();
+
+        PointCloud<XYZ_DATA> inCloud;
+        inPort.read(inCloud);
+
+        checkFalse(inCloud.dataSizeBytes() == testPC.dataSizeBytes(), "Checking size, correctly wrong");
+
+        bool ok = true;
+        for (int i=0; i<width*height; i++)
+        {
+            ok &= inCloud.data[i].x == i;
+            ok &= inCloud.data[i].y == i + 1;
+            ok &= inCloud.data[i].z == i + 2;
+        }
+
+        checkTrue(ok, "Checking data validity");
+
+        outPort.close();
+        inPort.close();
 
 
+    }
+    virtual void readWriteMisMatch2()
+    {
+        Network::setLocalMode(true);
+        report(0,"Testing the case in which we receive a smaller structure");
+        BufferedPort< PointCloud<XYZ_DATA> > outPort;
+        Port inPort;
+        checkTrue(outPort.open("/test/pointcloud/out"),"Opening output port");
+        checkTrue(inPort.open("/test/pointcloud/in"),"Opening input port");
+        checkTrue(NetworkBase::connect(outPort.getName(), inPort.getName()),"Checking connection");
+        PointCloud<XYZ_DATA>& testPC = outPort.prepare();
+        int width  = 200;
+        int height = 42;
+        testPC.resize(width, height);
 
+        for (int i=0; i<width*height; i++)
+        {
+            testPC.data[i].x = i;
+            testPC.data[i].y = i + 1;
+            testPC.data[i].z = i + 2;
+        }
 
-        printf("Hello Point cloud test\n");
+        yarp::os::Time::delay(0.3);
+
+        outPort.write();
+
+        PointCloud<XYZ_NORMAL_DATA> inCloud;
+        inPort.read(inCloud);
+
+        checkFalse(inCloud.dataSizeBytes() == testPC.dataSizeBytes(), "Checking size, correctly wrong");
+
+        bool ok = true;
+        for (int i=0; i<width*height; i++)
+        {
+            ok &= inCloud.data[i].x == i;
+            ok &= inCloud.data[i].y == i + 1;
+            ok &= inCloud.data[i].z == i + 2;
+            ok &= inCloud.data[i].normal_x == 0;// The data sent not contains normals...
+            ok &= inCloud.data[i].normal_y == 0;// The data sent not contains normals...
+            ok &= inCloud.data[i].normal_z == 0;// The data sent not contains normals...
+        }
+
+        checkTrue(ok, "Checking data validity");
+
+        outPort.close();
+        inPort.close();
+
+    }
+
+    virtual void runTests() override
+    {
+        readWriteMatch();
+        readWriteMisMatch1();
+        readWriteMisMatch2();
     }
 };
 
