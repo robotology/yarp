4d3335132d181adbe834932786d1256d4a3b18a6
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-02-15 17:26:17 +0100

    YARP_OS: Re-license BSD

--- src/libYARP_OS/src/DummyConnector.cpp
+++ src/libYARP_OS/src/DummyConnector.cpp
@@ -1,9 +1,12 @@
 /*
- * Copyright (C) 2007 Freyr Magnusson and RobotCub Consortium
- * Authors: Freyr Magnusson and Paul Fitzpatrick
- * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * Copyright (C) 2007 Freyr Magnusson
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
  */
-
 
 #include <yarp/os/DummyConnector.h>
 #include <yarp/os/impl/BufferedConnectionWriter.h>
--- src/libYARP_OS/src/TcpFace.cpp
+++ src/libYARP_OS/src/TcpFace.cpp
@@ -1,11 +1,12 @@
 /*
- * Copyright (C) 2006, 2010 RobotCub Consortium
- * Copyright (C) 2010 Daniel Krieg
- * Authors: Paul Fitzpatrick <paulfitz@alum.mit.edu>
- *          Daniel Krieg <krieg@fias.uni-frankfurt.de>
- * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * Copyright (C) 2010 Daniel Krieg <krieg@fias.uni-frankfurt.de>
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
  */
-
 
 #include <yarp/os/impl/TcpFace.h>
 #include <yarp/os/impl/Logger.h>
