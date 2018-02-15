c93ecab588024957884a573f06edd72817f13f44
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-02-15 20:06:37 +0100

    YARP_gsl: Fix GPL license

--- src/libYARP_gsl/src/Gsl.cpp
+++ src/libYARP_gsl/src/Gsl.cpp
@@ -1,8 +1,20 @@
 /*
-* Author: Lorenzo Natale.
-* Copyright (C) 2016 Istituto Italiano di Tecnologia (IIT)
-* CopyPolicy: Released under the terms of the GPL 2.0 license or later.
-*/
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 /*
  * This library provides functions for compatibility with GSL.
