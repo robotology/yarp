de1c9be832db8fd60098868aebe5dab2fbf70f20
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-02-16 20:04:21 +0100

    YARP_logger: Re-license BSD

--- src/libYARP_logger/include/yarp/logger/YarpLogger.h
+++ src/libYARP_logger/include/yarp/logger/YarpLogger.h
@@ -1,20 +1,10 @@
 /*
- * Copyright (C) 2014 Istituto Italiano di Tecnologia (IIT)
- * Author: Marco Randazzo
- * email:  marco.randazzo@iit.it
- * website: www.robotcub.org
- * Permission is granted to copy, distribute, and/or modify this program
- * under the terms of the GNU General Public License, version 2 or any
- * later version published by the Free Software Foundation.
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
  *
- * A copy of the license can be found at
- * http://www.robotcub.org/icub/license/gpl.txt
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details
-*/
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
 
 #ifndef YARP_LOGGER
 #define YARP_LOGGER
