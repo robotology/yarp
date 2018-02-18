45ae899da7e3f85cab00de2f86499faf763b9879
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-02-18 17:28:39 +0100

    Bindings: Re-license BSD

--- bindings/csharp/tests/TestStamp.cs
+++ bindings/csharp/tests/TestStamp.cs
@@ -1,9 +1,14 @@
 using System;
 
-// Copyright: (C) 2015 Istituto Italiano di Tecnologia (IIT)
-// Author: Paul Fitzpatrick
-// CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
- 
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
 // I don't really know C#, just bluffing from Java
 
 namespace HelloNameSpace
