d8cc388953120d32fd91b399855a3bbf9dd9abe1
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-02-22 19:43:12 +0100

    PC_NetworkHeader: fix valgrind issue.
    The header is packet in block of 4 bytes, we added 2 bytes for padding and
    making valgrind happy.

--- src/libYARP_sig/include/yarp/sig/PointCloud_NetworkHeader.hpp
+++ src/libYARP_sig/include/yarp/sig/PointCloud_NetworkHeader.hpp
@@ -179,25 +179,29 @@ class yarp::sig::PointCloud_NetworkHeader
 public:
     PointCloud_NetworkHeader() :    width(10),
                                     height(1),
+                                    pointType(0),
                                     isDense(false),
 //                                     pose(4, 0.0f),
 //                                     orientation(4, 0.0f),
-                                    hasCustomData(false)
-                                    { }
+                                    hasCustomData(false),
+                                    pad{0,0}
+                                    { YARP_UNUSED(pad); }
     // PCL like fields
     yarp::os::NetInt32  width;
     yarp::os::NetInt32  height;
+    yarp::os::NetInt32  pointType;       // bitwise of all possible informations -> could also be int64 or just an enum, but I thin bitwise gives more freedom about all possible combinations
     bool                isDense;         // this guy is mis-aligned // convert to (char?) when serialized
 //     yarp::sig::Vector   pose;            // translation from origin -- could be an Eigen::Vector4f for better PCL compatibility if yarp can afford to depend from it
 //     yarp::sig::Vector   orientation;     // orientation wrt origin  -- could be an Eigen::Quaternion for better PCL compatibility if yarp can afford to depend from it
 
     // YARPish fileds
-    yarp::os::NetInt32  pointType;       // bitwise of all possible informations -> could also be int64 or just an enum, but I thin bitwise gives more freedom about all possible combinations
 //     char                *data;           // actual pointCloud data.
 
     // Other
     bool                hasCustomData;   // this guy is mis-aligned --> for future use, to have addictional data not initially forseen or fields from ROS which does not fit into yarp pointTypes
 //     yarp::os::Bottle    *customData;     // used in case of custom data, to check if a different type can be better optimized for transmission?
+private:
+    char pad[2]; // char vector to align the bytes of the headers
 };
 // YARP_END_PACK
 
