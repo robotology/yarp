c7e9f32b624de75f4c5c9ad521fe8531ca9d41d5
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-03-23 17:48:14 +0100

    Regenerate rosmsg files

--- src/libYARP_rosmsg/idl_generated_code/include/sensor_msgs/SetCameraInfo.h
+++ src/libYARP_rosmsg/idl_generated_code/include/sensor_msgs/SetCameraInfo.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
 // This is an automatically generated file.
 
 #ifndef YARP_ROSMSG_deprecated2_sensor_msgs_SetCameraInfo_h
