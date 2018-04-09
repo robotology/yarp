robotology/yarp

commit fc610ccf30e2579c408702ac83883c73d2ddc674

logger singleton c++11-ized

src/libYARP_OS/include/yarp/os/impl/Logger.h
@@ -63,7 +63,7 @@ public:
         stream = nullptr;
         pid = -1;
 #ifdef YARP_HAS_ACE
-        if (this==root) {
+        if (this==&get()) {
             ACE_Log_Msg *acer = ACE_Log_Msg::instance();
             acer->set_flags(8);
             acer->clr_flags(1);
@@ -179,10 +179,10 @@ public:
     }
 
 private:
+    Logger() = default;
     void show(unsigned YARP_INT32 level, const ConstString& txt);
     void exit(int level);
 
-    static Logger *root;
     ConstString prefix;
     Logger *parent;
     int verbose;

src/libYARP_OS/src/Logger.cpp
@@ -14,16 +14,12 @@
 using namespace yarp::os::impl;
 using namespace yarp::os;
 
-Logger *Logger::root = nullptr;
-
 Logger& Logger::get() {
-    if (!root) root = new Logger("yarp");
-    return *root;
+    static Logger instance;
+    return instance;
 }
 
 void Logger::fini() {
-    if (root) delete root;
-    root = nullptr;
 }
 
 

commit eb9428eab75b5d04f6d0edc867662e9b308c6791

OS: Fix deadlock introduced by fc610ccf30e2579c408702ac83883c73d2ddc674

src/libYARP_OS/include/yarp/os/impl/Logger.h
@@ -63,7 +63,7 @@ public:
         stream = nullptr;
         pid = -1;
 #ifdef YARP_HAS_ACE
-        if (this==&get()) {
+        if (parent == nullptr) {
             ACE_Log_Msg *acer = ACE_Log_Msg::instance();
             acer->set_flags(8);
             acer->clr_flags(1);