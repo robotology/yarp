robotology/yarp

commit 824f53d892be3429876d1e959e2518401ddb834a

YarpIDL: Ensure that the tool used are the ones coming with YARP

cmake/YarpIDL.cmake
@@ -110,8 +110,9 @@ function(YARP_IDL_TO_DIR yarpidl_file_base output_dir)
     # Generate code at configuration time, so we know filenames.
     find_program(YARPIDL_${family}_LOCATION
                  NAMES yarpidl_${family}
-                 HINTS ${YARP_IDL_BINARY_HINT}
-                       "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}")
+                 HINTS "${YARP_IDL_BINARY_HINT}"
+                       "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}"
+                 NO_DEFAULT_PATH)
     # Make sure intermediate output directory exists.
     make_directory(${dir})
     # Generate a script controlling final layout of files.

commit 055250e5dbb0ba5222d89e3133621737696ef5c5

YarpIDL: Fix windows build
They were broken after 824f53d892be3429876d1e959e2518401ddb834a, since
YARP_IDL_BINARY_HINT is a list of directories on windows.

cmake/YarpIDL.cmake
@@ -110,7 +110,7 @@ function(YARP_IDL_TO_DIR yarpidl_file_base output_dir)
     # Generate code at configuration time, so we know filenames.
     find_program(YARPIDL_${family}_LOCATION
                  NAMES yarpidl_${family}
-                 HINTS "${YARP_IDL_BINARY_HINT}"
+                 HINTS ${YARP_IDL_BINARY_HINT} # This is a list of directories
                        "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}"
                  NO_DEFAULT_PATH)
     # Make sure intermediate output directory exists.