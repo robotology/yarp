824f53d892be3429876d1e959e2518401ddb834a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-05-14 14:52:56 +0200

    YarpIDL: Ensure that the tool used are the ones coming with YARP

--- cmake/YarpIDL.cmake
+++ cmake/YarpIDL.cmake
@@ -110,8 +110,9 @@ function(YARP_IDL_TO_DIR yarpidl_file_base output_dir)
     # Generate code at configuration time, so we know filenames.
     find_program(YARPIDL_${family}_LOCATION
                  NAMES yarpidl_${family}
-                 HINTS ${YARP_IDL_BINARY_HINT}
-                       "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}")
+                 HINTS "${YARP_IDL_BINARY_HINT}"
+                       "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}"
+                 NO_DEFAULT_PATH)
     # Make sure intermediate output directory exists.
     make_directory(${dir})
     # Generate a script controlling final layout of files.
