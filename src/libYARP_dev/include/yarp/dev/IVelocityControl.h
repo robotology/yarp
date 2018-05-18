c9228ba420281a1051f0311bcbef19b4233a33a5
Nicogene <nicolo.genesio@iit.it>
2018-05-18 13:48:50 +0200

    YARP_dev: deprecate IVelocityControl2Raw in favour of IVelocityControlRaw.
    IVelocityControlRaw now contains the method of the old and new interface.

--- src/libYARP_dev/include/yarp/dev/IVelocityControl.h
+++ src/libYARP_dev/include/yarp/dev/IVelocityControl.h
@@ -10,6 +10,12 @@
 #ifndef YARP_DEV_IVELOCITYCONTROL_H
 #define YARP_DEV_IVELOCITYCONTROL_H
 
+#include <yarp/dev/api.h>
+#include <yarp/dev/ControlBoardPid.h>
+
+//TO PROVIDE BACKWARD COMPATIBILITY FOR DEPRECATED METHODS: TO BE REMOVED LATER!
+#include <yarp/dev/IPidControl.h>
+#include <yarp/os/LogStream.h>
 namespace yarp {
     namespace dev {
         class IVelocityControl;
@@ -93,6 +99,59 @@ public:
      * @return true/false on success or failure
      */
     virtual bool stopRaw()=0;
+
+    /**
+     * Start motion at a given speed for a subset of joints.
+     * @param n_joint how many joints this command is referring to
+     * @param joints pointer to the array of joint numbers
+     * @param spds    pointer to the array containing the new speed values
+     * @return true/false upon success/failure
+     */
+    virtual bool velocityMoveRaw(const int n_joint, const int *joints, const double *spds)=0;
+
+     /** Get the last reference speed set by velocityMove for single joint.
+     * @param j joint number
+     * @param vel returns the requested reference.
+     * @return true/false on success/failure
+     */
+    virtual bool getRefVelocityRaw(const int joint, double *vel) {return false;}
+
+    /** Get the last reference speed set by velocityMove for all joints.
+     * @param vels pointer to the array containing the new speed values, one value for each joint
+     * @return true/false on success/failure
+     */
+    virtual bool getRefVelocitiesRaw(double *vels) {return false;}
+
+    /** Get the last reference speed set by velocityMove for a group of joints.
+     * @param n_joint how many joints this command is referring to
+     * @param joints of joints controlled. The size of this array is n_joints
+     * @param vels pointer to the array containing the requested values, one value for each joint.
+     *  The size of the array is n_joints.
+     * @return true/false on success/failure
+     */
+    virtual bool getRefVelocitiesRaw(const int n_joint, const int *joints, double *vels) { return false;}
+
+    /** Set reference acceleration for a subset of joints. This is the valure that is
+     * used during the generation of the trajectory.
+     * @param joints pointer to the array of joint numbers
+     * @param accs   pointer to the array containing acceleration values
+     * @return true/false upon success/failure
+     */
+    virtual bool setRefAccelerationsRaw(const int n_joint, const int *joints, const double *accs)=0;
+
+    /** Get reference acceleration for a subset of joints. These are the values used during the
+     * interpolation of the trajectory.
+     * @param joints pointer to the array of joint numbers
+     * @param accs   pointer to the array that will store the acceleration values.
+     * @return true/false on success or failure
+     */
+    virtual bool getRefAccelerationsRaw(const int n_joint, const int *joints, double *accs)=0;
+
+    /** Stop motion for a subset of joints
+     * @param joints pointer to the array of joint numbers
+     * @return true/false on success or failure
+     */
+    virtual bool stopRaw(const int n_joint, const int *joints)=0;
 };
 
 /**
@@ -173,5 +232,8 @@ public:
     virtual bool stop()=0;
 };
 
+#define VOCAB_VELOCITY_MOVE_GROUP VOCAB4('v','m','o','g')
+#define VOCAB_VEL_PID  VOCAB3('v','p','d')
+#define VOCAB_VEL_PIDS VOCAB4('v','p','d','s')
 
 #endif // YARP_DEV_IVELOCITYCONTROL_H
--- src/libYARP_dev/include/yarp/dev/IVelocityControl2.h
+++ src/libYARP_dev/include/yarp/dev/IVelocityControl2.h
@@ -11,16 +11,10 @@
 #define YARP_DEV_IVELOCITYCONTROL2_H
 
 #include <yarp/dev/IVelocityControl.h>
-#include <yarp/dev/ControlBoardPid.h>
-
-//TO PROVIDE BACKWARD COMPATIBILITY FOR DEPRECATED METHODS: TO BE REMOVED LATER!
-#include <yarp/dev/IPidControl.h>
-#include <yarp/os/LogStream.h>
-
 namespace yarp {
     namespace dev {
         class IVelocityControl2;
-        class IVelocityControl2Raw;
+YARP_DEPRECATED_TYPEDEF_MSG("Use yarp::dev::IVelocityControlRaw instead") IVelocityControlRaw IVelocityControl2Raw;
     }
 }
 
@@ -117,89 +111,5 @@ public:
      */
     virtual bool stop(const int n_joint, const int *joints)=0;
 };
-
-
-
-/**
- * @ingroup dev_iface_motor
- *
- * Interface for control boards implementig velocity control in encoder coordinates.
- */
-class yarp::dev::IVelocityControl2Raw : public yarp::dev::IVelocityControlRaw
-{
-public:
-    /**
-     * Destructor.
-     */
-    virtual ~IVelocityControl2Raw() {}
-
-    // Inherit from IVelocityControl
-    using IVelocityControlRaw::getAxes;
-    using IVelocityControlRaw::velocityMoveRaw;
-    using IVelocityControlRaw::setRefAccelerationRaw;
-    using IVelocityControlRaw::setRefAccelerationsRaw;
-    using IVelocityControlRaw::getRefAccelerationRaw;
-    using IVelocityControlRaw::getRefAccelerationsRaw;
-    using IVelocityControlRaw::stopRaw;
-
-    /**
-     * Start motion at a given speed for a subset of joints.
-     * @param n_joint how many joints this command is referring to
-     * @param joints pointer to the array of joint numbers
-     * @param spds    pointer to the array containing the new speed values
-     * @return true/false upon success/failure
-     */
-    virtual bool velocityMoveRaw(const int n_joint, const int *joints, const double *spds)=0;
-
-     /** Get the last reference speed set by velocityMove for single joint.
-     * @param j joint number
-     * @param vel returns the requested reference.
-     * @return true/false on success/failure
-     */
-    virtual bool getRefVelocityRaw(const int joint, double *vel) {return false;};
-
-    /** Get the last reference speed set by velocityMove for all joints.
-     * @param vels pointer to the array containing the new speed values, one value for each joint
-     * @return true/false on success/failure
-     */
-    virtual bool getRefVelocitiesRaw(double *vels) {return false;};
-
-    /** Get the last reference speed set by velocityMove for a group of joints.
-     * @param n_joint how many joints this command is referring to
-     * @param joints of joints controlled. The size of this array is n_joints
-     * @param vels pointer to the array containing the requested values, one value for each joint.
-     *  The size of the array is n_joints.
-     * @return true/false on success/failure
-     */
-    virtual bool getRefVelocitiesRaw(const int n_joint, const int *joints, double *vels) { return false;};
-
-    /** Set reference acceleration for a subset of joints. This is the valure that is
-     * used during the generation of the trajectory.
-     * @param joints pointer to the array of joint numbers
-     * @param accs   pointer to the array containing acceleration values
-     * @return true/false upon success/failure
-     */
-    virtual bool setRefAccelerationsRaw(const int n_joint, const int *joints, const double *accs)=0;
-
-    /** Get reference acceleration for a subset of joints. These are the values used during the
-     * interpolation of the trajectory.
-     * @param joints pointer to the array of joint numbers
-     * @param accs   pointer to the array that will store the acceleration values.
-     * @return true/false on success or failure
-     */
-    virtual bool getRefAccelerationsRaw(const int n_joint, const int *joints, double *accs)=0;
-
-    /** Stop motion for a subset of joints
-     * @param joints pointer to the array of joint numbers
-     * @return true/false on success or failure
-     */
-    virtual bool stopRaw(const int n_joint, const int *joints)=0;
-
-};
-
-#define VOCAB_VELOCITY_MOVE_GROUP VOCAB4('v','m','o','g')
-#define VOCAB_VEL_PID  VOCAB3('v','p','d')
-#define VOCAB_VEL_PIDS VOCAB4('v','p','d','s')
-
 
 #endif // YARP_DEV_IVELOCITYCONTROL2_H
