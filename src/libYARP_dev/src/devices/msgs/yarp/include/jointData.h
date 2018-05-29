268a06615360abdbb1f5b893f1b64c4188527d5f
Andrea Ruzzenenti <andrea.ruzzenenti@iit.it>
2018-05-29 16:38:10 +0200

    generated files licence updated

--- src/libYARP_dev/src/devices/msgs/yarp/include/jointData.h
+++ src/libYARP_dev/src/devices/msgs/yarp/include/jointData.h
@@ -1,4 +1,12 @@
-// This is an automatically-generated file.
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+// This is an automatically generated file.
 // It could get re-generated if the ALLOW_IDL_GENERATION flag is on.
 
 #ifndef YARP_THRIFT_GENERATOR_STRUCT_jointData
