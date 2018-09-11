robotology/yarp

commit 536b24eac04bf623a239ed6286b8127eebbe3efb

YARP_companion: Fix #coverity/289434 - unchecked return value

src/libYARP_companion/src/Companion.cpp
@@ -1860,7 +1860,12 @@ int Companion::write(const char *name, int ntargets, char *targets[]) {
         std::string slash=NetworkBase::getDirectorySeparator();
         hist_file += slash;
         hist_file += "yarp_write";
-        yarp::os::mkdir(hist_file.c_str());
+        if (yarp::os::mkdir(hist_file.c_str()) != 0)
+        {
+            fprintf(stderr, "Unable to create directory into \"%s\"\n",
+                    yarp::os::ResourceFinder::getDataHome().c_str());
+            return 1;
+        }
         std::string temp;
         if (ntargets>0) {
             temp = targets[0];

commit 9b2047ac7e4ed5dba86810d49ba72e7e29470370

yarp write: Fix "Unable to create directory" error
Introduced by 536b24eac04bf623a239ed6286b8127eebbe3efb

src/libYARP_companion/src/Companion.cpp
@@ -1860,7 +1860,7 @@ int Companion::write(const char *name, int ntargets, char *targets[]) {
         std::string slash=NetworkBase::getDirectorySeparator();
         hist_file += slash;
         hist_file += "yarp_write";
-        if (yarp::os::mkdir(hist_file.c_str()) != 0)
+        if (yarp::os::mkdir_p(hist_file.c_str(), 1) != 0)
         {
             fprintf(stderr, "Unable to create directory into \"%s\"\n",
                     yarp::os::ResourceFinder::getDataHome().c_str());