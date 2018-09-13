robotology/yarp

commit a95b11dda1cefb5f2d347292ec4fb8137374355d

OS/impl: get rid of NameConfig::createPath in favour of yarp::os::mkdir_p

src/libYARP_OS/src/NameConfig.cpp
@@ -126,11 +126,6 @@ std::string NameConfig::getConfigFileName(const char *stem, const char *ns) {
     return expandFilename(fname.c_str());
 }
 
-
-bool NameConfig::createPath(const std::string& fileName, int ignoreLevel) {
-    return yarp::os::mkdir_p(filename.c_str(), ignoreLevel) == 0;
-}
-
 std::string NameConfig::readConfig(const std::string& fileName) {
     char buf[25600];
     FILE *fin = fopen(fileName.c_str(), "r");
@@ -179,7 +174,7 @@ Contact NameConfig::getAddress() {
 
 
 bool NameConfig::writeConfig(const std::string& fileName, const std::string& text) {
-    if (!createPath(fileName)) {
+    if (!yarp::os::mkdir_p(fileName.c_str(), -1)) {
         return false;
     }
     FILE *fout = fopen(fileName.c_str(), "w");

src/libYARP_OS/include/yarp/os/impl/NameConfig.h
@@ -39,8 +39,6 @@ public:
     std::string getConfigFileName(const char *stem = nullptr,
                                   const char *ns = nullptr);
 
-    static bool createPath(const std::string& fileName, int ignoreLevel = 1);
-
     std::string readConfig(const std::string& fileName);
 
     bool writeConfig(const std::string& fileName, const std::string& text);

src/libYARP_OS/src/ResourceFinder.cpp
@@ -1033,7 +1033,7 @@ std::string ResourceFinder::createIfAbsent(bool mayCreate,
                                            const std::string& path)
 {
     if (!mayCreate) return path;
-    NameConfig::createPath(path, 0);
+    yarp::os::mkdir_p(path.c_str(), 0);
     return path;
 }
 

commit ea71f3984966d8d5cf33fe4acb860f5320500255

OS/impl/NameConfig: Fix writeConfig
Broken by a95b11dda1cefb5f2d347292ec4fb8137374355d

src/libYARP_OS/src/NameConfig.cpp
@@ -174,7 +174,7 @@ Contact NameConfig::getAddress() {
 
 
 bool NameConfig::writeConfig(const std::string& fileName, const std::string& text) {
-    if (!yarp::os::mkdir_p(fileName.c_str(), -1)) {
+    if (yarp::os::mkdir_p(fileName.c_str(), 1) != 0) {
         return false;
     }
     FILE *fout = fopen(fileName.c_str(), "w");