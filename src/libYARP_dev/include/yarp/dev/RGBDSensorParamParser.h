a7a3dbdff636ab639aa76c1ef61e82f94ba60fcf
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-09-18 16:07:48 +0200

    YARP_dev: add documentation for the RGBDSensorParamParser.
    It fixes #1863.

--- src/libYARP_dev/include/yarp/dev/RGBDSensorParamParser.h
+++ src/libYARP_dev/include/yarp/dev/RGBDSensorParamParser.h
@@ -20,6 +20,10 @@
 namespace yarp {
 namespace dev {
 
+/**
+ * @brief The RGBDSensorParamParser class.
+ * This class has been designed to uniform the parsing of RGBD yarp devices.
+ */
 
 class YARP_dev_API RGBDSensorParamParser
 {
@@ -28,13 +32,26 @@ public:
 #ifndef YARP_NO_DEPRECATED // Since YARP 3.2.0
     YARP_DEPRECATED_TYPEDEF_MSG("Use yarp::sig::IntrinsicParams instead") yarp::sig::IntrinsicParams IntrinsicParams;
 #endif
+    /**
+     * @brief The RGBDParam struct.
+     * A RGBD param has a name, can be a setting or a description for/of the RGBD device.
+     * The value(s) is stored in ad vector of yarp::os::Value.
+     */
     struct YARP_dev_API RGBDParam
     {
+        /**
+         * @brief RGBDParam, default constructor.
+         */
         RGBDParam() : name("unknown"), isSetting(false), isDescription(false), size(1)
         {
             val.resize(size);
         }
 
+        /**
+         * @brief RGBDParam
+         * @param _name, name of the parameter.
+         * @param _size, dimension of the parameter (e.g. the resolution is represented by 2 values).
+         */
         RGBDParam(const std::string& _name, const int _size) : name(_name), isSetting(false),
                                               isDescription(false), size(_size)
         {
@@ -55,10 +72,21 @@ public:
     yarp::sig::Matrix       transformationMatrix;
     bool                    isOptionalExtrinsic;
 
+    /**
+     * @brief RGBDSensorParamParser, default constructor.
+     */
     RGBDSensorParamParser(): depthIntrinsic(), rgbIntrinsic(),
                              transformationMatrix(4,4), isOptionalExtrinsic(true) {
         transformationMatrix.eye();
     }
+
+    /**
+     * @brief parseParam, parse the params stored in a Searchable.
+     * @param config[in], Searchable containing the parameters of the RGDB sensor
+     * @param params[out], vector containing all the description/settings of the
+     * RGBD sensor.
+     * @return true on success, false otherwise.
+     */
     bool parseParam(yarp::os::Searchable& config, std::vector<RGBDParam *> &params);
 };
 
