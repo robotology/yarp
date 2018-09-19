7379998645aa76b1350c05dfc271cf53797ae58e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-09-19 18:19:47 +0200

    OS/NetworkClock: Refactor
    * Use PIMPL
    * Do not inherit from PortReader
    * Cleanup

--- src/libYARP_OS/src/NetworkClock.cpp
+++ src/libYARP_OS/src/NetworkClock.cpp
@@ -7,78 +7,152 @@
  */
 
 #include <yarp/os/NetworkClock.h>
+
+#include <yarp/conf/numeric.h>
+#include <yarp/conf/system.h>
+
+#include <yarp/os/Mutex.h>
+#include <yarp/os/NestedContact.h>
+#include <yarp/os/NetInt32.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Os.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/PortReader.h>
+#include <yarp/os/Semaphore.h>
 #include <yarp/os/SystemClock.h>
 #include <yarp/os/SystemInfo.h>
-#include <yarp/os/NestedContact.h>
-#include <yarp/os/Os.h>
 #include <yarp/os/impl/Logger.h>
-#include <yarp/os/Network.h>
-#include <yarp/conf/system.h>
+
+#include <cstring>
 #include <list>
 #include <utility>
-#include <string.h>
 
 
 using namespace yarp::os;
 using namespace yarp::os::impl;
 
-typedef std::list<std::pair<double, Semaphore*> > Waiters;
 
-NetworkClock::NetworkClock()
-    : clockName(""), pwaiters(nullptr), sec(0), nsec(0), _time(0), closing(false), initted(false)
+class NetworkClock::Private : public yarp::os::PortReader
 {
-    pwaiters = new Waiters();
-    if(pwaiters==nullptr)
-    {
-        YARP_FAIL(Logger::get(), "NetworkClock: new Waiters() failed");
-    }
+public:
+    Private();
+    ~Private();
+
+    virtual bool read(ConnectionReader& reader) override;
+
+    std::string clockName;
+
+    typedef std::list<std::pair<double, Semaphore*>> Waiters;
+    Waiters* waiters;
+    Port port;
+
+    Mutex listMutex;
+    Mutex timeMutex;
+
+    std::int32_t sec;
+    std::int32_t nsec;
+    double _time;
+    bool closing;
+    bool initted;
+};
+
+NetworkClock::Private::Private() :
+        clockName(""),
+        waiters(new Waiters()),
+        sec(0),
+        nsec(0),
+        _time(0),
+        closing(false),
+        initted(false)
+{
 }
 
-NetworkClock::~NetworkClock() {
-    YARP_WARN(Logger::get(), "Destroying network clock");
-
+NetworkClock::Private::~Private()
+{
     listMutex.lock();
     closing = true;
     port.interrupt();
 
-    Waiters* waiters = static_cast<Waiters*>(pwaiters);
-    if (waiters) {
-        Waiters::iterator waiter_i;
-        waiter_i = waiters->begin();
-        while (waiter_i != waiters->end())
-        {
-            Semaphore *waiterSemaphore = waiter_i->second;
-            waiter_i = waiters->erase(waiter_i);
-            if (waiterSemaphore)
-                waiterSemaphore->post();
+    auto waiter_it = waiters->begin();
+    while (waiter_it != waiters->end()) {
+        Semaphore* waiterSemaphore = waiter_it->second;
+        waiter_it = waiters->erase(waiter_it);
+        if (waiterSemaphore) {
+            waiterSemaphore->post();
         }
-
-        delete waiters;
-        pwaiters = nullptr;
     }
-
+    delete waiters;
     listMutex.unlock();
+
     yarp::os::ContactStyle style;
     style.persistent = true;
     NetworkBase::disconnect(clockName, port.getName(), style);
 }
 
+bool NetworkClock::Private::read(ConnectionReader& reader)
+{
+    Bottle bot;
+    bool ok = bot.read(reader);
+
+    if (closing) {
+        _time = -1;
+        return false;
+    }
+
+    if (!ok && !closing) {
+        YARP_ERROR(Logger::get(), "Error reading clock port");
+        return false;
+    }
+
+    timeMutex.lock();
+    sec = bot.get(0).asInt32();
+    nsec = bot.get(1).asInt32();
+    _time = sec + (nsec * 1e-9);
+    initted = true;
+    timeMutex.unlock();
+
+    listMutex.lock();
+    auto waiter_it = waiters->begin();
+    while (waiter_it != waiters->end()) {
+        if (waiter_it->first - _time < 1E-12) {
+            Semaphore* waiterSemaphore = waiter_it->second;
+            waiter_it = waiters->erase(waiter_it);
+            if (waiterSemaphore) {
+                waiterSemaphore->post();
+            }
+        } else {
+            ++waiter_it;
+        }
+    }
+    listMutex.unlock();
+    return true;
+}
+
+
+NetworkClock::NetworkClock() :
+        mPriv(new Private)
+{
+}
+
+NetworkClock::~NetworkClock()
+{
+    YARP_WARN(Logger::get(), "Destroying network clock");
+    delete mPriv;
+}
 
 bool NetworkClock::open(const std::string& clockSourcePortName, std::string localPortName)
 {
-    port.setReadOnly();
-    port.setReader(*this);
+    mPriv->port.setReadOnly();
+    mPriv->port.setReader(*mPriv);
     NestedContact nc(clockSourcePortName);
-    clockName = clockSourcePortName;
+    mPriv->clockName = clockSourcePortName;
     yarp::os::ContactStyle style;
     style.persistent = true;
 
-    if(localPortName == "")
-    {
+    if (localPortName == "") {
         const int MAX_STRING_SIZE = 255;
-        char hostName [MAX_STRING_SIZE];
+        char hostName[MAX_STRING_SIZE];
         yarp::os::gethostname(hostName, MAX_STRING_SIZE);
-        hostName[strlen(hostName)] = '\0';      // manually set the null terminator, so it is ok in any case
 
         yarp::os::SystemInfo::ProcessInfo processInfo = yarp::os::SystemInfo::getProcessInfo();
 
@@ -88,100 +162,57 @@ bool NetworkClock::open(const std::string& clockSourcePortName, std::string loca
     }
 
     // if receiving port cannot be opened, return false.
-    bool ret = port.open(localPortName);
-    if (!ret)
+    bool ret = mPriv->port.open(localPortName);
+    if (!ret) {
         return false;
+    }
 
-    if (nc.getNestedName()=="")
-    {
+    if (nc.getNestedName() == "") {
         Contact src = NetworkBase::queryName(clockSourcePortName);
 
+        ret = NetworkBase::connect(clockSourcePortName, mPriv->port.getName(), style);
 
-        ret = NetworkBase::connect(clockSourcePortName, port.getName(), style);
-
-        if(!src.isValid())
-            fprintf(stderr,"Cannot find time port \"%s\"; for a time topic specify \"%s@\"\n", clockSourcePortName.c_str(), clockSourcePortName.c_str());
+        if (!src.isValid()) {
+            fprintf(stderr, "Cannot find time port \"%s\"; for a time topic specify \"%s@\"\n", clockSourcePortName.c_str(), clockSourcePortName.c_str());
+        }
     }
 
     return ret;
 }
 
-double NetworkClock::now() {
-    timeMutex.lock();
-    double result = _time;
-    timeMutex.unlock();
+double NetworkClock::now()
+{
+    mPriv->timeMutex.lock();
+    double result = mPriv->_time;
+    mPriv->timeMutex.unlock();
     return result;
 }
 
-void NetworkClock::delay(double seconds) {
-    if (seconds<=1E-12) {
+void NetworkClock::delay(double seconds)
+{
+    if (seconds <= 1E-12) {
         return;
     }
 
-    listMutex.lock();
-    if (closing) {
+    mPriv->listMutex.lock();
+    if (mPriv->closing) {
         // We are shutting down.  The time signal is no longer available.
         // Make a short delay and return.
-        listMutex.unlock();
+        mPriv->listMutex.unlock();
         SystemClock::delaySystem(seconds);
         return;
     }
 
-    Waiters* waiters = static_cast<Waiters*>(pwaiters);
-
     std::pair<double, Semaphore*> waiter(now() + seconds, new Semaphore(0));
-    waiters->insert(waiters->end(), waiter);
-    listMutex.unlock();
+    mPriv->waiters->push_back(waiter);
+    mPriv->listMutex.unlock();
 
     waiter.second->wait();
     delete waiter.second;
     waiter.second = nullptr;
 }
 
-bool NetworkClock::isValid() const {
-    return initted;
-}
-
-bool NetworkClock::read(ConnectionReader& reader) {
-    Bottle bot;
-    bool ok = bot.read(reader);
-
-    if(closing)
-    {
-        _time = -1;
-        return false;
-    }
-
-    if (!ok && !closing)
-    {
-        YARP_ERROR(Logger::get(), "Error reading clock port");
-        return false;
-    }
-
-    timeMutex.lock();
-    sec = bot.get(0).asInt32();
-    nsec = bot.get(1).asInt32();
-    _time = sec + (nsec*1e-9);
-    initted = true;
-    timeMutex.unlock();
-
-    listMutex.lock();
-    Waiters* waiters = static_cast<Waiters*>(pwaiters);
-    Waiters::iterator waiter_i;
-
-    waiter_i = waiters->begin();
-    while (waiter_i != waiters->end())
-    {
-        if (waiter_i->first - _time < 1E-12 )
-        {
-            Semaphore *waiterSemaphore = waiter_i->second;
-            waiter_i = waiters->erase(waiter_i);
-            if (waiterSemaphore)
-                waiterSemaphore->post();
-        }
-        else
-            ++waiter_i;
-    }
-    listMutex.unlock();
-    return true;
+bool NetworkClock::isValid() const
+{
+    return mPriv->initted;
 }
