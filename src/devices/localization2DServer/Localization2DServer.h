f091fbe0f3a517ad21cf49357eb210708938df54
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-10-03 13:46:57 +0200

    Fix license

--- src/devices/localization2DServer/Localization2DServer.h
+++ src/devices/localization2DServer/Localization2DServer.h
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ */
 
 #ifndef YARP_DEV_LOCALIZATION2DSERVER_H
 #define YARP_DEV_LOCALIZATION2DSERVER_H
