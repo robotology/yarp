e7f8838a7eb59afa7a9c2fb0c3fc77c6406860b7
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-10-04 19:44:20 +0200

    Add SystemInfoTest

--- tests/libYARP_OS/SystemInfoTest.cpp
+++ tests/libYARP_OS/SystemInfoTest.cpp
@@ -6,86 +6,103 @@
  * BSD-3-Clause license. See the accompanying LICENSE file for details.
  */
 
-#include <yarp/os/MessageStack.h>
 #include <yarp/os/SystemInfo.h>
-#include <yarp/os/impl/UnitTest.h>
+
 #include <sstream>
 
+#if defined(USE_SYSTEM_CATCH)
+#include <catch.hpp>
+#else
+#include "catch.hpp"
+#endif
 
-using namespace yarp::os::impl;
 using namespace yarp::os;
 
-class SystemInfoTest : public UnitTest {
-private:
+TEST_CASE("OS::SystemInfoTest", "[yarp::os]") {
+
     std::ostringstream str;
 
-    void emptyAndClearString() {
-        str.str(std::string());
-        str.clear();
-    }
-
-public:
-    virtual std::string getName() const override {
-        return "SystemInfoTest";
-    }
-
-    void startCheck() {
-        report(0, "Getting (system dependent) load info:");
+    SECTION("Getting (system dependent) load info:") {
         SystemInfo::LoadInfo ldinfo = SystemInfo::getLoadInfo();
-        str << "Instant: " << ldinfo.cpuLoadInstant << ", CPU1: " << ldinfo.cpuLoad5 << ", CPU5: " << ldinfo.cpuLoad5 << ", CPU15: " << ldinfo.cpuLoad15 << ".";
-        report(0, std::string(str.str()));
+        str << "Instant: " << ldinfo.cpuLoadInstant
+            << ", CPU1: " << ldinfo.cpuLoad1
+            << ", CPU5: " << ldinfo.cpuLoad5
+            << ", CPU15: " << ldinfo.cpuLoad15
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) memory info:");
+    SECTION("Getting (system dependent) memory info:") {
         SystemInfo::MemoryInfo meminfo = SystemInfo::getMemoryInfo();
-        str << "Free memory space: " << meminfo.freeSpace << ", total memory space: " << meminfo.totalSpace << ".\n";
-        report(0, std::string(str.str()));
+        str << "Free memory space: " << meminfo.freeSpace
+            << ", total memory space: " << meminfo.totalSpace
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) platform info:");
+    SECTION("Getting (system dependent) platform info:") {
         SystemInfo::PlatformInfo pltinfo = SystemInfo::getPlatformInfo();
-        str << "Codename: " << pltinfo.codename << ", distribution: " << pltinfo.distribution << ", kernel: " << pltinfo.kernel << ", name: " << pltinfo.name << ", release: " << pltinfo.release << ", environment variables: "<< pltinfo.environmentVars.toString() << ".\n";
-        report(0, std::string(str.str()));
+        str << "Codename: " << pltinfo.codename
+            << ", distribution: " << pltinfo.distribution
+            << ", kernel: " << pltinfo.kernel
+            << ", name: " << pltinfo.name
+            << ", release: " << pltinfo.release
+            << ", environment variables: "<< pltinfo.environmentVars.toString()
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) process info for process 1:");
+    SECTION("Getting (system dependent) process info for process 1:") {
         SystemInfo::ProcessInfo prctinfo = SystemInfo::getProcessInfo(1);
-        str << "Name: " << prctinfo.name << ", arguments: " << prctinfo.arguments << ", PID: " << prctinfo.pid << ", scheduler policy: " << prctinfo.schedPolicy << ", scheduler priority: " << prctinfo.schedPriority << ".\n";
-        report(0, std::string(str.str()));
+        str << "Name: " << prctinfo.name
+            << ", arguments: " << prctinfo.arguments
+            << ", PID: " << prctinfo.pid
+            << ", scheduler policy: " << prctinfo.schedPolicy
+            << ", scheduler priority: " << prctinfo.schedPriority
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) process info for current process:");
-        prctinfo = SystemInfo::getProcessInfo();
-        str << "Name: " << prctinfo.name << ", arguments: " << prctinfo.arguments << ", PID: " << prctinfo.pid << ", scheduler policy: " << prctinfo.schedPolicy << ", scheduler priority: " << prctinfo.schedPriority << ".\n";
-        report(0, std::string(str.str()));
+    SECTION("Getting (system dependent) process info for current process:") {
+        SystemInfo::ProcessInfo prctinfo = SystemInfo::getProcessInfo();
+        str << "Name: " << prctinfo.name
+            << ", arguments: " << prctinfo.arguments
+            << ", PID: " << prctinfo.pid
+            << ", scheduler policy: " << prctinfo.schedPolicy
+            << ", scheduler priority: " << prctinfo.schedPriority
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) processor info:");
+    SECTION("Getting (system dependent) processor info:") {
         SystemInfo::ProcessorInfo prcsinfo = SystemInfo::getProcessorInfo();
-        str << "Arch: " << prcsinfo.architecture << ", cores: " << prcsinfo.cores << ", family: " << prcsinfo.family << ", frequency: " << prcsinfo.frequency << ", model: " << prcsinfo.model << ", model number: " << prcsinfo.modelNumber << ", siblings: " << prcsinfo.siblings << ", vendor: " << prcsinfo.vendor << ".\n";
-        report(0, std::string(str.str()));
+        str << "Arch: " << prcsinfo.architecture
+            << ", cores: " << prcsinfo.cores
+            << ", family: " << prcsinfo.family
+            << ", frequency: " << prcsinfo.frequency
+            << ", model: " << prcsinfo.model
+            << ", model number: " << prcsinfo.modelNumber
+            << ", siblings: " << prcsinfo.siblings
+            << ", vendor: " << prcsinfo.vendor
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) storage info:");
+    SECTION("Getting (system dependent) storage info:") {
         SystemInfo::StorageInfo strginfo = SystemInfo::getStorageInfo();
-        str << "Free storage space: " << strginfo.freeSpace << ", total storage space: " << strginfo.totalSpace << ".\n";
-        report(0, std::string(str.str()));
+        str << "Free storage space: " << strginfo.freeSpace
+            << ", total storage space: " << strginfo.totalSpace
+            << ".\n";
+        INFO(str.str());
+    }
 
-        emptyAndClearString();
-        report(0, "Getting (system dependent) user info:");
+    SECTION("Getting (system dependent) user info:") {
         SystemInfo::UserInfo usrinfo = SystemInfo::getUserInfo();
-        str << "User ID: " << usrinfo.userID << ", username: " << usrinfo.userName << ", real name: " << usrinfo.realName << ", home directory: " << usrinfo.homeDir << ".\n";
-        report(0, std::string(str.str()));
+        str << "User ID: " << usrinfo.userID
+            << ", username: " << usrinfo.userName
+            << ", real name: " << usrinfo.realName
+            << ", home directory: " << usrinfo.homeDir
+            << ".\n";
+        INFO(str.str());
     }
-
-    virtual void runTests() override {
-        startCheck();
-    }
-};
-
-static SystemInfoTest theTest;
-
-UnitTest& getSystemInfoTest() {
-    return theTest;
 }
-
