05c9fb859063c872c0b4cd4f66910449e00e7201
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-10-04 14:49:25 +0200

    Add LogStreamTest and LogTest

--- tests/libYARP_OS/LogTest.cpp
+++ tests/libYARP_OS/LogTest.cpp
@@ -8,31 +8,41 @@
 
 #include <yarp/os/Log.h>
 
-#include <yarp/os/impl/UnitTest.h>
+#if defined(USE_SYSTEM_CATCH)
+#include <catch.hpp>
+#else
+#include "catch.hpp"
+#endif
 
+TEST_CASE("OS::LogTest", "[yarp::os]") {
+    int i = 42;
 
-class LogTest : public yarp::os::impl::UnitTest {
-public:
-    virtual std::string getName() const override { return "LogTest"; }
-
-    void checkLog() {
-        int i = 13;
-
+    SECTION("Test yTrace") {
         yTrace("This is a trace");
         yTrace("This is %s (%d)", "a trace", i);
+    }
 
+    SECTION("Test yDebug") {
         yDebug("This is a debug");
         yDebug("This is %s (%d)", "a debug", i);
+    }
 
+    SECTION("Test yInfo") {
         yInfo("This is info");
         yInfo("This is %s (%d)", "info", i);
+    }
 
+    SECTION("Test yWarning") {
         yWarning("This is a warning");
         yWarning("This is %s (%d)", "a warning", i);
+    }
 
+    SECTION("Test yError") {
         yError("This is an error");
         yError("This is %s (%d)", "an error", i);
+    }
 
+    SECTION("Other log tests") {
         yInfo("This is a very long long sentence. The purpose is to check if the logger is able to handle and display nicely very long messages, eventually displaying them on multiple lines or using other mechanisms (e.g. pop-up, user hints etc)");
 
         yInfo("This is a sentence that spawn on multiple lines:\n.This is line 2.\nThis is line 3.\nThis is the last line.");
@@ -42,14 +52,4 @@ public:
         yInfo("This is text contains an end of line at the end of the sentence\n");
 
     }
-
-    virtual void runTests() override {
-        checkLog();
-    }
-};
-
-static LogTest theLogTest;
-
-yarp::os::impl::UnitTest& getLogTest() {
-    return theLogTest;
 }
