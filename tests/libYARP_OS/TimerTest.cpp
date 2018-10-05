f5390952ba33e57bc2c250fcc9a3b7b3e75efca2
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-10-05 14:19:51 +0200

    Port TimerTest

--- tests/libYARP_OS/TimerTest.cpp
+++ tests/libYARP_OS/TimerTest.cpp
@@ -6,137 +6,159 @@
  * BSD-3-Clause license. See the accompanying LICENSE file for details.
  */
 
-#include <yarp/os/Timer.h>
+#include <string>
 #include <yarp/os/Network.h>
 #include <yarp/os/impl/UnitTest.h>
-#include <yarp/os/Thread.h>
+#include <yarp/os/Timer.h>
 #include <yarp/os/Time.h>
 #include <string>
 #include <yarp/os/LogStream.h>
-#include <string>
 #include <cmath>
+
+#if defined(USE_SYSTEM_CATCH)
+#include <catch.hpp>
+#else
+#include "catch.hpp"
+#endif
 
 using namespace yarp::os;
 using namespace yarp::os::impl;
 using namespace std;
 
-class TimerTest : public UnitTest {
 
-    bool         timingTestResult{false};
-    double       startTime{0};
-    double       timingTolerance{0.1};
-    double       spinDuration{0.5};
-    unsigned int runCount{ 1 };
 
-public:
-    virtual std::string getName() const override { return "TimerTest"; }
+static bool monoMultiThreadCallback(const YarpTimerEvent& timings)
+{
+    return true;
+}
 
+#ifdef BROKEN_TEST
+static bool         timingTestResult{false};
+static double       startTime{0};
+static double       timingTolerance{0.1};
+static double       spinDuration{0.5};
+static unsigned int runCount{ 1 };
+
+static bool timingCallback(const YarpTimerEvent& timings)
+{
+    timingTestResult = true; //the callback has been called
+    double now = yarp::os::Time::now();
+    vector<pair<std::function<bool()>, string>> checks //checks vector.. add new lines here to add new checks
+    {
+        {[&timings]       { return timings.currentExpected - startTime + spinDuration * runCount; }, "currentExpected error"},
+        {[&timings, &now] { return fabs(now - timings.currentReal) < 0.0001; }, "currentReal error"},
+        {[&timings]       { return runCount == 1 || (timings.lastDuration - spinDuration) < timingTolerance; }, "lastDuration error"},
+        {[&timings, &now] { return fabs((now - timings.lastExpected) - spinDuration) < timingTolerance; }, "lastExpexted error"},
+        {[&timings]       { return runCount == 1 || fabs(timings.lastExpected - timings.lastReal) < timingTolerance; }, "lastReal error"},
+        {[&timings]       { return timings.runCount == runCount; }, "runCount error"}
+    };
+
+    for (auto& c : checks)
+    {
+        timingTestResult = c.first();
+        if (!timingTestResult)
+        {
+            yError() << c.second;
+            return false;
+        }
+    }
+    runCount++;
+    return true;
+}
+static void timingTest(bool multiThread)
+{
+    Timer timing(TimerSettings(spinDuration), &timingCallback, multiThread);
+    startTime = yarp::os::Time::now();
+    timing.start();
+    yarp::os::Time::delay(5);
+    timing.stop();
+    CHECK(timingTestResult);
+    INFO(string((multiThread ? "MultiThread " : "MonoThread ")) + string("timing test").c_str());
+}
+#endif // BROKEN_TEST
+
+static void monoMultiThreadTest(bool multiThread)
+{
+    int threadCount = yarp::os::Thread::getCount();
+    vector<Timer*> timers
+    {
+        new Timer({0.5},&monoMultiThreadCallback, multiThread),
+        new Timer({0.4},&monoMultiThreadCallback, multiThread),
+        new Timer({0.3},&monoMultiThreadCallback, multiThread)
+    };
+
+    for (auto& t : timers)
+    {
+        t->start();
+    }
+
+    if (multiThread)
+        CHECK(yarp::os::Thread::getCount() - threadCount == 3); // multiThread test
+    else
+        CHECK(yarp::os::Thread::getCount() - threadCount == 1); // singleThread test
+
+    for (auto& t : timers)
+    {
+        t->stop();
+        delete t;
+    }
+}
+
+static void apiTest(bool multiThread)
+{
+    int i{ 0 }, j{ 0 };
+    Timer t(TimerSettings(0.1, 3, 10), [&i](const YarpTimerEvent& timings) {i++; return true; }, multiThread);
+    Timer t2(TimerSettings(0.1, 10, 0.4), [&j](const YarpTimerEvent& timings) {j++; return true; }, multiThread);
+
+    CHECK(t.getSettings() == TimerSettings(0.1, 3, 10)); // API getSettings check
+    CHECK_FALSE(t.isRunning()); // API isRunning check
+    t.step();
+    CHECK(i == 1); // API step check
+    t.start();
+    t2.start();
+    CHECK((t.isRunning() && t2.isRunning())); // API start check
+    yarp::os::Time::delay(1);
+    CHECK((2 < j && j < 5)); // time expiration
+    CHECK(i == 4); // run count expiration
+    CHECK_FALSE((t.isRunning() || t2.isRunning())); // timers automatically stopped
+}
+
+
+TEST_CASE("OS::TimerTest", "[yarp::os]") {
     //please note that the return values of the callbacks does not rappresent the result of the tests but simply keeps the timer alive..
-    bool timingCallback(const YarpTimerEvent& timings)
-    {
-        timingTestResult = true; //the callback has been called
-        double now = yarp::os::Time::now();
-        vector<pair<std::function<bool()>, string>> checks //checks vector.. add new lines here to add new checks
-        {
-            {[&timings, this]       { return timings.currentExpected - startTime + spinDuration * runCount; }, "currentExpected error"},
-            {[&timings, this, &now] { return fabs(now - timings.currentReal) < 0.0001; }, "currentReal error"},
-            {[&timings, this]       { return runCount == 1 || (timings.lastDuration - spinDuration) < timingTolerance; }, "lastDuration error"},
-            {[&timings, this, &now] { return fabs((now - timings.lastExpected) - spinDuration) < timingTolerance; }, "lastExpexted error"},
-            {[&timings, this]       { return runCount == 1 || fabs(timings.lastExpected - timings.lastReal) < timingTolerance; }, "lastReal error"},
-            {[&timings, this]       { return timings.runCount == runCount; }, "runCount error"}
-        };
 
-        for (auto& c : checks)
-        {
-            timingTestResult = c.first();
-            if (!timingTestResult)
-            {
-                yError() << c.second;
-                return false;
-            }
-        }
-        runCount++;
-        return true;
+#ifdef BROKEN_TEST
+    //fails with valgrind for valgrind slowing down the system
+    SECTION("timing multithread")
+    {
+        timingTest(true);
     }
 
-    bool monoMultiThreadCallback(const YarpTimerEvent& timings)
+    SECTION("timing monothread")
     {
-        return true;
+        runCount = 1;
+        timingTest(false);
     }
+#endif // BROKEN_TEST
 
-    void timingTest(bool multiThread)
+    SECTION("api test multithread")
     {
-        Timer timing(TimerSettings(spinDuration), &TimerTest::timingCallback, this, multiThread);
-        startTime = yarp::os::Time::now();
-        timing.start();
-        yarp::os::Time::delay(5);
-        timing.stop();
-        checkTrue(timingTestResult, (string((multiThread ? "MultiThread " : "MonoThread ")) + string("timing test")).c_str());
-    }
-
-    void monoMultiThreadTest(bool multiThread)
-    {
-        int threadCount = yarp::os::Thread::getCount();
-        vector<Timer*> timers
-        {
-            new Timer({0.5},&TimerTest::monoMultiThreadCallback, this, multiThread),
-            new Timer({0.4},&TimerTest::monoMultiThreadCallback, this, multiThread),
-            new Timer({0.3},&TimerTest::monoMultiThreadCallback, this, multiThread)
-        };
-
-        for (auto& t : timers)
-        {
-            t->start();
-        }
-
-        if (multiThread)
-            checkTrue(yarp::os::Thread::getCount() - threadCount == 3, "multiThread test");
-        else
-            checkTrue(yarp::os::Thread::getCount() - threadCount == 1, "singleThread test");
-
-        for (auto& t : timers)
-        {
-            t->stop();
-            delete t;
-        }
-    }
-
-    void apiTest(bool multiThread)
-    {
-        int i{ 0 }, j{ 0 };
-        Timer t(TimerSettings(0.1, 3, 10), [&i](const YarpTimerEvent& timings) {i++; return true; }, multiThread);
-        Timer t2(TimerSettings(0.1, 10, 0.4), [&j](const YarpTimerEvent& timings) {j++; return true; }, multiThread);
-
-        checkTrue(t.getSettings() == TimerSettings(0.1, 3, 10), "API getSettings check");
-        checkFalse(t.isRunning(), "API isRunning check");
-        t.step();
-        checkTrue(i == 1, "API step check");
-        t.start();
-        t2.start();
-        checkTrue(t.isRunning() && t2.isRunning(), "API start check");
-        yarp::os::Time::delay(1);
-        checkTrue(2 < j && j < 5, "time expiration");
-        checkTrue(i == 4, "run count expiration");
-        checkFalse(t.isRunning() || t2.isRunning(), "timers automatically stopped");
-    }
-
-    virtual void runTests() override
-    {
-        //fails with valgrind for valgrind slowing down the system
-        //timingTest(true);
-        //runCount = 1;
-        //timingTest(false);
-
         apiTest(true);
+    }
+
+    SECTION("api test monothread")
+    {
         apiTest(false);
+    }
+
+    SECTION("mono multithread test multithread")
+    {
         monoMultiThreadTest(true);
+    }
+
+    SECTION("mono multithread test monothread")
+    {
         monoMultiThreadTest(false);
     }
-};
 
-
-static TimerTest theTimerTest;
-
-UnitTest& getTimerTest() {
-    return theTimerTest;
 }
