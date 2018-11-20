b128b720ddf6e1a1d2af4345d6ae3121c0424e20
Nicolo' Genesio <nicolo.genesio@iit.it>
2018-11-20 11:11:18 +0100

    Add ServerTest

--- tests/libYARP_serversql/ServerTest.cpp
+++ tests/libYARP_serversql/ServerTest.cpp
@@ -12,7 +12,13 @@
 
 #include <yarp/os/all.h>
 #include <yarp/os/impl/NameClient.h>
-#include <yarp/os/impl/UnitTest.h>
+
+
+#if defined(USE_SYSTEM_CATCH)
+#include <catch.hpp>
+#else
+#include "catch.hpp"
+#endif
 
 using namespace yarp::os;
 using namespace yarp::os::impl;
@@ -22,109 +28,86 @@ using namespace yarp::os::impl;
  * Name server regression tests.
  *
  */
-class ServerTest : public UnitTest {
-public:
-    virtual std::string getName() const override { return "ServerTest"; }
+TEST_CASE("serversql::ServerTest", "[yarp::serversql]") {
 
-    void checkRegisterFree() {
-        report(0,"checking free register command...");
+    SECTION("check register free") {
+        INFO("checking free register command...");
         NameClient& nic = NameClient::getNameClient();
         nic.registerName("/check/register1");
         Contact addr1 = nic.queryName("/check/register1");
-        checkTrue(addr1.isValid(),"got an address");
-        checkEqual(addr1.getCarrier().c_str(),"tcp","correct carrier");
+        CHECK(addr1.isValid()); // got an address
+        CHECK(addr1.getCarrier() == "tcp"); // correct carrier
 
         nic.registerName("/check/register2");
         Contact addr2 = nic.queryName("/check/register2");
-        checkTrue(addr2.isValid(),"got a second address");
-        checkTrue(addr1.getPort()!=addr2.getPort(),"different port number");
-        checkTrue(addr1.getHost()==addr2.getHost(),"same machine");
+        CHECK(addr2.isValid()); // got a second address
+        CHECK(addr1.getPort()!=addr2.getPort()); // different port number
+        CHECK(addr1.getHost()==addr2.getHost()); // same machine
 
         Contact addr3 = nic.queryName("/check/register1");
-        checkTrue(addr3.isValid(),"first address still there");
-        checkEqual(addr1.getPort(),addr3.getPort(),"same port number");
+        CHECK(addr3.isValid()); // first address still there
+        CHECK(addr1.getPort() == addr3.getPort()); // same port number
 
         Contact addr4 = nic.queryName("/check/register2");
-        checkTrue(addr4.isValid(),"second address still there");
-        checkEqual(addr2.getPort(),addr4.getPort(),"same port number");
+        CHECK(addr4.isValid()); // second address still there
+        CHECK(addr2.getPort() == addr4.getPort()); // same port number
     }
 
-    void checkRegisterForced() {
-        report(0,"checking forced register command...");
+    SECTION("check register forced") {
+        INFO("checking forced register command...");
         NameClient& nic = NameClient::getNameClient();
         Contact addr1("tcp", "localhost", 9999);
         nic.registerName("/check/register/forced",addr1);
         Contact addr2 = nic.queryName("/check/register/forced");
-        checkTrue(addr1.isValid(),"got an address");
-        checkEqual(addr1.getHost(),addr2.getHost(),"same machine");
-        checkEqual(addr1.getPort(),addr2.getPort(),"same port number");
+        CHECK(addr1.isValid()); // got an address
+        CHECK(addr1.getHost() == addr2.getHost()); // same machine
+        CHECK(addr1.getPort() == addr2.getPort()); // same port number
         //Contact a2 = nic.queryName("/bar2");
-        //checkEqual(a2.isValid(),false,"non-existent address");
+        //CHECK(a2.isValid() == false); // non-existent address
     }
 
-    void checkUnregister() {
-        report(0,"checking unregister command...");
+    SECTION("check unregister") {
+        INFO("checking unregister command...");
         NameClient& nic = NameClient::getNameClient();
         nic.registerName("/check/unregister");
         Contact addr1 = nic.queryName("/check/unregister");
-        checkTrue(addr1.isValid(),"got an address");
+        CHECK(addr1.isValid()); // got an address
         nic.unregisterName("/check/unregister");
         Contact addr2 = nic.queryName("/check/unregister");
-        checkFalse(addr2.isValid(),"got no address");
+        CHECK_FALSE(addr2.isValid()); // got no address
     }
 
-    void checkPortRegister() {
-        report(0,"checking port registration...");
+    SECTION("check port register") {
+        INFO("checking port registration...");
         NameClient& nic = NameClient::getNameClient();
         Port p;
         Contact addr1 = nic.queryName("/check/port");
-        checkFalse(addr1.isValid(),"got an address");
+        CHECK_FALSE(addr1.isValid()); // got an address
         p.open("/check/port");
         Contact addr2 = nic.queryName("/check/port");
-        checkTrue(addr2.isValid(),"got no address");
+        CHECK(addr2.isValid()); // got no address
         p.close();
         Contact addr3 = nic.queryName("/check/port");
-        checkFalse(addr3.isValid(),"got an address");
+        CHECK_FALSE(addr3.isValid()); // got an address
     }
 
-    void checkList() {
-        report(0,"checking list...");
+    SECTION("check list") {
+        INFO("checking list...");
         NameClient& nic = NameClient::getNameClient();
         Contact addr1("tcp", "192.168.1.100", 9998);
         nic.registerName("/check/list",addr1);
         std::string result = nic.send("NAME_SERVER list",true);
         std::string target = "registration name /check/list ip 192.168.1.100 port 9998 type tcp";
-        checkTrue(result.find(target)!=std::string::npos,"listing found");
+        CHECK(result.find(target)!=std::string::npos); // listing found
     }
 
-    void checkSetGet() {
-        report(0,"checking set/get...");
+    SECTION("check set get") {
+        INFO("checking set/get...");
         NameClient& nic = NameClient::getNameClient();
         nic.registerName("/check/set");
         std::string result = nic.send("NAME_SERVER set /check/set prop val",true);
         result = nic.send("NAME_SERVER get /check/set prop",true);
         std::string target = "port /check/set property prop = val";
-        checkTrue(result.find(target)!=std::string::npos,"answer found");
+        CHECK(result.find(target)!=std::string::npos); // answer found
     }
-
-    virtual void runTests() override {
-        NetworkBase::setLocalMode(true);
-
-        checkRegisterFree();
-        checkRegisterForced();
-        checkUnregister();
-        checkPortRegister();
-        checkList();
-        checkSetGet();
-
-        NetworkBase::setLocalMode(false);
-
-    }
-};
-
-
-static ServerTest theServerTest;
-
-UnitTest& getServerTest() {
-    return theServerTest;
 }
