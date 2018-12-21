d0ff2b82ebbee2958496cd2b14cd6ea1736da3f1
Andrea Ruzzenenti <ruzzenenti.a@gmail.com>
2018-12-21 18:24:00 +0100

    added documentation and cleanup

--- src/libYARP_OS/include/yarp/os/RFPlugin.h
+++ src/libYARP_OS/include/yarp/os/RFPlugin.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
 
 #include <yarp/os/api.h>
 #include <string>
@@ -17,17 +24,32 @@ public:
     RFPlugin();
 
     virtual ~RFPlugin();
-
+    
+    /**
+     * opens the plugin
+     * @param command a string containing the equivalent command line
+     * @return true if opened succesfully
+     */
     virtual bool open(const std::string& command);
 
+    /**
+     * closes the plugin
+     */
     virtual void close();
 
+    /**
+     * check if the plugin is running
+     */
     virtual bool isRunning();
 
+    /**
+     * get the command line used to open the plugin
+     */
     virtual std::string getCmd();
 
-    virtual std::string getAlias();
-
+    /**
+     * get the thread id
+     */
     virtual int getThreadKey();
 };
 
--- src/libYARP_OS/src/RFPlugin.cpp
+++ src/libYARP_OS/src/RFPlugin.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
 
 #include <yarp/os/RFPlugin.h>
 #include <yarp/os/YarpPluginSelector.h>
@@ -53,11 +60,6 @@ RFPlugin::~RFPlugin()
 string RFPlugin::getCmd()
 {
     return impl->command;
-}
-
-string RFPlugin::getAlias()
-{
-    return impl->alias;
 }
 
 int RFPlugin::getThreadKey()
