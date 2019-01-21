79d8e6e810d9876aed43fcce037cbe5bfc904cea
Marco Randazzo <marco.randazzo@iit.it>
2019-01-21 17:43:05 +0100

    added tests for Map2DLocation and Map2DArea classes

--- tests/libYARP_dev/MapGrid2DTest.cpp
+++ tests/libYARP_dev/MapGrid2DTest.cpp
@@ -8,6 +8,8 @@
 
 #include <yarp/dev/MapGrid2D.h>
 #include <yarp/dev/IMap2D.h>
+#include <yarp/dev/Map2DLocation.h>
+#include <yarp/dev/Map2DArea.h>
 #include <yarp/os/Network.h>
 #include <yarp/dev/PolyDriver.h>
 
@@ -70,14 +72,14 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         yarp::dev::MapGrid2D::XYCell cell1_test(6, 4);
         yarp::dev::MapGrid2D::XYWorld world1(3, -1);
         cell1 = test_cnvutils_map1.world2Cell(world1);
-        CHECK(cell1 == cell1_test); // IMap2D world2Cell() operation successfull
+        CHECK(cell1 == cell1_test); // IMap2D world2Cell() operation successful
 
         MapGrid2D test_cnvutils_map2 = test_map;
         yarp::dev::MapGrid2D::XYCell cell2(6, 4);
         yarp::dev::MapGrid2D::XYWorld world2;
         yarp::dev::MapGrid2D::XYWorld world2_test(3, -1);
         world2 = test_cnvutils_map2.cell2World(cell2);
-        CHECK(world2 == world2_test); // IMap2D cell2World() operation successfull
+        CHECK(world2 == world2_test); // IMap2D cell2World() operation successful
 
         MapGrid2D test_cnvutils_map3 = test_map;
         yarp::dev::MapGrid2D::XYCell cell3_ok(0, 0);
@@ -92,7 +94,7 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(world4_err));
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(cell5_err));
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(world5_err));
-        // IMap2D isInsideMap() test successfull
+        // IMap2D isInsideMap() test successful
     }
 
     SECTION("Checking standard compliance of description")
@@ -103,7 +105,7 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         //Property& ros_prop = pmapserver_cfg.addGroup("ROS");
         //ros_prop.put("enable_ros_publisher", "0");
         //ros_prop.put("enable_ros_subscriber", "0");
-        REQUIRE(ddmapserver.open(pmapserver_cfg)); // ddmapserver open reported successfull
+        REQUIRE(ddmapserver.open(pmapserver_cfg)); // ddmapserver open successful
 
         IMap2D* imap = nullptr;
         PolyDriver ddmapclient;
@@ -111,9 +113,9 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         pmapclient_cfg.put("device", "map2DClient");
         pmapclient_cfg.put("local", "/mapClientTest");
         pmapclient_cfg.put("remote", "/mapServer");
-        REQUIRE(ddmapclient.open(pmapclient_cfg)); // ddmapclient open reported successfull
+        REQUIRE(ddmapclient.open(pmapclient_cfg)); // ddmapclient open successful
 
-        REQUIRE(ddmapclient.view(imap)); // IMap2D interface open reported successfull
+        REQUIRE(ddmapclient.view(imap)); // IMap2D interface open successful
 
         MapGrid2D test_store_map1;
         MapGrid2D test_store_map2;
@@ -121,37 +123,108 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
 
         test_store_map1.setMapName("test_map1");
         test_store_map2.setMapName("test_map2");
-        std::vector<std::string> names;
+        std::vector<std::string> map_names;
 
         imap->store_map(test_store_map1);
         imap->store_map(test_store_map2);
         imap->get_map("test_map1",test_get_map);
-        CHECK(test_store_map1.isIdenticalTo(test_get_map)); // IMap2D store/get operation successfull
+        CHECK(test_store_map1.isIdenticalTo(test_get_map)); // IMap2D store/get operation successful
 
-        imap->get_map_names(names);
-        bool b1 = (names.size() == 2);
+        imap->get_map_names(map_names);
+        bool b1 = (map_names.size() == 2);
         bool b2 = false;
         if (b1) {
-            b2 = (names[0] == "test_map1" && names[1] == "test_map2");
+            b2 = (map_names[0] == "test_map1" && map_names[1] == "test_map2");
         }
         CHECK(b1);
         CHECK(b2);
-        // IMap2D get_map_names operation successfull")
+        // IMap2D get_map_names operation successful")
 
         imap->remove_map("test_map1");
-        imap->get_map_names(names);
-        b1 = (names.size() == 1);
-        CHECK(b1); // IMap2D remove_map operation successfull
+        imap->get_map_names(map_names);
+        b1 = (map_names.size() == 1);
+        CHECK(b1); // IMap2D remove_map operation successful
 
         imap->clearAllMaps();
-        imap->get_map_names(names);
-        b1 = (names.size() == 0);
-        CHECK(b1); // IMap2D clear operation successfull
+        imap->get_map_names(map_names);
+        b1 = (map_names.size() == 0);
+        CHECK(b1); // IMap2D clear operation successful
 
-        //closing the drivers
+
+        //////////////// check locations and areas
+        std::vector <Map2DArea>      areas;
+        std::vector <Map2DLocation>  locs;
+        std::vector <std::string>    loc_names;
+        Map2DLocation loc;
+        Map2DArea     area;
+        bool ret;
+        ret = imap->clearAllLocations();   CHECK(ret);
+        ret = imap->clearAllAreas();       CHECK(ret);
+        ret = imap->getAreasList(loc_names);   CHECK(ret);
+        b1 = (loc_names.size() == 0);
+        CHECK(b1);
+        ret = imap->getLocationsList(loc_names);   CHECK(ret);
+        b1 = (loc_names.size() == 0);
+        CHECK(b1);
+
+        ret = imap->storeLocation("loc1",Map2DLocation("map1", 1, 2, 3));  CHECK(ret);
+        ret = imap->storeLocation("loc2",Map2DLocation("map2", 4, 5, 6));  CHECK(ret);
+
+        std::vector<Map2DLocation> vec1, vec2, vec3;
+        yarp::dev::Map2DLocation v;
+        v.x = 1.1; v.y = 1.1; vec1.push_back(v);
+        v.x = 1.2; v.y = 1.2; vec1.push_back(v);
+        v.x = 1.3; v.y = 1.3; vec1.push_back(v);
+        ret =  imap->storeArea("area1",Map2DArea("map1", vec1));  CHECK(ret);
+        v.x = 2.1; v.y = 2.1; vec2 = vec1;  vec2.push_back(v);
+        ret = imap->storeArea("area2",Map2DArea("map2", vec2));  CHECK(ret);
+        v.x = 3.1; v.y = 3.1; vec3 = vec2;  vec3.push_back(v);
+        ret = imap->storeArea("area3",Map2DArea("map3", vec3));  CHECK(ret);
+
+        ret = imap->getAreasList(loc_names);   CHECK(ret);
+        b1 = (loc_names.size() == 3);
+        CHECK(b1);
+        ret= imap->getLocationsList(loc_names);  CHECK(ret);
+        b1 = (loc_names.size() == 2);
+        CHECK(b1);
+
+        ret = imap->getArea("area1",area);   CHECK(ret);
+        b1 = (area == Map2DArea("map1", vec1));
+        CHECK(b1);
+        ret = imap->getArea("area_err", area);   CHECK(ret==false);
+
+        ret = imap->getLocation("loc1",loc);  CHECK(ret);
+        b1 = (loc == Map2DLocation("map1", 1, 2, 3));
+        CHECK(b1);
+        ret = imap->getLocation("loc_err", loc);   CHECK(ret==false);
+
+        ret = imap->deleteArea("area1");       CHECK(ret);
+        ret = imap->deleteLocation("loc1");  CHECK(ret);
+        ret = imap->deleteArea("area_err");       CHECK(ret==false);
+        ret = imap->deleteLocation("loc_err");  CHECK(ret==false);
+        ret = imap->getArea("area1",area);    CHECK(ret==false);
+        ret = imap->getLocation("loc1",loc);  CHECK(ret==false);
+
+        ret = imap->getArea("area2",area);  CHECK(ret);
+        b1 = (area == Map2DArea("map2", vec2));
+        CHECK(b1);
+        ret = imap->getLocation("loc2",loc);   CHECK(ret);
+        b1 = (loc == Map2DLocation("map2", 4, 5, 6));
+        CHECK(b1);
+
+        ret = imap->clearAllLocations();  CHECK(ret);
+        ret = imap->clearAllAreas();  CHECK(ret);
+        ret = imap->getAreasList(loc_names);  CHECK(ret);
+        b1 = (loc_names.size() == 0);
+        CHECK(b1);
+        imap->getLocationsList(loc_names);
+        b1 = (loc_names.size() == 0);
+        CHECK(b1);
+
+        //////////////// closing the drivers
         CHECK(ddmapclient.close());
         CHECK(ddmapserver.close());
-        // map2DServer and map2DClient polydrivers closed succesfully
+        // map2DServer and map2DClient polydrivers closed successfully
     }
 
     Network::setLocalMode(false);
