f75e2c401b252e0195fc030222cb2b584fcf5788
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-08-01 18:25:21 +0200

    yarprobotinterface: Remove unused code

--- src/yarprobotinterface/main.cpp
+++ src/yarprobotinterface/main.cpp
@@ -23,12 +23,6 @@
 #include <yarp/os/Time.h>
 #include <yarp/dev/Drivers.h>
 
-#ifdef ICUB_USE_REALTIME_LINUX
-#include <csignal>
-#include <unistd.h>
-#include <sys/mman.h>
-#endif //ICUB_USE_REALTIME_LINUX
-
 int main(int argc, char *argv[])
 {
     yarp::os::Network yarp; //initialize network, this goes before everything
@@ -36,16 +30,6 @@ int main(int argc, char *argv[])
     if (!yarp.checkNetwork()) {
         yFatal() << "Sorry YARP network does not seem to be available, is the yarp server available?";
     }
-
-#ifdef ICUB_USE_REALTIME_LINUX
-    struct sched_param sch_param;
-    sch_param.__sched_priority = sched_get_priority_max(SCHED_FIFO)/3; //33
-    if( sched_setscheduler(0, SCHED_FIFO, &sch_param) != 0 ) {
-        yWarning() << "Cannot set the scheduler to FIFO. (check superuser permission)";
-    }
-    //if( mlockall(MCL_CURRENT | MCL_FUTURE) != 0 )
-    //    yWarning() << "Cannot lock memory swapping (check superuser permission)";
-#endif //ICUB_USE_REALTIME_LINUX
 
     yarp::os::ResourceFinder &rf(yarp::os::ResourceFinder::getResourceFinderSingleton());
     rf.setVerbose();
