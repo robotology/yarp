88068bcf5829105b7c55a5c2a0560951485c0608
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-23 12:16:53 +0200

    dev: Split AudioVisualInterfaces classes in correspondent .h/.cpp files

---
+++ src/libYARP_dev/include/yarp/dev/IAudioVisualStream.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2006-2019 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+/**
+ * @ingroup dev_iface_media
+ *
+ * For streams capable of holding different kinds of content,
+ * check what they actually have.
+ *
+ */
+
+#ifndef YARP_DEV_IAUDIOVISUALSTREAM_H
+#define YARP_DEV_IAUDIOVISUALSTREAM_H
+
+#include <yarp/dev/api.h>
+
+namespace yarp {
+namespace dev {
+
+class YARP_dev_API IAudioVisualStream
+{
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~IAudioVisualStream();
+
+    virtual bool hasAudio() = 0;
+
+    virtual bool hasVideo() = 0;
+
+    virtual bool hasRawVideo() {
+        return hasVideo();
+    }
+};
+
+} // namespace dev
+} // namespace yarp
+
+#endif // YARP_DEV_IAUDIOVISUALSTREAM_H
---
+++ src/libYARP_dev/include/yarp/dev/IFrameWriterAudioVisual.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2006-2019 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_DEV_IFRAMEWRITERAUDIOVISUAL_H
+#define YARP_DEV_IFRAMEWRITERAUDIOVISUAL_H
+
+#include <yarp/dev/api.h>
+
+#include <yarp/sig/Image.h>
+#include <yarp/sig/Sound.h>
+
+namespace yarp {
+namespace dev {
+
+/**
+ * @ingroup dev_iface_media
+ *
+ * Write a YARP-format image and sound to a device.
+ */
+class YARP_dev_API IFrameWriterAudioVisual
+{
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~IFrameWriterAudioVisual();
+
+    /**
+     * Write an image and sound
+     *
+     * @param image the image to be written
+     * @param sound the sound to be written
+     * @return true/false upon success/failure
+     */
+    virtual bool putAudioVisual(yarp::sig::ImageOf<yarp::sig::PixelRgb>& image,
+                                yarp::sig::Sound& sound) = 0;
+};
+
+} // namespace dev
+} // namespace yarp
+
+#endif // YARP_DEV_IFRAMEWRITERAUDIOVISUAL_H
