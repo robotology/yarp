be0117a9fc95ebf7a199df3d32fbadb60babaadd
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-25 16:07:15 +0200

    dev: Split AudioGrabberInterfaces classes in correspondent .h/.cpp files

---
+++ src/libYARP_dev/include/yarp/dev/AudioGrabberVocabs.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2006-2019 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_DEV_AUDIOGRABBERVOCABS_H
+#define YARP_DEV_AUDIOGRABBERVOCABS_H
+
+#include <yarp/os/Vocab.h>
+
+constexpr yarp::conf::vocab32_t VOCAB_AUDIO_INTERFACE = yarp::os::createVocab('s', 'n', 'd');
+constexpr yarp::conf::vocab32_t VOCAB_AUDIO_START     = yarp::os::createVocab('r', 'u', 'n');
+constexpr yarp::conf::vocab32_t VOCAB_AUDIO_STOP      = yarp::os::createVocab('s', 't', 'o', 'p');
+
+#endif // YARP_DEV_AUDIOGRABBERVOCABS_H
--- src/libYARP_dev/src/IAudioGrabberSound.cpp
+++ src/libYARP_dev/src/IAudioGrabberSound.cpp
@@ -1,17 +1,11 @@
 /*
  * Copyright (C) 2006-2019 Istituto Italiano di Tecnologia (IIT)
- * Copyright (C) 2006-2010 RobotCub Consortium
  * All rights reserved.
  *
  * This software may be modified and distributed under the terms of the
  * BSD-3-Clause license. See the accompanying LICENSE file for details.
  */
 
-#ifndef YARP_DEV_AUDIOGRABBERINTERFACES_H
-#define YARP_DEV_AUDIOGRABBERINTERFACES_H
-
-#include <yarp/dev/AudioGrabberVocabs.h>
 #include <yarp/dev/IAudioGrabberSound.h>
-#include <yarp/dev/IAudioRender.h>
 
-#endif // YARP_DEV_AUDIOGRABBERINTERFACES_H
+yarp::dev::IAudioGrabberSound::~IAudioGrabberSound() = default;
