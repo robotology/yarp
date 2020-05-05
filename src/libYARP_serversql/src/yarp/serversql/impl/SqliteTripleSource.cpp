a0f1d858713edade5624ecce548217e9b94e34ac
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-05-05 11:09:07 +0200

    Try to make Visual Studio 2017 happy

--- src/libYARP_serversql/src/yarp/serversql/impl/SqliteTripleSource.cpp
+++ src/libYARP_serversql/src/yarp/serversql/impl/SqliteTripleSource.cpp
@@ -14,6 +14,7 @@
 
 using yarp::serversql::impl::SqliteTripleSource;
 using yarp::serversql::impl::Triple;
+using yarp::serversql::impl::TripleContext;
 
 SqliteTripleSource::SqliteTripleSource(sqlite3 *db) : db(db)
 {
