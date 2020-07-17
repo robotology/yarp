07cf64413fa0772f7ad4965c94c65db36029aa31
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-07-17 14:40:44 +0200

    Cleanup

--- src/libYARP_dev/src/yarp/dev/IControlDebug.h
+++ src/libYARP_dev/src/yarp/dev/IControlDebug.h
@@ -12,45 +12,43 @@
 
 #include <yarp/dev/api.h>
 
-namespace yarp
-{
-    namespace dev
-    {
-        class IControlDebug;
-    }
-}
+namespace yarp {
+namespace dev {
 
 /**
  * @ingroup dev_iface_motor
  *
  * Interface for control devices, debug commands.
  */
-class YARP_dev_API yarp::dev::IControlDebug
+class YARP_dev_API IControlDebug
 {
 public:
     /**
      * Destructor.
      */
-    virtual ~IControlDebug() {}
+    virtual ~IControlDebug() = default;
 
     /* Set the print function, pass here a pointer to your own function
      * to print. This function should implement "printf" like parameters.
      * @param a pointer to the print function
      * @return I don't see good reasons why it should return false.
      */
-    virtual bool setPrintFunction(int (*f) (const char *fmt, ...))=0;
+    virtual bool setPrintFunction(int (*f)(const char* fmt, ...)) = 0;
 
     /* Read the content of the board internal memory, this is usually done
      * at boot time, but can be forced by calling this method.
      * @return true/false on success failure
      */
-    virtual bool loadBootMemory()=0;
+    virtual bool loadBootMemory() = 0;
 
     /* Save the current board configuration to the internal memory,
      * this values are read at boot time or if loadBootMemory() is called.
      * @return true/false on success/failure
      */
-    virtual bool saveBootMemory()=0;
+    virtual bool saveBootMemory() = 0;
 };
+
+} // namespace dev
+} // namespace yarp
 
 #endif // YARP_DEV_ICONTROLDEBUG_H
