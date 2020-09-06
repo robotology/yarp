150a8ba253b9fe359ad16fa420a1a1adf75f7f16
fabio <valla.fabio@live.it>
2020-09-06 22:23:10 +0200

    Comment zfp dependency

--- src/carriers/libffmpeg_portmonitor/ffmpegPortmonitor.cpp
+++ src/carriers/libffmpeg_portmonitor/ffmpegPortmonitor.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 
 extern "C" {
-    #include "zfp.h"
+    //#include "zfp.h"
     #include <libavcodec/codec.h>
     #include <libavcodec/avcodec.h>
     #include <libavutil/opt.h>
