f3f36219bcc9a5fa6c972fe6e46b6171ec9b66a8
Marco Randazzo <marco.randazzo@iit.it>
2020-12-01 01:09:07 +0100

    Improved fakeMicrophone device. It can now generate various kind of signals

--- src/devices/fakeMicrophone/fakeMicrophone.cpp
+++ src/devices/fakeMicrophone/fakeMicrophone.cpp
@@ -26,8 +26,6 @@ using namespace yarp::dev;
 using namespace yarp::sig;
 
 constexpr size_t c_SAMPLES_TO_BE_COPIED=512; //samples
-constexpr size_t c_default_freq = 44100; //hz
-constexpr size_t c_channels = 2; //stereo
 constexpr size_t c_bytes_form = 2; //16bit
 
 namespace {
@@ -39,7 +37,6 @@ typedef unsigned short int audio_sample_16t;
 fakeMicrophone::fakeMicrophone() :
         PeriodicThread(DEFAULT_PERIOD)
 {
-    start_time = yarp::os::Time::now();
 }
 
 fakeMicrophone::~fakeMicrophone()
@@ -61,16 +58,51 @@ bool fakeMicrophone::open(yarp::os::Searchable &config)
         yCInfo(FAKEMICROPHONE) << "Using default period of " << DEFAULT_PERIOD << " s";
     }
 
-    m_audiorecorder_cfg.numSamples = c_default_freq *5; //5sec
-    m_audiorecorder_cfg.numChannels = c_channels;
-    m_audiorecorder_cfg.frequency = c_default_freq;
+    size_t tmp_freq = 44100; //Hz
+    size_t tmp_channels = 2;
+    if (config.check("channels"))
+    {
+        tmp_channels = config.find("channels").asInt32();
+    }
+
+    if (config.check("sampling_frequency"))
+    {
+        tmp_freq = config.find("frequency").asInt32();
+    }
+
+    if (config.check("signal_frequency"))
+    {
+        m_sig_freq = config.find("signal_frequency").asInt32();
+    }
+
+    if (config.check("waveform"))
+    {
+        std::string waveform = config.find("waveform").asString();
+        if      (config.find("waveform").toString() == "sine") { m_waveform = waveform_t::sine; }
+        else if (config.find("waveform").toString() == "sawtooth") { m_waveform = waveform_t::sawtooth; }
+        else if (config.find("waveform").toString() == "square") { m_waveform = waveform_t::square; }
+        else if (config.find("waveform").toString() == "constant") { m_waveform = waveform_t::constant; }
+        else if (config.check("waveform")) { yError() << "Unsupported value for waveform parameter"; return false; }
+
+        if      (m_waveform == waveform_t::sine)      { yCInfo(FAKEMICROPHONE) << "Using sine waveform, signal amplitude=" << m_wave_amplitude     << ", signal frequency=" << m_sig_freq; }
+        else if (m_waveform == waveform_t::sawtooth)  { yCInfo(FAKEMICROPHONE) << "Using sawtooth waveform, signal amplitude=" << m_wave_amplitude << ", signal frequency=" << m_sig_freq; }
+        else if (m_waveform == waveform_t::square)    { yCInfo(FAKEMICROPHONE) << "Using square waveform, signal amplitude=" << m_wave_amplitude   << ", signal frequency=" << m_sig_freq; }
+        else if (m_waveform == waveform_t::constant)  { yCInfo(FAKEMICROPHONE) << "Using constant waveform, signal amplitude="<< m_wave_amplitude  << ", signal frequency=" << m_sig_freq; }
+    }
+
+    //data structure initialization
+    m_audiorecorder_cfg.numSamples = tmp_freq * 5; //5sec
+    m_audiorecorder_cfg.numChannels = tmp_channels;
+    m_audiorecorder_cfg.frequency = tmp_freq;
     m_audiorecorder_cfg.bytesPerSample = c_bytes_form;
+
     const size_t EXTRA_SPACE = 2;
     AudioBufferSize buffer_size(m_audiorecorder_cfg.numSamples*EXTRA_SPACE, m_audiorecorder_cfg.numChannels, m_audiorecorder_cfg.bytesPerSample);
     m_inputBuffer = new yarp::dev::CircularAudioBuffer_16t("fake_mic_buffer", buffer_size);
 
+    m_max_count.resize(m_audiorecorder_cfg.numChannels);
+    m_counter.resize(m_audiorecorder_cfg.numChannels);
     //start the capture thread
-    start_time = yarp::os::Time::now();
     start();
     return true;
 }
@@ -78,6 +110,7 @@ bool fakeMicrophone::open(yarp::os::Searchable &config)
 bool fakeMicrophone::close()
 {
     fakeMicrophone::stop();
+
     //wait until the thread is stopped
 
     if (m_inputBuffer)
@@ -107,16 +140,56 @@ void fakeMicrophone::run()
     //each iteration, which occurs every xxx ms, I copy a bunch of samples in the buffer.
     for (size_t i = 0; i < c_SAMPLES_TO_BE_COPIED; i++)
     {
-        //this sine waveform has amplitude (-32000,32000)
-        constexpr double c_wave_ampl = 32000;
-        // on the left  channel it has frequency 440Hz (A4 note)
-        // on the right channel it has frequency 220Hz (A3 note)
-        double wt1 = double(t) / double (m_audiorecorder_cfg.frequency) * 440.0 * 2 * M_PI;
-        unsigned short elem1 = double(c_wave_ampl * sin(wt1));
-        double wt2 = double(t) / double(m_audiorecorder_cfg.frequency) * 220.0 * 2 * M_PI;
-        unsigned short elem2 = double(c_wave_ampl * sin(wt2));
-        m_inputBuffer->write(elem1); //chan1
-        m_inputBuffer->write(elem2); //chan2
-        t++;
+        // Default values:
+        // this signal has amplitude (-32000,32000)
+        // the first channel has frequency 440Hz (A4 note)
+        // the second channel has frequency 220Hz etc.
+        // and so on..
+        if (m_waveform == waveform_t::sine)
+        {
+            for (size_t i = 0; i < m_audiorecorder_cfg.numChannels; i++)
+            {
+                m_max_count[i] = double(m_audiorecorder_cfg.frequency) / m_sig_freq / double(i + 1);
+                unsigned short elem1 = double(m_wave_amplitude * sin(double(m_counter[i]) / m_max_count[i] * 2 * M_PI));
+                m_inputBuffer->write(elem1);
+                m_counter[i]++;
+                if (m_counter[i] >= m_max_count[i]) m_counter[i] = 0;
+            }
+       }
+        else if(m_waveform == waveform_t::sawtooth)
+        {
+            for (size_t i = 0; i < m_audiorecorder_cfg.numChannels; i++)
+            {
+                m_max_count[i] = double(m_audiorecorder_cfg.frequency) / m_sig_freq / double(i + 1);
+                unsigned short elem1 = m_wave_amplitude * 2 * (double(m_counter[i])/ m_max_count[i]) - m_wave_amplitude;
+                m_inputBuffer->write(elem1);
+                m_counter[i]++;
+                if (m_counter[i] >= m_max_count[i]) m_counter[i] = 0;
+            }
+        }
+        else if (m_waveform == waveform_t::square)
+        {
+            for (size_t i = 0; i < m_audiorecorder_cfg.numChannels; i++)
+            {
+                m_max_count[i] = double(m_audiorecorder_cfg.frequency) / m_sig_freq / double(i + 1);
+                unsigned short elem1 = m_counter[i] < m_max_count[i]/2  ? m_wave_amplitude : 0;
+                m_inputBuffer->write(elem1);
+                m_counter[i]++;
+                if (m_counter[i] >= m_max_count[i]) m_counter[i] = 0;
+            }
+        }
+        else if (m_waveform == waveform_t::constant)
+        {
+            for (size_t i = 0; i < m_audiorecorder_cfg.numChannels; i++)
+            {
+                m_inputBuffer->write(m_wave_amplitude / double(i + 1));
+                m_counter[i]++;
+                if (m_counter[i] >= m_max_count[i]) m_counter[i] = 0;
+            }
+        }
+        else
+        {
+            yCInfo(FAKEMICROPHONE) << "Not implemented/unreachable code";
+        }
     }
 }
--- src/devices/fakeMicrophone/fakeMicrophone.h
+++ src/devices/fakeMicrophone/fakeMicrophone.h
@@ -21,7 +21,18 @@
 /**
 * @ingroup dev_impl_fake dev_impl_media
 *
-* \brief `fakeMicrophone` : fake device implementing the IAudioGrabberSound device interface to play sound
+* \brief `fakeMicrophone` : fake microphone device implementing the IAudioGrabberSound interface to generate a test sound.
+* It can generate various signals, i.e. sine, sawtooth, square wave, constant
+*
+* Parameters required by this device are:
+* | Parameter name | SubParameter   | Type    | Units          | Default Value            | Required                    | Description                                                       | Notes |
+* |:--------------:|:--------------:|:-------:|:--------------:|:------------------------:|:--------------------------: |:-----------------------------------------------------------------:|:-----:|
+* | period         |      -         | double  | s              |  0.010                   | No                          | the period of processing thread                                   | A value of 10ms is recommended. Do to not modify it |
+* | channels       |      -         | size_t  | -              | 2                        | No                          | Number of channels (e.g. 1=mono, 2-stereo etc)                    | - |
+* | waveform       |      -         | string  | -              | sine                     | No                          | Defines the shape of the waveform. Can be one of the following: sine,sawtooth,square,constant | - |
+* | sampling_frequency |      -     | int     | Hz             | 44100                    | No                          | Sampling frequency | - |
+* | signal_frequency   |      -     | int     | Hz             | 440                      | No                          | Frequency of the generated signal | - |
+
 */
 class fakeMicrophone :
         public yarp::dev::DeviceDriver,
@@ -46,6 +57,16 @@ private:
     void run() override;
 
 private:
-    double t=0;
-    double start_time;
+    std::vector<size_t> m_counter;
+    std::vector<size_t> m_max_count;
+    size_t m_wave_amplitude = 32000;
+    double m_sig_freq=440; //Hz
+
+    enum waveform_t
+    {
+        sine = 0,
+        sawtooth = 1,
+        square = 2,
+        constant = 3
+    } m_waveform = sine;
 };
