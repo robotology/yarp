8892b68f8c1243cbf90dae3a79f5dcff3d0a72d5
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-05-13 23:18:47 +0200

    dev: Move IFrameGrabberImageRaw interface in separate header file

--- src/devices/usbCamera/common/USBcamera.h
+++ src/devices/usbCamera/common/USBcamera.h
@@ -30,7 +30,7 @@
 #include <yarp/dev/IFrameGrabberRgb.h>
 #include <yarp/dev/IFrameGrabberControls.h>
 #include <yarp/dev/IFrameGrabberImage.h>
-#include <yarp/dev/FrameGrabberInterfaces.h>
+#include <yarp/dev/IFrameGrabberImageRaw.h>
 #include <yarp/dev/IVisualParams.h>
 #include <yarp/dev/IPreciselyTimed.h>
 
--- src/libYARP_dev/src/yarp/dev/FrameGrabberInterfaces.h
+++ src/libYARP_dev/src/yarp/dev/FrameGrabberInterfaces.h
@@ -10,79 +10,14 @@
 #ifndef YARP_FRAMEGRABBERINTERFACES_H
 #define YARP_FRAMEGRABBERINTERFACES_H
 
-#include <string>
-
-#include <yarp/dev/api.h>
-#include <yarp/sig/Image.h>
-#include <yarp/sig/Vector.h>
-
 #include <yarp/dev/IFrameGrabber.h>
 #include <yarp/dev/IFrameGrabberRgb.h>
 #include <yarp/dev/IFrameGrabberImage.h>
+#include <yarp/dev/IFrameGrabberImageRaw.h>
 #include <yarp/dev/IFrameGrabberControls.h>
 #include <yarp/dev/IFrameGrabberControlsDC1394.h>
 #include <yarp/dev/IFrameWriterImage.h>
 
-/*! \file FrameGrabberInterfaces.h define common interfaces for frame
-  grabber devices */
-
-
-namespace yarp {
-namespace dev {
-
-/**
- * @ingroup dev_iface_media
- *
- * Read a YARP-format image from a device.
- */
-class YARP_dev_API IFrameGrabberImageRaw
-{
-public:
-    /**
-     * Destructor.
-     */
-    virtual ~IFrameGrabberImageRaw(){}
-    /**
-     * Get a raw image from the frame grabber
-     *
-     * @param image the image to be filled
-     * @return true/false upon success/failure
-     */
-    virtual bool getImage(yarp::sig::ImageOf<yarp::sig::PixelMono>& image) = 0;
-
-    /**
-     * Get a crop of the rgb image from the frame grabber, if required
-     * demosaicking/color reconstruction is applied
-     *
-     * Note: this is not configuring the camera sensor to acquire a crop
-     *       of the image, nor to generate a cropped version of the streaming.
-     *       Instead, the full image is acquired and then a crop is created from
-     *       it. The crop is meant to be created by the image producer upon user
-     *       request via RPC call.
-     *
-     * @param cropType enum specifying how the crop shall be generated, defined at FrameGrabberInterfaces.h
-     * @param vertices the input coordinate (u,v) required by the cropType
-     * @param image the image to be filled
-     * @return true/false upon success/failure
-     */
-    virtual bool getImageCrop(cropType_id_t cropType, yarp::sig::VectorOf<std::pair<int, int> > vertices, yarp::sig::ImageOf<yarp::sig::PixelMono>& image) { return false; };
-
-    /**
-     * Return the height of each frame.
-     * @return image height
-     */
-    virtual int height() const =0;
-
-    /**
-     * Return the width of each frame.
-     * @return image width
-     */
-    virtual int width() const =0;
-};
-
-
-} // namespace dev
-} // namespace yarp
 #ifndef YARP_NO_DEPRECATED // Since YARP 3.0.0
 #include <yarp/dev/FrameGrabberControl2.h>
 #endif
