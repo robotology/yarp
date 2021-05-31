e9f4b12604c5051a5c6a26ad875c8f73cbd82ff2
Ettore Landini <ettore.landini@iit.it>
2021-05-31 18:27:45 +0200

    frameTransformSet: nws and nwc added
    YARP nws and nwc have been added. To be tested

---
+++ src/devices/frameTransformSet/FrameTransformSet_nwc_yarp.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "FrameTransformSet_nwc_yarp.h"
+#include <yarp/os/Log.h>
+#include <yarp/os/LogComponent.h>
+#include <yarp/os/LogStream.h>
+
+using namespace std;
+using namespace yarp::dev;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+
+namespace {
+YARP_LOG_COMPONENT(FRAMETRANSFORSETNWCYARP, "yarp.device.frameTransformSet_nwc_yarp")
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+FrameTransformSet_nwc_yarp::FrameTransformSet_nwc_yarp(double tperiod) :
+PeriodicThread(tperiod),
+m_thriftPortName("/frameTransformSet/rpc")
+{
+}
+
+bool FrameTransformSet_nwc_yarp::open(yarp::os::Searchable& config)
+{
+    if (!yarp::os::NetworkBase::checkNetwork()) {
+        yCError(FRAMETRANSFORSETNWCYARP,"Error! YARP Network is not initialized");
+        return false;
+    }
+
+    bool okGeneral = config.check("GENERAL");
+    if(okGeneral)
+    {
+        yarp::os::Searchable& general_config = config.findGroup("GENERAL");
+        if (general_config.check("period"))         {m_period = general_config.find("period").asDouble();}
+        if (general_config.check("rpc_port"))       {m_thriftPortName = general_config.find("thrift_port").asString();}
+    }
+    if(!m_thriftPort.open(m_thriftPortName))
+    {
+        yCError(FRAMETRANSFORSETNWCYARP,"Could not open \"%s\" port",m_thriftPortName.c_str());
+        return false;
+    }
+    if (!m_setRPC.yarp().attachAsClient(m_thriftPort))
+    {
+        yCError(FRAMETRANSFORSETNWCYARP, "Error! Cannot attach the port as a client");
+        return false;
+    }
+
+    return true;
+}
+
+bool FrameTransformSet_nwc_yarp::close()
+{
+    return true;
+}
+
+void FrameTransformSet_nwc_yarp::run()
+{
+    std::lock_guard <std::mutex> lg(m_pd_mutex);
+    return;
+}
+
+bool FrameTransformSet_nwc_yarp::setTransform(const yarp::math::FrameTransform& transform)
+{
+    std::lock_guard <std::mutex> lg(m_pd_mutex);
+    if(!m_setRPC.setTransform(transform))
+    {
+        yCError(FRAMETRANSFORSETNWCYARP, "Unable to set transformation");
+        return false;
+    }
+    return true;
+}
+
+bool FrameTransformSet_nwc_yarp::setTransforms(const std::vector<yarp::math::FrameTransform>& transforms)
+{
+    std::lock_guard <std::mutex> lg(m_pd_mutex);
+    if(!m_setRPC.setTransforms(transforms))
+    {
+        yCError(FRAMETRANSFORSETNWCYARP, "Unable to set transformations");
+        return false;
+    }
+    return true;
+}
