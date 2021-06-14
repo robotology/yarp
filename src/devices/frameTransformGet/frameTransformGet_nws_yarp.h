672e0a65349190f64aa2c83d8ee070f89d30f868
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2021-06-14 17:33:10 +0200

    added frametransform get wrappers and multiplexer

--- src/devices/frameTransformGet/frameTransformGet_nws_yarp.h
+++ src/devices/frameTransformGet/frameTransformGet_nws_yarp.h
@@ -10,14 +10,13 @@
 #define YARP_DEV_FRAMETRANSFORMGET_NWS_YARP_H
 
 #include <yarp/dev/DeviceDriver.h>
-#include <yarp/dev/IMultipleWrapper.h>
+#include <yarp/dev/WrapperSingle.h>
 #include <yarp/dev/IFrameTransformStorage.h>
 
-#include <yarp/os/PeriodicThread.h>
 
 #include <FrameTransformStorageGetRPC.h>
 
-constexpr double DEFAULT_THREAD_PERIOD = 0.01; // 10 millisecond
+constexpr double const_default_thread_period = 0.01; // 10 millisecond
 // TODO FIXME STE need to check subdevice option
 /*
  * \section FrameTransformGet_nws_yarp_device_parameters Description of input parameters
@@ -27,7 +26,6 @@ constexpr double DEFAULT_THREAD_PERIOD = 0.01; // 10 millisecond
  *   Parameters required by this device are:
  * | Parameter name | SubParameter            | Type    | Units          | Default Value             | Required     | Description                                    | Notes        |
  * |:--------------:|:-----------------------:|:-------:|:--------------:|:-------------------------:|:-----------: |:----------------------------------------------:|:------------:|
- * | period         |      -                  | double  | sec            |   0.01                    | No           | refresh period of the broadcasted values in ms | default 10ms |
  * | rpc_port       |      -                  | string  | -              |   /frameTransformGet/rpc  | No           | port opened by the device for rpc calls        |              |
  *
  * Some example of configuration files:
@@ -36,16 +34,14 @@ constexpr double DEFAULT_THREAD_PERIOD = 0.01; // 10 millisecond
  *
  * \code{.unparsed}
  * device FrameTransformGet_nws_yarp
- * period 0.01
- * rpc_port /frameTransformGet/rpc
+\ * rpc_port /frameTransformGet/rpc
  * \endcode
  */
 
 class FrameTransformGet_nws_yarp :
         public yarp::dev::DeviceDriver,
         public FrameTransformStorageGetRPC,
-        public yarp::dev::IMultipleWrapper,
-        public yarp::os::PeriodicThread
+        public yarp::dev::WrapperSingle
 {
 
 public:
@@ -54,7 +50,7 @@ public:
     FrameTransformGet_nws_yarp(FrameTransformGet_nws_yarp&&) = delete;
     FrameTransformGet_nws_yarp& operator=(const FrameTransformGet_nws_yarp&) = delete;
     FrameTransformGet_nws_yarp& operator=(FrameTransformGet_nws_yarp&&) = delete;
-    ~FrameTransformGet_nws_yarp() override = default;
+    ~FrameTransformGet_nws_yarp() = default;
 
     // yarp::dev::DeviceDriver
     bool  open(yarp::os::Searchable &params) override;
@@ -62,45 +58,23 @@ public:
     bool  close() override;
 
     // yarp::dev::IWrapper
-    bool  attachAll(const yarp::dev::PolyDriverList& devices2attach) override;
-    bool  detachAll() override;
-
-    // yarp::os::PeriodicThread
-    void  run() override;
+    bool  attach(yarp::dev::PolyDriver* deviceToAttach) override;
+    bool  detach() override;
 
     // FrameTransformStorageGetRPC
     return_getAllTransforms getTransforms() override;
 
 private:
-    mutable std::vector<std::mutex>                    m_PolyDriver_mutex;
-    double m_period{DEFAULT_THREAD_PERIOD};
+    // mutable std::vector<std::mutex> m_PolyDriver_mutex;
+    // double m_period{const_default_thread_period};
     int    m_verbose{4};
 
     // for requesting the transforms to FrameTransformStorageGetMultiplexer
-    std::vector<yarp::dev::IFrameTransformStorageGet*> m_iFrameTransformStorageGetList;
-
-    // to read all the transforms
-    typedef yarp::os::BufferedPort<return_getAllTransforms> FrameTransformPortType;
+    yarp::dev::IFrameTransformStorageGet* m_iFrameTransformStorageGet;
 
     // for the RPC with the NWC
     yarp::os::Port      m_thrift_rpcPort;
     std::string         m_thrift_rpcPort_Name;
-
-    // this gives the parameters to configure the device
-    yarp::os::Property    m_configurationProperty;
-
-    // to manage the various parameters read from the configuration file
-    template <class T>
-    struct param
-    {
-        param(T& inVar, std::string inName)
-        {
-            var          = &inVar;
-            parname      = inName;
-        }
-        T*              var;
-        std::string     parname;
-    };
 };
 
 #endif   // YARP_DEV_FRAMETRANSFORMGET_NWS_YARP_H
