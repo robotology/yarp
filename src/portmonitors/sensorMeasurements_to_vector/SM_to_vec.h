25f2bcada31d3d2398d59f39548a4b2e7b358d63
Marco Randazzo <marco.randazzo@iit.it>
2021-06-15 15:18:27 +0200

    added new portMonitor `sensorMeasurements_to_vector`

---
+++ src/portmonitors/sensorMeasurements_to_vector/SM_to_vec.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_CARRIER_SM2VEC_CONVERTER_H
+#define YARP_CARRIER_SM2VEC_CONVERTER_H
+
+#include <yarp/os/Bottle.h>
+#include <yarp/os/Things.h>
+#include <yarp/os/MonitorObject.h>
+#include <yarp/sig/Vector.h>
+
+//example usage:
+//yarp connect /tracking/measures:o /in tcp+recv.portmonitor+type.dll+file.sensorMeasurements_to_vector
+
+class SensorMeasurements_to_vector : public yarp::os::MonitorObject
+{
+    yarp::sig::Vector    out;
+    yarp::os::Things     th;
+
+public:
+    bool create(const yarp::os::Property& options) override;
+    void destroy() override;
+
+    bool setparam(const yarp::os::Property& params) override;
+    bool getparam(yarp::os::Property& params) override;
+
+    bool accept(yarp::os::Things& thing) override;
+    yarp::os::Things& update(yarp::os::Things& thing) override;
+};
+
+#endif  // YARP_CARRIER_SM2VEC_CONVERTER_H
