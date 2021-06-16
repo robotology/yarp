455eccd2fc37b0730a30daf43afc1c6dd56e4c6a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-06-16 14:47:55 +0200

    sig/ImageNetworkHeader: Cleanup

--- src/libYARP_sig/src/yarp/sig/ImageNetworkHeader.h
+++ src/libYARP_sig/src/yarp/sig/ImageNetworkHeader.h
@@ -12,77 +12,60 @@
 
 #include <yarp/conf/system.h>
 
-#include <yarp/os/NetInt16.h>
 #include <yarp/os/NetInt32.h>
+#include <yarp/os/NetInt16.h>
 #include <yarp/os/Bottle.h>
 
 #include <yarp/sig/Image.h>
 
 namespace yarp {
-    namespace sig {
-        class ImageNetworkHeader;
-    }
-}
+namespace sig {
 
 /**
- *
  * Byte order in image header for network transmission.
- *
  */
 YARP_BEGIN_PACK
-class yarp::sig::ImageNetworkHeader
+class ImageNetworkHeader
 {
 public:
+    const yarp::os::NetInt32 listTag{BOTTLE_TAG_LIST};
+    const yarp::os::NetInt32 listLen{4};
+    const yarp::os::NetInt32 paramNameTag{BOTTLE_TAG_VOCAB32};
+    const yarp::os::NetInt32 paramName{yarp::os::createVocab('m','a','t')};
+    const yarp::os::NetInt32 paramIdTag{BOTTLE_TAG_VOCAB32};
+    yarp::os::NetInt32 id{0};
+    const yarp::os::NetInt32 paramListTag{BOTTLE_TAG_LIST + BOTTLE_TAG_INT32};
+    // WARNING This is 5 and not 6 because quantum and topIsLow are
+    //         transmitted in the same 32 bits for compatibility with
+    //         YARP 3.4 and older
+    const yarp::os::NetInt32 paramListLen{5};
+    yarp::os::NetInt32 depth{0};
+    yarp::os::NetInt32 imgSize{0};
+    yarp::os::NetInt16 quantum{0};
+    // WARNING The topIsLowIndex field in the ImageNetworkHeader is `0` for
+    //         `true` and `1` for `false` for compatibility with YARP 3.4
+    //         and older
+    yarp::os::NetInt16 topIsLow{0};
+    yarp::os::NetInt32 width{0};
+    yarp::os::NetInt32 height{0};
+    const yarp::os::NetInt32 paramBlobTag{BOTTLE_TAG_BLOB};
+    yarp::os::NetInt32 paramBlobLen{0};
 
-    yarp::os::NetInt32 listTag;
-    yarp::os::NetInt32 listLen;
-    yarp::os::NetInt32 paramNameTag;
-    yarp::os::NetInt32 paramName;
-    yarp::os::NetInt32 paramIdTag;
-    yarp::os::NetInt32 id;
-    yarp::os::NetInt32 paramListTag;
-    yarp::os::NetInt32 paramListLen;
-    yarp::os::NetInt32 depth;
-    yarp::os::NetInt32 imgSize;
-    yarp::os::NetInt16 quantum;
-    yarp::os::NetInt16 topIsLow;
-    yarp::os::NetInt32 width;
-    yarp::os::NetInt32 height;
-    yarp::os::NetInt32 paramBlobTag;
-    yarp::os::NetInt32 paramBlobLen;
-
-    ImageNetworkHeader() : listTag(0), listLen(0), paramNameTag(0),
-                           paramName(0), paramIdTag(0), id(0),
-                           paramListTag(0), paramListLen(0), depth(0),
-                           imgSize(0), quantum(0), topIsLow(0), width(0),
-                           height(0), paramBlobTag(0), paramBlobLen(0) {}
-
-    void setFromImage(const Image& image) {
-        listTag = BOTTLE_TAG_LIST;
-        listLen = 4;
-        paramNameTag = BOTTLE_TAG_VOCAB32;
-        paramName = yarp::os::createVocab('m','a','t');
-        paramIdTag = BOTTLE_TAG_VOCAB32;
+    void setFromImage(const Image& image)
+    {
         id = image.getPixelCode();
-        paramListTag = BOTTLE_TAG_LIST + BOTTLE_TAG_INT32;
-        // WARNING This is 5 and not 6 because quantum and topIsLow are
-        //         transmitted in the same 32 bits for compatibility with
-        //         YARP 3.4 and older
-        paramListLen = 5;
         depth = image.getPixelSize();
         imgSize = image.getRawImageSize();
         quantum = static_cast<yarp::os::NetInt16>(image.getQuantum());
-        // WARNING The topIsLowIndex field in the ImageNetworkHeader is `0` for
-        //         `true` and `1` for `false` for compatibility with YARP 3.4
-        //         and older
         topIsLow = image.topIsLowIndex() ? 0 : 1;
         width = image.width();
         height = image.height();
-        paramBlobTag = BOTTLE_TAG_BLOB;
         paramBlobLen = image.getRawImageSize();
     }
-
 };
 YARP_END_PACK
+
+} // namespace sig
+} // namespace yarp
 
 #endif // YARP_SIG_IMAGENETWORKHEADER_H
