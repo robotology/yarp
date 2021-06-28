56f07734b5f59a7185870c8a83ef598e2a801a96
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-06-28 18:24:53 +0200

    examples: Integrate "motorcontrol" example in build

--- example/dev/motorcontrol/motorcontrol.cpp
+++ example/dev/motorcontrol/motorcontrol.cpp
@@ -8,33 +8,30 @@
  */
 
 #include <yarp/os/Network.h>
-#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/os/Property.h>
+
+#include <yarp/dev/IControlCalibration.h>
 #include <yarp/dev/PolyDriver.h>
 
-using namespace yarp::os;
-using namespace yarp::dev;
-
-/** Lorenzo Natale, Dec 2007. Test remotization of calibrate
- *  functions. Useful for general tests on device remotization
- *  (see fakebot for example).
- */
-int main(int argc, const char **argv)
+int main(int argc, const char* argv[])
 {
-    Network yarp;
-    IControlCalibration2 *ical;
+    YARP_UNUSED(argc);
+    YARP_UNUSED(argv);
 
-    Property p;
+    yarp::os::Network yarp;
+    yarp::dev::IControlCalibration* ical;
+
+    yarp::os::Property p;
     p.put("device", "remote_controlboard");
     p.put("local", "/motortest");     //prefix for local names
     p.put("remote", "/controlboard"); //prefix for remote names
 
-    PolyDriver device;
+    yarp::dev::PolyDriver device;
     device.open(p);
     device.view(ical);
 
-    //ical->calibrate();
-    ical->calibrate2(1,1000,1.1,1.1,1.1);
-    ical->done(2);
+    ical->calibrateAxisWithParams(1, 1000, 1.1, 1.1, 1.1);
+    ical->calibrationDone(2);
     ical->park();
 
     device.close();
