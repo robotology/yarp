6977f5ba2581d670857cdb17245d86aa892356bc
Marco Randazzo <marco.randazzo@iit.it>
2021-08-09 16:53:09 +0200

    rpLidar3 improvements

--- src/devices/rpLidar3/rpLidar3.h
+++ src/devices/rpLidar3/rpLidar3.h
@@ -39,11 +39,11 @@ typedef unsigned char byte;
  * | GENERAL        | serial_port     | string  | -              |   -           | Yes          | Name of the serial port                                           |       |
  * | GENERAL        | serial_baudrate | int     | -              |   -           | Yes          | Baud rate of the serial port                                       |       |
  * | GENERAL        | sample_buffer_life | int  | -              |   -           | Yes          | Keeps data in memory for some iterations, in order to complete the scan with the missing values (the scan is not always complete)       |  |
- * | GENERAL        | motor_pwm       | int     | -              |   0           | No           | Used by internal RPLidar APIs                                     |       |
  * | GENERAL        | thread_period   | int     | ms             |   0           | No           | Acquisition thread period. The default value = 0 means maximum speed (measured duration ~75ms). It is useful to change it only if you need to slow down the device (e.g. 100ms)    |  |
- * | GENERAL        | express_mode    | bool    |                | false         | No           | Check sensor datasheet  |  |
- * | GENERAL        | force_scan      | bool    |                | false         | No           | Check sensor datasheet  |  |
- * | GENERAL        | scan_mode       | string  |                | Boost         | No           | Check sensor datasheet  |  |
+ * | RPLIDAR        | motor_pwm       | int     | -              |   0           | No           | Used by internal RPLidar APIs                                     |       |
+ * | RPLIDAR        | express_mode    | bool    |                | false         | No           | Check sensor datasheet  |  |
+ * | RPLIDAR        | force_scan      | bool    |                | false         | No           | Check sensor datasheet  |  |
+ * | RPLIDAR        | scan_mode       | string  |                | Boost         | No           | Check sensor datasheet  |  |
  */
 
 class RpLidar3 : public PeriodicThread, public yarp::dev::Lidar2DDeviceBase, public DeviceDriver
@@ -51,7 +51,7 @@ class RpLidar3 : public PeriodicThread, public yarp::dev::Lidar2DDeviceBase, pub
     typedef rp::standalone::rplidar::RPlidarDriver rplidardrv;
 
     void                  handleError(u_result error);
-    std::string deviceinfo();
+
 protected:
     int                   m_buffer_life = 0;
     bool                  m_inExpressMode = false;
@@ -60,13 +60,18 @@ protected:
     int                   m_pwm_val = 600;
     std::string           m_serialPort;
     rplidardrv*           m_drv = nullptr;
+    const size_t          m_nodes_num = 8192;
+    rplidar_response_measurement_node_hq_t* m_nodes = nullptr;
 
 public:
     RpLidar3(double period = 0) : PeriodicThread(period) //period=0 allows to run the thread as fast as possible, but it is not a busy loop since yield() is called internally
-    {}
+    {
+        m_nodes = new rplidar_response_measurement_node_hq_t[m_nodes_num];
+    }
 
     ~RpLidar3()
     {
+        delete [] m_nodes;
     }
 
     bool open(yarp::os::Searchable& config) override;
@@ -78,6 +83,7 @@ public:
 private:
     bool startMotor();
     bool startScan();
+    bool deviceinfo();
 
 public:
     //IRangefinder2D interface
