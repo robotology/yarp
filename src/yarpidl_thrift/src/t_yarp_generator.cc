6f7d2bf8fc484fba9df110e77c4945bff366fe73
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-09-01 17:11:00 +0200

    yarpidl_thrift: Add 'yarp.editor' annotation
    When set to 'false' the editor for that struct will not be generated.

--- src/yarpidl_thrift/src/t_yarp_generator.cc
+++ src/yarpidl_thrift/src/t_yarp_generator.cc
@@ -2146,7 +2146,14 @@ void t_yarp_generator::generate_struct(t_struct* tstruct)
     generate_struct_unwrapped_helper(tstruct, f_h_, f_cpp_);
 
     // Add editor class, if not disabled
-    if (!no_editor_) {
+    bool editor_enabled = !no_editor_;
+    if (annotations.find("yarp.editor") != annotations.end()) {
+        if (annotations.at("yarp.editor") == "false") {
+            editor_enabled = false;
+        }
+    }
+
+    if (editor_enabled) {
         generate_struct_editor(tstruct, f_h_, f_cpp_);
     }
 
