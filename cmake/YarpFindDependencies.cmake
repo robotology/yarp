0b00869a48208a54710a50dcab2bc9842895655c
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-09-28 16:11:50 +0200

    bindings: Require SWIG 4 or later

--- cmake/YarpFindDependencies.cmake
+++ cmake/YarpFindDependencies.cmake
@@ -319,7 +319,7 @@ set(YARP_CUSTOM_REASON_QGVCore "customized version")
 find_package(QGVCore QUIET)
 checkbuildandset_dependency(QGVCore Qt5 Graphviz)
 
-set(SWIG_REQUIRED_VERSION 3.0.12)
+set(SWIG_REQUIRED_VERSION 4)
 find_package(SWIG ${SWIG_REQUIRED_VERSION} QUIET)
 checkandset_dependency(SWIG)
 
---
+++ doc/release/master/swig4.md
@@ -0,0 +1,7 @@
+swig4 {#master}
+-----
+
+Important Changes
+-----------------
+
+* SWIG 4 or later is now required to build YARP bindings
