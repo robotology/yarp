5d889b613f7a5b9cac4834f1e90d8736413c933c
Marco Randazzo <marco.randazzo@iit.it>
2021-10-14 14:06:47 +0200

    * MapGrid2DTest.cpp refactored * Added tests for `Map2DClient -> Map2DServer` communication * Added tests for `Map2D_nwc_yarp -> Map2D_nws_yarp` communication * fixes to Map2DServerImpl.cpp detected by the new tests

---
+++ tests/libYARP_dev/IMap2DTest.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef IMAP2DTEST_H
+#define IMAP2DTEST_H
+
+#include <yarp/dev/IMap2D.h>
+
+using namespace yarp::dev;
+using namespace yarp::dev::Nav2D;
+using namespace yarp::sig;
+using namespace yarp::os;
+
+void exec_iMap2D_test_1(IMap2D* imap);
+void exec_iMap2D_test_2(IMap2D* imap);
+
+#endif
--- tests/libYARP_dev/MapGrid2DTest.cpp
+++ tests/libYARP_dev/MapGrid2DTest.cpp
@@ -93,14 +93,14 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         yarp::dev::Nav2D::XYCell cell1_test(6, 4);
         yarp::dev::Nav2D::XYWorld world1(3, -1);
         cell1 = test_cnvutils_map1.world2Cell(world1);
-        CHECK(cell1 == cell1_test); // IMap2D world2Cell() operation successful
+        CHECK(cell1 == cell1_test); // MapGrid2D::world2Cell() operation successful
 
         Nav2D::MapGrid2D test_cnvutils_map2 = test_map;
         yarp::dev::Nav2D::XYCell cell2(6, 4);
         yarp::dev::Nav2D::XYWorld world2;
         yarp::dev::Nav2D::XYWorld world2_test(3, -1);
         world2 = test_cnvutils_map2.cell2World(cell2);
-        CHECK(world2 == world2_test); // IMap2D cell2World() operation successful
+        CHECK(world2 == world2_test); // MapGrid2D::cell2World() operation successful
 
         Nav2D::MapGrid2D test_cnvutils_map3 = test_map;
         yarp::dev::Nav2D::XYCell cell3_ok(0, 0);
@@ -115,7 +115,7 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(world4_err));
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(cell5_err));
         CHECK_FALSE(test_cnvutils_map3.isInsideMap(world5_err));
-        // IMap2D isInsideMap() test successful
+        // MapGrid2D::isInsideMap() test successful
     }
 
     SECTION("Test copyPortable MapGrid2D")
@@ -311,252 +311,6 @@ TEST_CASE("dev::MapGrid2DTest", "[yarp::dev]")
         b = area3.checkLocationInsideArea(Map2DLocation("maptest", 0, -0.501, 0)); CHECK(b); //on the vertex
         b = area3.checkLocationInsideArea(Map2DLocation("maptest", 0, 0.5, 0)); CHECK(b==false); //outside
         b = area3.checkLocationInsideArea(Map2DLocation("maptest", 0.9, 0.5, 0)); CHECK(b); //inside
-    }
-
-    SECTION("Checking IMap2D methods")
-    {
-        PolyDriver ddmapserver;
-        PolyDriver ddmapclient;
-        IMap2D* imap = nullptr;
-
-        ////////"Checking opening map2DServer and map2DClient polydrivers"
-        {
-            Property pmapserver_cfg;
-            pmapserver_cfg.put("device", "map2DServer");
-            //Property& ros_prop = pmapserver_cfg.addGroup("ROS");
-            //ros_prop.put("enable_ros_publisher", "0");
-            //ros_prop.put("enable_ros_subscriber", "0");
-            REQUIRE(ddmapserver.open(pmapserver_cfg)); // ddmapserver open successful
-
-            Property pmapclient_cfg;
-            pmapclient_cfg.put("device", "map2DClient");
-            pmapclient_cfg.put("local", "/mapClientTest");
-            pmapclient_cfg.put("remote", "/mapServer");
-            REQUIRE(ddmapclient.open(pmapclient_cfg)); // ddmapclient open successful
-            REQUIRE(ddmapclient.view(imap)); // IMap2D interface open successful
-        }
-
-        //////////"Checking IMap2D methods which involve usage of classes Map2DArea or Map2DLocation"
-        {
-            std::vector <Map2DArea>      areas;
-            std::vector <Map2DLocation>  locs;
-            std::vector <std::string>    loc_names;
-            Map2DLocation loc;
-            Map2DArea     area;
-            bool ret;
-            bool b1;
-            ret = imap->clearAllLocations();   CHECK(ret);
-            ret = imap->clearAllAreas();       CHECK(ret);
-            ret = imap->getAreasList(loc_names);   CHECK(ret);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-            ret = imap->getLocationsList(loc_names);   CHECK(ret);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-
-            ret = imap->storeLocation("loc1", Map2DLocation("map1", 1, 2, 3));  CHECK(ret);
-            ret = imap->storeLocation("loc2", Map2DLocation("map2", 4, 5, 6));  CHECK(ret);
-
-            std::vector<Map2DLocation> vec1, vec2, vec3;
-            Map2DLocation v;
-            v.x = 1.1; v.y = 1.1; vec1.push_back(v);
-            v.x = 1.2; v.y = 1.2; vec1.push_back(v);
-            v.x = 1.3; v.y = 1.3; vec1.push_back(v);
-            ret = imap->storeArea("area1", Map2DArea("map1", vec1));  CHECK(ret);
-            v.x = 2.1; v.y = 2.1; vec2 = vec1;  vec2.push_back(v);
-            ret = imap->storeArea("area2", Map2DArea("map2", vec2));  CHECK(ret);
-            v.x = 3.1; v.y = 3.1; vec3 = vec2;  vec3.push_back(v);
-            ret = imap->storeArea("area3", Map2DArea("map3", vec3));  CHECK(ret);
-
-            ret = imap->getAreasList(loc_names);   CHECK(ret);
-            b1 = (loc_names.size() == 3);
-            CHECK(b1);
-            ret = imap->getLocationsList(loc_names);  CHECK(ret);
-            b1 = (loc_names.size() == 2);
-            CHECK(b1);
-
-            ret = imap->getArea("area1", area);   CHECK(ret);
-            b1 = (area == Map2DArea("map1", vec1));
-            CHECK(b1);
-            ret = imap->getArea("area_err", area);   CHECK(ret == false);
-
-            ret = imap->getLocation("loc1", loc);  CHECK(ret);
-            b1 = (loc == Map2DLocation("map1", 1, 2, 3));
-            CHECK(b1);
-            ret = imap->getLocation("loc_err", loc);   CHECK(ret == false);
-
-            ret = imap->deleteArea("area1");       CHECK(ret);
-            ret = imap->deleteLocation("loc1");  CHECK(ret);
-            ret = imap->deleteArea("area_err");       CHECK(ret == false);
-            ret = imap->deleteLocation("loc_err");  CHECK(ret == false);
-            ret = imap->getArea("area1", area);    CHECK(ret == false);
-            ret = imap->getLocation("loc1", loc);  CHECK(ret == false);
-
-            ret = imap->getArea("area2", area);  CHECK(ret);
-            b1 = (area == Map2DArea("map2", vec2));
-            CHECK(b1);
-            ret = imap->getLocation("loc2", loc);   CHECK(ret);
-            b1 = (loc == Map2DLocation("map2", 4, 5, 6));
-            CHECK(b1);
-
-            ret = imap->clearAllLocations();  CHECK(ret);
-            ret = imap->clearAllAreas();  CHECK(ret);
-            ret = imap->getAreasList(loc_names);  CHECK(ret);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-            imap->getLocationsList(loc_names);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-
-            ret = imap->storeArea("area", Map2DArea("map1", vec1));  CHECK(ret);
-            ret = imap->storeLocation("loc", Map2DLocation("map2", 4, 5, 6));  CHECK(ret);
-            ret = imap->renameArea("area_fail", "area_new");  CHECK(ret == false);
-            ret = imap->renameLocation("loc_fail", "loc_new");  CHECK(ret == false);
-            ret = imap->renameArea("area", "area_new");  CHECK(ret);
-            ret = imap->renameLocation("loc", "loc_new");  CHECK(ret);
-            ret = imap->getArea("area", area);         CHECK(ret==false);
-            ret = imap->getArea("area_new", area);      CHECK(ret);
-            ret = imap->getLocation("loc", loc);       CHECK(ret==false);
-            ret = imap->getLocation("loc_new", loc);    CHECK(ret);
-
-            //final cleanup, already tested
-            ret = imap->clearAllLocations();  CHECK(ret);
-            ret = imap->clearAllAreas();  CHECK(ret);
-        }
-
-        //////////"Checking IMap2D methods which involve usage of classes Map2DPath/Map2DLocation"
-        {
-            std::vector <Map2DPath>      paths;
-            std::vector <Map2DLocation>  locs;
-            std::vector <std::string>    loc_names;
-            std::vector <std::string>    path_names;
-            Map2DLocation loc;
-            Map2DPath     path;
-            bool ret;
-            bool b1;
-            ret = imap->clearAllLocations();   CHECK(ret);
-            ret = imap->clearAllPaths();       CHECK(ret);
-            ret = imap->getPathsList(path_names);   CHECK(ret);
-            b1 = (path_names.size() == 0);
-            CHECK(b1);
-            ret = imap->getLocationsList(loc_names);   CHECK(ret);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-
-            ret = imap->storeLocation("loc1", Map2DLocation("map1", 1, 2, 3));  CHECK(ret);
-            ret = imap->storeLocation("loc2", Map2DLocation("map2", 4, 5, 6));  CHECK(ret);
-
-            std::vector<Map2DLocation> vec1, vec2, vec3;
-            Map2DLocation v;
-            v.x = 1.1; v.y = 1.1; vec1.push_back(v);
-            v.x = 1.2; v.y = 1.2; vec1.push_back(v);
-            v.x = 1.3; v.y = 1.3; vec1.push_back(v);
-            ret = imap->storePath("path1", Map2DPath(vec1));  CHECK(ret);
-            v.x = 2.1; v.y = 2.1; vec2 = vec1;  vec2.push_back(v);
-            ret = imap->storePath("path2", Map2DPath(vec2));  CHECK(ret);
-            v.x = 3.1; v.y = 3.1; vec3 = vec2;  vec3.push_back(v);
-            ret = imap->storePath("path3", Map2DPath(vec3));  CHECK(ret);
-
-            ret = imap->getPathsList(path_names);   CHECK(ret);
-            b1 = (path_names.size() == 3);
-            CHECK(b1);
-            ret = imap->getLocationsList(loc_names);  CHECK(ret);
-            b1 = (loc_names.size() == 2);
-            CHECK(b1);
-
-            ret = imap->getPath("path1", path);   CHECK(ret);
-            b1 = (path == Map2DPath(vec1));
-            CHECK(b1);
-            ret = imap->getPath("path_err", path);   CHECK(ret == false);
-
-            ret = imap->getLocation("loc1", loc);  CHECK(ret);
-            b1 = (loc == Map2DLocation("map1", 1, 2, 3));
-            CHECK(b1);
-            ret = imap->getLocation("loc_err", loc);   CHECK(ret == false);
-
-            ret = imap->deletePath("path1");       CHECK(ret);
-            ret = imap->deleteLocation("loc1");  CHECK(ret);
-            ret = imap->deletePath("path_err");       CHECK(ret == false);
-            ret = imap->deleteLocation("loc_err");  CHECK(ret == false);
-            ret = imap->getPath("path1", path);    CHECK(ret == false);
-            ret = imap->getLocation("loc1", loc);  CHECK(ret == false);
-
-            ret = imap->getPath("path2", path);  CHECK(ret);
-            b1 = (path == Map2DPath(vec2));
-            CHECK(b1);
-            ret = imap->getLocation("loc2", loc);   CHECK(ret);
-            b1 = (loc == Map2DLocation("map2", 4, 5, 6));
-            CHECK(b1);
-
-            ret = imap->clearAllLocations();  CHECK(ret);
-            ret = imap->clearAllPaths();  CHECK(ret);
-            ret = imap->getPathsList(path_names);  CHECK(ret);
-            b1 = (path_names.size() == 0);
-            CHECK(b1);
-            imap->getLocationsList(loc_names);
-            b1 = (loc_names.size() == 0);
-            CHECK(b1);
-
-            ret = imap->storePath("path", Map2DPath(vec1));  CHECK(ret);
-            ret = imap->storeLocation("loc", Map2DLocation("map2", 4, 5, 6));  CHECK(ret);
-            ret = imap->renamePath("path_fail", "path_new");  CHECK(ret == false);
-            ret = imap->renameLocation("loc_fail", "loc_new");  CHECK(ret == false);
-            ret = imap->renamePath("path", "path_new");  CHECK(ret);
-            ret = imap->renameLocation("loc", "loc_new");  CHECK(ret);
-            ret = imap->getPath("path", path);         CHECK(ret == false);
-            ret = imap->getPath("path_new", path);      CHECK(ret);
-            ret = imap->getLocation("loc", loc);       CHECK(ret == false);
-            ret = imap->getLocation("loc_new", loc);    CHECK(ret);
-
-            path.toString();
-
-            //final cleanup, already tested
-            ret = imap->clearAllLocations();  CHECK(ret);
-            ret = imap->clearAllPaths();  CHECK(ret);
-        }
-
-        //////////"Checking IMap2D methods which involve usage of classes MapGrid2D"
-        {
-            Nav2D::MapGrid2D test_store_map1;
-            Nav2D::MapGrid2D test_store_map2;
-            Nav2D::MapGrid2D test_get_map;
-
-            test_store_map1.setMapName("test_map1");
-            test_store_map2.setMapName("test_map2");
-            std::vector<std::string> map_names;
-
-            imap->store_map(test_store_map1);
-            imap->store_map(test_store_map2);
-            imap->get_map("test_map1", test_get_map);
-            CHECK(test_store_map1.isIdenticalTo(test_get_map)); // IMap2D store/get operation successful
-
-            imap->get_map_names(map_names);
-            bool b1 = (map_names.size() == 2);
-            bool b2 = false;
-            if (b1) {
-                b2 = (map_names[0] == "test_map1" && map_names[1] == "test_map2");
-            }
-            CHECK(b1);
-            CHECK(b2);
-            // IMap2D get_map_names operation successful")
-
-            imap->remove_map("test_map1");
-            imap->get_map_names(map_names);
-            b1 = (map_names.size() == 1);
-            CHECK(b1); // IMap2D remove_map operation successful
-
-            imap->clearAllMaps();
-            imap->get_map_names(map_names);
-            b1 = (map_names.size() == 0);
-            CHECK(b1); // IMap2D clear operation successful
-        }
-
-        /////////"Checking map client/server polydrivers closure"
-        {
-            CHECK(ddmapclient.close());
-            CHECK(ddmapserver.close());
-            // map2DServer and map2DClient polydrivers closed successfully
-        }
     }
 
     Network::setLocalMode(false);
