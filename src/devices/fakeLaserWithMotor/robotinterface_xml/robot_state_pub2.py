8346ef5099987216fd253ceb40ec62d6d6f3da8f
Marco Randazzo <marco.randazzo@iit.it>
2022-08-01 14:19:36 +0200

    ros2 test added

---
+++ src/devices/fakeLaserWithMotor/robotinterface_xml/robot_state_pub2.py
@@ -0,0 +1,27 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf = os.path.abspath("./robot.urdf")
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc, 'publish_frequency': 200.0}],
+            arguments=[urdf])
+    ])
