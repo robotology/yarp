12e9402a80fc755d6e7b1880711e51084ad3156e
Marco Randazzo <marco.randazzo@iit.it>
2023-01-18 15:09:59 +0100

    added function in create_device_test() function in YarpCatchUtils.cmake

--- cmake/YarpCatchUtils.cmake
+++ cmake/YarpCatchUtils.cmake
@@ -18,3 +18,27 @@ function(yarp_catch_discover_tests _target)
       SKIP_RETURN_CODE 254
     )
 endfunction()
+
+
+function(create_device_test test_name)
+    if (DEFINED ${ARGV1})
+        set (extra_libs ${ARGV1})
+    endif()
+    
+    add_executable(harness_dev_${test_name})
+    target_sources(harness_dev_${test_name}
+      PRIVATE
+        ${test_name}_test.cpp
+    )
+    target_link_libraries(harness_dev_${test_name}
+      PRIVATE
+        YARP_harness
+        YARP::YARP_os
+        YARP::YARP_sig
+        YARP::YARP_dev
+        YARP::YARP_dev_tests
+        ${extra_libs}
+    )
+    set_property(TARGET harness_dev_${test_name} PROPERTY FOLDER "Test")
+    yarp_catch_discover_tests(harness_dev_${test_name})
+endfunction()
