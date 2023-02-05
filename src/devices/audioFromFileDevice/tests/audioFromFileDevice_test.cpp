76e212301eaa2a1659d2538cea9587d70a2af647
Marco Randazzo <marco.randazzo@iit.it>
2023-02-05 11:50:43 +0100

    fixed audioFromFileDevice test

--- src/devices/audioFromFileDevice/tests/audioFromFileDevice_test.cpp
+++ src/devices/audioFromFileDevice/tests/audioFromFileDevice_test.cpp
@@ -7,6 +7,7 @@
 #include <yarp/os/Network.h>
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/WrapperSingle.h>
+#include <yarp/os/ResourceFinder.h>
 
 #include <catch2/catch_amalgamated.hpp>
 #include <harness.h>
@@ -27,17 +28,22 @@ TEST_CASE("dev::audioFromFileDevice", "[yarp::dev]")
 
         ////////"Checking opening audioFromFileDevice polydriver"
         {
+            yarp::os::ResourceFinder res;
+            res.setDefaultContext("tests\\audioFromFileDevice");
+            std::string filepath = res.findFileByName("440.wav");
+#if 0
             std::string cpp_path = __FILE__;
             std::size_t pos = cpp_path.find("audioFromFileDevice_test.cpp");
             if (pos != std::string::npos)
             {
                 cpp_path.erase(pos, 28);
             }
+#endif
             Property p_cfg;
             p_cfg.put("device", "audioFromFileDevice");
             Property& p_cfg_audio_base = p_cfg.addGroup("AUDIO_BASE");
             p_cfg_audio_base.put("channels", 1);
-            p_cfg.put("file_name", cpp_path+"440.wav");
+            p_cfg.put("file_name", filepath);
             REQUIRE(dd.open(p_cfg));
         }
 
