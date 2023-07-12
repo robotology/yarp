f7616894364c489e54fd7fc6005ab976cf2fb46d
Marco Randazzo <marco.randazzo@iit.it>
2023-07-12 15:52:14 +0200

    added streaming test for frameGrabber_nwc_yarp_test

--- src/devices/frameGrabber_nwc_yarp/tests/frameGrabber_nwc_yarp_test.cpp
+++ src/devices/frameGrabber_nwc_yarp/tests/frameGrabber_nwc_yarp_test.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-FileCopyrightText: 2006-2023 Istituto Italiano di Tecnologia (IIT)
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -25,6 +25,64 @@ using namespace yarp::os;
 using namespace yarp::dev;
 using namespace yarp::sig;
 
+void do_nws_nwc_test(bool use_stream)
+{
+    PolyDriver dd_fake;
+    PolyDriver dd_nws;
+    PolyDriver dd_nwc;
+    Property p_fake;
+    Property p_nws;
+    Property p_nwc;
+
+    p_nws.put("device", "frameGrabber_nws_yarp");
+    p_fake.put("device", "fakeFrameGrabber");
+    REQUIRE(dd_fake.open(p_fake));
+    REQUIRE(dd_nws.open(p_nws));
+    yarp::os::SystemClock::delaySystem(0.5);
+
+    {yarp::dev::WrapperSingle* ww_nws; dd_nws.view(ww_nws);
+    REQUIRE(ww_nws);
+    bool result_att = ww_nws->attach(&dd_fake);
+    REQUIRE(result_att); }
+
+    p_nwc.put("device", "frameGrabber_nwc_yarp");
+    p_nwc.put("remote", "/grabber");
+    p_nwc.put("local", "/grabber/client");
+    p_nwc.put("no_stream", !use_stream);
+    REQUIRE(dd_nwc.open(p_nwc));
+
+    IFrameGrabberImage* igrabber = nullptr;
+    IRgbVisualParams* irgbParams = nullptr;
+    REQUIRE(dd_nwc.view(igrabber));
+    REQUIRE(dd_nwc.view(irgbParams));
+
+    yarp::os::SystemClock::delaySystem(0.5);
+
+    {
+        ImageOf<PixelRgb> img;
+        igrabber->getImage(img);
+        CHECK(img.width() > 0);
+    }
+    {
+        ImageOf<PixelRgb> crop;
+        yarp::sig::VectorOf<std::pair< int, int>> vertices;
+        vertices.resize(2);
+        vertices[0] = std::pair <int, int>(0, 0);
+        vertices[1] = std::pair <int, int>(10, 10); // Configure a doable crop.
+        CHECK(igrabber->getImageCrop(YARP_CROP_RECT, vertices, crop));
+        CHECK(crop.width() > 0);
+        CHECK(crop.height() > 0);
+    }
+
+    yarp::dev::tests::exec_IFrameGrabberImage_test_1(igrabber);
+    yarp::dev::tests::exec_IRgbVisualParams_test_1(irgbParams);
+
+    yarp::os::SystemClock::delaySystem(0.5);
+
+    CHECK(dd_nwc.close());
+    CHECK(dd_nws.close());
+    CHECK(dd_fake.close());
+}
 
 TEST_CASE("dev::frameGrabber_nwc_yarpTest", "[yarp::dev]")
 {
@@ -36,61 +94,11 @@ TEST_CASE("dev::frameGrabber_nwc_yarpTest", "[yarp::dev]")
 
     SECTION("Test the frameGrabber_nwc_yarp device with a frameGrabber_nws_yarp device")
     {
-        PolyDriver dd_fake;
-        PolyDriver dd_nws;
-        PolyDriver dd_nwc;
-        Property p_fake;
-        Property p_nws;
-        Property p_nwc;
+        //do the test using streaming data between nws and nwc
+        do_nws_nwc_test(true);
 
-        p_nws.put("device","frameGrabber_nws_yarp");
-        p_fake.put("device","fakeFrameGrabber");
-        REQUIRE(dd_fake.open(p_fake));
-        REQUIRE(dd_nws.open(p_nws));
-        yarp::os::SystemClock::delaySystem(0.5);
-
-        {yarp::dev::WrapperSingle* ww_nws; dd_nws.view(ww_nws);
-        REQUIRE(ww_nws);
-        bool result_att = ww_nws->attach(&dd_fake);
-        REQUIRE(result_att); }
-
-        p_nwc.put("device", "frameGrabber_nwc_yarp");
-        p_nwc.put("remote", "/grabber");
-        p_nwc.put("local", "/grabber/client");
-        p_nwc.put("no_stream", 1);
-        REQUIRE(dd_nwc.open(p_nwc));
-
-        IFrameGrabberImage* igrabber = nullptr;
-        IRgbVisualParams* irgbParams = nullptr;
-        REQUIRE(dd_nwc.view(igrabber));
-        REQUIRE(dd_nwc.view(irgbParams));
-
-        yarp::os::SystemClock::delaySystem(0.5);
-
-        {
-            ImageOf<PixelRgb> img;
-            igrabber->getImage(img);
-            CHECK(img.width() > 0);
-        }
-        {
-            ImageOf<PixelRgb> crop;
-            yarp::sig::VectorOf<std::pair< int, int>> vertices;
-            vertices.resize(2);
-            vertices[0] = std::pair <int, int>(0, 0);
-            vertices[1] = std::pair <int, int>(10, 10); // Configure a doable crop.
-            CHECK(igrabber->getImageCrop(YARP_CROP_RECT, vertices, crop));
-            CHECK(crop.width() > 0);
-            CHECK(crop.height() > 0);
-        }
-
-        yarp::dev::tests::exec_IFrameGrabberImage_test_1(igrabber);
-        yarp::dev::tests::exec_IRgbVisualParams_test_1(irgbParams);
-
-        yarp::os::SystemClock::delaySystem(0.5);
-
-        CHECK(dd_nwc.close());
-        CHECK(dd_nws.close());
-        CHECK(dd_fake.close());
+        //do the test using RPC calls only
+        do_nws_nwc_test(false);
     }
 
     Network::setLocalMode(false);
