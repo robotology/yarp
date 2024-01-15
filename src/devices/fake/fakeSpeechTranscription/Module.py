db8a37ee40633201b34b1bfbf74abe5c80478a47
SimoneMic <simone_micheletti@outlook.it>
2024-01-15 10:05:01 +0000

    first python embedding update

---
+++ src/devices/fake/fakeSpeechTranscription/Module.py
@@ -0,0 +1,15 @@
+def open():
+    print("Opening python module")
+    return True
+
+def close():
+    print("Closing python module")
+    return True
+
+def get_language(lang):
+    print("getting: " + lang)
+    return lang
+
+def set_language(lang):
+    print("setting: " + lang)
+    return True
--- src/devices/fake/fakeSpeechTranscription/fakeSpeechTranscription.cpp
+++ src/devices/fake/fakeSpeechTranscription/fakeSpeechTranscription.cpp
@@ -12,6 +12,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include "Python.h"
+
 using namespace yarp::os;
 using namespace yarp::dev;
 
@@ -30,6 +32,68 @@ FakeSpeechTranscription::~FakeSpeechTranscription()
 
 bool FakeSpeechTranscription::open(yarp::os::Searchable& config)
 {
+    //Simple Function Calling without passing any parameters
+
+    PyObject *pName,    //Interpreted name of the module
+             *pModule,  //Imported py Module loaded in this object
+             *pDict,    //Interpreted name of the module
+             *pFunc,    //Interpreted name of the module
+             *pValue;   //Interpreted name of the module
+
+    // Initialize the Python Interpreter
+    Py_Initialize();
+
+    // Build the name object (of the module)
+    pName = PyUnicode_DecodeFSDefault("Module");    //The string "Module" should be the name of the python file: i.e. Module.py
+
+    // Load the module object
+    pModule = PyImport_Import(pName);
+    // Destroy the pName object: not needed anymore
+    Py_DECREF(pName);
+
+    if (pModule != NULL) // Check if the Module has been found and loaded
+    {
+        // Get the function inside the module
+        pFunc = PyObject_GetAttrString(pModule, "open");
+
+        if (pFunc && PyCallable_Check(pFunc))   // Check if the function has been found and is callable
+        {
+            // Call the function
+            pValue = PyObject_CallObject(pFunc, NULL);
+            // Check the return value
+            if (pValue != NULL) 
+            {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue)); // 0 or 1
+                Py_DECREF(pValue);
+            }
+            else 
+            {   // Call Failed
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return false;
+            }
+        }
+        else 
+        {   // Function not found in the py module
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", "open");
+            Py_XDECREF(pFunc);
+            Py_DECREF(pModule);
+            return false;
+        }
+        // Clear memory
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else 
+    {   // Module not found or loaded
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", "Module");
+        return false;
+    }
     return true;
 }
 
