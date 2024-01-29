d6119145c832257f72f334ab05a8d2344fa5e069
Marco Randazzo <marco.randazzo@iit.it>
2024-01-29 11:48:06 +0100

    AudioToFileDevice example updated

--- src/devices/audioToFileDevice/AudioToFileDevice.h
+++ src/devices/audioToFileDevice/AudioToFileDevice.h
@@ -8,7 +8,7 @@
 #include <yarp/sig/Sound.h>
 #include <yarp/sig/SoundFile.h>
 #include <yarp/dev/AudioPlayerDeviceBase.h>
-#include "audioToFileDeviceParams.h"
+#include "AudioToFileDevice_ParamsParser.h"
 
 #include <string>
 #include <mutex>
@@ -20,26 +20,40 @@
 * \brief `audioToFileDevice` : This device driver, wrapped by default by AudioPlayerWrapper,
 * is used to save to an audio stream to a file on disk.
 *
-* See audioToFileDeviceParams for the documentation of the accepted parameters.
-*
 * This device driver derives from yarp::dev::AudioPlayerDeviceBase base class.
 * Please check its documentation for additional configuration parameters.
 *
+* Parameters required by this device are shown in class: AudioToFileDevice_ParamsParser and AudioRecorderDeviceBase
+*
+* Three different operating modes are available, defined by the optional string parameter `save_mode`:
+* if save_mode == "append_data", the file is written only when the module terminates.
+* Every start/stop operation just pauses the module. On resume, the new data is concatenated at the end of the file.
+*
+* if save_mode == "overwrite_file", the output file is written every time the stop() method is called or when the module terminates.
+* If the file already exists, it will be overwritten with the new data.
+*
+* if save_mode = "rename_file", the output file is written to a NEW file every time the stop() method is called or when the module terminates.
+* The file name is modified, using an incremental counter appended at the end of the file name.
+*
+* if save_mode = "break_file", the output file is written to a NEW file every time a yarp::sig::sound is received or when the module terminates.
+* The file name is modified, using an incremental counter appended at the end of the file name.
+
 * See \ref AudioDoc for additional documentation on YARP audio.
+* 
 */
 
-class audioToFileDevice :
+class AudioToFileDevice :
         public yarp::dev::DeviceDriver,
         public yarp::dev::AudioPlayerDeviceBase,
-        public audioToFileDevice_params
+        public AudioToFileDevice_ParamsParser
 {
 public:
-    audioToFileDevice();
-    audioToFileDevice(const audioToFileDevice&) = delete;
-    audioToFileDevice(audioToFileDevice&&) = delete;
-    audioToFileDevice& operator=(const audioToFileDevice&) = delete;
-    audioToFileDevice& operator=(audioToFileDevice&&) = delete;
-    ~audioToFileDevice() override;
+    AudioToFileDevice();
+    AudioToFileDevice(const AudioToFileDevice&) = delete;
+    AudioToFileDevice(AudioToFileDevice&&) = delete;
+    AudioToFileDevice& operator=(const AudioToFileDevice&) = delete;
+    AudioToFileDevice& operator=(AudioToFileDevice&&) = delete;
+    ~AudioToFileDevice() override;
 
     // Device Driver interface
     bool open(yarp::os::Searchable &config) override;
@@ -65,7 +79,7 @@ private:
         save_append_data =1,
         save_rename_file =2,
         save_break_file =3
-    } m_save_mode = save_overwrite_file;
+    } m_save_mode_enum = save_overwrite_file;
 
     void save_to_file();
 };
