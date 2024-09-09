d12d6833399c833feec02fa3a01cdab180d38c93
Marco Randazzo <marco.randazzo@iit.it>
2024-09-09 13:07:46 +0200

    Removed some IplImage stuff, mainly C functions for image processing

--- src/libYARP_sig/src/yarp/sig/impl/IplImage.h
+++ src/libYARP_sig/src/yarp/sig/impl/IplImage.h
@@ -123,39 +123,9 @@ typedef struct _IplROI {
 }
 IplROI;
 
-typedef struct _IplConvKernel
-{
-    int  nCols;
-    int  nRows;
-    int  anchorX;
-    int  anchorY;
-    int *values;
-    int  nShiftR;
-}
-IplConvKernel;
-
-typedef struct _IplConvKernelFP
-{
-    int  nCols;
-    int  nRows;
-    int  anchorX;
-    int  anchorY;
-    float *values;
-}
-IplConvKernelFP;
-
 #define IPL_IMAGE_HEADER 1
 #define IPL_IMAGE_DATA   2
 #define IPL_IMAGE_ROI    4
-
-#ifndef IPL_IMAGE_MAGIC_VAL
-#define IPL_IMAGE_MAGIC_VAL  ((int)sizeof(IplImage))
-#endif
-
-/* for file storages make the value independent from arch */
-#ifndef IPL_IMAGE_FILE_MAGIC_VAL
-#define IPL_IMAGE_FILE_MAGIC_VAL  112
-#endif
 
 #ifndef IPL_DEPTH_64F
 #define IPL_DEPTH_64F  64 /* for storing double-precision
@@ -163,64 +133,10 @@ IplConvKernelFP;
 #endif
 
 
-// to fool a few Windows declarations.
-//typedef int HDC;
-//typedef char _TCHAR;
-
-/**
- * Computes the ipl image padding.
- * @param lineSize the total length in bytes of the row of the image.
- * @param align is the alignment bytes (e.g. typically 8).
- * @return the number of bytes to add to the line to round with modulo 8.
- */
-int _iplCalcPadding (int lineSize, int align);
-
 /**
  * Definition for functions implemented within YARP_sig.
  */
 #define IPLAPIIMPL(type,name,arg) extern type name arg
-
-
-IPLAPIIMPL(IplConvKernel*,
-           iplCreateConvKernel,(int nCols, int nRows,
-                                int anchorX, int anchorY,
-                                int* values, int nShiftR));
-
-IPLAPIIMPL(IplConvKernelFP*,
-           iplCreateConvKernelFP,(int nCols, int nRows,
-                                  int anchorX, int anchorY, float* values));
-
-IPLAPIIMPL(void,iplGetConvKernel,
-           (IplConvKernel* kernel, int* nCols, int* nRows,
-            int* anchorX, int* anchorY, int** values, int *nShiftR));
-
-
-IPLAPIIMPL(void,iplGetConvKernelFP,
-           (IplConvKernelFP* kernel,int* nCols, int* nRows,
-            int* anchorX, int* anchorY, float** values));
-
-IPLAPIIMPL(void, iplDeleteConvKernel,(IplConvKernel* kernel));
-
-IPLAPIIMPL(void, iplDeleteConvKernelFP,(IplConvKernelFP* kernel));
-
-IPLAPIIMPL(void, iplConvolve2D,
-           (IplImage* srcImage, IplImage* dstImage,
-            IplConvKernel** kernel, int nKernels, int combineMethod));
-
-IPLAPIIMPL(void, iplConvolve2DFP,
-           (IplImage* srcImage, IplImage* dstImage,
-            IplConvKernelFP** kernel, int nKernels, int combineMethod));
-
-IPLAPIIMPL(void, iplConvolveSep2DFP,(IplImage* srcImage,
-                                     IplImage* dstImage,
-                                     IplConvKernelFP* xKernel,
-                                     IplConvKernelFP* yKernel));
-
-//IPLAPIIMPL(IPLStatus, iplFixedFilter,(IplImage* srcImage, IplImage* dstImage,
-//           IplFilter filter));
-
-IPLAPIIMPL(void, iplConvolveSep2D,(IplImage* srcImage, IplImage* dstImage,
-                                   IplConvKernel* xKernel, IplConvKernel* yKernel));
 
 IPLAPIIMPL(void, iplAllocateImage,(IplImage* image, int doFill, int fillValue));
 
@@ -236,50 +152,11 @@ IPLAPIIMPL(IplImage*, iplCreateImageHeader,
             int   width,      int   height, IplROI* roi, IplImage* maskROI,
             void* imageId,    IplTileInfo* tileInfo));
 
-IPLAPIIMPL(IplImage*, iplCloneImage, ( const IplImage* img ) );
-
-IPLAPIIMPL(void, iplCopy, (IplImage* srcImage, IplImage* dstImage));
-
 IPLAPIIMPL(void, iplDeallocateHeader,(IplImage* image));
 
 IPLAPIIMPL(void, iplDeallocate,(IplImage* image, int flag));
 
 IPLAPIIMPL(void,iplSetBorderMode,(IplImage *src,int mode,int border,int constVal));
-
-IPLAPIIMPL(void, iplSet, (IplImage* image, int fillValue));
-
-IPLAPIIMPL(void, iplSetFP, (IplImage* image, float fillValue));
-
-IPLAPIIMPL(void, iplAddS,(IplImage* srcImage, IplImage* dstImage, int value));
-
-IPLAPIIMPL(void, iplAdd,(IplImage* srcImageA, IplImage* srcImageB,
-                         IplImage* dstImage));
-
-IPLAPIIMPL(void, iplSubtract,(IplImage* srcImageA, IplImage* srcImageB,
-                              IplImage* dstImage));
-
-IPLAPIIMPL(void, iplSubtractS,(IplImage* srcImage, IplImage* dstImage, int value,
-                               bool flip));
-
-IPLAPIIMPL(void, iplMultiplySFP,(IplImage* srcImage, IplImage* dstImage,
-                                 float value));
-
-IPLAPIIMPL(void, iplAbs,(IplImage* srcImage, IplImage* dstImage));
-
-IPLAPIIMPL(void, iplThreshold, (IplImage* srcImage, IplImage* dstImage, int threshold));
-
-IPLAPIIMPL(void, iplColorToGray,(IplImage* srcImage, IplImage* dstImage));
-
-IPLAPIIMPL(IplROI *,iplCreateROI,(int coi,    int xOffset, int   yOffset,
-                                  int width, int height ));
-
-
-IPLAPIIMPL(void, iplRGB2HSV,(IplImage* rgbImage, IplImage* hsvImage));
-
-IPLAPIIMPL(void, iplHSV2RGB,(IplImage* hsvImage, IplImage* rgbImage));
-
-
-IPLAPIIMPL(void, iplXorS,(IplImage* srcImage, IplImage* dstImage, unsigned int value));
 
 #define IPL_BORDER_CONSTANT 0
 
