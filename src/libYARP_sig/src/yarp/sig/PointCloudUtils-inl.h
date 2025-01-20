96bce2e5c89ebf6887c4e575f78a1c53009d587e
Marco Randazzo <marco.randazzo@iit.it>
2025-01-20 10:42:50 +0100

    Added new functionalities to pointCloud library

--- src/libYARP_sig/src/yarp/sig/PointCloudUtils-inl.h
+++ src/libYARP_sig/src/yarp/sig/PointCloudUtils-inl.h
@@ -8,15 +8,13 @@
 
 #include <type_traits>
 
-
 namespace {
 
 template<typename T1,
          typename T2,
          std::enable_if_t<std::is_same<T1, yarp::sig::DataXYZRGBA>::value &&
                           (std::is_same<T2, yarp::sig::PixelRgb>::value ||
-                           std::is_same<T2, yarp::sig::PixelBgr>::value), int> = 0
->
+                           std::is_same<T2, yarp::sig::PixelBgr>::value), int> = 0>
 inline void copyColorData(yarp::sig::PointCloud<T1>& pointCloud,
                           const yarp::sig::ImageOf<T2>& color,
                           const size_t u,
@@ -31,8 +29,7 @@ template<typename T1,
          typename T2,
          std::enable_if_t<std::is_same<T1, yarp::sig::DataXYZRGBA>::value &&
                           (std::is_same<T2, yarp::sig::PixelRgba>::value ||
-                           std::is_same<T2, yarp::sig::PixelBgra>::value), int> = 0
->
+                           std::is_same<T2, yarp::sig::PixelBgra>::value), int> = 0>
 inline void copyColorData(yarp::sig::PointCloud<T1>& pointCloud,
                           const yarp::sig::ImageOf<T2>& color,
                           const size_t u,
@@ -65,7 +62,10 @@ template<typename T1, typename T2>
 yarp::sig::PointCloud<T1> yarp::sig::utils::depthRgbToPC(const yarp::sig::ImageOf<yarp::sig::PixelFloat>& depth,
                                                          const yarp::sig::ImageOf<T2>& color,
                                                          const yarp::sig::IntrinsicParams& intrinsic,
-                                                         const yarp::sig::utils::OrganizationType organizationType)
+                                                         const yarp::sig::utils::OrganizationType organizationType,
+                                                         size_t step_x,
+                                                         size_t step_y,
+                                                         const std::string& output_order)
 {
     yAssert(depth.width()  != 0);
     yAssert(depth.height() != 0);
@@ -74,28 +74,48 @@ yarp::sig::PointCloud<T1> yarp::sig::utils::depthRgbToPC(const yarp::sig::ImageO
     size_t w = depth.width();
     size_t h = depth.height();
     yarp::sig::PointCloud<T1> pointCloud;
-    if (organizationType == yarp::sig::utils::OrganizationType::Organized){
-        pointCloud.resize(w, h);
+    if (organizationType == yarp::sig::utils::OrganizationType::Organized)
+    {
+        pointCloud.resize(w/step_x, h/step_y);
     }
-    for (size_t u = 0; u < w; ++u) {
-        for (size_t v = 0; v < h; ++v) {
-            if (organizationType == yarp::sig::utils::OrganizationType::Organized){
+
+    const char* mapping = output_order.c_str();
+    double values_xyz[3];
+    char axes[3] = {mapping[1], mapping[3], mapping[5]};
+    for (size_t u = 0, cu = 0; u < w; u+=step_x, cu++)
+    {
+        for (size_t v = 0, cv = 0; v < h; v+=step_y, cv++)
+        {
+            double dp = depth.pixel(u, v);
+            if (organizationType == yarp::sig::utils::OrganizationType::Organized)
+            {
                 // Depth
                 // De-projection equation (pinhole model):
                 //                          x = (u - ppx)/ fx * z
                 //                          y = (v - ppy)/ fy * z
                 //                          z = z
-                pointCloud(u,v).x = (u - intrinsic.principalPointX)/intrinsic.focalLengthX*depth.pixel(u,v);
-                pointCloud(u,v).y = (v - intrinsic.principalPointY)/intrinsic.focalLengthY*depth.pixel(u,v);
-                pointCloud(u,v).z = depth.pixel(u,v);
-                copyColorData(pointCloud, color, u, v);
-
-            } else if (organizationType == yarp::sig::utils::OrganizationType::Unorganized) {
-                if (depth.pixel(u,v) > 0){
+                values_xyz[0] = (u - intrinsic.principalPointX)/intrinsic.focalLengthX*depth.pixel(u,v);
+                values_xyz[1] = (v - intrinsic.principalPointY)/intrinsic.focalLengthY*depth.pixel(u,v);
+                values_xyz[2] = dp;
+                pointCloud(cu, cv).x = (mapping[0] == '-') ? (-values_xyz[axes[0] - 'X']) : (values_xyz[axes[0] - 'X']);
+                pointCloud(cu, cv).y = (mapping[2] == '-') ? (-values_xyz[axes[1] - 'X']) : (values_xyz[axes[1] - 'X']);
+                pointCloud(cu, cv).z = (mapping[4] == '-') ? (-values_xyz[axes[2] - 'X']) : (values_xyz[axes[2] - 'X']);
+                //copyColorData(pointCloud, color, u, v);
+                pointCloud(cu,cv).r = color.pixel(u,v).r;
+                pointCloud(cu,cv).g = color.pixel(u,v).g;
+                pointCloud(cu,cv).b = color.pixel(u,v).b;
+            }
+            else if (organizationType == yarp::sig::utils::OrganizationType::Unorganized)
+            {
+                //if ( dp > 0) //why?
+                {
                     T1 point;
-                    point.x = (u - intrinsic.principalPointX)/intrinsic.focalLengthX*depth.pixel(u,v);
-                    point.y = (v - intrinsic.principalPointY)/intrinsic.focalLengthY*depth.pixel(u,v);
-                    point.z = depth.pixel(u,v);
+                    values_xyz[0] = (u - intrinsic.principalPointX)/intrinsic.focalLengthX*depth.pixel(u,v);
+                    values_xyz[1] = (v - intrinsic.principalPointY)/intrinsic.focalLengthY*depth.pixel(u,v);
+                    values_xyz[2] = dp;
+                    point.x = (mapping[0] == '-') ? (-values_xyz[axes[0] - 'X']) : (values_xyz[axes[0] - 'X']);
+                    point.y = (mapping[2] == '-') ? (-values_xyz[axes[1] - 'X']) : (values_xyz[axes[1] - 'X']);
+                    point.z = (mapping[4] == '-') ? (-values_xyz[axes[2] - 'X']) : (values_xyz[axes[2] - 'X']);
                     point.r = color.pixel(u,v).r;
                     point.g = color.pixel(u,v).g;
                     point.b = color.pixel(u,v).b;
--- src/libYARP_sig/src/yarp/sig/PointCloudUtils.h
+++ src/libYARP_sig/src/yarp/sig/PointCloudUtils.h
@@ -31,7 +31,7 @@ struct PCL_ROI
  * @param[in] intrinsic, intrinsic parameter of the camera.
  * @note the intrinsic parameters are the one of the depth sensor if the depth frame IS NOT aligned with the
  * colored one. On the other hand use the intrinsic parameters of the RGB camera if the frames are aligned.
- * @return the pointcloud obtained by the de-projection.
+ * @return the point cloud obtained by the de-projection.
  */
 YARP_sig_API yarp::sig::PointCloud<yarp::sig::DataXYZ> depthToPC(const yarp::sig::ImageOf<yarp::sig::PixelFloat>& depth,
                                                                  const yarp::sig::IntrinsicParams& intrinsic);
@@ -42,16 +42,19 @@ YARP_sig_API yarp::sig::PointCloud<yarp::sig::DataXYZ> depthToPC(const yarp::sig
  * @param[in] intrinsic, intrinsic parameter of the camera.
  * @param[in] roi, the Region Of Interest intrinsic of the depth image that we want to convert.
  * @param[in] step_x, the depth image size can be decimated, by selecting a column every step_x;
- * @param[in] step_t, the depth image size can be decimated, by selecting a row every step_y;
+ * @param[in] step_y, the depth image size can be decimated, by selecting a row every step_y;
+ * @param[in] output_order, the string will optionally rearrange data in any combination of positive and negative axes. e.g. "+Z-Y+X".
+ * Useful when dealing with not right-handed coordinate systems. Optimized for efficiency during point cloud creation with no additional computation.
  * @note the intrinsic parameters are the one of the depth sensor if the depth frame IS NOT aligned with the
  * colored one. On the other hand use the intrinsic parameters of the RGB camera if the frames are aligned.
- * @return the pointcloud obtained by the de-projection.
+ * @return the point cloud obtained by the de-projection.
  */
 YARP_sig_API yarp::sig::PointCloud<yarp::sig::DataXYZ> depthToPC(const yarp::sig::ImageOf<yarp::sig::PixelFloat>& depth,
                                                                  const yarp::sig::IntrinsicParams& intrinsic,
                                                                  const yarp::sig::utils::PCL_ROI& roi,
-                                                                 size_t step_x,
-                                                                 size_t step_y);
+                                                                 size_t step_x=1,
+                                                                 size_t step_y=1,
+                                                                 const std::string& output_order = "+X+Y+Z");
 
 /**
  * @brief depthRgbToPC, compute the colored PointCloud given depth image, color image and the intrinsic
@@ -59,15 +62,24 @@ YARP_sig_API yarp::sig::PointCloud<yarp::sig::DataXYZ> depthToPC(const yarp::sig
  * @param[in] depth, the input depth image.
  * @param[in] color, the input color image.
  * @param[in] intrinsic, intrinsic parameter of the camera.
- * @note the intrinsic parameters are the one of the depth sensor if the depth frame IS NOT aligned with the
+ * @param[in] organizationType, if organized the point cloud has size (width, depth). if unorganized the point cloud
+ * has size (width*height, 1). Note that in this case the data are organized column-wise (i.e. y,x and not x,y)
+ * @param[in] step_x, the depth image size can be decimated, by selecting a column every step_x;
+ * @param[in] step_y, the depth image size can be decimated, by selecting a row every step_y;
+ * @param[in] output_order, the string will optionally rearrange data in any combination of positive and negative axes. e.g. "+Z-Y+X".
+ * Useful when dealing with not right-handed coordinate systems. Optimized for efficiency during point cloud creation with no additional computation.
+  * @note the intrinsic parameters are the one of the depth sensor if the depth frame IS NOT aligned with the
  * colored one. On the other hand use the intrinsic parameters of the RGB camera if the frames are aligned.
- * @return the pointcloud obtained by the de-projection.
+ * @return the point cloud obtained by the de-projection.
  */
 template<typename T1, typename T2>
 yarp::sig::PointCloud<T1> depthRgbToPC(const yarp::sig::ImageOf<yarp::sig::PixelFloat>& depth,
                                        const yarp::sig::ImageOf<T2>& color,
                                        const yarp::sig::IntrinsicParams& intrinsic,
-                                       const yarp::sig::utils::OrganizationType organizationType = yarp::sig::utils::OrganizationType::Organized);
+                                       const yarp::sig::utils::OrganizationType organizationType = yarp::sig::utils::OrganizationType::Organized,
+                                       size_t step_x=1,
+                                       size_t step_y=1,
+                                       const std::string& output_order = "+X+Y+Z");
 } // namespace yarp::sig::utils
 
 #include <yarp/sig/PointCloudUtils-inl.h>
