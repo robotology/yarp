3b9e5f2cefbf7a7509db86343ed65bc87c248919
Nicogene <nicolo.genesio@iit.it>
2025-08-27 12:03:11 +0200

    IJacobianCoupling: fix linker error under windows
    It needs the destructor definition in the .cpp. It fixes #3278

---
+++ src/libYARP_dev/src/yarp/dev/IJacobianCoupling.cpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2006-2023 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <yarp/dev/IJacobianCoupling.h>
+
+yarp::dev::IJacobianCoupling::~IJacobianCoupling() = default;
