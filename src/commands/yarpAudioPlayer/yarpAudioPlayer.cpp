8a58207f1d56fe746b18c8c0e323734155bfb1d9
Marco Randazzo <marco.randazzo@iit.it>
2026-01-17 17:04:13 +0100

    fixed subSound method added option split_size to yarpAudioPlayer

--- src/commands/yarpAudioPlayer/yarpAudioPlayer.cpp
+++ src/commands/yarpAudioPlayer/yarpAudioPlayer.cpp
@@ -18,10 +18,11 @@ using namespace yarp::sig;
 class PlayerModule: public yarp::os::RFModule, public yarpAudioPlayer_IDL
 {
     protected:
-    std::string         m_name;
+    std::string         m_name = "/yarpAudioPlayer";
     yarp::sig::Sound    m_audioFile;
     yarp::os::Port      m_audioPort;
     yarp::os::Port      m_rpcPort;
+    size_t              m_split_size = 16000;
 
     public: //yarpAudioPlayer_IDL methods
     bool play(const std::string& filename) override;
@@ -39,9 +40,10 @@ class PlayerModule: public yarp::os::RFModule, public yarpAudioPlayer_IDL
 
         // generic configuration
         if (rf.check("name"))
-            m_name = std::string("/") + rf.find("name").asString().c_str();
-        else
-            m_name = "/yarpAudioPlayer";
+        { m_name = std::string("/") + rf.find("name").asString().c_str(); }
+
+        if (rf.check("split_size"))
+        { m_split_size = rf.find("split_size").asInt32(); }
 
         // rpc port
         ret = m_rpcPort.open((m_name + "/rpc").c_str());
@@ -116,7 +118,7 @@ bool PlayerModule::test(double duration)
     size_t channels = 1;
     size_t sampleRate = 16000;
     ret = yarp::sig::utils::makeTone(m_audioFile,duration,channels,sampleRate);
-    yInfo() << "Generated tone of " << duration << "s, with the following properties: samples:" << m_audioFile.getSamples() << " channels:"<< m_audioFile.getChannels() << " bytes per samples:" << m_audioFile.getBytesPerSample();
+    yInfo() << "Generated tone of " << duration << "s, with the following properties: samples:" << m_audioFile.getSamples() << " channels:"<< m_audioFile.getChannels() << " frequency:" << m_audioFile.getFrequency() << " bytes per samples:" << m_audioFile.getBytesPerSample();
     if (!ret) { return false; }
 
     ret = send();
@@ -140,7 +142,7 @@ bool PlayerModule::openFile(const std::string& file_name)
         return false;
     }
     yInfo() << "File loaded in: " << en - st << "s";
-    yInfo() << "Loaded file has the following properties: samples:" << m_audioFile.getSamples() << " channels:"<< m_audioFile.getChannels() << " bytes per samples:" << m_audioFile.getBytesPerSample();
+    yInfo() << "Loaded file has the following properties: samples:" << m_audioFile.getSamples() << " channels:"<< m_audioFile.getChannels() << " frequency:" << m_audioFile.getFrequency() << " bytes per samples:" << m_audioFile.getBytesPerSample();
     return true;
 }
 
@@ -151,7 +153,27 @@ bool PlayerModule::send()
     yInfo() << "Sending sound...";
     if (m_audioPort.getOutputCount() > 0)
     {
-        ret = m_audioPort.write(m_audioFile);
+        if (m_split_size==0)
+        {
+            //send the whole file
+            ret = m_audioPort.write(m_audioFile);
+        }
+        else
+        {
+            //split the file in blocks of fixed size
+            size_t totalSamples = m_audioFile.getSamples();
+            size_t iterations = totalSamples/m_split_size;
+            size_t block_counter =0;
+            for (size_t first = 0; first < totalSamples; first += m_split_size)
+            {
+                Sound block = m_audioFile.subSound(first, m_split_size);
+                double st_block = yarp::os::Time::now();
+                ret = m_audioPort.write(block);
+                double en_block = yarp::os::Time::now();
+                yInfo() << "Sent block" << block_counter << "/" << iterations << " with " << block.getSamples() << " samples in" << en_block - st_block << "s";
+                block_counter++;
+            }
+        }
     }
     else
     {
