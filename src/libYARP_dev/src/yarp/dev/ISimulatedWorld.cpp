b015489590a5d9cf360f0130603b5fdee23585e9
Marco Randazzo <marco.randazzo@iit.it>
2026-01-18 11:57:06 +0100

    added missing files, style fixes

---
+++ src/libYARP_dev/src/yarp/dev/ISimulatedWorld.cpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026-2026 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <yarp/dev/ISimulatedWorld.h>
+
+yarp::dev::ISimulatedWorld::~ISimulatedWorld() = default;
